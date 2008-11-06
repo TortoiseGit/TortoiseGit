b729980a97ae6f875f997652c90bec743e8f1bcb
Frank Li <lznuaa@gmail.com>
2008-11-06 11:22:23 +0800

    Build Success but link error

---
+++ Resources/svg/readme.txt
@@ -0,0 +1,5 @@
+These svg files are used as the source for the icons we need in
+TortoiseSVN.
+
+Do _not_ just edit the icon files but these svg files and then create
+the icon files from these svg files.
---
+++ version.h
@@ -0,0 +1,18 @@
+#define FILEVER        0,0,0,0
+#define PRODUCTVER     0,0,0,0
+#define STRFILEVER     "0, 0, 0, 0\0"
+#define STRPRODUCTVER  "0, 0, 0, 0\0"
+
+#define TSVN_VERMAJOR             0
+#define TSVN_VERMINOR             0
+#define TSVN_VERMICRO             0
+#define TSVN_VERBUILD			  0
+#define TSVN_VERDATE			  "date unknown\n"
+
+#ifdef _WIN64
+#define TSVN_PLATFORM		"64 Bit $DEVBUILD$"
+#else
+#define TSVN_PLATFORM		"32 Bit $DEVBUILD$"
+#endif
+
+
