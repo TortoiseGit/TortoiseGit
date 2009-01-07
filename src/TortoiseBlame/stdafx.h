e56129668544bfb8a228b91ca8e6266a1af2c3f3
Frank Li <lznuaa@gmail.com>
2009-01-07 13:18:45 +0800

    add TortoiseBlame

---
+++ src/TortoiseBlame/stdafx.h
@@ -0,0 +1,29 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define _WIN32_WINNT 0x0500
+
+// Windows Header Files:
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <shlwapi.h>
+#include <richedit.h>
+#include <commctrl.h>
+
+#pragma warning(push)
+#pragma warning(disable: 4702)	// Unreachable code warnings in xtree
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#pragma warning(pop)
