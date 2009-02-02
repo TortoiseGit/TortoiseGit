8d3c0fc0fb02b293ea14f74af47c5d1900f96afa
Johan t Hart <johanthart@gmail.com>
2009-02-02 22:22:53 +0100

    Added extra helper methods to BYTE_VECTOR

--- src/Git/gittype.h
+++ src/Git/gittype.h
@@ -38,12 +38,42 @@ public:
 
 		return pos;
 	}
+	int findData(const BYTE* dataToFind, size_t dataSize, int start=0)
+	{
+		const BYTE* pos=&*begin()+start;
+		const BYTE* dataEnd=&*end()-(dataSize-1);//Set end one step after last place to search
+		if(pos>=dataEnd)
+			return -1;//Started over end. Return not found
+		if(dataSize==0)
+			return start;//No search data. Return current position
+		BYTE firstByte=dataToFind[0];
+		while(pos<dataEnd)
+		{
+			//memchr for first character
+			const BYTE* found=(const BYTE*)memchr(pos,firstByte,dataEnd-pos);
+			if(found==NULL)
+				return -1;//Not found
+			//check rest of characters
+			if(memcmp(found,dataToFind,dataSize)==0)
+				return found-&*begin();//Match. Return position.
+			//No match. Set position on next byte and continue search
+			pos=found+1;
+		}
+		return -1;
+	}
 	int append( std::vector<BYTE> &v,int start=0,int end=-1)
 	{
 		if(end<0)
 			end=v.size();
 		for(int i=start;i<end;i++)
 			this->push_back(v[i]);
+		return 0;
+	}
+	int append(const BYTE* data, size_t dataSize)
+	{
+		size_t oldsize=size();
+		resize(oldsize+dataSize);
+		memcpy(&*(begin()+oldsize),data,dataSize);
 		return 0;
 	}
 };
