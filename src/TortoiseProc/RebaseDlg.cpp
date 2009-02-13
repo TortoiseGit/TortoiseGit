88061f601dbd51e453900a471ac5cbc67fee2f22
Frank Li <lznuaa@gmail.com>
2009-02-13 16:38:06 +0800

    Git Rebase Dialog Resize and modify split position okay
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/TortoiseProc/RebaseDlg.cpp
+++ src/TortoiseProc/RebaseDlg.cpp
@@ -31,6 +31,8 @@ void CRebaseDlg::DoDataExchange(CDataExchange* pDX)
     DDX_Check(pDX, IDC_PICK_ALL, m_bPickAll);
     DDX_Check(pDX, IDC_SQUASH_ALL, m_bSquashAll);
     DDX_Check(pDX, IDC_EDIT_ALL, m_bEditAll);
+	DDX_Control(pDX, IDC_REBASE_SPLIT, m_wndSplitter);
+	DDX_Control(pDX,IDC_COMMIT_LIST,m_CommitList);
 }
 
 
@@ -39,7 +41,23 @@ BEGIN_MESSAGE_MAP(CRebaseDlg, CResizableStandAloneDialog)
     ON_BN_CLICKED(IDC_SQUASH_ALL, &CRebaseDlg::OnBnClickedSquashAll)
     ON_BN_CLICKED(IDC_EDIT_ALL, &CRebaseDlg::OnBnClickedEditAll)
     ON_BN_CLICKED(IDC_REBASE_SPLIT, &CRebaseDlg::OnBnClickedRebaseSplit)
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
+
+void CRebaseDlg::AddRebaseAnchor()
+{
+	AddAnchor(IDC_REBASE_TAB,TOP_LEFT,BOTTOM_RIGHT);
+	AddAnchor(IDC_COMMIT_LIST,TOP_LEFT, TOP_RIGHT);
+	AddAnchor(IDC_REBASE_SPLIT,TOP_LEFT, TOP_RIGHT);
+	AddAnchor(IDC_STATUS_STATIC, BOTTOM_LEFT,BOTTOM_RIGHT);
+	AddAnchor(IDC_REBASE_CONTINUE,BOTTOM_RIGHT);
+	AddAnchor(IDC_REBASE_ABORT, BOTTOM_RIGHT);
+	AddAnchor(IDC_REBASE_PROGRESS,BOTTOM_LEFT, BOTTOM_RIGHT);
+	AddAnchor(IDC_PICK_ALL,TOP_LEFT);
+	AddAnchor(IDC_SQUASH_ALL,TOP_LEFT);
+	AddAnchor(IDC_EDIT_ALL,TOP_LEFT);
+	
+}
 
 BOOL CRebaseDlg::OnInitDialog()
 {
@@ -48,10 +66,19 @@ BOOL CRebaseDlg::OnInitDialog()
 	CRect rectDummy;
 	//IDC_REBASE_DUMY_TAB
 	
+	GetClientRect(m_DlgOrigRect);
+	m_CommitList.GetClientRect(m_CommitListOrigRect);
+
 	CWnd *pwnd=this->GetDlgItem(IDC_REBASE_DUMY_TAB);
 	pwnd->GetWindowRect(&rectDummy);
+
+	rectDummy.top-=20;
+	rectDummy.bottom-=20;
+
+	rectDummy.left-=5;
+	rectDummy.right-=5;
 	
-	if (!m_ctrlTabCtrl.Create(CMFCTabCtrl::STYLE_FLAT, rectDummy, this, 0))
+	if (!m_ctrlTabCtrl.Create(CMFCTabCtrl::STYLE_FLAT, rectDummy, this, IDC_REBASE_TAB))
 	{
 		TRACE0("Failed to create output tab window\n");
 		return FALSE;      // fail to create
@@ -73,8 +100,35 @@ BOOL CRebaseDlg::OnInitDialog()
 		return FALSE;
 	}
 
+	m_FileListCtrl.Init(SVNSLC_COLEXT | SVNSLC_COLSTATUS , _T("RebaseDlg"));
+
 	m_ctrlTabCtrl.AddTab(&m_FileListCtrl,_T("Modified File"));
 	m_ctrlTabCtrl.AddTab(&m_LogMessageCtrl,_T("Log Message"),1);
+	AddRebaseAnchor();
+
+
+	EnableSaveRestore(_T("RebaseDlg"));
+
+	DWORD yPos = CRegDWORD(_T("Software\\TortoiseGit\\TortoiseProc\\ResizableState\\RebaseDlgSizer"));
+	RECT rcDlg, rcLogMsg, rcFileList;
+	GetClientRect(&rcDlg);
+	m_CommitList.GetWindowRect(&rcLogMsg);
+	ScreenToClient(&rcLogMsg);
+	this->m_ctrlTabCtrl.GetWindowRect(&rcFileList);
+	ScreenToClient(&rcFileList);
+	if (yPos)
+	{
+		RECT rectSplitter;
+		m_wndSplitter.GetWindowRect(&rectSplitter);
+		ScreenToClient(&rectSplitter);
+		int delta = yPos - rectSplitter.top;
+		if ((rcLogMsg.bottom + delta > rcLogMsg.top)&&(rcLogMsg.bottom + delta < rcFileList.bottom - 30))
+		{
+			m_wndSplitter.SetWindowPos(NULL, 0, yPos, 0, 0, SWP_NOSIZE);
+			DoSize(delta);
+		}
+	}
+
 
 	return TRUE;
 }
@@ -98,4 +152,87 @@ void CRebaseDlg::OnBnClickedEditAll()
 void CRebaseDlg::OnBnClickedRebaseSplit()
 {
     // TODO: Add your control notification handler code here
+}
+
+LRESULT CRebaseDlg::DefWindowProc(UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_NOTIFY:
+		if (wParam == IDC_REBASE_SPLIT)
+		{ 
+			SPC_NMHDR* pHdr = (SPC_NMHDR*) lParam;
+			DoSize(pHdr->delta);
+		}
+		break;
+	}
+
+	return __super::DefWindowProc(message, wParam, lParam);
+}
+
+void CRebaseDlg::DoSize(int delta)
+{
+	
+	this->RemoveAllAnchors();
+
+	CSplitterControl::ChangeHeight(GetDlgItem(IDC_COMMIT_LIST), delta, CW_TOPALIGN);
+	//CSplitterControl::ChangeHeight(GetDlgItem(), delta, CW_TOPALIGN);
+	CSplitterControl::ChangeHeight(GetDlgItem(IDC_REBASE_TAB), -delta, CW_BOTTOMALIGN);
+	//CSplitterControl::ChangeHeight(GetDlgItem(), -delta, CW_BOTTOMALIGN);
+	CSplitterControl::ChangePos(GetDlgItem(IDC_SQUASH_ALL),0,delta);
+	CSplitterControl::ChangePos(GetDlgItem(IDC_PICK_ALL),0,delta);
+	CSplitterControl::ChangePos(GetDlgItem(IDC_EDIT_ALL),0,delta);
+	
+	this->AddRebaseAnchor();
+	// adjust the minimum size of the dialog to prevent the resizing from
+	// moving the list control too far down.
+	CRect rcLogMsg;
+	m_CommitList.GetClientRect(rcLogMsg);
+	SetMinTrackSize(CSize(m_DlgOrigRect.Width(), m_DlgOrigRect.Height()-m_CommitListOrigRect.Height()+rcLogMsg.Height()));
+
+	SetSplitterRange();
+//	m_CommitList.Invalidate();
+
+//	GetDlgItem(IDC_LOGMESSAGE)->Invalidate();
+
+	this->m_ctrlTabCtrl.Invalidate();
+	this->m_CommitList.Invalidate();
+	this->m_FileListCtrl.Invalidate();
+	this->m_LogMessageCtrl.Invalidate();
+
+}
+
+void CRebaseDlg::SetSplitterRange()
+{
+	if ((m_CommitList)&&(m_ctrlTabCtrl))
+	{
+		CRect rcTop;
+		m_CommitList.GetWindowRect(rcTop);
+		ScreenToClient(rcTop);
+		CRect rcMiddle;
+		m_ctrlTabCtrl.GetWindowRect(rcMiddle);
+		ScreenToClient(rcMiddle);
+		if (rcMiddle.Height() && rcMiddle.Width())
+			m_wndSplitter.SetRange(rcTop.top+60, rcMiddle.bottom-80);
+	}
+}
+
+void CRebaseDlg::OnSize(UINT nType,int cx, int cy)
+{
+	 // first, let the resizing take place
+    __super::OnSize(nType, cx, cy);
+
+    //set range
+    SetSplitterRange();
+}
+
+void CRebaseDlg::SaveSplitterPos()
+{
+	if (!IsIconic())
+	{
+		CRegDWORD regPos = CRegDWORD(_T("Software\\TortoiseGit\\TortoiseProc\\ResizableState\\RebaseDlgSizer"));
+		RECT rectSplitter;
+		m_wndSplitter.GetWindowRect(&rectSplitter);
+		ScreenToClient(&rectSplitter);
+		regPos = rectSplitter.top;
+	}
 }
--- src/TortoiseProc/RebaseDlg.h
+++ src/TortoiseProc/RebaseDlg.h
@@ -4,7 +4,9 @@
 #include "StandAloneDlg.h"
 #include "GitStatusListCtrl.h"
 #include "SciEdit.h"
+#include "SplitterControl.h"
 // CRebaseDlg dialog
+#define IDC_REBASE_TAB 0x1000000
 
 class CRebaseDlg : public CResizableStandAloneDialog
 {
@@ -21,19 +23,34 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	virtual BOOL OnInitDialog();
 	DECLARE_MESSAGE_MAP()
+	virtual LRESULT DefWindowProc(UINT message, WPARAM wParam, LPARAM lParam);
+	void DoSize(int delta);
+	void AddRebaseAnchor();
+	void RemoveAnchor();
+
+	void SetSplitterRange();
+	void SaveSplitterPos();
+	
+	CRect				m_DlgOrigRect;
+	CRect				m_CommitListOrigRect;
+
 public:
    
     afx_msg void OnBnClickedPickAll();
     afx_msg void OnBnClickedSquashAll();
     afx_msg void OnBnClickedEditAll();
     afx_msg void OnBnClickedRebaseSplit();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+
     CProgressCtrl m_ProgressBar;
     CStatic m_CtrlStatusText;
     BOOL m_bPickAll;
     BOOL m_bSquashAll;
     BOOL m_bEditAll;
 
+	CSplitterControl	m_wndSplitter;
 	CMFCTabCtrl m_ctrlTabCtrl;
 	CGitStatusListCtrl m_FileListCtrl;
 	CSciEdit		   m_LogMessageCtrl;
+	CListCtrl		   m_CommitList;
 };
