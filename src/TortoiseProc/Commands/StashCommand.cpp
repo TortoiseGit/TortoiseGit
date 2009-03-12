a6a192baa48ce58105ca254aff66bda135b376f6
Frank Li <lznuaa@gmail.com>
2009-03-12 23:19:57 +0800

    Add missed stash file. Support save and applied
    Signed-off-by: Frank Li <lznuaa@gmail.com>

---
+++ src/TortoiseProc/Commands/StashCommand.cpp
@@ -0,0 +1,71 @@
+// TortoiseGit - a Windows shell extension for easy version control
+
+// Copyright (C) 2008-2009 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "StashCommand.h"
+
+#include "MessageBox.h"
+#include "RenameDlg.h"
+#include "InputLogDlg.h"
+#include "Git.h"
+#include "DirFileEnum.h"
+#include "ShellUpdater.h"
+#include "ChangedDlg.h"
+
+bool StashSaveCommand::Execute()
+{
+	bool bRet = false;
+
+	CString cmd,out;
+	cmd=_T("git.exe stash");
+	
+	if(g_Git.Run(cmd,&out,CP_ACP))
+	{
+		CMessageBox::Show(NULL,CString(_T("<ct=0x0000FF>Stash Fail!!!</ct>\n"))+out,_T("TortoiseGit"),MB_OK|MB_ICONERROR);
+	}else
+	{
+ 		CMessageBox::Show(NULL,CString(_T("<ct=0xff0000>Stash Success</ct>\n"))+out,_T("TortoiseGit"),MB_OK|MB_ICONINFORMATION);
+		bRet = true;
+	}
+	return bRet;
+}
+
+bool StashApplyCommand::Execute()
+{
+	bool bRet = false;
+
+	CString cmd,out;
+	cmd=_T("git.exe stash apply");
+	
+	if(g_Git.Run(cmd,&out,CP_ACP))
+	{
+		CMessageBox::Show(NULL,CString(_T("<ct=0x0000FF>Stash Apply Fail!!!</ct>\n"))+out,_T("TortoiseGit"),MB_OK|MB_ICONERROR);
+
+	}else
+	{
+ 		if(CMessageBox::Show(NULL,CString(_T("<ct=0xff0000>Stash Apply Success</ct>\nDo you want to show change?"))
+			,_T("TortoiseGit"),MB_YESNO|MB_ICONINFORMATION) == IDYES)
+		{
+			CChangedDlg dlg;
+			dlg.m_pathList = pathList;
+			dlg.DoModal();			
+		}
+		bRet = true;
+	}
+	return bRet;
+}
