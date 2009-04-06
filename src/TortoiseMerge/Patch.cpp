f31db5cce6f6f73b66e9fd339acb6ff0ba15b3fa
Frank Li <lznuaa@gmail.com>
2009-04-06 11:16:25 +0800

    TortoiseMerge Basic Support Git patch created by format patch
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/TortoiseMerge/Patch.cpp
+++ src/TortoiseMerge/Patch.cpp
@@ -34,6 +34,7 @@ static char THIS_FILE[] = __FILE__;
 CPatch::CPatch(void)
 {
 	m_nStrip = 0;
+	m_IsGitPatch = false;
 }
 
 CPatch::~CPatch(void)
@@ -76,10 +77,20 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 	//now we got all the lines of the patch file
 	//in our array - parsing can start...
 
+	for(nIndex=0;PatchLines.GetCount();nIndex++)
+	{
+		sLine = PatchLines.GetAt(nIndex);
+		if(sLine.Left(10).Compare(_T("diff --git")) == 0)
+		{
+			this->m_IsGitPatch=true;
+			break;
+		}
+	}
+
 	//first, skip possible garbage at the beginning
 	//garbage is finished when a line starts with "Index: "
 	//and the next line consists of only "=" characters
-	for (; nIndex<PatchLines.GetCount(); nIndex++)
+	for (nIndex=0; nIndex<PatchLines.GetCount(); nIndex++)
 	{
 		sLine = PatchLines.GetAt(nIndex);
 		if (sLine.Left(4).Compare(_T("--- "))==0)
@@ -87,6 +98,10 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 		if ((nIndex+1)<PatchLines.GetCount())
 		{
 			sLine = PatchLines.GetAt(nIndex+1);
+
+			if(sLine.IsEmpty()&&m_IsGitPatch)
+				continue;
+
 			sLine.Replace(_T("="), _T(""));
 			if (sLine.IsEmpty())
 				break;
@@ -114,7 +129,7 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 			ending = EOL_AUTOLINE;
 		if (state == 0)
 		{
-			if ((sLine.Left(4).Compare(_T("--- "))==0)&&(sLine.Find('\t') >= 0))
+			if ((sLine.Left(4).Compare(_T("--- "))==0)&&((sLine.Find('\t') >= 0)||this->m_IsGitPatch))
 			{
 				state = 2;
 				if (chunks)
@@ -124,10 +139,19 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 					m_arFileDiffs.Add(chunks);
 				}
 				chunks = new Chunks();
+				
 				int nTab = sLine.Find('\t');
+
+				int filestart = 4;
+				if(m_IsGitPatch)
+				{
+					nTab=sLine.GetLength();
+					filestart = 6;
+				}
+
 				if (nTab >= 0)
 				{
-					chunks->sFilePath = sLine.Mid(4, nTab-4).Trim();
+					chunks->sFilePath = sLine.Mid(filestart, nTab-filestart).Trim();
 				}
 			}
 		}
