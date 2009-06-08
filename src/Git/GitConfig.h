4b68b59d15aac1ccdd34063a227cd7809de27bd2
Frank Li <lznuaa@gmail.com>
2009-06-08 11:49:43 +0800

    Fix Issue #89: Can't locate msysgit on x64
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/Git/GitConfig.h
+++ src/Git/GitConfig.h
@@ -8,4 +8,9 @@ public:
 };
 
 #define REG_MSYSGIT_PATH _T("Software\\TortoiseGit\\MSysGit")
+
+#ifndef WIN64
 #define REG_MSYSGIT_INSTALL _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1\\InstallLocation")
+#else
+#define REG_MSYSGIT_INSTALL	_T("SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1\\InstallLocation")
+#endif
