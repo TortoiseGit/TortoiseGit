TortoiseGit/TortoiseGit

commit 25bd4c961830590ffdb51202e935fd168b7b2484

Pull/Fetch Dlg: Used git config to determine default remote and remote branch. Also enabled 'branch' option when pulling from known remote. Also started implementing rebase option (rebase instead of merge) when pulling. Disabled this though, because I think the rebase dialog should be used for this. It needs some work.

src/TortoiseProc/PullFetchDlg.cpp
@@ -6,13 +6,14 @@
 #include "PullFetchDlg.h"
 #include "Git.h"
 #include "AppUtils.h"
-
+#include "BrowseRefsDlg.h"
 // CPullFetchDlg dialog
 
 IMPLEMENT_DYNAMIC(CPullFetchDlg, CResizableStandAloneDialog)
 
 CPullFetchDlg::CPullFetchDlg(CWnd* pParent /*=NULL*/)
-	: CResizableStandAloneDialog(CPullFetchDlg::IDD, pParent)
+	: CResizableStandAloneDialog(CPullFetchDlg::IDD, pParent),
+	  m_bRebase(false)
 {
 	m_IsPull=TRUE;
     m_bAutoLoad = CAppUtils::IsSSHPutty();
@@ -31,6 +32,7 @@ void CPullFetchDlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_REMOTE_BRANCH, this->m_RemoteBranch);
     DDX_Control(pDX,IDC_REMOTE_MANAGE, this->m_RemoteManage);
     DDX_Check(pDX,IDC_PUTTYKEY_AUTOLOAD,m_bAutoLoad);
+    DDX_Check(pDX,IDC_CHECK_REBASE,m_bRebase);
 
 }
 
@@ -40,6 +42,7 @@ BEGIN_MESSAGE_MAP(CPullFetchDlg,CResizableStandAloneDialog )
 	ON_BN_CLICKED(IDC_OTHER_RD, &CPullFetchDlg::OnBnClickedRd)
 	ON_BN_CLICKED(IDOK, &CPullFetchDlg::OnBnClickedOk)
     ON_STN_CLICKED(IDC_REMOTE_MANAGE, &CPullFetchDlg::OnStnClickedRemoteManage)
+	ON_BN_CLICKED(IDC_BUTTON_BROWSE_REF, &CPullFetchDlg::OnBnClickedButtonBrowseRef)
 END_MESSAGE_MAP()
 
 BOOL CPullFetchDlg::OnInitDialog()
@@ -54,6 +57,7 @@ BOOL CPullFetchDlg::OnInitDialog()
 	AddAnchor(IDCANCEL,BOTTOM_RIGHT);
     AddAnchor(IDC_GROUPT_REMOTE,TOP_LEFT,BOTTOM_RIGHT);
     AddAnchor(IDC_PUTTYKEY_AUTOLOAD,BOTTOM_LEFT);
+	AddAnchor(IDC_CHECK_REBASE,BOTTOM_LEFT);
     AddAnchor(IDC_REMOTE_MANAGE,BOTTOM_LEFT);
 	AddAnchor(IDHELP, BOTTOM_RIGHT);
 
@@ -64,7 +68,12 @@ BOOL CPullFetchDlg::OnInitDialog()
 	CheckRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD,IDC_REMOTE_RD);
 	m_Remote.EnableWindow(TRUE);
 	m_Other.EnableWindow(FALSE);
-	m_RemoteBranch.EnableWindow(FALSE);
+	if(!m_IsPull)
+		m_RemoteBranch.EnableWindow(FALSE);
+
+//	if(!m_IsPull)
+		//Todo: implement rebase option sometime with rebase dialog
+		GetDlgItem(IDC_CHECK_REBASE)->ShowWindow(SW_HIDE);
 
 	m_Other.SetURLHistory(TRUE);
 	m_Other.LoadHistory(_T("Software\\TortoiseGit\\History\\PullURLS"), _T("url"));
@@ -90,12 +99,26 @@ BOOL CPullFetchDlg::OnInitDialog()
 	m_RemoteReg = remote;
 	int sel=0;
 
+	//Select pull-remote from current branch
+	CString currentBranch = g_Git.GetSymbolicRef();
+	CString configName;
+	configName.Format(L"branch.%s.remote", currentBranch);
+	CString pullRemote = g_Git.GetConfigValue(configName);
+
+	//Select pull-branch from current branch
+	configName.Format(L"branch.%s.merge", currentBranch);
+	CString pullBranch = CGit::StripRefName(g_Git.GetConfigValue(configName));
+	m_RemoteBranch.AddString(pullBranch);
+
+	if(pullRemote.IsEmpty())
+		pullRemote = remote;
+
 	if(!g_Git.GetRemoteList(list))
 	{	
 		for(unsigned int i=0;i<list.size();i++)
 		{
 			m_Remote.AddString(list[i]);
-			if(list[i] == remote)
+			if(list[i] == pullRemote)
 				sel = i;
 		}
 	}
@@ -115,14 +138,15 @@ void CPullFetchDlg::OnBnClickedRd()
 	{
 		m_Remote.EnableWindow(TRUE);
 		m_Other.EnableWindow(FALSE);
-		m_RemoteBranch.EnableWindow(FALSE);
-
+		if(!m_IsPull)
+			m_RemoteBranch.EnableWindow(FALSE);
 	}
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_OTHER_RD)
 	{
 		m_Remote.EnableWindow(FALSE);
 		m_Other.EnableWindow(TRUE);;
-		m_RemoteBranch.EnableWindow(TRUE);
+		if(!m_IsPull)
+			m_RemoteBranch.EnableWindow(TRUE);
 	}
 	
 
@@ -134,7 +158,10 @@ void CPullFetchDlg::OnBnClickedOk()
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_REMOTE_RD)
 	{
 		m_RemoteURL=m_Remote.GetString();
-		m_RemoteBranchName.Empty();
+		if(!m_IsPull)
+			m_RemoteBranchName.Empty();
+		else
+			m_RemoteBranchName=m_RemoteBranch.GetString();
 		
 	}
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_OTHER_RD)
@@ -155,4 +182,26 @@ void CPullFetchDlg::OnStnClickedRemoteManage()
 {
     // TODO: Add your control notification handler code here
     CAppUtils::LaunchRemoteSetting();
+}
+
+void CPullFetchDlg::OnBnClickedButtonBrowseRef()
+{
+	CString initialRef;
+	initialRef.Format(L"refs/remotes/%s/%s", m_Remote.GetString(), m_RemoteBranch.GetString());
+	CString selectedRef = CBrowseRefsDlg::PickRef(false, initialRef, gPickRef_Remote);
+	if(selectedRef.Left(13) != "refs/remotes/")
+		return;
+
+	selectedRef = selectedRef.Mid(13);
+	int ixSlash = selectedRef.Find('/');
+
+	CString remoteName   = selectedRef.Left(ixSlash);
+	CString remoteBranch = selectedRef.Mid(ixSlash + 1);
+	
+	int ixFound = m_Remote.FindStringExact(0, remoteName);
+	if(ixFound >= 0)
+		m_Remote.SetCurSel(ixFound);
+	m_RemoteBranch.AddString(remoteBranch);
+
+	CheckRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD,IDC_REMOTE_RD);
 }

src/TortoiseProc/PullFetchDlg.h
@@ -29,6 +29,7 @@ protected:
 public:
 	BOOL        m_IsPull;
     BOOL        m_bAutoLoad;
+	BOOL		m_bRebase;
     BOOL        m_bAutoLoadEnable;
     CHyperLink  m_RemoteManage;
 
@@ -37,4 +38,5 @@ public:
 	CString m_RemoteURL;
 	CString m_RemoteBranchName;
     afx_msg void OnStnClickedRemoteManage();
+	afx_msg void OnBnClickedButtonBrowseRef();
 };

src/TortoiseProc/Commands/PullCommand.cpp
@@ -44,7 +44,10 @@ bool PullCommand::Execute()
 		
 		CString cmd;
 		CString hashOld = g_Git.GetHash(L"HEAD");
-		cmd.Format(_T("git.exe pull \"%s\" %s"),url,dlg.m_RemoteBranchName);
+		CString cmdRebase;
+		if(dlg.m_bRebase)
+			cmdRebase = "--rebase ";
+		cmd.Format(_T("git.exe pull %s\"%s\" %s"),cmdRebase, url, dlg.m_RemoteBranchName);
 		CProgressDlg progress;
 		progress.m_GitCmd = cmd;
 		progress.m_bAutoCloseOnSuccess = true;

commit 1796c9009969441c213659570379b89ca1c49b8a

Pull: Fixed bug that when pulling from the configured remote branch, git did not update the remote tracking branches.
the command:
git pull origin master
Does not update the remote tracking branches. The command
git pull origin
does.

This bug was introduced in commit 25bd4c961830590ffdb51202e935fd168b7b2484

src/TortoiseProc/PullFetchDlg.cpp
@@ -103,11 +103,11 @@ BOOL CPullFetchDlg::OnInitDialog()
 	CString currentBranch = g_Git.GetSymbolicRef();
 	CString configName;
 	configName.Format(L"branch.%s.remote", currentBranch);
-	CString pullRemote = g_Git.GetConfigValue(configName);
+	CString pullRemote = m_configPullRemote = g_Git.GetConfigValue(configName);
 
 	//Select pull-branch from current branch
 	configName.Format(L"branch.%s.merge", currentBranch);
-	CString pullBranch = CGit::StripRefName(g_Git.GetConfigValue(configName));
+	CString pullBranch = m_configPullBranch = CGit::StripRefName(g_Git.GetConfigValue(configName));
 	m_RemoteBranch.AddString(pullBranch);
 
 	if(pullRemote.IsEmpty())
@@ -158,7 +158,10 @@ void CPullFetchDlg::OnBnClickedOk()
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_REMOTE_RD)
 	{
 		m_RemoteURL=m_Remote.GetString();
-		if(!m_IsPull)
+		if( !m_IsPull ||
+			(m_configPullRemote == m_RemoteURL && m_configPullBranch == m_RemoteBranch.GetString() ))
+			//When fetching or when pulling from the configured tracking branch, dont explicitly set the remote branch name,
+			//because otherwise git will not update the remote tracking branches.
 			m_RemoteBranchName.Empty();
 		else
 			m_RemoteBranchName=m_RemoteBranch.GetString();

src/TortoiseProc/PullFetchDlg.h
@@ -39,4 +39,7 @@ public:
 	CString m_RemoteBranchName;
     afx_msg void OnStnClickedRemoteManage();
 	afx_msg void OnBnClickedButtonBrowseRef();
+
+	CString		m_configPullRemote;
+	CString		m_configPullBranch;
 };