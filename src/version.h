104c2ca1f5429a31445cde9b262a4e5f1b3170b8
Frank Li <lznuaa@gmail.com>
2009-07-03 22:13:51 +0800

    Update Version Number to 0.8.1.0

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        0,7,2,0
-#define PRODUCTVER     0,7,2,0
-#define STRFILEVER     "0, 7, 2, 0\0"
-#define STRPRODUCTVER  "0, 7, 2, 0\0"
+#define FILEVER        0,8,1,0
+#define PRODUCTVER     0,8,1,0
+#define STRFILEVER     "0, 8, 1, 0\0"
+#define STRPRODUCTVER  "0, 8, 1, 0\0"
 
 #define TSVN_VERMAJOR             0
-#define TSVN_VERMINOR             7
-#define TSVN_VERMICRO             2
+#define TSVN_VERMINOR             8
+#define TSVN_VERMICRO             1
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
 
