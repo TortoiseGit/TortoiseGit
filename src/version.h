c6adc25449654276f5398cc019f6005681e34f50
Frank Li <lznuaa@gmail.com>
2009-08-01 12:20:34 +0800

    Update changelog and version number

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define FILEVER        0,8,1,0
-#define PRODUCTVER     0,8,1,0
-#define STRFILEVER     "0, 8, 1, 0\0"
-#define STRPRODUCTVER  "0, 8, 1, 0\0"
+#define FILEVER        0,9,1,0
+#define PRODUCTVER     0,9,1,0
+#define STRFILEVER     "0, 9, 1, 0\0"
+#define STRPRODUCTVER  "0, 9, 1, 0\0"
 
 #define TSVN_VERMAJOR             0
-#define TSVN_VERMINOR             8
+#define TSVN_VERMINOR             9
 #define TSVN_VERMICRO             1
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
