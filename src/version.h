7297f4141e110ffcbfd8736bb5f41e03183ebc02
Frank Li <lznuaa@gmail.com>
2009-10-13 12:41:46 +0800

    Update changelog and version number
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        1,0,2,0
-#define PRODUCTVER     1,0,2,0
-#define STRFILEVER     "1, 0, 2, 0\0"
-#define STRPRODUCTVER  "1, 0, 2, 0\0"
+#define FILEVER        1,1,1,0
+#define PRODUCTVER     1,1,1,0
+#define STRFILEVER     "1, 1, 1, 0\0"
+#define STRPRODUCTVER  "1, 1, 1, 0\0"
 
 #define TSVN_VERMAJOR             1
-#define TSVN_VERMINOR             0
-#define TSVN_VERMICRO             2
+#define TSVN_VERMINOR             1
+#define TSVN_VERMICRO             1
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
 
