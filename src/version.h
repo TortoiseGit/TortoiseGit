9547d79152951817a5c6861b99e248b9d9a1eabb
Frank Li <lznuaa@gmail.com>
2009-11-12 21:28:57 -0600

    Update version number to 1.2.1.0
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define FILEVER        1,1,1,0
-#define PRODUCTVER     1,1,1,0
-#define STRFILEVER     "1, 1, 1, 0\0"
-#define STRPRODUCTVER  "1, 1, 1, 0\0"
+#define FILEVER        1,2,1,0
+#define PRODUCTVER     1,2,1,0
+#define STRFILEVER     "1, 2, 1, 0\0"
+#define STRPRODUCTVER  "1, 2, 1, 0\0"
 
 #define TSVN_VERMAJOR             1
-#define TSVN_VERMINOR             1
+#define TSVN_VERMINOR             2
 #define TSVN_VERMICRO             1
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
