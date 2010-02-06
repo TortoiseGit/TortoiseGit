aedf7e4134d557c8a654541e41bd1ba7dd429e81
Frank Li <lznuaa@gmail.com>
2010-02-06 08:41:48 +0800

    Update version number to 1.3.6.0
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        1,3,5,0
-#define PRODUCTVER     1,3,5,0
-#define STRFILEVER     "1, 3, 5, 0\0"
-#define STRPRODUCTVER  "1, 3, 5, 0\0"
+#define FILEVER        1,3,6,0
+#define PRODUCTVER     1,3,6,0
+#define STRFILEVER     "1, 3, 6, 0\0"
+#define STRPRODUCTVER  "1, 3, 6, 0\0"
 
 #define TSVN_VERMAJOR             1
 #define TSVN_VERMINOR             3
-#define TSVN_VERMICRO             5
+#define TSVN_VERMICRO             6
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
 
