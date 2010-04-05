c8d32184779fd2a3918140e45c45f448c26184f1
Frank Li <lznuaa@gmail.com>
2010-04-05 18:42:17 +0800

    Update version number to 1.4.2.0
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        1,4,1,0
-#define PRODUCTVER     1,4,1,0
-#define STRFILEVER     "1, 4, 1, 0\0"
-#define STRPRODUCTVER  "1, 4, 1, 0\0"
+#define FILEVER        1,4,2,0
+#define PRODUCTVER     1,4,2,0
+#define STRFILEVER     "1, 4, 2, 0\0"
+#define STRPRODUCTVER  "1, 4, 2, 0\0"
 
 #define TSVN_VERMAJOR             1
 #define TSVN_VERMINOR             4
-#define TSVN_VERMICRO             1
+#define TSVN_VERMICRO             2
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  "date unknown\n"
 
