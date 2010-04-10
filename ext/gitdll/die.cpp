7ac8b24cdc386398c87305f4c50346df56b3b4b3
Frank Li <lznuaa@gmail.com>
2010-04-10 13:15:14 +0800

    Avoid .git\* locked when remove git repository
    record gitdll open handle and close all when die happen.
    
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- ext/gitdll/die.cpp
+++ ext/gitdll/die.cpp
@@ -4,10 +4,12 @@
 
 #define MAX_ERROR_STR_SIZE 512
 extern "C" char g_last_error[MAX_ERROR_STR_SIZE]={0};
+extern "C" int close_all();
 
 extern "C" void die_dll(const char *err, va_list params)
 {
 	memset(g_last_error,0,MAX_ERROR_STR_SIZE);
 	vsnprintf(g_last_error, MAX_ERROR_STR_SIZE-1, err, params);	
+	close_all();
 	throw g_last_error;
 }
