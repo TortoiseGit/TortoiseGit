deeb4e17d57ca128da373f6369477bbc371b4f7d
Frank Li <lznuaa@gmail.com>
2010-10-02 14:37:41 +0800

    Fixed issue #270: Clone fails: bash: X.X.X.X: command not found
    Merge tortoisesvn 18803
    Apply this patch:
    http://svn.tartarus.org/sgt?view=rev&revision=8651
    to solve the following bug:
    a host name specified on Default Settings is used in addition to the host specified
    at the command line, causing confusing connection errors:
    http://www.chiark.greenend.org.uk/~sgtatham/putty/wishlist/plink-hostname.html
    
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/TortoisePlink/Windows/WINPLINK.C
+++ src/TortoisePlink/Windows/WINPLINK.C
@@ -288,6 +288,7 @@ int main(int argc, char **argv)
     int skcount, sksize;
     int exitcode;
     int errors;
+	int got_host = FALSE;
     int use_subsystem = 0;
     long now, next;
 
@@ -355,7 +356,7 @@ int main(int argc, char **argv)
 		errors = 1;
 	    }
 	} else if (*p) {
-	    if (!cfg_launchable(&cfg)) {
+	    if (!cfg_launchable(&cfg) || !(got_host || loaded_session)) {
 		char *q = p;
 		/*
 		 * If the hostname starts with "telnet:", set the
@@ -381,6 +382,7 @@ int main(int argc, char **argv)
 			cfg.port = -1;
 		    strncpy(cfg.host, q, sizeof(cfg.host) - 1);
 		    cfg.host[sizeof(cfg.host) - 1] = '\0';
+			got_host = TRUE;
 		} else {
 		    char *r, *user, *host;
 		    /*
@@ -434,8 +436,10 @@ int main(int argc, char **argv)
 			    strncpy(cfg.host, host, sizeof(cfg.host) - 1);
 			    cfg.host[sizeof(cfg.host) - 1] = '\0';
 			    cfg.port = default_port;
+				got_host = TRUE;
 			} else {
 			    cfg = cfg2;
+				loaded_session = TRUE;
 			}
 		    }
 
@@ -482,7 +486,7 @@ int main(int argc, char **argv)
     if (errors)
 	return 1;
 
-    if (!cfg_launchable(&cfg)) {
+    if (!cfg_launchable(&cfg) || !(got_host || loaded_session)) {
 	usage();
     }
 
