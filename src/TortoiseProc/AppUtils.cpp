3a355ded2606df2798e50467cacc29f9721b91ed
Sven Strickroth <email@cs-ware.de>
2011-01-08 04:27:07 +0100

    UTF->ASCII and LF->CRLF convertion

--- src/TortoiseProc/AppUtils.cpp
+++ src/TortoiseProc/AppUtils.cpp
@@ -1,4 +1,4 @@
-﻿// TortoiseGit - a Windows shell extension for easy version control
+// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2008 - TortoiseGit
 
