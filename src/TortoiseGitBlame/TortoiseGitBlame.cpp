ceb44fd4bd7768722bed8ac0b2a9d794190fbd9c
Sven Strickroth <email@cs-ware.de>
2011-01-21 15:48:00 +0100

    use better (TortoiseGit) registry key for storing app. settings
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseGitBlame/TortoiseGitBlame.cpp
+++ src/TortoiseGitBlame/TortoiseGitBlame.cpp
@@ -79,7 +79,7 @@ BOOL CTortoiseGitBlameApp::InitInstance()
 	// Change the registry key under which our settings are stored
 	// TODO: You should modify this string to be something appropriate
 	// such as the name of your company or organization
-	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+	SetRegistryKey(_T("TortoiseGit"));
 	LoadStdProfileSettings(4);  // Load standard INI file options (including MRU)
 
 	InitContextMenuManager();
