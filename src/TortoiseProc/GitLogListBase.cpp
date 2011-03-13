2899e9db4d27db5926754db5170eea73315b251e
Sven Strickroth <email@cs-ware.de>
2011-03-13 01:39:48 +0100

    added icon for push
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/GitLogListBase.cpp
+++ src/TortoiseProc/GitLogListBase.cpp
@@ -1777,7 +1777,7 @@ void CGitLogListBase::OnContextMenu(CWnd* pWnd, CPoint point)
 				}
 				if (isLocal)
 				{
-					popup.AppendMenuIcon(ID_PUSH, IDS_LOG_PUSH);
+					popup.AppendMenuIcon(ID_PUSH, IDS_LOG_PUSH, IDI_PUSH);
 					bAddSeparator = true;
 				}
 			}
