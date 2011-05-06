264b190d4585162348663c65aae7bf6185717ad6
Sven Strickroth <email@cs-ware.de>
2011-05-06 23:49:38 +0200

    more information hiding
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/GitLogList.h
+++ src/TortoiseProc/GitLogList.h
@@ -24,12 +24,11 @@ class CGitLogListBase;
 class CGitLogList : public CGitLogListBase
 {
 	DECLARE_DYNAMIC(CGitLogList)
+
 protected:
-	
 	void SetSelectedAction(int action);
 	int	 CherryPickFrom(CString from, CString to);
 	int  RevertSelectedCommits();
 	void CloseHandles();
-public:
 	void ContextMenuAction(int cmd,int FirstSelect, int LastSelect, CMenu * menu);
 };
