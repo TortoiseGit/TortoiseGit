TortoiseGit/TortoiseGit

commit cb489c6563cfb20284770786cdf036ef69f0afbd

replace calls to CString::Replace("c","") with calls to the much faster CString::Remove('c')
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ProgressDlg.cpp
@@ -367,7 +367,7 @@ void CProgressDlg::ParserCmdOutput(char ch)
 }
 int CProgressDlg::ClearESC(CStringA &str)
 {
-	return str.Replace("\033[K","");
+	return str.Remove('\033[K');
 }
 void CProgressDlg::ParserCmdOutput(CRichEditCtrl &log,CProgressCtrl &progressctrl,HWND m_hWnd,CComPtr<ITaskbarList3> m_pTaskbarList,CStringA &oneline, char ch, CWnd *CurrentWork)
 {

src/TortoiseMerge/BaseView.cpp
@@ -1,6 +1,6 @@
 // TortoiseMerge - a Diff/Patch program
 
-// Copyright (C) 2003-2009 - TortoiseSVN
+// Copyright (C) 2003-2009,2011 - TortoiseSVN
 // Copyright (C) 2011 Sven Strickroth <email@cs-ware.de>
 
 // This program is free software; you can redistribute it and/or
@@ -607,14 +607,14 @@ bool CBaseView::IsBlockWhitespaceOnly(int nLineIndex, bool& bIdentical)
 	CString other = GetWhitespaceBlock(m_pOtherViewData, min(nLineIndex, m_pOtherViewData->GetCount() - 1));
 	bIdentical = mine == other;
 	
-	mine.Replace(_T(" "), _T(""));
-	mine.Replace(_T("\t"), _T(""));
-	mine.Replace(_T("\r"), _T(""));
-	mine.Replace(_T("\n"), _T(""));
-	other.Replace(_T(" "), _T(""));
-	other.Replace(_T("\t"), _T(""));
-	other.Replace(_T("\r"), _T(""));
-	other.Replace(_T("\n"), _T(""));
+	mine.Remove(' ');
+	mine.Remove('\t');
+	mine.Remove('\r');
+	mine.Remove('\n');
+	other.Remove(' ');
+	other.Remove('\t');
+	other.Remove('\r');
+	other.Remove('\n');
 	
 	return (mine == other) && (!mine.IsEmpty());
 }

src/TortoiseMerge/Patch.cpp
@@ -1,6 +1,6 @@
 // TortoiseMerge - a Diff/Patch program
 
-// Copyright (C) 2004-2009 - TortoiseSVN
+// Copyright (C) 2004-2009,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -365,7 +365,7 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 				if(sLine.IsEmpty()&&m_IsGitPatch)
 					continue;
 
-				sLine.Replace(_T("="), _T(""));
+				sLine.Remove('=');
 				if (sLine.IsEmpty())
 					break;
 			}
@@ -433,7 +433,7 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 					nextLine = PatchLines.GetAt(nIndex+1);
 					if (!nextLine.IsEmpty())
 					{
-						nextLine.Replace(_T("="), _T(""));
+						nextLine.Remove('=');
 						if (nextLine.IsEmpty())
 						{
 							if (chunks)
@@ -477,7 +477,7 @@ BOOL CPatch::OpenUnifiedDiffFile(const CString& filename)
 		break;
 		case 1:	//====================
 			{
-				sLine.Replace(_T("="), _T(""));
+				sLine.Remove('=');
 				if (sLine.IsEmpty())
 				{
 					// if the next line is already the start of the chunk,

src/TortoiseProc/AppUtils.cpp
@@ -762,7 +762,7 @@ bool CAppUtils::FormatTextInRichEditControl(CWnd * pWnd)
 	pWnd->GetWindowText(sText);
 	// the rich edit control doesn't count the CR char!
 	// to be exact: CRLF is treated as one char.
-	sText.Replace(_T("\r"), _T(""));
+	sText.Remove('\r');
 
 	// style each line separately
 	int offset = 0;

src/TortoiseProc/EditPropertiesDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2008 - TortoiseSVN
+// Copyright (C) 2003-2008,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -190,7 +190,7 @@ UINT CEditPropertiesDlg::PropsThread()
 				it->second.value = prop_value;
 				CString stemp = value.c_str();
 				stemp.Replace('\n', ' ');
-				stemp.Replace(_T("\r"), _T(""));
+				stemp.Remove('\r');
 				it->second.value_without_newlines = stdstring((LPCTSTR)stemp);
 				it->second.count = 1;
 				it->second.allthesamevalue = true;

src/TortoiseProc/HistoryDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2008 - TortoiseSVN
+// Copyright (C) 2003-2008,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -70,7 +70,7 @@ BOOL CHistoryDlg::OnInitDialog()
 	for (size_t i = 0; i < m_history->GetCount(); ++i)
 	{
 		CString sEntry = m_history->GetEntry(i);
-		sEntry.Replace(_T("\r"), _T(""));
+		sEntry.Remove('\r');
 		sEntry.Replace('\n', ' ');
 		m_List.AddString(sEntry);
 		itemExtent = pDC->GetTextExtent(sEntry);

src/TortoiseProc/LogDlg.cpp
@@ -646,7 +646,7 @@ void CLogDlg::FillLogMessageCtrl(bool bShow /* = true*/)
 			pMsgView->GetWindowText(text);
 			// the rich edit control doesn't count the CR char!
 			// to be exact: CRLF is treated as one char.
-			text.Replace(_T("\r"), _T(""));
+			text.Remove('\r');
 
 			m_LogList.m_ProjectProperties.FindBugID(text, pMsgView);
 			CAppUtils::FormatTextInRichEditControl(pMsgView);
@@ -785,8 +785,8 @@ CString CLogDlg::MakeShortMessage(const CString& message)
 		sShortMessage = message;
 	}
 	// Remove newlines and tabs 'cause those are not shown nicely in the list control
-	sShortMessage.Replace(_T("\r"), _T(""));
-	sShortMessage.Replace(_T("\t"), _T(" "));
+	sShortMessage.Remove('\r');
+	sShortMessage.Replace(_T('\t'), _T(' '));
 
 	// Suppose the first empty line separates 'summary' from the rest of the message.
 	int found = sShortMessage.Find(_T("\n\n"));
@@ -1514,7 +1514,7 @@ void CLogDlg::EditAuthor(const CLogDataVector& /*logs*/)
 	dlg.m_bUseLogWidth = false;
 	if (dlg.DoModal() == IDOK)
 	{
-		dlg.m_sInputText.Replace(_T("\r"), _T(""));
+		dlg.m_sInputText.Remove('\r');
 
 		LogCache::CCachedLogInfo* toUpdate = GetLogCache (CTGitPath (m_sRepositoryRoot));
 
@@ -1593,7 +1593,7 @@ void CLogDlg::EditLogMessage(int /*index*/)
 	dlg.m_bUseLogWidth = true;
 	if (dlg.DoModal() == IDOK)
 	{
-		dlg.m_sInputText.Replace(_T("\r"), _T(""));
+		dlg.m_sInputText.Remove('\r');
 		if (!RevPropertySet(name, dlg.m_sInputText, sOldValue, CTGitPath(url), pLogEntry->Rev))
 		{
 			CMessageBox::Show(this->m_hWnd, GetLastErrorMessage(), _T("TortoiseGit"), MB_ICONERROR);

src/TortoiseProc/ProjectProperties.cpp
@@ -1,6 +1,6 @@
-// TortoiseSVN - a Windows shell extension for easy version control
+// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2008 - TortoiseGit
+// Copyright (C) 2003-2011 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -223,7 +223,7 @@ BOOL ProjectProperties::ReadProps(CTGitPath path)
 			if ((!bFoundLogTemplate)&&(sPropName.Compare(PROJECTPROPNAME_LOGTEMPLATE)==0))
 			{
 				sLogTemplate = sPropVal;
-				sLogTemplate.Replace(_T("\r"), _T(""));
+				sLogTemplate.Remove('\r');
 				sLogTemplate.Replace(_T("\n"), _T("\r\n"));
 				bFoundLogTemplate = TRUE;
 			}
@@ -881,8 +881,8 @@ CString ProjectProperties::MakeShortMessage(const CString& message)
 		sShortMessage = message;
 	}
 	// Remove newlines and tabs 'cause those are not shown nicely in the list control
-	sShortMessage.Replace(_T("\r"), _T(""));
-	sShortMessage.Replace(_T("\t"), _T(" "));
+	sShortMessage.Remove('\r');
+	sShortMessage.Replace(_T('\t'), _T(' '));
 	
 	// Suppose the first empty line separates 'summary' from the rest of the message.
 	int found = sShortMessage.Find(_T("\n\n"));

commit 6f03bd00abe0487eec373a93284cf9c04b2892e5

fix bug introduced in cb489c6563cfb20284770786cdf036ef69f0afbd
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ProgressDlg.cpp
@@ -367,7 +367,7 @@ void CProgressDlg::ParserCmdOutput(char ch)
 }
 int CProgressDlg::ClearESC(CStringA &str)
 {
-	return str.Remove('\033[K');
+	return str.Replace("\033[K", "");
 }
 void CProgressDlg::ParserCmdOutput(CRichEditCtrl &log,CProgressCtrl &progressctrl,HWND m_hWnd,CComPtr<ITaskbarList3> m_pTaskbarList,CStringA &oneline, char ch, CWnd *CurrentWork)
 {