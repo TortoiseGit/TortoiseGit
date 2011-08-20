579a6095599e2483e7e7e2027c6e3dd69ce1e07f
Frank Li <lznuaa@gmail.com>
2011-08-21 01:54:10 +0800

    Fixed issue #851 Windows Explorer Shell Crashed when empty repos
    fixes issue 851
    
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/Git/gitindex.h
+++ src/Git/gitindex.h
@@ -257,7 +257,7 @@ public:
 	{
 		CAutoLocker lock(m_critIndexSec);
 		if(this->find(path) == end())
-			return SHARED_INDEX_PTR();
+			return SHARED_INDEX_PTR(new CGitIndexList);
 		else
 			return (*this)[path];
 	}
@@ -367,7 +367,7 @@ public:
 	{
 		CAutoLocker lock(m_critTreeSec);
 		if(this->find(path) == end())
-			return SHARED_TREE_PTR();
+			return SHARED_TREE_PTR(new CGitHeadFileList);
 		else
 			return (*this)[path];
 	}
