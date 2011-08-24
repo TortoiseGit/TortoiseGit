e7d4950e3c9af72323ae0f6f97f2e093bf24c8a1
Frank Li <lznuaa@gmail.com>
2011-08-24 20:57:11 +0800

    update version number 1.7.3
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        1,7,2,0
-#define PRODUCTVER     1,7,2,0
-#define STRFILEVER     "1, 7, 2, 0\0"
-#define STRPRODUCTVER  "1, 7, 2, 0\0"
+#define FILEVER        1,7,3,0
+#define PRODUCTVER     1,7,3,0
+#define STRFILEVER     "1, 7, 3, 0\0"
+#define STRPRODUCTVER  "1, 7, 3, 0\0"
 
 #define TSVN_VERMAJOR             1
 #define TSVN_VERMINOR             7
-#define TSVN_VERMICRO             2
+#define TSVN_VERMICRO             3
 #define TSVN_VERBUILD			  0
 #define TSVN_VERDATE			  __DATE__
 
