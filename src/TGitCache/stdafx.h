20a2f846071eb39426937f23fe90b503dcff41fd
Sven Strickroth <email@cs-ware.de>
2011-09-30 22:35:02 +0200

    use common targetver.h
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TGitCache/stdafx.h
+++ src/TGitCache/stdafx.h
@@ -5,10 +5,7 @@
 
 #pragma once
 
-#define WINVER 0x0600
-#define _WIN32_IE 0x700
-#define _WIN32_WINNT 0x0601
-#define _WIN32_WINDOWS 0x0410
+#include "..\targetver.h"
 
 #include <tchar.h>
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
