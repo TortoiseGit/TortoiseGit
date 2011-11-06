TortoiseGit/TortoiseGit

commit a57adcc8f64b849e1d07832d36db27175141f961

fixed "diff to base" on amend
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -3555,7 +3555,7 @@ void CGitStatusListCtrl::StartDiff(int fileindex)
 	if(this->m_CurrentVersion.IsEmpty() || m_CurrentVersion== GIT_REV_ZERO)
 	{
 		CString fromwhere;
-		if(m_amend)
+		if(m_amend && m_CurrentVersion != GIT_REV_ZERO)
 			fromwhere = _T("~1");
 		if( g_Git.IsInitRepos())
 			CGitDiff::DiffNull((CTGitPath*)GetItemData(fileindex),

commit 68c5e1bae23a90e484d7b9023b0c3849421cfde5

fixed "Compare with base" on amend (also for added files)
"Compare with base" on amend was completely broken since revision a57adcc8f64b849e1d07832d36db27175141f961 (tried to fix diff of added files).

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -3592,7 +3592,7 @@ void CGitStatusListCtrl::StartDiff(int fileindex)
 	if(this->m_CurrentVersion.IsEmpty() || m_CurrentVersion== GIT_REV_ZERO)
 	{
 		CString fromwhere;
-		if(m_amend && m_CurrentVersion != GIT_REV_ZERO)
+		if(m_amend && (file1.m_Action & CTGitPath::LOGACTIONS_ADDED) == 0)
 			fromwhere = _T("~1");
 		if( g_Git.IsInitRepos())
 			CGitDiff::DiffNull((CTGitPath*)GetItemData(fileindex),