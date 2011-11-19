TortoiseGit/TortoiseGit

commit 20400d83993be35b86d091610b374179c0792a90

Rename a lot of SVN references to GIT
Signed-off-by: Stefan Kueng <tortoisesvn@gmail.com>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TGitCache/TGITCache.cpp
@@ -20,7 +20,7 @@
 
 #include "stdafx.h"
 #include "shellapi.h"
-#include "TSVNCache.h"
+#include "TGITCache.h"
 #include "GitStatusCache.h"
 #include "CacheInterface.h"
 #include "Resource.h"
@@ -155,7 +155,7 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
 	HANDLE hPipeThread; 
 	HANDLE hCommandWaitThread;
 	MSG msg;
-	TCHAR szWindowClass[] = {TSVN_CACHE_WINDOW_NAME};
+	TCHAR szWindowClass[] = {TGIT_CACHE_WINDOW_NAME};
 
 	// create a hidden window to receive window messages.
 	WNDCLASSEX wcex;
@@ -172,7 +172,7 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
 	wcex.lpszClassName	= szWindowClass;
 	wcex.hIconSm		= 0;
 	RegisterClassEx(&wcex);
-	hWnd = CreateWindow(TSVN_CACHE_WINDOW_NAME, TSVN_CACHE_WINDOW_NAME, WS_CAPTION, 0, 0, 800, 300, NULL, 0, hInstance, 0);
+	hWnd = CreateWindow(TGIT_CACHE_WINDOW_NAME, TGIT_CACHE_WINDOW_NAME, WS_CAPTION, 0, 0, 800, 300, NULL, 0, hInstance, 0);
 	hTrayWnd = hWnd;
 	if (hWnd == NULL)
 	{
@@ -478,13 +478,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 //////////////////////////////////////////////////////////////////////////
 
-VOID GetAnswerToRequest(const TSVNCacheRequest* pRequest, TSVNCacheResponse* pReply, DWORD* pResponseLength)
+VOID GetAnswerToRequest(const TGITCacheRequest* pRequest, TGITCacheResponse* pReply, DWORD* pResponseLength)
 {
 	CTGitPath path;
 	*pResponseLength = 0;
-	if(pRequest->flags & TSVNCACHE_FLAGS_FOLDERISKNOWN)
+	if(pRequest->flags & TGITCACHE_FLAGS_FOLDERISKNOWN)
 	{
-		path.SetFromWin(pRequest->path, !!(pRequest->flags & TSVNCACHE_FLAGS_ISFOLDER));
+		path.SetFromWin(pRequest->path, !!(pRequest->flags & TGITCACHE_FLAGS_ISFOLDER));
 	}
 	else
 	{
@@ -666,7 +666,7 @@ DWORD WINAPI CommandWaitThread(LPVOID lpvParam)
 DWORD WINAPI InstanceThread(LPVOID lpvParam) 
 { 
 	ATLTRACE("InstanceThread started\n");
-	TSVNCacheResponse response; 
+	TGITCacheResponse response; 
 	DWORD cbBytesRead, cbWritten; 
 	BOOL fSuccess; 
 	HANDLE hPipe; 
@@ -678,7 +678,7 @@ DWORD WINAPI InstanceThread(LPVOID lpvParam)
 	while (bRun) 
 	{ 
 		// Read client requests from the pipe. 
-		TSVNCacheRequest request;
+		TGITCacheRequest request;
 		fSuccess = ReadFile( 
 			hPipe,        // handle to pipe 
 			&request,    // buffer to receive data 
@@ -748,7 +748,7 @@ DWORD WINAPI CommandThread(LPVOID lpvParam)
 	while (bRun) 
 	{ 
 		// Read client requests from the pipe. 
-		TSVNCacheCommand command;
+		TGITCacheCommand command;
 		fSuccess = ReadFile( 
 			hPipe,				// handle to pipe 
 			&command,			// buffer to receive data 
@@ -766,13 +766,13 @@ DWORD WINAPI CommandThread(LPVOID lpvParam)
 		
 		switch (command.command)
 		{
-			case TSVNCACHECOMMAND_END:
+			case TGITCACHECOMMAND_END:
 				FlushFileBuffers(hPipe); 
 				DisconnectNamedPipe(hPipe); 
 				CloseHandle(hPipe); 
 				ATLTRACE("Command thread exited\n");
 				return 0;
-			case TSVNCACHECOMMAND_CRAWL:
+			case TGITCACHECOMMAND_CRAWL:
 				{
 					CTGitPath changedpath;
 					changedpath.SetFromWin(CString(command.path), true);
@@ -783,12 +783,12 @@ DWORD WINAPI CommandThread(LPVOID lpvParam)
 					CGitStatusCache::Instance().AddFolderForCrawling(changedpath.GetDirectory());
 				}
 				break;
-			case TSVNCACHECOMMAND_REFRESHALL:
+			case TGITCACHECOMMAND_REFRESHALL:
 				CGitStatusCache::Instance().WaitToWrite();
 				CGitStatusCache::Instance().Refresh();
 				CGitStatusCache::Instance().Done();
 				break;
-			case TSVNCACHECOMMAND_RELEASE:
+			case TGITCACHECOMMAND_RELEASE:
 				{
 					CTGitPath changedpath;
 					changedpath.SetFromWin(CString(command.path), true);

src/TGitCache/TGITCache.vcproj
@@ -400,6 +400,10 @@
 				>
 			</File>
 			<File
+				RelativePath=".\GITStatusCache.cpp"
+				>
+			</File>
+			<File
 				RelativePath="..\Utils\PathUtils.cpp"
 				>
 			</File>
@@ -460,15 +464,11 @@
 				</FileConfiguration>
 			</File>
 			<File
-				RelativePath=".\SVNStatusCache.cpp"
+				RelativePath=".\TGITCache.cpp"
 				>
 			</File>
 			<File
 				RelativePath="..\Git\TGitPath.cpp"
-				>
-			</File>
-			<File
-				RelativePath=".\TSVNCache.cpp"
 				>
 			</File>
 			<File

commit 197bdbaf95aed90953309dc3856240663870bb97

fixed build of TGitCache
issue introduced in revision 20400d83993be35b86d091610b374179c0792a90

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TGitCache/TGITCache.cpp
@@ -198,7 +198,7 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
 		// load the icon
 		niData.hIcon =
 			(HICON)LoadImage(hInstance,
-			MAKEINTRESOURCE(IDI_TSVNCACHE),
+			MAKEINTRESOURCE(IDI_TGITCACHE),
 			IMAGE_ICON,
 			GetSystemMetrics(SM_CXSMICON),
 			GetSystemMetrics(SM_CYSMICON),

src/TGitCache/TGITCache.rc
@@ -28,7 +28,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 
 // Icon with lowest ID value placed first to ensure application icon
 // remains consistent on all systems.
-IDI_TSVNCACHE           ICON                    "..\\Resources\\TortoiseCache.ico"
+IDI_TGITCACHE           ICON                    "..\\Resources\\TortoiseCache.ico"
 #endif    // Neutral resources
 /////////////////////////////////////////////////////////////////////////////
 
@@ -62,7 +62,7 @@ END
 3 TEXTINCLUDE 
 BEGIN
     "\r\n"
-    "#include ""TSVNCache.rc2""  // non-Microsoft Visual C++ edited resources\r\n"
+    "#include ""TGITCache.rc2""  // non-Microsoft Visual C++ edited resources\r\n"
     "\0"
 END
 
@@ -79,7 +79,7 @@ END
 // Generated from the TEXTINCLUDE 3 resource.
 //
 
-#include "TSVNCache.rc2"  // non-Microsoft Visual C++ edited resources
+#include "TGITCache.rc2"  // non-Microsoft Visual C++ edited resources
 
 /////////////////////////////////////////////////////////////////////////////
 #endif    // not APSTUDIO_INVOKED

src/TGitCache/TGITCache.rc2
@@ -1,5 +1,5 @@
 //
-// TSVNCache.RC2 - resources Microsoft Visual C++ does not edit directly
+// TGitCache.RC2 - resources Microsoft Visual C++ does not edit directly
 //
 
 #ifdef APSTUDIO_INVOKED

src/TGitCache/TGITCache.vcproj
@@ -4,7 +4,7 @@
 	Version="9.00"
 	Name="TGitCache"
 	ProjectGUID="{A00D2EDC-B4F5-4B41-94BA-13961DE5D296}"
-	RootNamespace="TSVNCache"
+	RootNamespace="TGitCache"
 	Keyword="Win32Proj"
 	TargetFrameworkVersion="131072"
 	>
@@ -164,7 +164,7 @@
 				LinkIncremental="2"
 				IgnoreDefaultLibraryNames=""
 				GenerateDebugInformation="true"
-				ProgramDatabaseFile="$(OutDir)/TSVNCache.pdb"
+				ProgramDatabaseFile="$(OutDir)/TGitCache.pdb"
 				SubSystem="2"
 				RandomizedBaseAddress="1"
 				DataExecutionPrevention="0"
@@ -550,7 +550,7 @@
 				>
 			</File>
 			<File
-				RelativePath=".\TSVNCache.h"
+				RelativePath=".\TGitCache.h"
 				>
 			</File>
 			<File
@@ -572,7 +572,7 @@
 				>
 			</File>
 			<File
-				RelativePath=".\TSVNCache.rc"
+				RelativePath=".\TGitCache.rc"
 				>
 			</File>
 		</Filter>

src/TGitCache/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by TSVNCache.rc
 //
-#define IDI_TSVNCACHE                   101
+#define IDI_TGITCACHE                   101
 
 // Next default values for new objects
 // 