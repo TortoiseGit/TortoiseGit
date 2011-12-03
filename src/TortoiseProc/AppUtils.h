3fdf6bce8b5d94a12580ff7fb33bd905d527ce15
Sven Strickroth <email@cs-ware.de>
2011-12-03 18:16:40 +0100

    fixed default parameter values
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/AppUtils.h
+++ src/TortoiseProc/AppUtils.h
@@ -191,8 +191,8 @@ public:
 
 	static CString GetMergeTempFile(CString str,CTGitPath &merge);
 	static bool	StashSave();
-	static int	StashApply(CString ref, bool showChanges = false);
-	static int	StashPop(bool showChanges = false);
+	static int	StashApply(CString ref, bool showChanges = true);
+	static int	StashPop(bool showChanges = true);
 
 	static bool IsSSHPutty();
 
