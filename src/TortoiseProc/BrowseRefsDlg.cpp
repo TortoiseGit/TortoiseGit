TortoiseGit/TortoiseGit

commit 7ae6602064d8663b14c00c73763ccb17062c6a22

Fixed issue #858: Tortoise Git Client crash when showing the log dialog
Do not insert invalid refnames into the list control.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/BrowseRefsDlg.cpp
@@ -231,8 +231,6 @@ void CBrowseRefsDlg::Refresh(CString selectRef)
 	m_RefTreeCtrl.DeleteAllItems();
 	m_ListRefLeafs.DeleteAllItems();
 	m_TreeRoot.m_ShadowTree.clear();
-	m_TreeRoot.m_csRefName="refs";
-//	m_TreeRoot.m_csShowName="Refs";
 	m_TreeRoot.m_hTree=m_RefTreeCtrl.InsertItem(L"Refs",NULL,NULL);
 	m_RefTreeCtrl.SetItemData(m_TreeRoot.m_hTree,(DWORD_PTR)&m_TreeRoot);
 
@@ -399,13 +397,16 @@ void CBrowseRefsDlg::FillListCtrlForShadowTree(CShadowTree* pTree, CString refNa
 {
 	if(pTree->IsLeaf())
 	{
-		int indexItem=m_ListRefLeafs.InsertItem(m_ListRefLeafs.GetItemCount(),L"");
+		if (!pTree->m_csRefName.IsEmpty())
+		{
+			int indexItem = m_ListRefLeafs.InsertItem(m_ListRefLeafs.GetItemCount(), L"");
 
-		m_ListRefLeafs.SetItemData(indexItem,(DWORD_PTR)pTree);
-		m_ListRefLeafs.SetItemText(indexItem,eCol_Name,	refNamePrefix+pTree->m_csRefName);
-		m_ListRefLeafs.SetItemText(indexItem,eCol_Date,	pTree->m_csDate);
-		m_ListRefLeafs.SetItemText(indexItem,eCol_Msg,	pTree->m_csSubject);
-		m_ListRefLeafs.SetItemText(indexItem,eCol_Hash,	pTree->m_csRefHash);
+			m_ListRefLeafs.SetItemData(indexItem,(DWORD_PTR)pTree);
+			m_ListRefLeafs.SetItemText(indexItem,eCol_Name, refNamePrefix+pTree->m_csRefName);
+			m_ListRefLeafs.SetItemText(indexItem,eCol_Date, pTree->m_csDate);
+			m_ListRefLeafs.SetItemText(indexItem,eCol_Msg, pTree->m_csSubject);
+			m_ListRefLeafs.SetItemText(indexItem,eCol_Hash, pTree->m_csRefHash);
+		}
 	}
 	else
 	{

commit 5199fcc1197b96d7ad6ed29e2d08a26ee462c326

better fix for issue #858
Fixes a bug introduced in revision 7ae6602064d8663b14c00c73763ccb17062c6a22: deleting of branches was not possible any more.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/BrowseRefsDlg.cpp
@@ -231,6 +231,7 @@ void CBrowseRefsDlg::Refresh(CString selectRef)
 	m_RefTreeCtrl.DeleteAllItems();
 	m_ListRefLeafs.DeleteAllItems();
 	m_TreeRoot.m_ShadowTree.clear();
+	m_TreeRoot.m_csRefName = "refs";
 	m_TreeRoot.m_hTree=m_RefTreeCtrl.InsertItem(L"Refs",NULL,NULL);
 	m_RefTreeCtrl.SetItemData(m_TreeRoot.m_hTree,(DWORD_PTR)&m_TreeRoot);
 
@@ -397,7 +398,7 @@ void CBrowseRefsDlg::FillListCtrlForShadowTree(CShadowTree* pTree, CString refNa
 {
 	if(pTree->IsLeaf())
 	{
-		if (!pTree->m_csRefName.IsEmpty())
+		if (!(pTree->m_csRefName.IsEmpty() || pTree->m_csRefName == "refs" && pTree->m_pParent == NULL))
 		{
 			int indexItem = m_ListRefLeafs.InsertItem(m_ListRefLeafs.GetItemCount(), L"");
 