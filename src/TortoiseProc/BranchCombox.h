672e901ed6c31e8be9349c2e2e7b52e2db75e1c1
Sven Strickroth <email@cs-ware.de>
2011-12-16 02:43:56 +0100

    improved tooltip
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/BranchCombox.h
+++ src/TortoiseProc/BranchCombox.h
@@ -135,7 +135,7 @@ protected:
 				return;
 			}
 
-			tooltip.Format(_T("CommitHash:%s\nCommit by: %s  %s\n <b>%s</b> \n %s"),
+			tooltip.Format(_T("CommitHash: %s\nCommit by: %s\nDate:%s\n%s\n%s"),
 				rev.m_CommitHash.ToString(),
 				rev.GetAuthorName(),
 				CAppUtils::FormatDateAndTime(rev.GetAuthorDate(),DATE_LONGDATE),
