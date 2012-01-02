07b8d1f6872616967a0b07c94af218fe99ac64ee
Sven Strickroth <email@cs-ware.de>
2012-01-02 08:45:40 +0100

    allow users to toggle --topo-order
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/resource.h
+++ src/TortoiseProc/resource.h
@@ -241,6 +241,7 @@
 #define IDC_RAM                         1073
 #define IDC_CHECKNEWERVERSION           1073
 #define IDC_SHOWUNVERSIONED             1073
+#define IDC_TOPOORDER                   1073
 #define IDC_EXPORT_CHECKOUTDIR          1074
 #define IDC_VERSIONBOX                  1075
 #define IDC_LABEL                       1076
