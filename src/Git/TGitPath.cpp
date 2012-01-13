TortoiseGit/TortoiseGit

commit b2d6822ac9fc5deec5cc80bdfe9fb53b5d4719dd

make CTGitPath work with separate-git-dir
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/TGitPath.cpp
@@ -809,7 +809,7 @@ bool CTGitPath::HasSubmodules() const
 int CTGitPath::GetAdminDirMask() const
 {
 	int status = 0;
-	CString topdir,path;
+	CString topdir;
 	if(!g_GitAdminDir.HasAdminDir(GetWinPathString(),&topdir))
 	{
 		return status;
@@ -825,30 +825,19 @@ int CTGitPath::GetAdminDirMask() const
 			status |= ITEMIS_WCROOT;
 	}
 
-	path=topdir;
-	path += _T("\\");
-	path += g_GitAdminDir.GetAdminDirName();
-	path += _T("\\BISECT_START");
-	if (PathFileExists(path))
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(g_Git.m_CurrentDir, dotGitPath);
+
+	if (PathFileExists(dotGitPath + _T("BISECT_START")))
 		status |= ITEMIS_BISECT;
 
-	path=topdir;
-	path += _T("\\");
-	path += g_GitAdminDir.GetAdminDirName();
-	path += _T("\\refs\\stash");
-	if( PathFileExists(path) )
+	if (PathFileExists(dotGitPath + _T("refs\\stash")))
 		status |= ITEMIS_STASH;
 
-	path=topdir;
-	path += _T("\\");
-	path += g_GitAdminDir.GetAdminDirName();
-	path += _T("\\svn");
-	if( PathFileExists(path) )
+	if (PathFileExists(dotGitPath + _T("svn")))
 		status |= ITEMIS_GITSVN;
 
-	path=topdir;
-	path += _T("\\.gitmodules");
-	if( PathFileExists(path) )
+	if (PathFileExists(topdir + _T("\\.gitmodules")))
 		status |= ITEMIS_SUBMODULECONTAINER;
 
 	return status;
@@ -861,10 +850,11 @@ bool CTGitPath::HasStashDir() const
 	{
 		return false;
 	}
-	topdir += _T("\\");
-	topdir += g_GitAdminDir.GetAdminDirName();
-	topdir += _T("\\refs\\stash");
-	return !!PathFileExists(topdir);
+
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);
+
+	return !!PathFileExists(dotGitPath + _T("\\refs\\stash"));
 }
 bool CTGitPath::HasGitSVNDir() const
 {
@@ -873,10 +863,11 @@ bool CTGitPath::HasGitSVNDir() const
 	{
 		return false;
 	}
-	topdir += _T("\\");
-	topdir += g_GitAdminDir.GetAdminDirName();
-	topdir += _T("\\svn");
-	return !!PathFileExists(topdir);
+
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);
+
+	return !!PathFileExists(dotGitPath + _T("svn"));
 }
 bool CTGitPath::IsBisectActive() const
 {
@@ -885,10 +876,11 @@ bool CTGitPath::IsBisectActive() const
 	{
 		return false;
 	}
-	topdir += _T("\\");
-	topdir += g_GitAdminDir.GetAdminDirName();
-	topdir += _T("\\BISECT_START");
-	return !!PathFileExists(topdir);
+
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);
+
+	return !!PathFileExists(dotGitPath + _T("BISECT_START"));
 }
 bool CTGitPath::IsMergeActive() const
 {
@@ -897,10 +889,11 @@ bool CTGitPath::IsMergeActive() const
 	{
 		return false;
 	}
-	topdir += _T("\\");
-	topdir += g_GitAdminDir.GetAdminDirName();
-	topdir += _T("\\MERGE_HEAD");
-	return !!PathFileExists(topdir);
+
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);	
+
+	return !!PathFileExists(dotGitPath + _T("MERGE_HEAD"));
 }
 bool CTGitPath::HasRebaseApply() const
 {
@@ -909,10 +902,11 @@ bool CTGitPath::HasRebaseApply() const
 	{
 		return false;
 	}
-	topdir += _T("\\");
-	topdir += g_GitAdminDir.GetAdminDirName();
-	topdir += _T("\\rebase-apply");
-	return !!PathFileExists(topdir);
+
+	CString dotGitPath;
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);
+
+	return !!PathFileExists(dotGitPath + _T("rebase-apply"));
 }
 
 bool CTGitPath::HasAdminDir(CString *ProjectTopDir) const

commit a30891b076e317280001771db4fa0e9c2c704199

Fixed issue #1038: Stash Pop/List/bisect menu items are not shown
Regression introduced in revision b2d6822ac9fc5deec5cc80bdfe9fb53b5d4719dd.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/TGitPath.cpp
@@ -827,7 +827,7 @@ int CTGitPath::GetAdminDirMask() const
 	}
 
 	CString dotGitPath;
-	g_GitAdminDir.GetAdminDirPath(g_Git.m_CurrentDir, dotGitPath);
+	g_GitAdminDir.GetAdminDirPath(topdir, dotGitPath);
 
 	if (PathFileExists(dotGitPath + _T("BISECT_START")))
 		status |= ITEMIS_BISECT;