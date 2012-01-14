f4d6187700a7492e066dfac35345d85da99da10b
Sven Strickroth <email@cs-ware.de>
2012-01-14 02:47:29 +0100

    refactored
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/LoglistUtils.h
+++ src/TortoiseProc/LoglistUtils.h
@@ -33,16 +33,15 @@ public:
 	 * FUNCTION    :   FormatDateAndTime
 	 * DESCRIPTION :   Generates a displayable string from a CTime object in
 	 *                 system short or long format  or as a relative value
-	 *				   cTime - the time
-	 *				   option - DATE_SHORTDATE or DATE_LONGDATE
-	 *				   bIncluedeTime - whether to show time as well as date
-	 *				   bRelative - if true then relative time is shown if reasonable
-	 *				   If HKCU\Software\TortoiseGit\UseSystemLocaleForDates is 0 then use fixed format
-	 *				   rather than locale
+	 *                 cTime - the time
+	 *                 option - DATE_SHORTDATE or DATE_LONGDATE
+	 *                 bIncluedeTime - whether to show time as well as date
+	 *                 bRelative - if true then relative time is shown if reasonable
+	 *                 If HKCU\Software\TortoiseGit\UseSystemLocaleForDates is 0 then use fixed format
+	 *                 rather than locale
 	 * RETURN      :   CString containing date/time
 	 */
-	static CString FormatDateAndTime( const CTime& cTime, DWORD option, bool bIncludeTime=true,
-		bool bRelative=false );
+	static CString FormatDateAndTime(const CTime& cTime, DWORD option, bool bIncludeTime=true, bool bRelative=false);
 	/**
 	 *	Converts a given time to a relative display string (relative to current time)
 	 *	Given time must be in local timezone
@@ -53,6 +52,6 @@ private:
 	/**
 	 *	Generates a display string showing the relative time between the two given times as COleDateTimes
 	 */
-	static CString ToRelativeTimeString(COleDateTime time,COleDateTime RelativeTo);
-	static CString ExpandRelativeTime( int count, UINT format_1, UINT format_n );
+	static CString ToRelativeTimeString(COleDateTime time, COleDateTime RelativeTo);
+	static CString ExpandRelativeTime(int count, UINT format_1, UINT format_n);
 };
