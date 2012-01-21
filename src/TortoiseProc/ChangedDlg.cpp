TortoiseGit/TortoiseGit

commit 62bb9e8e7c19a433ba19d0164b79e73fa42a5726

added RunTortoiseProc-method to call tortoiseproc.exe and centralize calling code
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ChangedDlg.cpp
@@ -360,16 +360,14 @@ void CChangedDlg::UpdateStatistics()
 
 void CChangedDlg::OnBnClickedCommit()
 {
-	CString proc = CPathUtils::GetAppDirectory();
-	proc += _T("TortoiseProc.exe /command:commit");
-	proc += _T(" /path:\"");
+	CString cmd = _T("/path:\"");
 	bool bSingleFile = ((m_pathList.GetCount()==1)&&(!m_pathList[0].IsEmpty())&&(!m_pathList[0].IsDirectory()));
 	if (bSingleFile)
-		proc += m_pathList[0].GetWinPathString();
+		cmd += m_pathList[0].GetWinPathString();
 	else
-		proc += m_FileListCtrl.GetCommonDirectory(false);
+		cmd += m_FileListCtrl.GetCommonDirectory(false);
 
-	CAppUtils::LaunchApplication(proc, IDS_ERROR_CANNON_FIND_TORTOISEPROC, false);
+	CAppUtils::RunTortoiseProc(cmd);
 }
 
 void CChangedDlg::OnBnClickedStash()

src/Git/GitStatusListCtrl.cpp
@@ -2235,8 +2235,8 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 							break;
 
 						CString sCmd;
-						sCmd.Format(_T("\"%s\" /command:diff /path:\"%s\" /path2:\"%s\" /hwnd:%ld"), (LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")), firstfilepath->GetWinPath(), secondfilepath->GetWinPath(), (unsigned long)m_hWnd);
-						CAppUtils::LaunchApplication(sCmd, NULL, false);
+						sCmd.Format(_T("/command:diff /path:\"%s\" /path2:\"%s\" /hwnd:%ld"), firstfilepath->GetWinPath(), secondfilepath->GetWinPath(), (unsigned long)m_hWnd);
+						CAppUtils::RunTortoiseProc(sCmd);
 					}
 				}
 				break;
@@ -2402,10 +2402,8 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 			case IDGITLC_LOG:
 				{
 					CString sCmd;
-					sCmd.Format(_T("\"%s\" /command:log /path:\"%s\""),
-						(LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), g_Git.m_CurrentDir+_T("\\")+filepath->GetWinPath());
-
-					CAppUtils::LaunchApplication(sCmd, NULL, false);
+					sCmd.Format(_T("/command:log /path:\"%s\""), g_Git.m_CurrentDir + _T("\\") + filepath->GetWinPath());
+					CAppUtils::RunTortoiseProc(sCmd);
 				}
 				break;
 
@@ -2413,10 +2411,8 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 				{
 					CTGitPath oldName(filepath->GetGitOldPathString());
 					CString sCmd;
-					sCmd.Format(_T("\"%s\" /command:log /path:\"%s\""),
-						(LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")), g_Git.m_CurrentDir + _T("\\") + oldName.GetWinPath());
-
-					CAppUtils::LaunchApplication(sCmd, NULL, false);
+					sCmd.Format(_T("/command:log /path:\"%s\""), g_Git.m_CurrentDir + _T("\\") + oldName.GetWinPath());
+					CAppUtils::RunTortoiseProc(sCmd);
 				}
 				break;
 

src/Resources/TortoiseProcENG.rc
@@ -3595,7 +3595,6 @@ BEGIN
     IDS_MENU_VIEWWITHMERGE  "Review Path with TortoiseMerge"
     IDS_MENU_APPLY          "Apply Patch..."
     IDS_MENU_SENDMAIL       "Send Mail..."
-    IDS_ERR_PROC            "Can't open TortoiseProc"
     IDS_ERROR_NOREF         "No reference found"
     IDS_ERROR_INDEX         "Out of ref array"
     IDS_ERROR_HEAD          "Can't delete ""HEAD"""
@@ -3610,7 +3609,6 @@ END
 
 STRINGTABLE 
 BEGIN
-    IDS_ERROR_CANNON_FIND_TORTOISEPROC "Can't found application TortoiseProc"
     IDS_REBASE_FASTFORWARD_FMT 
                             "Branch %s behind %s\r\n%s will fastforward to %s"
     IDS_REBASE_EQUAL_FMT    "Nothing need rebase\r\n%s equal %s"

src/TortoiseGitBlame/LogListBlameAction.cpp
@@ -24,7 +24,7 @@
 #include "TortoiseGitBlameDoc.h"
 #include "TortoiseGitBlameView.h"
 #include "MainFrm.h"
-#include "PathUtils.h"
+#include "CommonAppUtils.h"
 
 IMPLEMENT_DYNAMIC(CGitBlameLogList, CHintListCtrl)
 
@@ -55,7 +55,7 @@ void CGitBlameLogList::ContextMenuAction(int cmd,int FirstSelect, int LastSelect
 
 	bool bOpenWith = false;
 
-	procCmd += _T(" /path:\"");
+	procCmd += _T("/path:\"");
 	procCmd += ((CMainFrame*)::AfxGetApp()->GetMainWnd())->GetActiveView()->GetDocument()->GetPathName();
 	procCmd += _T("\" ");
 	procCmd += _T(" /rev:")+this->m_logEntries.GetGitRevAt(indexNext).m_CommitHash.ToString();
@@ -471,17 +471,5 @@ void CGitBlameLogList::ContextMenuAction(int cmd,int FirstSelect, int LastSelect
 
 		} // switch (cmd)
 
-
-	STARTUPINFO startup;
-	PROCESS_INFORMATION process;
-	memset(&startup, 0, sizeof(startup));
-	startup.cb = sizeof(startup);
-	memset(&process, 0, sizeof(process));
-	CString tortoiseProcPath = CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe");
-
-	if (CreateProcess(tortoiseProcPath, procCmd.GetBuffer(), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-	{
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
-	}
+		CCommonAppUtils::RunTortoiseProc(procCmd);
 }

src/TortoiseGitBlame/TortoiseGitBlameView.cpp
@@ -25,7 +25,7 @@
 
 #include "stdafx.h"
 #include "TortoiseGitBlame.h"
-
+#include "CommonAppUtils.h"
 #include "TortoiseGitBlameDoc.h"
 #include "TortoiseGitBlameView.h"
 #include "MainFrm.h"
@@ -35,7 +35,6 @@
 #include "UniCodeUtils.h"
 #include "MenuEncode.h"
 #include "gitdll.h"
-#include "PathUtils.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -880,72 +879,36 @@ void CTortoiseGitBlameView::CopySelectedLogToClipboard()
 
 void CTortoiseGitBlameView::BlamePreviousRevision()
 {
-	CString  procCmd;
-	procCmd += _T(" /path:\"");
+	CString procCmd = _T("/path:\"");
 	procCmd += ((CMainFrame*)::AfxGetApp()->GetMainWnd())->GetActiveView()->GetDocument()->GetPathName();
 	procCmd += _T("\" ");
 	procCmd += _T(" /command:blame");
 	procCmd += _T(" /endrev:") + this->GetLogData()->GetGitRevAt(this->GetLogData()->size()-m_ID[m_MouseLine]+1).m_CommitHash.ToString();
 
-	STARTUPINFO startup;
-	PROCESS_INFORMATION process;
-	memset(&startup, 0, sizeof(startup));
-	startup.cb = sizeof(startup);
-	memset(&process, 0, sizeof(process));
-	CString tortoiseProcPath = CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe");
-
-	if (CreateProcess(tortoiseProcPath, procCmd.GetBuffer(), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-	{
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
-	}
+	CCommonAppUtils::RunTortoiseProc(procCmd);
 }
 
 void CTortoiseGitBlameView::DiffPreviousRevision()
 {
-	CString  procCmd;
-	procCmd += _T(" /path:\"");
+	CString procCmd = _T("/path:\"");
 	procCmd += ((CMainFrame*)::AfxGetApp()->GetMainWnd())->GetActiveView()->GetDocument()->GetPathName();
 	procCmd += _T("\" ");
 	procCmd += _T(" /command:diff");
 	procCmd += _T(" /startrev:") + this->GetLogData()->GetGitRevAt(this->GetLogData()->size() - m_ID[m_MouseLine]).m_CommitHash.ToString();
 	procCmd += _T(" /endrev:") + this->GetLogData()->GetGitRevAt(this->GetLogData()->size() - m_ID[m_MouseLine] + 1).m_CommitHash.ToString();
 
-	STARTUPINFO startup;
-	PROCESS_INFORMATION process;
-	memset(&startup, 0, sizeof(startup));
-	startup.cb = sizeof(startup);
-	memset(&process, 0, sizeof(process));
-	CString tortoiseProcPath = CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe");
-
-	if (CreateProcess(tortoiseProcPath, procCmd.GetBuffer(), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-	{
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
-	}
+	CCommonAppUtils::RunTortoiseProc(procCmd);
 }
 
 void CTortoiseGitBlameView::ShowLog()
 {
-	CString  procCmd;
-	procCmd += _T(" /path:\"");
+	CString procCmd = _T("/path:\"");
 	procCmd += ((CMainFrame*)::AfxGetApp()->GetMainWnd())->GetActiveView()->GetDocument()->GetPathName();
 	procCmd += _T("\" ");
 	procCmd += _T(" /command:log");
 	procCmd += _T(" /rev:") + this->GetLogData()->GetGitRevAt(this->GetLogData()->size() - m_ID[m_MouseLine]).m_CommitHash.ToString();
 
-	STARTUPINFO startup;
-	PROCESS_INFORMATION process;
-	memset(&startup, 0, sizeof(startup));
-	startup.cb = sizeof(startup);
-	memset(&process, 0, sizeof(process));
-	CString tortoiseProcPath = CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe");
-
-	if (CreateProcess(tortoiseProcPath, procCmd.GetBuffer(), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-	{
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
-	}
+	CCommonAppUtils::RunTortoiseProc(procCmd);
 }
 
 void CTortoiseGitBlameView::Notify(SCNotification *notification)

src/TortoiseMerge/MainFrm.cpp
@@ -1,5 +1,6 @@
 // TortoiseMerge - a Diff/Patch program
 
+// Copyright (C) 2008-2012 - TortoiseGit
 // Copyright (C) 2004-2009 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -1102,25 +1103,9 @@ bool CMainFrame::FileSave(bool bCheckResolved /*=true*/)
 	if (bDoesNotExist)
 	{
 		// call TortoiseProc to add the new file to version control
-		CString cmd = _T("\"") + CPathUtils::GetAppDirectory();
-		cmd += _T("TortoiseProc.exe\" /command:add /noui /path:\"");
+		CString cmd = _T("/command:add /noui /path:\"");
 		cmd += m_Data.m_mergedFile.GetFilename() + _T("\"");
-		TCHAR * buf = new TCHAR[cmd.GetLength()+1];
-		_tcscpy_s(buf, cmd.GetLength()+1, cmd);
-		STARTUPINFO startup;
-		PROCESS_INFORMATION process;
-		memset(&startup, 0, sizeof(startup));
-		startup.cb = sizeof(startup);
-		memset(&process, 0, sizeof(process));
-		if (CreateProcess(NULL, buf, NULL, NULL, FALSE, 0, 0, 0, &startup, &process)==0)
-		{
-			delete [] buf;
-			MessageBox(CFormatMessageWrapper(), _T("TortoiseGit"), MB_OK | MB_ICONINFORMATION);
-			return FALSE;
-		}
-		delete [] buf;
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
+		CAppUtils::RunTortoiseProc(cmd);
 	}
 	return true;
 }
@@ -1815,43 +1800,15 @@ BOOL CMainFrame::MarkAsResolved()
 {
 	if (m_bReadOnly)
 		return FALSE;
-	if ((m_pwndBottomView)&&(m_pwndBottomView->IsWindowVisible()))
-	{
-		TCHAR buf[MAX_PATH*3];
-		GetModuleFileName(NULL, buf, MAX_PATH);
-		TCHAR * end = _tcsrchr(buf, '\\');
-		end++;
-		(*end) = 0;
-		_tcscat_s(buf, MAX_PATH*3, _T("TortoiseProc.exe /command:resolve /path:\""));
-		_tcscat_s(buf, MAX_PATH*3, m_Data.m_mergedFile.GetFilename());
-		_tcscat_s(buf, MAX_PATH*3, _T("\" /closeonend:1 /noquestion /skipcheck"));
-		STARTUPINFO startup;
-		PROCESS_INFORMATION process;
-		memset(&startup, 0, sizeof(startup));
-		startup.cb = sizeof(startup);
-		memset(&process, 0, sizeof(process));
-		if (CreateProcess(NULL, buf, NULL, NULL, FALSE, 0, 0, 0, &startup, &process)==0)
-		{
-			LPVOID lpMsgBuf;
-			FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-				FORMAT_MESSAGE_FROM_SYSTEM | 
-				FORMAT_MESSAGE_IGNORE_INSERTS,
-				NULL,
-				GetLastError(),
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-				(LPTSTR) &lpMsgBuf,
-				0,
-				NULL 
-				);
-			MessageBox((LPCTSTR)lpMsgBuf, _T("TortoiseMerge"), MB_OK | MB_ICONINFORMATION);
-			LocalFree( lpMsgBuf );
-			return FALSE;
-		}
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
-	}
-	else
+	if (!((m_pwndBottomView) && (m_pwndBottomView->IsWindowVisible())))
 		return FALSE;
+
+	CString cmd = _T("/command:resolve /path:\"");
+	cmd += m_Data.m_mergedFile.GetFilename();
+	cmd += _T("\" /closeonend:1 /noquestion /skipcheck");
+	if (!CAppUtils::RunTortoiseProc(cmd))
+		return FALSE;
+
 	return TRUE;
 }
 

src/TortoiseProc/AppUtils.cpp
@@ -1038,9 +1038,9 @@ bool CAppUtils::PerformSwitch(CString ref, bool bForce /* false */, CString sNew
 	if (gitPath.HasSubmodules() && ret == IDC_PROGRESS_BUTTON1)
 	{
 		CString sCmd;
-		sCmd.Format(_T("\"%s\" /command:subupdate /bkpath:\"%s\""), (LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")), (LPCTSTR)g_Git.m_CurrentDir);
+		sCmd.Format(_T("/command:subupdate /bkpath:\"%s\""), g_Git.m_CurrentDir);
 
-		LaunchApplication(sCmd, NULL, false);
+		RunTortoiseProc(sCmd);
 		return TRUE;
 	}
 	else if (ret == IDOK)
@@ -1141,9 +1141,9 @@ bool CAppUtils::GitReset(CString *CommitHash,int type)
 		if (gitPath.HasSubmodules() && dlg.m_ResetType == 2 && ret == IDC_PROGRESS_BUTTON1)
 		{
 			CString sCmd;
-			sCmd.Format(_T("\"%s\" /command:subupdate /bkpath:\"%s\""), (LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), (LPCTSTR)g_Git.m_CurrentDir);
+			sCmd.Format(_T("/command:subupdate /bkpath:\"%s\""), g_Git.m_CurrentDir);
 
-			LaunchApplication(sCmd, NULL, false);
+			RunTortoiseProc(sCmd);
 			return TRUE;
 		}
 		else if (ret == IDOK)
@@ -1908,11 +1908,9 @@ bool CAppUtils::Fetch(CString remoteName, bool allowRebase, bool autoClose)
 
 		if (userResponse == IDC_PROGRESS_BUTTON1)
 		{
-			CString cmd;
-			cmd = CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe");
-			cmd += _T(" /command:log");
+			CString cmd = _T("/command:log");
 			cmd += _T(" /path:\"") + g_Git.m_CurrentDir + _T("\"");
-			CAppUtils::LaunchApplication(cmd, IDS_ERR_PROC, false);
+			RunTortoiseProc(cmd);
 			return TRUE;
 		}
 		else if ((userResponse == IDC_PROGRESS_BUTTON1 + 1) || (progress.m_GitStatus == 0 && dlg.m_bRebase))

src/TortoiseProc/Commands/BisectCommand.cpp
@@ -20,7 +20,6 @@
 #include "BisectCommand.h"
 #include "BisectStartDlg.h"
 #include "AppUtils.h"
-#include "PathUtils.h"
 #include "ProgressDlg.h"
 #include "MessageBox.h"
 
@@ -64,9 +63,9 @@ bool BisectCommand::Execute()
 			if (path.HasSubmodules() && ret == IDC_PROGRESS_BUTTON1)
 			{
 				CString sCmd;
-				sCmd.Format(_T("\"%s\" /command:subupdate /bkpath:\"%s\""), (LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")), (LPCTSTR)g_Git.m_CurrentDir);
+				sCmd.Format(_T("/command:subupdate /bkpath:\"%s\""), g_Git.m_CurrentDir);
 
-				CAppUtils::LaunchApplication(sCmd, NULL, false);
+				CAppUtils::RunTortoiseProc(sCmd);
 				return true;
 			}
 			else if (ret == IDOK)
@@ -111,17 +110,14 @@ bool BisectCommand::Execute()
 		if (path.HasSubmodules() && ret == IDC_PROGRESS_BUTTON1)
 		{
 			CString sCmd;
-			sCmd.Format(_T("\"%s\" /command:subupdate /bkpath:\"%s\""), (LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")), (LPCTSTR)g_Git.m_CurrentDir);
+			sCmd.Format(_T("/command:subupdate /bkpath:\"%s\""), g_Git.m_CurrentDir);
 
-			CAppUtils::LaunchApplication(sCmd, NULL, false);
+			CAppUtils::RunTortoiseProc(sCmd);
 			return true;
 		}
 		else if (reset >= 0 && ret == IDC_PROGRESS_BUTTON1 + reset)
 		{
-			CString sCmd;
-			sCmd.Format(_T("\"%s\" /command:bisect /reset"), (LPCTSTR)(CPathUtils::GetAppDirectory() + _T("TortoiseProc.exe")));
-
-			CAppUtils::LaunchApplication(sCmd, NULL, false);
+			CAppUtils::RunTortoiseProc(_T("/command:bisect /reset"));
 			return true;
 		}
 		else if (ret == IDOK)

commit f994dd7215f6d73d864f19d1e3d4ecf10284437a

fixed commit button on changed files dialog
issue introduced in revision 62bb9e8e7c19a433ba19d0164b79e73fa42a5726.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ChangedDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2011 - TortoiseGit
+// Copyright (C) 2008-2012 - TortoiseGit
 // Copyright (C) 2003-2008 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -360,7 +360,7 @@ void CChangedDlg::UpdateStatistics()
 
 void CChangedDlg::OnBnClickedCommit()
 {
-	CString cmd = _T("/path:\"");
+	CString cmd = _T("/command:commit /path:\"");
 	bool bSingleFile = ((m_pathList.GetCount()==1)&&(!m_pathList[0].IsEmpty())&&(!m_pathList[0].IsDirectory()));
 	if (bSingleFile)
 		cmd += m_pathList[0].GetWinPathString();