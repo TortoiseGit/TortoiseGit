a908d8372de60b59673198dc7047114de17618bd
Sven Strickroth <email@cs-ware.de>
2012-01-27 21:46:44 +0100

    make it compile with Win32
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/MassiveGitTask.h
+++ src/TortoiseProc/MassiveGitTask.h
@@ -23,7 +23,7 @@
 
 #define MAX_COMMANDLINE_LENGTH 30000
 
-typedef BOOL (__cdecl CGitProgressDlg::*NOTIFY_CALLBACK)(const CTGitPath& path, git_wc_notify_action_t action, int status, CString *strErr);
+typedef BOOL (CGitProgressDlg::*NOTIFY_CALLBACK)(const CTGitPath& path, git_wc_notify_action_t action, int status, CString *strErr);
 
 class MassiveGitTask
 {
