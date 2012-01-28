baff83634d3bc4109b542bfb862db0c1ef19f652
Sven Strickroth <email@cs-ware.de>
2012-01-28 22:02:53 +0100

    bump version number to 1.7.7
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
-#define FILEVER        1,7,6,0
-#define PRODUCTVER     1,7,6,0
-#define STRFILEVER     "1, 7, 6, 0\0"
-#define STRPRODUCTVER  "1, 7, 6, 0\0"
+#define FILEVER        1,7,7,0
+#define PRODUCTVER     1,7,7,0
+#define STRFILEVER     "1, 7, 7, 0\0"
+#define STRPRODUCTVER  "1, 7, 7, 0\0"
 
 #define TGIT_VERMAJOR             1
 #define TGIT_VERMINOR             7
-#define TGIT_VERMICRO             6
+#define TGIT_VERMICRO             7
 #define TGIT_VERBUILD			  0
 #define TGIT_VERDATE			  __DATE__
 
