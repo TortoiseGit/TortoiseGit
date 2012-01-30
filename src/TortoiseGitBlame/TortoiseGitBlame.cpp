14805ec06054a809ef0e5703aadc6fedb6a768d5
Sven Strickroth <email@cs-ware.de>
2012-01-30 23:27:20 +0100

    correctly initialize sOrigCWD before using it
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseGitBlame/TortoiseGitBlame.cpp
+++ src/TortoiseGitBlame/TortoiseGitBlame.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2011 - TortoiseGit
+// Copyright (C) 2008-2012 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -73,8 +73,8 @@ CTortoiseGitBlameApp::CTortoiseGitBlameApp()
 
 // The one and only CTortoiseGitBlameApp object
 
-CTortoiseGitBlameApp theApp;
 CString sOrigCWD;
+CTortoiseGitBlameApp theApp;
 
 // CTortoiseGitBlameApp initialization
 
