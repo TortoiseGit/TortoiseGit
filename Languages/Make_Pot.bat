f3aa14fa5fd0cb76f4b68ad5a55e0d39147d2105
Frank Li <lznuaa@gmail.com>
2012-05-03 15:36:35 +0800

    Update Chinese po file add TortoiseIDiffLang to solution
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- Languages/Make_Pot.bat
+++ Languages/Make_Pot.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-..\bin\release\bin\ResText extract ..\bin\release\bin\TortoiseProcLang.dll ..\bin\release\bin\TortoiseMergeLang.dll ..\bin\release\bin\TortoiseGit32.dll  Tortoise.pot
+..\bin\release\bin\ResText extract ..\bin\release\bin\TortoiseProcLang.dll ..\bin\release\bin\TortoiseMergeLang.dll ..\bin\release\bin\TortoiseIDiffLang.dll ..\bin\release\bin\TortoiseGit32.dll  Tortoise.pot 
 
 rem leave the next two lines commented. The msgremove tool changes the
 rem sequence "\r\n" to "r\n" - removing the backslash before the r!!!
