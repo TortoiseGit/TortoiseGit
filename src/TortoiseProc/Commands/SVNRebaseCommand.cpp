f471f2b830621e24c33b7be935361412641cd5cf
Sven Strickroth <email@cs-ware.de>
2012-05-03 23:49:17 +0200

    Allow users to "stash pop" after successful SVN rebase
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Commands/SVNRebaseCommand.cpp
+++ src/TortoiseProc/Commands/SVNRebaseCommand.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2008 - TortoiseGit
+// Copyright (C) 2009-2012 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -126,6 +126,8 @@ bool SVNRebaseCommand::Execute()
 	//need rebase
 	if(dlg.DoModal() == IDOK)
 	{
+		if(isStash)
+			askIfUserWantsToStashPop();
 		return true;
 	}
 	return false;
