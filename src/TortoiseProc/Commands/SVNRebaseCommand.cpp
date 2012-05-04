TortoiseGit/TortoiseGit

commit b139beafc5df3071effa77d490657146d604164f

Fixed issue #1155: TortoiseGit SVN rebase finish dialog produces very big dialog box
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/Commands/SVNRebaseCommand.cpp
@@ -106,12 +106,13 @@ bool SVNRebaseCommand::Execute()
 	CString ff;
 	if(g_Git.IsFastForward(CString(_T("HEAD")),out))
 	{
-		cmd.Format(_T("git.exe reset --hard %s"),out);
-		if (g_Git.Run(cmd, &ff, CP_UTF8))
-		{
-			CMessageBox::Show(NULL,ff,_T("TortoiseGit"),MB_OK|MB_ICONERROR);
+		CProgressDlg progressReset;
+		cmd.Format(_T("git.exe reset --hard %s"), out);
+		progressReset.m_GitCmd = cmd;
+		progressReset.m_bAutoCloseOnSuccess = true;
+
+		if (progressReset.DoModal() != IDOK)
 			return false;
-		}
 		else
 		{
 			CMessageBox::Show(NULL,CString(_T("Fast Forward: "))+ff,_T("TortoiseGit"),MB_OK);

commit 8cd2d431f2bc8acc26be93c2ff1babf91961e20d

Correctly show "git reset" output to user
There was a regression introduced in revision b139beafc5df3071effa77d490657146d604164f. Thanks to ch3cooli for pointing this out.

(fixes issue #1155)

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/Commands/SVNRebaseCommand.cpp
@@ -103,7 +103,6 @@ bool SVNRebaseCommand::Execute()
 	}
 
 	//fast forward;
-	CString ff;
 	if(g_Git.IsFastForward(CString(_T("HEAD")),out))
 	{
 		CProgressDlg progressReset;
@@ -115,7 +114,7 @@ bool SVNRebaseCommand::Execute()
 			return false;
 		else
 		{
-			CMessageBox::Show(NULL,CString(_T("Fast Forward: "))+ff,_T("TortoiseGit"),MB_OK);
+			MessageBox(NULL, _T("Fast Forward:\n") + progressReset.m_LogText, _T("TortoiseGit"), MB_OK);
 			if(isStash)
 				askIfUserWantsToStashPop();
 