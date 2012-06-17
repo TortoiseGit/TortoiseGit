4975bd843f7c3fda50186b3740721891ccfa0703
Sven Strickroth <email@cs-ware.de>
2012-06-17 21:31:45 +0200

    cleanup
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Commands/IgnoreCommand.cpp
+++ src/TortoiseProc/Commands/IgnoreCommand.cpp
@@ -24,7 +24,6 @@
 #include "PathUtils.h"
 #include "AppUtils.h"
 #include "ShellUpdater.h"
-//#include "SVNProperties.h"
 
 bool IgnoreCommand::Execute()
 {
@@ -79,69 +78,4 @@ bool IgnoreCommand::Execute()
 	CShellUpdater::Instance().Flush();
 
 	return ret;
-#if 0
-	CString filelist;
-	BOOL err = FALSE;
-	for(int nPath = 0; nPath < pathList.GetCount(); nPath++)
-	{
-		CString name = CPathUtils::PathPatternEscape(pathList[nPath].GetFileOrDirectoryName());
-		if (parser.HasKey(_T("onlymask")))
-		{
-			name = _T("*")+pathList[nPath].GetFileExtension();
-		}
-		filelist += name + _T("\n");
-		CTSVNPath parentfolder = pathList[nPath].GetContainingDirectory();
-		SVNProperties props(parentfolder, SVNRev::REV_WC, false);
-		CStringA value;
-		for (int i=0; i<props.GetCount(); i++)
-		{
-			CString propname(props.GetItemName(i).c_str());
-			if (propname.CompareNoCase(_T("svn:ignore"))==0)
-			{
-				//treat values as normal text even if they're not
-				value = (char *)props.GetItemValue(i).c_str();
-			}
-		}
-		if (value.IsEmpty())
-			value = name;
-		else
-		{
-			// make sure we don't have duplicate entries
-			std::set<CStringA> ignoreItems;
-			ignoreItems.insert(CUnicodeUtils::GetUTF8(name));
-			CStringA token;
-			int curPos = 0;
-			token= value.Tokenize("\n",curPos);
-			while (token != _T(""))
-			{
-				token.Trim();
-				ignoreItems.insert(token);
-				token = value.Tokenize("\n", curPos);
-			};
-			value.Empty();
-			for (std::set<CStringA>::iterator it = ignoreItems.begin(); it != ignoreItems.end(); ++it)
-			{
-				value += *it;
-				value += "\n";
-			}
-		}
-		if (!props.Add(_T("svn:ignore"), (LPCSTR)value))
-		{
-			CString temp;
-			temp.Format(IDS_ERR_FAILEDIGNOREPROPERTY, (LPCTSTR)name);
-			temp += _T("\n");
-			temp += props.GetLastErrorMsg().c_str();
-			CMessageBox::Show(hwndExplorer, temp, _T("TortoiseGit"), MB_ICONERROR);
-			err = TRUE;
-			break;
-		}
-	}
-	if (err == FALSE)
-	{
-		CString temp;
-		temp.Format(IDS_PROC_IGNORESUCCESS, (LPCTSTR)filelist);
-		CMessageBox::Show(hwndExplorer, temp, _T("TortoiseGit"), MB_ICONINFORMATION);
-		return true;
-	}
-#endif
 }
