96cdf08a020a9da5bc53985f6a1df94e028ef572
Sven Strickroth <email@cs-ware.de>
2012-07-07 05:42:07 +0200

    Prepared release and bumped version numbers to 1.7.11.3
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
-#define FILEVER        1,7,11,2
-#define PRODUCTVER     1,7,11,2
-#define STRFILEVER     "1, 7, 11, 2\0"
-#define STRPRODUCTVER  "1, 7, 11, 2\0"
+#define FILEVER        1,7,11,3
+#define PRODUCTVER     1,7,11,3
+#define STRFILEVER     "1, 7, 11, 3\0"
+#define STRPRODUCTVER  "1, 7, 11, 3\0"
 
 #define TGIT_VERMAJOR             1
 #define TGIT_VERMINOR             7
 #define TGIT_VERMICRO             11
-#define TGIT_VERBUILD			  2
+#define TGIT_VERBUILD			  3
 #define TGIT_VERDATE			  __DATE__
 
 #ifdef _WIN64
