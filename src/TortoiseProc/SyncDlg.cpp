TortoiseGit/TortoiseGit

commit d721b01ee4bd42496767bd48c5676b3c9ca045ff

Fixed issue #626: add "show log" in Git Sync dialog
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/SyncDlg.cpp
@@ -83,6 +83,7 @@ BEGIN_MESSAGE_MAP(CSyncDlg, CResizableStandAloneDialog)
 	ON_WM_TIMER()
 	ON_REGISTERED_MESSAGE(WM_TASKBARBTNCREATED, OnTaskbarBtnCreated)
 	ON_BN_CLICKED(IDC_CHECK_FORCE, &CSyncDlg::OnBnClickedCheckForce)
+	ON_BN_CLICKED(IDC_LOG, &CSyncDlg::OnBnClickedLog)
 END_MESSAGE_MAP()
 
 
@@ -734,6 +735,7 @@ BOOL CSyncDlg::OnInitDialog()
 	AddAnchor(IDC_STATIC_STATUS, BOTTOM_LEFT, BOTTOM_RIGHT);
 	AddAnchor(IDC_ANIMATE_SYNC,TOP_LEFT);
 	AddAnchor(IDC_BUTTON_COMMIT,BOTTOM_LEFT);
+	AddAnchor(IDC_BUTTON_LOG, BOTTOM_LEFT);
 
 	// do not use BRANCH_COMBOX_ADD_ANCHOR here, we want to have different stylings
 	AddAnchor(IDC_COMBOBOXEX_LOCAL_BRANCH, TOP_LEFT,TOP_CENTER);
@@ -1207,4 +1209,14 @@ LRESULT CSyncDlg::OnTaskbarBtnCreated(WPARAM /*wParam*/, LPARAM /*lParam*/)
 void CSyncDlg::OnBnClickedCheckForce()
 {
 	UpdateData();
+}
+
+void CSyncDlg::OnBnClickedLog()
+{
+	CString cmd = _T("/command:log");
+	cmd += _T(" /path:\"");
+	cmd += g_Git.m_CurrentDir;
+	cmd += _T("\"");
+
+	CAppUtils::RunTortoiseProc(cmd);
 }

src/Resources/TortoiseProcENG.rc
@@ -1455,8 +1455,9 @@ BEGIN
     PUSHBUTTON      "SubModule",IDC_BUTTON_SUBMODULE,166,215,87,14
     PUSHBUTTON      "&Apply Patch",IDC_BUTTON_APPLY,260,215,71,14
     PUSHBUTTON      "&Email Patch",IDC_BUTTON_EMAIL,338,215,50,14
-    PUSHBUTTON      "&Commit",IDC_BUTTON_COMMIT,7,232,73,14
-    LTEXT           "",IDC_STATIC_STATUS,10,247,251,8
+    PUSHBUTTON      "Show &Log",IDC_LOG,7,232,73,14
+    PUSHBUTTON      "&Commit",IDC_BUTTON_COMMIT,89,232,70,14
+    LTEXT           "",IDC_STATIC_STATUS,10,248,251,8
     PUSHBUTTON      "OK",IDOK,281,241,50,14
     PUSHBUTTON      "Help",IDHELP,338,241,50,14
 END

src/TortoiseProc/SyncDlg.h
@@ -208,4 +208,5 @@ protected:
 	afx_msg void OnBnClickedButtonSubmodule();
 	afx_msg void OnLvnInLogListColumnClick(NMHDR *pNMHDR, LRESULT *pResult);
 	afx_msg void OnBnClickedCheckForce();
+	afx_msg void OnBnClickedLog();
 };

commit 5b8e41098acbd958273d21c852c94d772ac85da2

Fixed typo introduced in d721b01ee4bd42496767bd48c5676b3c9ca045ff
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/SyncDlg.cpp
@@ -735,7 +735,7 @@ BOOL CSyncDlg::OnInitDialog()
 	AddAnchor(IDC_STATIC_STATUS, BOTTOM_LEFT, BOTTOM_RIGHT);
 	AddAnchor(IDC_ANIMATE_SYNC,TOP_LEFT);
 	AddAnchor(IDC_BUTTON_COMMIT,BOTTOM_LEFT);
-	AddAnchor(IDC_BUTTON_LOG, BOTTOM_LEFT);
+	AddAnchor(IDC_LOG, BOTTOM_LEFT);
 
 	// do not use BRANCH_COMBOX_ADD_ANCHOR here, we want to have different stylings
 	AddAnchor(IDC_COMBOBOXEX_LOCAL_BRANCH, TOP_LEFT,TOP_CENTER);