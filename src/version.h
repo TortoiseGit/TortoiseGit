46bbf742c8f50e20790130f285d74397dad85e90
Sven Strickroth <email@cs-ware.de>
2012-08-10 08:37:40 +0200

    Prepare release of 1.7.12.0
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
-#define FILEVER        1,7,11,3
-#define PRODUCTVER     1,7,11,3
-#define STRFILEVER     "1, 7, 11, 3\0"
-#define STRPRODUCTVER  "1, 7, 11, 3\0"
+#define FILEVER        1,7,12,0
+#define PRODUCTVER     1,7,12,0
+#define STRFILEVER     "1, 7, 12, 0\0"
+#define STRPRODUCTVER  "1, 7, 12, 0\0"
 
 #define TGIT_VERMAJOR             1
 #define TGIT_VERMINOR             7
-#define TGIT_VERMICRO             11
-#define TGIT_VERBUILD			  3
+#define TGIT_VERMICRO             12
+#define TGIT_VERBUILD			  0
 #define TGIT_VERDATE			  __DATE__
 
 #ifdef _WIN64
