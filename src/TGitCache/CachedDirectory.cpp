TortoiseGit/TortoiseGit

commit 3cf14b01341d3d537077afa7be55156dd90d0f9f

TGitCache: Correctly display icons for empty working trees with no .git/index file
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TGitCache/CachedDirectory.cpp
@@ -447,7 +447,8 @@ int CCachedDirectory::EnumFiles(CTGitPath *path , bool IsFull)
 		m_mostImportantFileStatus = git_wc_status_normal;
 		pStatus->EnumDirStatus(sProjectRoot, sSubPath, &status, IsFull, false, true, GetStatusCallback,this);
 
-		m_ownStatus = git_wc_status_normal;
+		if (sSubPath.IsEmpty())
+			m_ownStatus = git_wc_status_normal;
 	}
 
 	m_mostImportantFileStatus = GitStatus::GetMoreImportant(m_mostImportantFileStatus, status);

src/Git/GitStatus.cpp
@@ -625,10 +625,53 @@ int GitStatus::EnumDirStatus(const CString &gitdir,const CString &subpath,git_wc
 			SHARED_INDEX_PTR indexptr = g_IndexFileMap.SafeGet(gitdir);
 			SHARED_TREE_PTR treeptr = g_HeadFileMap.SafeGet(gitdir);
 
-			if( indexptr.get() == NULL)
-				return -1;
-
 			std::vector<CGitFileName>::iterator it;
+
+			// new git working tree has no index file
+			if (indexptr.get() == NULL)
+			{
+				for(it = filelist.begin(); it < filelist.end(); it++)
+				{
+					CString casepath = path + it->m_CaseFileName;
+
+					bool bIsDir = false;
+					if (casepath.GetLength() > 0 && casepath[casepath.GetLength() - 1] == _T('/'))
+						bIsDir = true;
+
+					if (bIsDir) /*check if it is directory*/
+					{
+						if (::PathFileExists(gitdir + casepath + _T("\\.git")))
+						{ /* That is git submodule */
+							*status = git_wc_status_unknown;
+							if (callback)
+								callback(gitdir + _T("/") + casepath, *status, bIsDir, pData, false);
+							continue;
+						}
+					}
+
+					if (IsIgnore)
+					{
+						if (g_IgnoreList.CheckIgnoreChanged(gitdir, casepath))
+							g_IgnoreList.LoadAllIgnoreFile(gitdir, casepath);
+
+						if (g_IgnoreList.IsIgnore(casepath, gitdir))
+							*status = git_wc_status_ignored;
+						else if (bIsDir)
+							continue;
+						else
+							*status = git_wc_status_unversioned;
+					}
+					else if (bIsDir)
+						continue;
+					else
+						*status = git_wc_status_unversioned;
+
+					if(callback)
+						callback(gitdir + _T("/") + casepath, *status, bIsDir, pData, false);
+				}
+				return 0;
+			}
+
 			CString onepath;
 			CString casepath;
 			for(it = filelist.begin(); it<filelist.end();it++)

commit 3efd593df3ee3f62a86b51388cb02ec3dc7216b0

GitCache: Folders which contain an ignored file are displayed as ignored
Issue introduced in revision 3cf14b01341d3d537077afa7be55156dd90d0f9f.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TGitCache/CachedDirectory.cpp
@@ -457,6 +457,8 @@ int CCachedDirectory::EnumFiles(CTGitPath *path , bool IsFull)
 			}
 			m_ownStatus = git_wc_status_normal;
 		}
+		else if (m_ownStatus.GetEffectiveStatus() == git_wc_status_ignored) // otherwise folders which contain at least one ignored item gets displayed as ignored
+			m_ownStatus = git_wc_status_normal;
 	}
 
 	return 0;