TortoiseGit/TortoiseGit

commit b426798195340a6617d3af25011c2b08584fa000

Refactored
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseShell/GITPropertyPage.cpp
@@ -26,6 +26,8 @@
 #include "GitStatus.h"
 #include "git2.h"
 #include "UnicodeUtils.h"
+#include "CreateProcessHelper.h"
+#include "FormatMessageWrapper.h"
 
 #define MAX_STRING_LENGTH	4096	//should be big enough
 
@@ -208,21 +210,11 @@ void CGitPropertyPage::PageProcOnCommand(WPARAM wParam)
 	{
 	case IDC_SHOWLOG:
 		{
-			STARTUPINFO startup;
-			PROCESS_INFORMATION process;
-			memset(&startup, 0, sizeof(startup));
-			startup.cb = sizeof(startup);
-			memset(&process, 0, sizeof(process));
-			tstring tortoiseProcPath = CPathUtils::GetAppDirectory(g_hmodThisDll) + _T("TortoiseProc.exe");
-			stdstring gitCmd = _T(" /command:");
+			tstring gitCmd = _T("/command:");
 			gitCmd += _T("log /path:\"");
 			gitCmd += filenames.front().c_str();
 			gitCmd += _T("\"");
-			if (CreateProcess(tortoiseProcPath.c_str(), const_cast<TCHAR*>(gitCmd.c_str()), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-			{
-				CloseHandle(process.hThread);
-				CloseHandle(process.hProcess);
-			}
+			RunCommand(gitCmd);
 		}
 		break;
 	case IDC_SHOWSETTINGS:
@@ -232,22 +224,11 @@ void CGitPropertyPage::PageProcOnCommand(WPARAM wParam)
 			if(!path.HasAdminDir(&projectTopDir))
 				return;
 
-			STARTUPINFO startup;
-			PROCESS_INFORMATION process;
-			memset(&startup, 0, sizeof(startup));
-			startup.cb = sizeof(startup);
-			memset(&process, 0, sizeof(process));
-			tstring tortoiseProcPath = CPathUtils::GetAppDirectory(g_hmodThisDll) + _T("TortoiseProc.exe");
-
-			stdstring gitCmd = _T(" /command:");
+			tstring gitCmd = _T("/command:");
 			gitCmd += _T("settings /path:\"");
 			gitCmd += projectTopDir;
 			gitCmd += _T("\"");
-			if (CreateProcess(tortoiseProcPath.c_str(), const_cast<TCHAR*>(gitCmd.c_str()), NULL, NULL, FALSE, 0, 0, 0, &startup, &process))
-			{
-				CloseHandle(process.hThread);
-				CloseHandle(process.hProcess);
-			}
+			RunCommand(gitCmd);
 		}
 		break;
 	case IDC_ASSUMEVALID:
@@ -257,6 +238,19 @@ void CGitPropertyPage::PageProcOnCommand(WPARAM wParam)
 		break;
 	}
 }
+
+void CGitPropertyPage::RunCommand(const tstring& command)
+{
+	tstring tortoiseProcPath = CPathUtils::GetAppDirectory(g_hmodThisDll) + _T("TortoiseProc.exe");
+	if (CCreateProcessHelper::CreateProcessDetached(tortoiseProcPath.c_str(), const_cast<TCHAR*>(command.c_str())))
+	{
+		// process started - exit
+		return;
+	}
+
+	MessageBox(NULL, CFormatMessageWrapper(), _T("TortoiseProc launch failed"), MB_OK | MB_ICONINFORMATION);
+}
+
 void CGitPropertyPage::Time64ToTimeString(__time64_t time, TCHAR * buf, size_t buflen)
 {
 	struct tm newtime;

src/TortoiseShell/GITPropertyPage.h
@@ -80,6 +80,7 @@ protected:
 	virtual void InitWorkfileView();
 	void Time64ToTimeString(__time64_t time, TCHAR * buf, size_t buflen);
 	void PageProcOnCommand(WPARAM wParam);
+	void RunCommand(const tstring& command);
 
 	HWND m_hwnd;
 	std::vector<stdstring> filenames;

commit 15a10ea1d1609742bc229c0da547f81774b8532a

Fixed issue #1342: Shell Property page log / settings buttons not function
Regression introuduced in b42679819534

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseShell/GITPropertyPage.cpp
@@ -210,7 +210,7 @@ void CGitPropertyPage::PageProcOnCommand(WPARAM wParam)
 	{
 	case IDC_SHOWLOG:
 		{
-			tstring gitCmd = _T("/command:");
+			tstring gitCmd = _T(" /command:");
 			gitCmd += _T("log /path:\"");
 			gitCmd += filenames.front().c_str();
 			gitCmd += _T("\"");
@@ -224,7 +224,7 @@ void CGitPropertyPage::PageProcOnCommand(WPARAM wParam)
 			if(!path.HasAdminDir(&projectTopDir))
 				return;
 
-			tstring gitCmd = _T("/command:");
+			tstring gitCmd = _T(" /command:");
 			gitCmd += _T("settings /path:\"");
 			gitCmd += projectTopDir;
 			gitCmd += _T("\"");