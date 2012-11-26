9a7e70c166457756e169581e47169bcc956dc63c
Frank Li <lznuaa@gmail.com>
2012-11-26 16:03:44 +0800

    Add TortoiseSVN 217990 RevisionGraphCommand.cpp and RevisionGraphCommand.h
    Signed-off-by: Frank Li <lznuaa@gmail.com>

---
+++ src/TortoiseProc/Commands/RevisionGraphCommand.cpp
@@ -0,0 +1,42 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2007, 2009-2011 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "RevisionGraphCommand.h"
+
+#include "RevisionGraph\RevisionGraphDlg.h"
+
+bool RevisionGraphCommand::Execute()
+{
+    CString val = parser.GetVal(_T("pegrev"));
+    SVNRev pegrev = val.IsEmpty() ? SVNRev() : SVNRev(val);
+
+    std::unique_ptr<CRevisionGraphDlg> dlg(new CRevisionGraphDlg());
+    dlg->SetPath(cmdLinePath.GetUIPathString());
+    dlg->SetPegRevision(pegrev);
+    if (parser.HasVal(L"output"))
+    {
+        dlg->SetOutputFile(parser.GetVal(L"output"));
+        if (parser.HasVal(L"options"))
+            dlg->SetOptions(parser.GetLongVal(L"options"));
+        dlg->StartHidden();
+    }
+    dlg->DoModal();
+
+    return true;
+}
