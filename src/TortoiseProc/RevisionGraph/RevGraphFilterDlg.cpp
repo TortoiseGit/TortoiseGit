62c55f8b735260db1f9249563dcfd99b18b823d2
Frank Li <lznuaa@gmail.com>
2012-12-09 10:13:12 +0800

    RevisionGraph: add tortoisesvn 19274 RevGraphFilter dialog
    Signed-off-by: Frank Li <lznuaa@gmail.com>

---
+++ src/TortoiseProc/RevisionGraph/RevGraphFilterDlg.cpp
@@ -0,0 +1,121 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2006 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "TortoiseProc.h"
+#include "RevGraphFilterDlg.h"
+
+
+IMPLEMENT_DYNAMIC(CRevGraphFilterDlg, CDialog)
+
+CRevGraphFilterDlg::CRevGraphFilterDlg(CWnd* pParent /*=NULL*/)
+    : CDialog(CRevGraphFilterDlg::IDD, pParent)
+    , m_sFilterPaths(_T(""))
+    , m_removeSubTree (FALSE)
+    , m_sFromRev(_T(""))
+    , m_sToRev(_T(""))
+    , m_minrev (1)
+    , m_maxrev (1)
+{
+
+}
+
+CRevGraphFilterDlg::~CRevGraphFilterDlg()
+{
+}
+
+void CRevGraphFilterDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CDialog::DoDataExchange(pDX);
+    DDX_Text(pDX, IDC_PATHFILTER, m_sFilterPaths);
+    DDX_Check(pDX, IDC_REMOVESUBTREE, m_removeSubTree);
+    DDX_Control(pDX, IDC_FROMSPIN, m_cFromSpin);
+    DDX_Control(pDX, IDC_TOSPIN, m_cToSpin);
+    DDX_Text(pDX, IDC_FROMREV, m_sFromRev);
+    DDX_Text(pDX, IDC_TOREV, m_sToRev);
+}
+
+
+BEGIN_MESSAGE_MAP(CRevGraphFilterDlg, CDialog)
+    ON_NOTIFY(UDN_DELTAPOS, IDC_FROMSPIN, &CRevGraphFilterDlg::OnDeltaposFromspin)
+    ON_NOTIFY(UDN_DELTAPOS, IDC_TOSPIN, &CRevGraphFilterDlg::OnDeltaposTospin)
+END_MESSAGE_MAP()
+
+BOOL CRevGraphFilterDlg::OnInitDialog()
+{
+    CDialog::OnInitDialog();
+
+    m_cFromSpin.SetBuddy(GetDlgItem(IDC_FROMREV));
+    m_cFromSpin.SetRange32(1, m_HeadRev);
+    m_cFromSpin.SetPos32(m_minrev);
+
+    m_cToSpin.SetBuddy(GetDlgItem(IDC_TOREV));
+    m_cToSpin.SetRange32(1, m_HeadRev);
+    m_cToSpin.SetPos32(m_maxrev);
+
+    return TRUE;
+}
+
+void CRevGraphFilterDlg::OnDeltaposFromspin(NMHDR *pNMHDR, LRESULT *pResult)
+{
+    LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
+
+    // the 'from' revision is about to be changed.
+    // Since the 'from' revision must not be higher or the same as the 'to'
+    // revision, we have to block this if the user tries to do that.
+    if ((pNMUpDown->iPos + pNMUpDown->iDelta) >= m_cToSpin.GetPos32())
+    {
+        // block the change
+        pNMUpDown->iDelta = 0;
+    }
+    *pResult = 0;
+}
+
+void CRevGraphFilterDlg::OnDeltaposTospin(NMHDR *pNMHDR, LRESULT *pResult)
+{
+    LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
+
+    // the 'to' revision is about to be changed.
+    // Since the 'to' revision must not be lower or the same as the 'from'
+    // revision, we have to block this if the user tries to do that.
+    if ((pNMUpDown->iPos + pNMUpDown->iDelta) <= m_cFromSpin.GetPos32())
+    {
+        // block the change
+        pNMUpDown->iDelta = 0;
+    }
+    *pResult = 0;
+}
+
+void CRevGraphFilterDlg::GetRevisionRange(svn_revnum_t& minrev, svn_revnum_t& maxrev)
+{
+    minrev = m_minrev;
+    maxrev = m_maxrev;
+}
+
+void CRevGraphFilterDlg::SetRevisionRange (svn_revnum_t minrev, svn_revnum_t maxrev)
+{
+    m_minrev = minrev;
+    m_maxrev = maxrev;
+}
+
+void CRevGraphFilterDlg::OnOK()
+{
+    m_minrev = m_cFromSpin.GetPos32();
+    m_maxrev = m_cToSpin.GetPos32();
+    CDialog::OnOK();
+}
---
+++ src/TortoiseProc/RevisionGraph/RevGraphFilterDlg.h
@@ -0,0 +1,64 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2006 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+#include "afxcmn.h"
+
+/**
+ * \ingroup TortoiseProc
+ * helper dialog to enter filter data for the revision graph.
+ */
+class CRevGraphFilterDlg : public CDialog
+{
+    DECLARE_DYNAMIC(CRevGraphFilterDlg)
+
+public:
+    CRevGraphFilterDlg(CWnd* pParent = NULL);   // standard constructor
+    virtual ~CRevGraphFilterDlg();
+
+    void    SetMaxRevision (svn_revnum_t rev) {m_HeadRev = rev;}
+    void    GetRevisionRange (svn_revnum_t& minrev, svn_revnum_t& maxrev);
+    void    SetRevisionRange (svn_revnum_t minrev, svn_revnum_t maxrev);
+    CString GetFilterString() {return m_sFilterPaths;}
+    void    SetFilterString (const CString& str) {m_sFilterPaths = str;}
+    bool    GetRemoveSubTrees() {return m_removeSubTree != FALSE;}
+    void    SetRemoveSubTrees (bool value) {m_removeSubTree = value ? TRUE : FALSE;}
+
+// Dialog Data
+    enum { IDD = IDD_REVGRAPHFILTER };
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+    virtual BOOL OnInitDialog();
+    virtual void OnOK();
+
+    afx_msg void OnDeltaposFromspin(NMHDR *pNMHDR, LRESULT *pResult);
+    afx_msg void OnDeltaposTospin(NMHDR *pNMHDR, LRESULT *pResult);
+
+    DECLARE_MESSAGE_MAP()
+protected:
+    CString             m_sFilterPaths;
+    BOOL                m_removeSubTree;
+    CSpinButtonCtrl     m_cFromSpin;
+    CSpinButtonCtrl     m_cToSpin;
+    CString             m_sFromRev;
+    CString             m_sToRev;
+    svn_revnum_t        m_HeadRev;
+    svn_revnum_t        m_minrev;
+    svn_revnum_t        m_maxrev;
+};
