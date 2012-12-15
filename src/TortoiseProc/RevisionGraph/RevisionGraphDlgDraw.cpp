485f90f41b13f6711677c9a71fde53760946d588
Sup Yut Sum <ch3cooli@gmail.com>
2012-12-15 17:34:51 +0800

    RevisionGraph: Give numbers of the selection order
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TortoiseProc/RevisionGraph/RevisionGraphDlgDraw.cpp
+++ src/TortoiseProc/RevisionGraph/RevisionGraphDlgDraw.cpp
@@ -815,11 +815,30 @@ void CRevisionGraphWnd::DrawMarker
 	, const RectF& noderect
 	, MarkerPosition /*position*/
 	, int /*relPosition*/
-	, Color &penColor)
+	, Color &penColor
+	, int num)
 {
 	REAL width = 4*this->m_fZoomFactor<1? 1: 4*this->m_fZoomFactor;
 	Pen pen(penColor,width);
 	DrawRoundedRect(graphics, penColor, (int)width, &pen, Color(0,0,0), NULL, noderect);
+	if (num == 1)
+	{
+		// Roman number 1
+		REAL x = max(1, 10 * this->m_fZoomFactor);
+		REAL y1 = max(1, 25 * this->m_fZoomFactor);
+		REAL y2 = max(1, 5 * this->m_fZoomFactor);
+		graphics.graphics->DrawLine(&pen, noderect.X + x, noderect.Y - y1, noderect.X + x, noderect.Y - y2);
+	}
+	else if (num == 2)
+	{
+		// Roman number 2
+		REAL x1 = max(1, 5 * this->m_fZoomFactor);
+		REAL x2 = max(1, 15 * this->m_fZoomFactor);
+		REAL y1 = max(1, 25 * this->m_fZoomFactor);
+		REAL y2 = max(1, 5 * this->m_fZoomFactor);
+		graphics.graphics->DrawLine(&pen, noderect.X + x1, noderect.Y - y1, noderect.X + x1, noderect.Y - y2);
+		graphics.graphics->DrawLine(&pen, noderect.X + x2, noderect.Y - y1, noderect.X + x2, noderect.Y - y2);
+	}
 }
 
 #if 0
@@ -1244,10 +1263,10 @@ void CRevisionGraphWnd::DrawTexts (GraphicsDevice& graphics, const CRect& /*logR
 			}
 		}
 		if ((m_SelectedEntry1 == v))
-			DrawMarker (graphics, noderect, mpLeft, 0, Color(0,0, 255));
+			DrawMarker(graphics, noderect, mpLeft, 0, Color(0,0, 255), 1);
 
 		if ((m_SelectedEntry2 == v))
-			DrawMarker (graphics, noderect, mpLeft, 0, Color(136,0, 21));
+			DrawMarker(graphics, noderect, mpLeft, 0, Color(136,0, 21), 2);
 
 	}
 }
