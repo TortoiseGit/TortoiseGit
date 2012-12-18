4e2754ea951d491ba6ebdf6e2bea7f23fff0b8ec
Sup Yut Sum <ch3cooli@gmail.com>
2012-12-18 08:03:00 +0800

    Remove the obsolete CMC stuffs because the header is removed in VS2012
    Based on CrashRpt r1348
    
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/Utils/MailMsg.h
+++ src/Utils/MailMsg.h
@@ -34,7 +34,7 @@
 //
 //  Module: MailMsg.h
 //
-//    Desc: This class encapsulates the MAPI and CMC mail functions.
+//    Desc: This class encapsulates the MAPI mail functions.
 //
 // Copyright (c) 2003 Michael Carruth
 //
@@ -46,27 +46,6 @@
 #include "stdafx.h"
 
 typedef std::map<std::string, std::string> TStrStrMap;
-
-//
-// Define CMC entry points
-//
-typedef CMC_return_code (FAR PASCAL *LPCMCLOGON) \
-(CMC_string, CMC_string, CMC_string, CMC_object_identifier, \
-	CMC_ui_id, CMC_uint16, CMC_flags, CMC_session_id FAR*, \
-	CMC_extension FAR*);
-
-typedef CMC_return_code (FAR PASCAL *LPCMCSEND) \
-(CMC_session_id, CMC_message FAR*, CMC_flags, \
-	CMC_ui_id, CMC_extension FAR*);
-
-typedef CMC_return_code (FAR PASCAL *LPCMCLOGOFF) \
-(CMC_session_id, CMC_ui_id, CMC_flags, CMC_extension FAR*);
-
-typedef CMC_return_code (FAR PASCAL *LPCMCQUERY) \
-(CMC_session_id, CMC_enum, CMC_buffer, CMC_extension FAR*);
-
-
-////////////////////////////// Class Definitions /////////////////////////////
 
 // ===========================================================================
 // CMailMsg
@@ -95,8 +74,6 @@ public:
 	static BOOL DetectMailClient(CString& sMailClientName);
 	CString GetEmailClientName();
 	BOOL Send();
-	BOOL MAPISend();
-	BOOL CMCSend();
 	CString GetLastErrorMsg(){ return m_sErrorMsg; }
 
 protected:
@@ -108,10 +85,6 @@ protected:
 	std::string					m_sMessage;                   // EMail message
 
 	HMODULE						m_hMapi;                      // Handle to MAPI32.DLL
-	LPCMCQUERY					m_lpCmcQueryConfiguration;    // Cmc func pointer
-	LPCMCLOGON					m_lpCmcLogon;                 // Cmc func pointer
-	LPCMCSEND					m_lpCmcSend;                  // Cmc func pointer
-	LPCMCLOGOFF					m_lpCmcLogoff;                // Cmc func pointer
 	LPMAPISENDMAIL				m_lpMapiSendMail;             // Mapi func pointer
 
 	BOOL						m_bReady;                     // MAPI is loaded
