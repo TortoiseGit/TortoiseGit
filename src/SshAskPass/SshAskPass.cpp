ffcce192577ad73dcb10dcf7d7b966a6093b600b
Sup Yut Sum <ch3cooli@gmail.com>
2012-12-28 23:33:36 +0800

    SSHAskPass: Enable UI theme in XP and later
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/SshAskPass/SshAskPass.cpp
+++ src/SshAskPass/SshAskPass.cpp
@@ -26,6 +26,8 @@
 #include "propsys.h"
 #include "PropKey.h"
 
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #define MAX_LOADSTRING 100
 
 // Global Variables:
