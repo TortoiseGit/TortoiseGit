TortoiseGit/TortoiseGit

commit 02fbc263b2da8d4cd92e10ff1509dcb277426890

Improve error-handling of GetHash
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RebaseDlg.cpp
@@ -1,7 +1,7 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2012 - TortoiseGit
-// Copyright (C) 2011-2012 - Sven Strickroth <email@cs-ware.de>
+// Copyright (C) 2008-2013 - TortoiseGit
+// Copyright (C) 2011-2013 - Sven Strickroth <email@cs-ware.de>
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -496,12 +496,23 @@ void CRebaseDlg::OnCbnSelchangeUpstream()
 
 void CRebaseDlg::FetchLogList()
 {
-	CGitHash base,hash;
+	CGitHash base,hash,upstream;
 	m_IsFastForward=FALSE;
 
-	hash=g_Git.GetHash(m_BranchCtrl.GetString());
+	if (g_Git.GetHash(hash, m_BranchCtrl.GetString()))
+	{
+		m_CommitList.ShowText(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_BranchCtrl.GetString() + _T("\".")));
+		this->GetDlgItem(IDC_REBASE_CONTINUE)->EnableWindow(false);
+		return;
+	}
+	if (g_Git.GetHash(upstream, m_UpstreamCtrl.GetString()))
+	{
+		m_CommitList.ShowText(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_UpstreamCtrl.GetString() + _T("\".")));
+		this->GetDlgItem(IDC_REBASE_CONTINUE)->EnableWindow(false);
+		return;
+	}
 
-	if(hash == g_Git.GetHash(this->m_UpstreamCtrl.GetString()))
+	if (hash == upstream)
 	{
 		m_CommitList.Clear();
 		CString text,fmt;
@@ -531,13 +542,9 @@ void CRebaseDlg::FetchLogList()
 		return ;
 	}
 
-	hash.Empty();
-
 	if(!this->m_bForce)
 	{
-		hash=g_Git.GetHash(m_UpstreamCtrl.GetString());
-
-		if( base == hash )
+		if (base == upstream)
 		{
 			m_CommitList.Clear();
 			CString text,fmt;
@@ -559,12 +566,10 @@ void CRebaseDlg::FetchLogList()
 	if( m_CommitList.GetItemCount() == 0 )
 		m_CommitList.ShowText(CString(MAKEINTRESOURCE(IDS_PROC_NOTHINGTOREBASE)));
 
-	hash=g_Git.GetHash(m_UpstreamCtrl.GetString());
-
 #if 0
 	if(m_CommitList.m_logEntries[m_CommitList.m_logEntries.size()-1].m_ParentHash.size() >=0 )
 	{
-		if(hash ==  m_CommitList.m_logEntries[m_CommitList.m_logEntries.size()-1].m_ParentHash[0])
+		if(upstream ==  m_CommitList.m_logEntries[m_CommitList.m_logEntries.size()-1].m_ParentHash[0])
 		{
 			m_CommitList.Clear();
 			m_CommitList.ShowText(_T("Nothing Rebase"));
@@ -810,10 +815,9 @@ int CRebaseDlg::StartRebase()
 	}
 
 	m_OrigUpstreamHash.Empty();
-	m_OrigUpstreamHash= g_Git.GetHash(this->m_UpstreamCtrl.GetString());
-	if(m_OrigUpstreamHash.IsEmpty())
+	if (g_Git.GetHash(m_OrigUpstreamHash, m_UpstreamCtrl.GetString()))
 	{
-		this->AddLogString(m_OrigUpstreamHash);
+		MessageBox(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_UpstreamCtrl.GetString() + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
 		return -1;
 	}
 
@@ -838,10 +842,9 @@ int CRebaseDlg::StartRebase()
 	CString log;
 	if( !this->m_IsCherryPick )
 	{
-		m_OrigBranchHash = g_Git.GetHash(this->m_BranchCtrl.GetString());
-		if(m_OrigBranchHash.IsEmpty())
+		if (g_Git.GetHash(m_OrigBranchHash, m_BranchCtrl.GetString()))
 		{
-			this->AddLogString(m_OrigBranchHash.ToString());
+			MessageBox(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_BranchCtrl.GetString() + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
 			return -1;
 		}
 		log.Format(_T("%s\r\n"), CString(MAKEINTRESOURCE(IDS_PROC_REBASE_STARTREBASE)));
@@ -873,7 +876,12 @@ int CRebaseDlg::FinishRebase()
 	if(this->m_IsCherryPick) //cherry pick mode no "branch", working at upstream branch
 		return 0;
 
-	git_revnum_t head = g_Git.GetHash(_T("HEAD"));
+	CGitHash head;
+	if (g_Git.GetHash(head, _T("HEAD")))
+	{
+		MessageBox(g_Git.GetGitLastErr(_T("Could not get HEAD hash.")), _T("TortoiseGit"), MB_ICONERROR);
+		return -1;
+	}
 	CString out,cmd;
 
 	cmd.Format(_T("git.exe checkout -f %s"),this->m_BranchCtrl.GetString());
@@ -947,8 +955,16 @@ void CRebaseDlg::OnBnClickedContinue()
 		}
 		AddLogString(out);
 		out.Empty();
-		m_OrigBranchHash = g_Git.GetHash(m_BranchCtrl.GetString());
-		m_OrigUpstreamHash = g_Git.GetHash(this->m_UpstreamCtrl.GetString());
+		if (g_Git.GetHash(m_OrigBranchHash, m_BranchCtrl.GetString()))
+		{
+			MessageBox(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_BranchCtrl.GetString() + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
+			return;
+		}
+		if (g_Git.GetHash(m_OrigUpstreamHash, m_UpstreamCtrl.GetString()))
+		{
+			MessageBox(g_Git.GetGitLastErr(_T("Could not get hash of \"") + m_UpstreamCtrl.GetString() + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
+			return;
+		}
 
 		if(!g_Git.IsFastForward(this->m_BranchCtrl.GetString(),this->m_UpstreamCtrl.GetString()))
 		{

src/Git/Git.cpp
@@ -1000,13 +1000,14 @@ int CGit::RunLogFile(CString cmd,const CString &filename)
 //	return 0;
 }
 
-CGitHash CGit::GetHash(TCHAR* friendname)
+int CGit::GetHash(CGitHash &hash, TCHAR* friendname)
 {
 	// no need to parse a ref if it's already a 40-byte hash
 	if (CGitHash::IsValidSHA1(friendname))
 	{
 		CString sHash(friendname);
-		return CGitHash(sHash);
+		hash = CGitHash(sHash);
+		return 0;
 	}
 
 	if (m_IsUseLibGit2)
@@ -1016,7 +1017,7 @@ CGitHash CGit::GetHash(TCHAR* friendname)
 		if (git_repository_open(&repo, gitdirA.GetBuffer()))
 		{
 			gitdirA.ReleaseBuffer();
-			return CGitHash();
+			return -1;
 		}
 		gitdirA.ReleaseBuffer();
 
@@ -1027,7 +1028,7 @@ CGitHash CGit::GetHash(TCHAR* friendname)
 		{
 			refnameA.ReleaseBuffer();
 			git_repository_free(repo);
-			return CGitHash();
+			return -1;
 		}
 		refnameA.ReleaseBuffer();
 
@@ -1036,23 +1037,23 @@ CGitHash CGit::GetHash(TCHAR* friendname)
 		{
 			git_object_free(gitObject);
 			git_repository_free(repo);
-			return CGitHash();
+			return -1;
 		}
 
-		CGitHash hash((char *)oid->id);
+		hash = CGitHash((char *)oid->id);
 
 		git_object_free(gitObject); // also frees oid
 		git_repository_free(repo);
 
-		return hash;
+		return 0;
 	}
 	else
 	{
 		CString cmd;
-		CString out;
 		cmd.Format(_T("git.exe rev-parse %s" ),FixBranchName(friendname));
-		Run(cmd, &out, NULL, CP_UTF8);
-		return CGitHash(out.Trim());
+		int ret = Run(cmd, &gitLastErr, NULL, CP_UTF8);
+		hash = CGitHash(gitLastErr.Trim());
+		return ret;
 	}
 }
 
@@ -1864,7 +1865,7 @@ bool CGit::IsFastForward(const CString &from, const CString &to, CGitHash * comm
 	}
 	basehash = base.Trim();
 
-	hash = GetHash(from);
+	GetHash(hash, from);
 
 	if (commonAncestor)
 		*commonAncestor = basehash;

src/Git/Git.h
@@ -284,8 +284,8 @@ public:
 					  CString *from=NULL,CString *to=NULL, bool paramonly=false,
 					  CFilterData * filter =NULL);
 
-	CGitHash GetHash(TCHAR* friendname);
-	CGitHash GetHash(CString ref){return GetHash(ref.GetBuffer());}
+	int GetHash(CGitHash &hash, TCHAR* friendname);
+	int GetHash(CGitHash &hash, CString ref) { return GetHash(hash, ref.GetBuffer()); }
 
 	int BuildOutputFormat(CString &format,bool IsFull=TRUE);
 	//int GetShortLog(const CString &log,CTGitPath * path=NULL, int count =-1);

src/TortoiseProc/Commands/PullCommand.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2012 - TortoiseGit
+// Copyright (C) 2008-2013 - TortoiseGit
 // Copyright (C) 2007-2008 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -52,16 +52,11 @@ bool PullCommand::Execute()
 		}
 
 		CString cmd;
-		CString hashOld;
-		try
+		CGitHash hashOld;
+		if (g_Git.GetHash(hashOld, _T("HEAD")))
 		{
-			hashOld = g_Git.GetHash(_T("HEAD"));
-		}
-		catch (char* msg)
-		{
-			CString err(msg);
-			MessageBox(NULL, _T("Could not get HEAD hash.\nlibgit reports:\n") + err, _T("TortoiseGit"), MB_ICONERROR);
-			ExitProcess(1);
+			MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get HEAD hash.")), _T("TortoiseGit"), MB_ICONERROR);
+			return false;
 		}
 		CString cmdRebase;
 		if(dlg.m_bRebase)
@@ -117,7 +112,12 @@ bool PullCommand::Execute()
 			return FALSE;
 		}
 
-		CString hashNew = g_Git.GetHash(L"HEAD");
+		CGitHash hashNew;
+		if (g_Git.GetHash(hashNew, L"HEAD"))
+		{
+			MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get HEAD hash after pulling.")), _T("TortoiseGit"), MB_ICONERROR);
+			return FALSE;
+		}
 
 		if( ret == IDC_PROGRESS_BUTTON1)
 		{
@@ -129,7 +129,7 @@ bool PullCommand::Execute()
 			}
 
 			CFileDiffDlg dlg;
-			dlg.SetDiff(NULL, hashNew, hashOld);
+			dlg.SetDiff(NULL, hashNew.ToString(), hashOld.ToString());
 			dlg.DoModal();
 
 			return TRUE;

src/TortoiseProc/Commands/SVNFetchCommand.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2012 - TortoiseGit
+// Copyright (C) 2008-2013 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -53,8 +53,12 @@ bool SVNFetchCommand::Execute()
 		return false;
 	}
 
-	CString upstreamOldHash, upstreamNewHash;
-	upstreamOldHash = g_Git.GetHash(out);
+	CGitHash upstreamOldHash, upstreamNewHash;
+	if (g_Git.GetHash(upstreamOldHash, out))
+	{
+		MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get upstream hash.")), _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
 
 	CProgressDlg progress;
 	progress.m_GitCmd=_T("git.exe svn fetch");
@@ -63,7 +67,11 @@ bool SVNFetchCommand::Execute()
 	progress.m_bAutoCloseOnSuccess = autoClose;
 
 	INT_PTR userResponse = progress.DoModal();
-	upstreamNewHash = g_Git.GetHash(out);
+	if (g_Git.GetHash(upstreamNewHash, out))
+	{
+		MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get upstream hash after fetching.")), _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
 	if (userResponse == IDC_PROGRESS_BUTTON1)
 	{
 		if (upstreamOldHash == upstreamNewHash)
@@ -88,7 +96,7 @@ bool SVNFetchCommand::Execute()
 		}
 
 		CFileDiffDlg dlg;
-		dlg.SetDiff(NULL, upstreamNewHash, upstreamOldHash);
+		dlg.SetDiff(NULL, upstreamNewHash.ToString(), upstreamOldHash.ToString());
 		dlg.DoModal();
 		return true;
 	}

src/TortoiseProc/Commands/SVNRebaseCommand.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2012 - TortoiseGit
+// Copyright (C) 2009-2013 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -90,8 +90,16 @@ bool SVNRebaseCommand::Execute()
 	dlg.m_Upstream=out;
 
 	CGitHash UpStreamOldHash,HeadHash,UpStreamNewHash;
-	UpStreamOldHash=g_Git.GetHash(out);
-	HeadHash = g_Git.GetHash(_T("HEAD"));
+	if (g_Git.GetHash(UpStreamOldHash, out))
+	{
+		MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get hash of SVN branch.")), _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
+	if (g_Git.GetHash(HeadHash, _T("HEAD")))
+	{
+		MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get HEAD hash.")), _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
 	CProgressDlg progress;
 	progress.m_GitCmd=_T("git.exe svn fetch");
 	progress.m_bAutoCloseOnSuccess = true;
@@ -102,7 +110,11 @@ bool SVNRebaseCommand::Execute()
 	if(progress.m_GitStatus)
 		return false;
 
-	UpStreamNewHash=g_Git.GetHash(out);
+	if (g_Git.GetHash(UpStreamNewHash, out))
+	{
+		MessageBox(hwndExplorer, g_Git.GetGitLastErr(_T("Could not get upstream hash after fetching.")), _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
 
 	//everything updated
 	if(UpStreamNewHash==HeadHash)

src/TortoiseProc/CommitDlg.cpp
@@ -1,7 +1,7 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2008 - TortoiseSVN
-// Copyright (C) 2008-2012 - TortoiseGit
+// Copyright (C) 2008-2013 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -822,7 +822,9 @@ void CCommitDlg::OnOK()
 
 				BSTR logMessage = m_sLogMessage.AllocSysString();
 
-				CGitHash hash=g_Git.GetHash(_T("HEAD"));
+				CGitHash hash;
+				if (g_Git.GetHash(hash, _T("HEAD")))
+					MessageBox(g_Git.GetGitLastErr(_T("Could not get HEAD hash after committing.")), _T("TortoiseGit"), MB_ICONERROR);
 				LONG version = g_Git.Hash2int(hash);
 
 				BSTR temp = NULL;
@@ -1054,14 +1056,9 @@ UINT CCommitDlg::StatusThread()
 				GetDlgItem(IDC_COMMIT_AMEND)->EnableWindow(TRUE);
 
 			CGitHash hash;
-			try
+			if (g_Git.GetHash(hash, _T("HEAD")))
 			{
-				hash = g_Git.GetHash(_T("HEAD"));
-			}
-			catch (char* msg)
-			{
-				CString err(msg);
-				MessageBox(_T("Could not get HEAD hash.\nlibgit reports:\n") + err, _T("TortoiseGit"), MB_ICONERROR);
+				MessageBox(g_Git.GetGitLastErr(_T("Could not get HEAD hash.")), _T("TortoiseGit"), MB_ICONERROR);
 			}
 			if (!hash.IsEmpty())
 			{

src/TortoiseProc/GitDiff.cpp
@@ -1,7 +1,7 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2008 - TortoiseSVN
-// Copyright (C) 2008-2012 - TortoiseGit
+// Copyright (C) 2008-2013 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -95,7 +95,16 @@ int CGitDiff::DiffNull(CTGitPath *pPath, git_revnum_t rev1,bool bIsAdd)
 {
 	CString temppath;
 	GetTempPath(temppath);
-	rev1 = g_Git.GetHash(rev1); // make sure we have a HASH here, otherwise filenames might be invalid
+	if (rev1 != GIT_REV_ZERO)
+	{
+		CGitHash rev1Hash;
+		if (g_Git.GetHash(rev1Hash, rev1)) // make sure we have a HASH here, otherwise filenames might be invalid
+		{
+			MessageBox(NULL, g_Git.GetGitLastErr(_T("Could not get hash of \"") + rev1 + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
+			return -1;
+		}
+		rev1 = rev1Hash.ToString();
+	}
 	CString file1;
 	CString nullfile;
 	CString cmd;
@@ -335,8 +344,26 @@ int CGitDiff::Diff(CTGitPath * pPath,CTGitPath * pPath2, git_revnum_t rev1, git_
 	GetTempPath(temppath);
 
 	// make sure we have HASHes here, otherwise filenames might be invalid
-	rev1 = g_Git.GetHash(rev1);
-	rev2 = g_Git.GetHash(rev2);
+	if (rev1 != GIT_REV_ZERO)
+	{
+		CGitHash rev1Hash;
+		if (g_Git.GetHash(rev1Hash, rev1))
+		{
+			MessageBox(NULL, g_Git.GetGitLastErr(_T("Could not get hash of \"") + rev1 + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
+			return -1;
+		}
+		rev1 = rev1Hash.ToString();
+	}
+	if (rev2 != GIT_REV_ZERO)
+	{
+		CGitHash rev2Hash;
+		if (g_Git.GetHash(rev2Hash, rev2))
+		{
+			MessageBox(NULL, g_Git.GetGitLastErr(_T("Could not get hash of \"") + rev2 + _T("\".")), _T("TortoiseGit"), MB_ICONERROR);
+			return -1;
+		}
+		rev2 = rev2Hash.ToString();
+	}
 
 	CString file1;
 	CString title1;

commit 697eb6f53dcc71f4a214a3a41aa327db42e6c61e

Fixed regression introduced in revision 02fbc263b2da8d4cd92e10ff1509dcb277426890
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RebaseDlg.cpp
@@ -900,7 +900,7 @@ int CRebaseDlg::FinishRebase()
 	}
 	AddLogString(out);
 
-	cmd.Format(_T("git.exe reset --hard %s"),head);
+	cmd.Format(_T("git.exe reset --hard %s"), head.ToString());
 	AddLogString(cmd);
 	while (true)
 	{