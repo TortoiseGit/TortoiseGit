79f1b2250c27f5dc35a9504e4e0b33285ed2ab19
Sup Yut Sum <ch3cooli@gmail.com>
2013-02-13 19:41:23 +0800

    Use CAppUtils::IsAdminLogin()
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TortoiseProc/Settings/SettingGitConfig.cpp
+++ src/TortoiseProc/Settings/SettingGitConfig.cpp
@@ -76,24 +76,6 @@ BEGIN_MESSAGE_MAP(CSettingGitConfig, CPropertyPage)
 	ON_BN_CLICKED(IDC_VIEWSYSTEMGITCONFIG, &CSettingGitConfig::OnBnClickedViewsystemgitconfig)
 END_MESSAGE_MAP()
 
-BOOL IsAdmin()
-{
-	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
-	PSID AdministratorsGroup;
-	// Initialize SID.
-	if (!AllocateAndInitializeSid(&NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &AdministratorsGroup))
-		return false;
-
-	// Check whether the token is present in admin group.
-	BOOL IsInAdminGroup = FALSE;
-	if (!CheckTokenMembership(NULL, AdministratorsGroup, &IsInAdminGroup))
-		IsInAdminGroup = FALSE;
-
-	// Free SID and return.
-	FreeSid(AdministratorsGroup);
-	return IsInAdminGroup;
-}
-
 BOOL CSettingGitConfig::OnInitDialog()
 {
 	ISettingsPropPage::OnInitDialog();
@@ -144,7 +126,7 @@ BOOL CSettingGitConfig::OnInitDialog()
 	if (isBareRepo)
 		this->GetDlgItem(IDC_EDITLOCALGITCONFIG)->SetWindowText(CString(MAKEINTRESOURCE(IDS_PROC_GITCONFIG_EDITLOCALGONCFIG)));
 
-	if (!IsAdmin())
+	if (!CAppUtils::IsAdminLogin())
 	{
 		((CButton *)this->GetDlgItem(IDC_EDITSYSTEMGITCONFIG))->SetShield(TRUE);
 		this->GetDlgItem(IDC_VIEWSYSTEMGITCONFIG)->ShowWindow(SW_SHOW);
