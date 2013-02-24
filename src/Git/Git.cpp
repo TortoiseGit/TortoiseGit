TortoiseGit/TortoiseGit

commit 51f5d844dbed5ce4be60aca71e4f75cbb77a5d2a

Include hash/branch name in log command just once
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -855,7 +855,8 @@ CString CGit::GetLogCmd( const CString &hash, CTGitPath *path, int count, int ma
 		range.Format(_T(" %s "), FixBranchName(*to));
 		param += range;
 	}
-	param+=hash;
+	else
+		param += hash;
 
 	CString st1,st2;
 
@@ -902,7 +903,7 @@ CString CGit::GetLogCmd( const CString &hash, CTGitPath *path, int count, int ma
 	}
 
 	if (CRegDWORD(_T("Software\\TortoiseGit\\LogTopoOrder"), TRUE))
-		param += _T(" --topo-order");
+		param += _T(" --date-order");
 
 	if(paramonly) //tgit.dll.Git.cpp:setup_revisions() only looks at args[1] and greater.  To account for this, pass a dummy parameter in the 0th place
 		cmd.Format(_T("--ignore-this-parameter %s -z %s --parents "), num, param);

commit 1257f6e53e6757fa4d062e937921ae080344f5ab

Fixed typo introduced in 51f5d844dbed5ce4be60aca71e4f75cbb77a5d2a.
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -897,7 +897,7 @@ CString CGit::GetLogCmd( const CString &hash, CTGitPath *path, int count, int ma
 	}
 
 	if (CRegDWORD(_T("Software\\TortoiseGit\\LogTopoOrder"), TRUE))
-		param += _T(" --date-order");
+		param += _T(" --topo-order");
 
 	if(paramonly) //tgit.dll.Git.cpp:setup_revisions() only looks at args[1] and greater.  To account for this, pass a dummy parameter in the 0th place
 		cmd.Format(_T("--ignore-this-parameter %s -z %s --parents "), num, param);