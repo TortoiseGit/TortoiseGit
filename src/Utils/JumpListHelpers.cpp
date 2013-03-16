0b470d7501763bcae11ab9b0d24fe46246f20eae
Sup Yut Sum <ch3cooli@gmail.com>
2013-03-16 17:27:22 +0800

    Remove unneeded variable assignments
    Based on TortoiseSVN revision 24009
    
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/Utils/JumpListHelpers.cpp
+++ src/Utils/JumpListHelpers.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2012 - TortoiseSVN
+// Copyright (C) 2009-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -128,6 +128,6 @@ void DeleteJumpList(LPCTSTR appID)
 	ATL::CComPtr<ICustomDestinationList> pcdl;
 	HRESULT hr = pcdl.CoCreateInstance(CLSID_DestinationList, NULL, CLSCTX_INPROC_SERVER);
 	if (SUCCEEDED(hr)) {
-		hr = pcdl->DeleteList(appID);
+		pcdl->DeleteList(appID);
 	}
 }
