90bacd21dffb306fd17790727f263cbbf7dec649
Sven Strickroth <email@cs-ware.de>
2013-05-09 17:25:35 +0200

    Added missing GPL headers
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/LogFile.cpp
+++ src/TortoiseProc/LogFile.cpp
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2008 - TortoiseGit
 // Copyright (C) 2007 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
