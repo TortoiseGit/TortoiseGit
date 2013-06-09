c3f9c741db52b185329d7b1d53649c62fe23d3ac
Sup Yut Sum <ch3cooli@gmail.com>
2013-06-09 18:18:47 +0800

    TGitCache/stdafx.h: remove fallback defines for older SDKs
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TGitCache/stdafx.h
+++ src/TGitCache/stdafx.h
@@ -43,17 +43,6 @@ using namespace ATL;
 
 typedef CComCritSecLock<CComAutoCriticalSection> AutoLocker;
 
-// Temporary fix for people not using the latest SDK
-#ifndef PROCESS_MODE_BACKGROUND_BEGIN
-#define PROCESS_MODE_BACKGROUND_BEGIN 0x00100000
-#endif
-#ifndef THREAD_MODE_BACKGROUND_BEGIN
-#define THREAD_MODE_BACKGROUND_BEGIN 0x00010000
-#endif
-#ifndef THREAD_MODE_BACKGROUND_END
-#define THREAD_MODE_BACKGROUND_END 0x00020000
-#endif
-
 #ifdef _WIN64
 #	define APP_X64_STRING	"x64"
 #else
