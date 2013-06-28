1711d3a5dcd3ef65faa30d6bc73d2dac412847d0
Sven Strickroth <email@cs-ware.de>
2013-06-28 13:31:07 +0200

    Use redirection service for special TortoiseGit web pages
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/CheckForUpdatesDlg.cpp
+++ src/TortoiseProc/CheckForUpdatesDlg.cpp
@@ -50,7 +50,7 @@ CCheckForUpdatesDlg::CCheckForUpdatesDlg(CWnd* pParent /*=NULL*/)
 	, m_pDownloadThread(NULL)
 	, m_bThreadRunning(FALSE)
 {
-	m_sUpdateDownloadLink = _T("http://code.google.com/p/tortoisegit/wiki/Download?tm=2");
+	m_sUpdateDownloadLink = _T("http://redir.tortoisegit.org/download");
 }
 
 CCheckForUpdatesDlg::~CCheckForUpdatesDlg()
