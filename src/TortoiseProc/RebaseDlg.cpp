TortoiseGit/TortoiseGit

commit eb38f0ba42b69c21e1ee24952cd3c82a8f811076

Fixed issue #1833: Fast-Forward rebase does not preserve SHA-1 of commits
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RebaseDlg.cpp
@@ -1572,6 +1572,8 @@ int CRebaseDlg::DoRebase()
 	CString cherryPickedFrom;
 	if (m_bAddCherryPickedFrom)
 		cherryPickedFrom = _T("-x ");
+	else if (!m_IsCherryPick)
+		cherryPickedFrom = _T("--ff "); // for issue #1833: "If the current HEAD is the same as the parent of the cherry-pick’ed commit, then a fast forward to this commit will be performed."
 
 	cmd.Format(_T("git.exe cherry-pick %s%s %s"), cherryPickedFrom, nocommit, pRev->m_CommitHash.ToString());
 

commit 0e2e23ceb8416e421c54718a21e83827750e4c09

Cherry-pick --ff does not work in combination with --no-commit
This fixes a regression introduced in revision eb38f0ba42b69c21e1ee24952cd3c82a8f811076.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RebaseDlg.cpp
@@ -1572,7 +1572,7 @@ int CRebaseDlg::DoRebase()
 	CString cherryPickedFrom;
 	if (m_bAddCherryPickedFrom)
 		cherryPickedFrom = _T("-x ");
-	else if (!m_IsCherryPick)
+	else if (!m_IsCherryPick && nocommit.IsEmpty())
 		cherryPickedFrom = _T("--ff "); // for issue #1833: "If the current HEAD is the same as the parent of the cherry-pick’ed commit, then a fast forward to this commit will be performed."
 
 	cmd.Format(_T("git.exe cherry-pick %s%s %s"), cherryPickedFrom, nocommit, pRev->m_CommitHash.ToString());