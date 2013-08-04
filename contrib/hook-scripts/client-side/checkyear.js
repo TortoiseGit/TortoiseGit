1c7a43820d9f470c11b54e847df92931d899df2a
Sup Yut Sum <ch3cooli@gmail.com>
2013-08-04 10:41:32 +0800

    Update contrib files from TortoiseSVN r24612
    Thanks to XhmikosR for providing a patch
    
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- contrib/hook-scripts/client-side/checkyear.js
+++ contrib/hook-scripts/client-side/checkyear.js
@@ -1,91 +1,100 @@
-// this script is a local pre-commit hook script.
-// Used to check whether the copyright year of modified files has been bumped
-// up to the current (2007) year.
-//
-// Only *.cpp and *.h files are checked
-//
-// Set the local hook scripts like this (pre-commit hook):
-// WScript path/to/this/script/file.js
-// and set "Wait for the script to finish"
+/* This script is a local pre-commit hook script.
+ * It's used to check whether the copyright year of modified files has been
+ * bumped up to the current (2013) year.
+ *
+ * Only *.cpp, *.h and *.idl files are checked
+ *
+ * Set the local hook scripts like this (pre-commit hook):
+ * WScript path/to/this/script/file.js
+ * and set "Wait for the script to finish"
+ */
 
-var objArgs,num;
+var ForReading = 1;
+var objArgs, num;
 
 objArgs = WScript.Arguments;
 num = objArgs.length;
-if (num != 4)
+if (num !== 4)
 {
     WScript.Echo("Usage: [CScript | WScript] checkyear.js path/to/pathsfile depth path/to/messagefile path/to/CWD");
     WScript.Quit(1);
 }
 
-var re = /^\/\/ Copyright.+(2008)(.*)/;
+var re = /^\/\/ Copyright.+(2013)(.*)/;
 var basere = /^\/\/ Copyright(.*)/;
-var filere = /(\.cpp$)|(\.h$)/;
+var filere = /(\.cpp$)|(\.h$)|(\.idl$)/;
 var found = true;
-var fs, a, ForAppending, rv, r;
-ForReading = 1;
+var fs, a, rv, r;
 fs = new ActiveXObject("Scripting.FileSystemObject");
 // remove the quotes
 var files = readPaths(objArgs(0));
-var fileindex=0;
+// going backwards with while is believed to be faster
+var fileindex = files.length;
 var errormsg = "";
-while (fileindex < files.length)
+
+while (fileindex--)
 {
-	var f = files[fileindex];
-    if (f.match(filere) != null)
+    var f = files[fileindex];
+    if (f.match(filere) !== null)
     {
-		if (fs.FileExists(f))
-		{
-			a = fs.OpenTextFile(f, ForReading, false);
-			var currentfound = false;
-			while ((!a.AtEndOfStream)&&(!currentfound))
-			{
-				r =  a.ReadLine();
-				rv = r.match(basere);
-				if (rv != null)
-				{
-					rv = r.match(re);
-					if (rv == null)
-					{
-						if (errormsg != "")
-							errormsg += "\n";
-						errormsg += f;
-						found = false;
-					}
-					currentfound = true;
-				}
-			}
-			a.Close();
-		}
+        if (fs.FileExists(f))
+        {
+            a = fs.OpenTextFile(f, ForReading, false);
+            var copyrightFound = false;
+            var yearFound = false;
+            while ((!a.AtEndOfStream) && (!yearFound))
+            {
+                r = a.ReadLine();
+                rv = r.match(basere);
+                if (rv !== null)
+                {
+                    rv = r.match(re);
+                    if (rv !== null)
+                    {
+                        yearFound = true;
+                    }
+
+                    copyrightFound = true;
+                }
+            }
+            a.Close();
+
+            if (copyrightFound && (!yearFound))
+            {
+                if (errormsg !== "")
+                {
+                    errormsg += "\n";
+                }
+                errormsg += f;
+                found = false;
+            }
+        }
     }
-    fileindex+=1;
 }
 
-if (found == false)
+if (found === false)
 {
-	errormsg = "the file(s):\n" + errormsg + "\nhave not the correct copyright year!";
-	WScript.stderr.writeLine(errormsg);
+    errormsg = "the file(s):\n" + errormsg + "\nhave not the correct copyright year!";
+    WScript.stderr.writeLine(errormsg);
 }
 
 WScript.Quit(!found);
 
 
+// readFileLines
 function readPaths(path)
 {
-	var retPaths = new Array();
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	if (fs.FileExists(path))
-	{
-		var a = fs.OpenTextFile(path, 1, false);
-		var i = 0;
-		while (!a.AtEndOfStream)
-		{
-			var line = a.ReadLine();
-			retPaths[i] = line;
-			i = i + 1;
-		}
-		a.Close();
-	}
-	return retPaths;
-	
+    var retPaths = [];
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    if (fs.FileExists(path))
+    {
+        var a = fs.OpenTextFile(path, ForReading);
+        while (!a.AtEndOfStream)
+        {
+            var line = a.ReadLine();
+            retPaths.push(line);
+        }
+        a.Close();
+    }
+    return retPaths;
 }
