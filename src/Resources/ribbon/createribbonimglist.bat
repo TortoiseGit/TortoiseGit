bf51299bcc291a15e367f799f21c1937f60e807c
Sup Yut Sum <ch3cooli@gmail.com>
2013-08-16 21:12:08 +0800

    createribbonimglist.bat: switch to the script dir before executing any commands
    Based on TortoiseSVN revision 24675
    
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/Resources/ribbon/createribbonimglist.bat
+++ src/Resources/ribbon/createribbonimglist.bat
@@ -1,10 +1,14 @@
 @echo off
 
+pushd %~dp0
+
 ..\tools\convert.exe -resize 32x32 @ribbonimagelist.txt +append ribbonlarge.png
 ..\tools\convert.exe -resize 16x16 @ribbonimagelist.txt +append ribbonsmall.png
 ..\tools\convert.exe -resize 48x48 @ribbonimagelist.txt +append ribbonlargehdpi.png
 ..\tools\convert.exe -resize 24x24 @ribbonimagelist.txt +append ribbonsmallhdpi.png
 
 if exist ..\*.aps del ..\*.aps
+
+popd
 
 pause
