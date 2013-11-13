294a6903a8356c042885720f592b14d1b48447dd
Sup Yut Sum <ch3cooli@gmail.com>
2013-11-13 20:17:15 +0800

    SyncDlg: Get all refs in new thread to avoid blocking UI
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TortoiseProc/SyncDlg.cpp
+++ src/TortoiseProc/SyncDlg.cpp
@@ -138,10 +138,6 @@ void CSyncDlg::OnBnClickedButtonPull()
 	m_refList.Clear();
 	m_newHashMap.clear();
 	m_oldHashMap.clear();
-	if (g_Git.GetMapHashToFriendName(m_oldHashMap))
-	{
-		MessageBox(g_Git.GetGitLastErr(_T("Could not get all refs.")), _T("TortoiseGit"), MB_ICONERROR);
-	}
 
 	if( CurrentEntry == 0)
 	{
@@ -1205,6 +1201,9 @@ void CSyncDlg::OnCbnEditchangeComboboxex()
 
 UINT CSyncDlg::ProgressThread()
 {
+	if (g_Git.GetMapHashToFriendName(m_oldHashMap))
+		MessageBox(g_Git.GetGitLastErr(_T("Could not get all refs.")), _T("TortoiseGit"), MB_ICONERROR);
+
 	m_startTick = GetTickCount();
 	m_bDone = false;
 	CProgressDlg::RunCmdList(this, m_GitCmdList, true, NULL, &this->m_bAbort, &this->m_Databuf);
