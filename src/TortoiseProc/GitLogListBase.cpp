e0d2f97b75dd9949eebde16742233d58977d01da
Sup Yut Sum <ch3cooli@gmail.com>
2014-04-04 20:25:34 +0800

    Log list full text search can find notes text
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TortoiseProc/GitLogListBase.cpp
+++ src/TortoiseProc/GitLogListBase.cpp
@@ -3891,6 +3891,8 @@ LRESULT CGitLogListBase::OnFindDialogMessage(WPARAM /*wParam*/, LPARAM /*lParam*
 			str+=_T("\n");
 			str+=pLogEntry->GetSubject();
 			str+=_T("\n");
+			str+=pLogEntry->m_Notes;
+			str+=_T("\n");
 
 
 			/*Because changed files list is loaded on demand when gui show,
