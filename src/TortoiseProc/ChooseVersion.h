012a0404cad0b7b6ea13ea7fe7daafb4bbb5e713
Sven Strickroth <email@cs-ware.de>
2014-04-18 11:13:24 +0200

    Do not call IsBranchTagNameUnique with empty parameter
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/ChooseVersion.h
+++ src/TortoiseProc/ChooseVersion.h
@@ -109,12 +109,12 @@ protected:
 			break;
 		case IDC_RADIO_BRANCH:
 			this->m_VersionName=m_ChooseVersioinBranch.GetString();
-			if (!g_Git.IsBranchTagNameUnique(this->m_VersionName))
+			if (!m_VersionName.IsEmpty() && !g_Git.IsBranchTagNameUnique(this->m_VersionName))
 				this->m_VersionName = L"refs/heads/" + this->m_VersionName;
 			break;
 		case IDC_RADIO_TAGS:
 			this->m_VersionName = m_ChooseVersioinTags.GetString();
-			if (!g_Git.IsBranchTagNameUnique(this->m_VersionName))
+			if (!m_VersionName.IsEmpty() && !g_Git.IsBranchTagNameUnique(this->m_VersionName))
 				this->m_VersionName = L"refs/tags/" + m_ChooseVersioinTags.GetString();
 			break;
 		case IDC_RADIO_VERSION:
