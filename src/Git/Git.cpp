TortoiseGit/TortoiseGit

commit 31ee06f132116bbbdbf5576b571fbf3ffdca7c9e

Fixed issue #2143: PowerShell not working in git hooks
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -245,8 +245,9 @@ int CGit::RunAsync(CString cmd, PROCESS_INFORMATION *piOut, HANDLE *hReadOut, HA
 	LPTSTR pEnv = (!m_Environment.empty()) ? &m_Environment[0]: NULL;
 	DWORD dwFlags = pEnv ? CREATE_UNICODE_ENVIRONMENT : 0;
 
-	//DETACHED_PROCESS make ssh recognize that it has no console to launch askpass to input password.
-	dwFlags |= DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP;
+	// CREATE_NEW_CONSOLE makes ssh recognize that it has no console in order to launch askpass to ask for the password.
+	// DETACHED_PROCESS which was originally used here has the same effect, however, it prevents PowerShell from working (cf. issue #2143)
+	dwFlags |= CREATE_NEW_PROCESS_GROUP | CREATE_NEW_CONSOLE;
 
 	memset(&this->m_CurrentGitPi,0,sizeof(PROCESS_INFORMATION));
 	memset(&pi, 0, sizeof(PROCESS_INFORMATION));

commit 4f76e77f1da464dc0a6bd54ab94e77b592e9a0d7

Fixed issue #2257: Fetching & pushing using ssh.exe not working with TortoiseGit 1.8.10
This commit fixes a regression introduced in commit 31ee06f132116bbbdbf5576b571fbf3ffdca7c9e.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -258,9 +258,15 @@ int CGit::RunAsync(CString cmd, PROCESS_INFORMATION *piOut, HANDLE *hReadOut, HA
 	LPTSTR pEnv = (!m_Environment.empty()) ? &m_Environment[0]: NULL;
 	DWORD dwFlags = pEnv ? CREATE_UNICODE_ENVIRONMENT : 0;
 
-	// CREATE_NEW_CONSOLE makes ssh recognize that it has no console in order to launch askpass to ask for the password.
-	// DETACHED_PROCESS which was originally used here has the same effect, however, it prevents PowerShell from working (cf. issue #2143)
-	dwFlags |= CREATE_NEW_PROCESS_GROUP | CREATE_NEW_CONSOLE;
+	dwFlags |= CREATE_NEW_PROCESS_GROUP;
+
+	// CREATE_NEW_CONSOLE makes git (but not ssh.exe, see issue #2257) recognize that it has no console in order to launch askpass to ask for the password,
+	// DETACHED_PROCESS which was originally used here has the same effect (but works with git.exe AND ssh.exe), however, it prevents PowerShell from working (cf. issue #2143)
+	// => we keep using DETACHED_PROCESS as the default, but if cmd contains pwershell we use CREATE_NEW_CONSOLE
+	if (cmd.Find(_T("powershell")) >= 0)
+		dwFlags |= CREATE_NEW_CONSOLE;
+	else
+		dwFlags |= DETACHED_PROCESS;
 
 	memset(&this->m_CurrentGitPi,0,sizeof(PROCESS_INFORMATION));
 	memset(&pi, 0, sizeof(PROCESS_INFORMATION));