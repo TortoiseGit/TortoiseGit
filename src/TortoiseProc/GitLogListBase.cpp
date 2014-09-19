85158732e522401e24bfa06d2b915db404b00cd3
Yue Lin Ho <b8732003@student.nsysu.edu.tw>
2014-09-19 16:23:57 +0800

    Highlight with current branch color
    I am not sure if it is good!
    (It jumps over Theme.)
    
    Signed-off-by: Yue Lin Ho <b8732003@student.nsysu.edu.tw>

--- src/TortoiseProc/GitLogListBase.cpp
+++ src/TortoiseProc/GitLogListBase.cpp
@@ -586,6 +586,7 @@ void DrawUpTriangle(HDC hdc, CRect rect, COLORREF color, int bold)
 void CGitLogListBase::DrawTagBranchMessage(HDC hdc, CRect &rect, INT_PTR index, std::vector<REFLABEL> &refList)
 {
 	GitRev* data = (GitRev*)m_arShownList.SafeGetAt(index);
+	BOOL isHighlight = data->m_CommitHash == m_highlight && !m_highlight.IsEmpty();
 	CRect rt=rect;
 	LVITEM rItem;
 	SecureZeroMemory(&rItem, sizeof(LVITEM));
@@ -616,7 +617,7 @@ void CGitLogListBase::DrawTagBranchMessage(HDC hdc, CRect &rect, INT_PTR index, 
 		rt.left += oneSpaceSize.cx;
 	}
 
-	if (IsAppThemed() && SysInfo::Instance().IsVistaOrLater())
+	if (IsAppThemed() && SysInfo::Instance().IsVistaOrLater() && !isHighlight)
 	{
 		int txtState = LISS_NORMAL;
 		if (rItem.state & LVIS_SELECTED)
@@ -626,16 +627,24 @@ void CGitLogListBase::DrawTagBranchMessage(HDC hdc, CRect &rect, INT_PTR index, 
 	}
 	else
 	{
-		if (rItem.state & LVIS_SELECTED)
+		COLORREF clrOld = ::GetTextColor(hdc);
+		COLORREF clrNew;
+
+		if (isHighlight)
 		{
-			COLORREF clrOld = ::SetTextColor(hdc,::GetSysColor(COLOR_HIGHLIGHTTEXT));
-			::DrawText(hdc,data->GetSubject(), data->GetSubject().GetLength(), &rt, DT_NOPREFIX | DT_LEFT | DT_SINGLELINE | DT_VCENTER | DT_END_ELLIPSIS);
-			::SetTextColor(hdc, clrOld);
+			CColors clr;
+			clrNew = clr.GetColor(CColors::CurrentBranch);
 		}
+		else if (rItem.state & LVIS_SELECTED)
+			clrNew = ::GetSysColor(COLOR_HIGHLIGHTTEXT);
 		else
-		{
-			::DrawText(hdc, data->GetSubject(), data->GetSubject().GetLength(), &rt, DT_NOPREFIX | DT_LEFT | DT_SINGLELINE | DT_VCENTER | DT_END_ELLIPSIS);
-		}
+			clrNew = clrOld;
+
+		if (clrOld != clrNew)
+			::SetTextColor(hdc, clrNew);
+		::DrawText(hdc, data->GetSubject(), data->GetSubject().GetLength(), &rt, DT_NOPREFIX | DT_LEFT | DT_SINGLELINE | DT_VCENTER | DT_END_ELLIPSIS);
+		if (clrOld != clrNew)
+			::SetTextColor(hdc, clrOld);
 	}
 
 	if (m_bTagsBranchesOnRightSide)
@@ -1204,6 +1213,12 @@ void CGitLogListBase::OnNMCustomdrawLoglist(NMHDR *pNMHDR, LRESULT *pResult)
 						hGdiObj = m_boldFont;
 					else if (isHighlight)
 						hGdiObj = m_FontItalics;
+
+					if (isHighlight)
+					{
+						CColors clr;
+						crText = clr.GetColor(CColors::CurrentBranch);
+					}
 
 					if (hGdiObj)
 					{
