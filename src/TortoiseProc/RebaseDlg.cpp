42282f9e15e94da12b40567225ca3b3728b69d9a
Sven Strickroth <email@cs-ware.de>
2014-09-25 22:58:16 +0200

    RebaseDlg: Make Squash->Edit->Squash not to introduce unwanted commit
    (fixes issue #2126)
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/RebaseDlg.cpp
+++ src/TortoiseProc/RebaseDlg.cpp
@@ -1276,7 +1276,26 @@ void CRebaseDlg::OnBnClickedContinue()
 		::DeleteFile(tempfile);
 		AddLogString(out);
 		if (CheckNextCommitIsSquash() == 0 && m_RebaseStage != REBASE_SQUASH_EDIT) // remember commit msg after edit if next commit if squash; but don't do this if ...->squash(reset here)->pick->squash
+		{
 			m_SquashMessage = str;
+			// reset parent so that we can do "git cherry-pick --no-commit" w/o introducing an unwanted commit
+			cmd = _T("git.exe reset --soft HEAD~1");
+			m_ctrlTabCtrl.SetActiveTab(REBASE_TAB_LOG);
+			while (true)
+			{
+				out.Empty();
+				if (g_Git.Run(cmd, &out, CP_UTF8))
+				{
+					AddLogString(cmd);
+					AddLogString(CString(MAKEINTRESOURCE(IDS_FAIL)));
+					AddLogString(out);
+					if (CMessageBox::Show(m_hWnd, out + _T("\nRetry?"), _T("TortoiseGit"), MB_YESNO | MB_ICONERROR) != IDYES)
+						return;
+				}
+				else
+					break;
+			}
+		}
 		else
 			m_SquashMessage.Empty();
 		this->m_ctrlTabCtrl.SetActiveTab(REBASE_TAB_LOG);
