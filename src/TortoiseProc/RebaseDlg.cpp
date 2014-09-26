82d3cd5652707fa42a48462f647d33ec4fccaac7
Sven Strickroth <email@cs-ware.de>
2014-09-26 13:56:56 +0200

    RebaseDlg: Fix incorrect warning on unclean working tree on Squash(Conflict)->Squash sequence
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/RebaseDlg.cpp
+++ src/TortoiseProc/RebaseDlg.cpp
@@ -1230,7 +1230,8 @@ void CRebaseDlg::OnBnClickedContinue()
 		}
 	}
 
-	if (m_RebaseStage == REBASE_EDIT || m_RebaseStage == REBASE_CONTINUE) {
+	if ((m_RebaseStage == REBASE_EDIT || m_RebaseStage == REBASE_CONTINUE) && CheckNextCommitIsSquash())
+	{
 		while (!g_Git.CheckCleanWorkTree())
 		{
 			if (CMessageBox::Show(nullptr, IDS_PROC_REBASE_CONTINUE_NOTCLEAN, IDS_APPNAME, 1, IDI_ERROR, IDS_MSGBOX_RETRY, IDS_IGNOREBUTTON) == 2)
