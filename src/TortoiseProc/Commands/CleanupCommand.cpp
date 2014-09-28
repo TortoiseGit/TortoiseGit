TortoiseGit/TortoiseGit

commit 749f359919983821fe05795b8b878a72f688a61d

Can run cleanup without using recycle bin and add dry run option
Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

src/TortoiseProc/Commands/CleanupCommand.cpp
@@ -21,6 +21,7 @@
 #include "CleanupCommand.h"
 
 #include "MessageBox.h"
+#include "ProgressDlg.h"
 #include "ShellUpdater.h"
 #include "CleanTypeDlg.h"
 #include "..\Utils\UnicodeUtils.h"
@@ -59,7 +60,9 @@ bool CleanupCommand::Execute()
 		pp.GetBOOLProps(quotepath, _T("core.quotepath"));
 
 		CString cmd;
-		cmd.Format(_T("git clean -n"));
+		cmd.Format(_T("git clean"));
+		if (dlg.m_bDryRun || !dlg.m_bNoRecycleBin)
+			cmd += _T(" -n ");
 		if(dlg.m_bDir)
 			cmd += _T(" -d ");
 		switch(dlg.m_CleanType)
@@ -75,53 +78,72 @@ bool CleanupCommand::Execute()
 			break;
 		}
 
-		CSysProgressDlg sysProgressDlg;
-		sysProgressDlg.SetAnimation(IDR_CLEANUPANI);
-		sysProgressDlg.SetTitle(CString(MAKEINTRESOURCE(IDS_APPNAME)));
-		sysProgressDlg.SetLine(1, CString(MAKEINTRESOURCE(IDS_PROC_CLEANUP_INFO1)));
-		sysProgressDlg.SetLine(2, CString(MAKEINTRESOURCE(IDS_PROGRESSWAIT)));
-		sysProgressDlg.SetShowProgressBar(false);
-		sysProgressDlg.ShowModeless((HWND)NULL, true);
-
-		CString cmdout, cmdouterr;
-		if (g_Git.Run(cmd, &cmdout, &cmdouterr, CP_UTF8)) {
-			MessageBox(NULL, cmdouterr, _T("TortoiseGit"), MB_ICONERROR);
-			return FALSE;
-		}
-
-		if (sysProgressDlg.HasUserCancelled())
+		if (dlg.m_bDryRun || dlg.m_bNoRecycleBin)
 		{
-			CMessageBox::Show(NULL, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
-			return FALSE;
-		}
-
-		int pos = 0;
-		CString token = cmdout.Tokenize(_T("\n"), pos);
-		CTGitPathList delList;
-		while (!token.IsEmpty())
-		{
-			if (token.Mid(0, 13) == _T("Would remove "))
+			CProgressDlg progress;
+			for (int i = 0; i < this->pathList.GetCount(); i++)
 			{
-				CString tempPath = token.Mid(13).TrimRight();
-				if (quotepath)
-				{
-					tempPath = UnescapeQuotePath(tempPath.Trim(_T('"')));
-				}
-				delList.AddPath(CTGitPath(tempPath));
+				CString path;
+				if (this->pathList[i].IsDirectory())
+					path = pathList[i].GetGitPathString();
+				else
+					path = pathList[i].GetContainingDirectory().GetGitPathString();
+
+				progress.m_GitCmdList.push_back(cmd + _T(" \"") + path + _T("\""));
+			}
+			if (progress.DoModal()==IDOK)
+				return TRUE;
+		}
+		else
+		{
+			CSysProgressDlg sysProgressDlg;
+			sysProgressDlg.SetAnimation(IDR_CLEANUPANI);
+			sysProgressDlg.SetTitle(CString(MAKEINTRESOURCE(IDS_APPNAME)));
+			sysProgressDlg.SetLine(1, CString(MAKEINTRESOURCE(IDS_PROC_CLEANUP_INFO1)));
+			sysProgressDlg.SetLine(2, CString(MAKEINTRESOURCE(IDS_PROGRESSWAIT)));
+			sysProgressDlg.SetShowProgressBar(false);
+			sysProgressDlg.ShowModeless((HWND)NULL, true);
+
+			CString cmdout, cmdouterr;
+			if (g_Git.Run(cmd, &cmdout, &cmdouterr, CP_UTF8)) {
+				MessageBox(NULL, cmdouterr, _T("TortoiseGit"), MB_ICONERROR);
+				return FALSE;
 			}
 
-			token = cmdout.Tokenize(_T("\n"), pos);
+			if (sysProgressDlg.HasUserCancelled())
+			{
+				CMessageBox::Show(NULL, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
+				return FALSE;
+			}
+
+			int pos = 0;
+			CString token = cmdout.Tokenize(_T("\n"), pos);
+			CTGitPathList delList;
+			while (!token.IsEmpty())
+			{
+				if (token.Mid(0, 13) == _T("Would remove "))
+				{
+					CString tempPath = token.Mid(13).TrimRight();
+					if (quotepath)
+					{
+						tempPath = UnescapeQuotePath(tempPath.Trim(_T('"')));
+					}
+					delList.AddPath(CTGitPath(tempPath));
+				}
+
+				token = cmdout.Tokenize(_T("\n"), pos);
+			}
+
+			if (sysProgressDlg.HasUserCancelled())
+			{
+				CMessageBox::Show(NULL, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
+				return FALSE;
+			}
+
+			delList.DeleteAllFiles(true, false);
+
+			sysProgressDlg.Stop();
 		}
-
-		if (sysProgressDlg.HasUserCancelled())
-		{
-			CMessageBox::Show(NULL, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
-			return FALSE;
-		}
-
-		delList.DeleteAllFiles(true, false);
-
-		sysProgressDlg.Stop();
 	}
 #if 0
 	CProgressDlg progress;

Languages/Tortoise.pot
@@ -3243,6 +3243,10 @@ msgstr ""
 msgid "Do not show the context menu for the following paths:"
 msgstr ""
 
+#. Resource IDs: (1730)
+msgid "Do not use recycle bin"
+msgstr ""
+
 #. Resource IDs: (70)
 #, c-format
 msgid "Do you really want to <ct=0x0000FF>delete</ct> <b>%s</b>?"
@@ -3340,6 +3344,10 @@ msgstr ""
 
 #. Resource IDs: (1079)
 msgid "Drive Types"
+msgstr ""
+
+#. Resource IDs: (1731)
+msgid "Dry run"
 msgstr ""
 
 #. Resource IDs: (1279)

src/Resources/TortoiseProcENG.rc
@@ -1497,7 +1497,7 @@ BEGIN
     PUSHBUTTON      "Help",IDHELP,338,241,50,14
 END
 
-IDD_CLEAN DIALOGEX 0, 0, 261, 117
+IDD_CLEAN DIALOGEX 0, 0, 261, 157
 STYLE DS_SETFONT | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME
 CAPTION "Clean"
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
@@ -1510,10 +1510,12 @@ BEGIN
     CONTROL         "Remove ignored files (-fX)",IDC_RADIO_CLEAN_IGNORE,
                     "Button",BS_AUTORADIOBUTTON,15,45,232,10
     CONTROL         "Remove untracked directories (-d)",IDC_CHECK_DIR,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,15,68,232,10
-    DEFPUSHBUTTON   "OK",IDOK,81,96,50,14
-    PUSHBUTTON      "Cancel",IDCANCEL,142,96,50,14
-    PUSHBUTTON      "Help",IDHELP,204,96,50,14
-    LTEXT           "Attention: This command affects the whole working tree!",IDC_STATIC,14,83,233,10
+    CONTROL         "Do not use recycle bin",IDC_CHECK_NORECYCLEBIN,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,15,81,87,10
+    CONTROL         "Dry run",IDC_CHECK_DRYRUN,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,15,95,40,10
+    DEFPUSHBUTTON   "OK",IDOK,81,128,50,14
+    PUSHBUTTON      "Cancel",IDCANCEL,142,128,50,14
+    PUSHBUTTON      "Help",IDHELP,204,128,50,14
+    LTEXT           "Attention: This command affects the whole working tree!",IDC_STATIC,14,114,233,10
 END
 
 IDD_PATCH_VIEW DIALOGEX 0, 0, 248, 195
@@ -2395,7 +2397,7 @@ BEGIN
         LEFTMARGIN, 7
         RIGHTMARGIN, 254
         TOPMARGIN, 7
-        BOTTOMMARGIN, 106
+        BOTTOMMARGIN, 146
     END
 
     IDD_PATCH_VIEW, DIALOG

src/TortoiseProc/CleanTypeDlg.cpp
@@ -40,6 +40,8 @@ CCleanTypeDlg::CCleanTypeDlg(CWnd* pParent /*=NULL*/)
 
 	this->m_bDir = this->m_regDir;
 	this->m_CleanType = this->m_regType;
+	m_bNoRecycleBin = FALSE;
+	m_bDryRun = FALSE;
 }
 
 CCleanTypeDlg::~CCleanTypeDlg()
@@ -50,6 +52,8 @@ void CCleanTypeDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	DDX_Check(pDX, IDC_CHECK_DIR, m_bDir);
+	DDX_Check(pDX, IDC_CHECK_NORECYCLEBIN, m_bNoRecycleBin);
+	DDX_Check(pDX, IDC_CHECK_DRYRUN, m_bDryRun);
 	DDX_Radio(pDX, IDC_RADIO_CLEAN_ALL,m_CleanType);
 }
 
@@ -70,6 +74,8 @@ BOOL CCleanTypeDlg::OnInitDialog()
 	AdjustControlSize(IDC_RADIO_CLEAN_NO);
 	AdjustControlSize(IDC_RADIO_CLEAN_IGNORE);
 	AdjustControlSize(IDC_CHECK_DIR);
+	AdjustControlSize(IDC_CHECK_NORECYCLEBIN);
+	AdjustControlSize(IDC_CHECK_DRYRUN);
 
 	CString sWindowTitle;
 	GetWindowText(sWindowTitle);

src/TortoiseProc/CleanTypeDlg.h
@@ -49,5 +49,7 @@ protected:
 public:
 	BOOL	m_bDir;
 	int		m_CleanType;
+	BOOL	m_bNoRecycleBin;
+	BOOL	m_bDryRun;
 	afx_msg void OnBnClickedHelp();
 };

src/TortoiseProc/resource.h
@@ -1213,6 +1213,8 @@
 #define IDC_STATIC_TAGOPT               1727
 #define IDC_COMBO_TAGOPT                1728
 #define IDC_REV1BTN1                    1729
+#define IDC_CHECK_NORECYCLEBIN          1730
+#define IDC_CHECK_DRYRUN                1731
 #define IDS_DLGTITLE_ADD_DIFF_TOOL      1750
 #define IDS_DLGTITLE_ADD_MERGE_TOOL     1751
 #define IDS_DLGTITLE_EDIT_DIFF_TOOL     1752
@@ -1653,7 +1655,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        345
 #define _APS_NEXT_COMMAND_VALUE         32860
-#define _APS_NEXT_CONTROL_VALUE         1730
+#define _APS_NEXT_CONTROL_VALUE         1732
 #define _APS_NEXT_SYMED_VALUE           201
 #endif
 #endif

commit 85fac283de9464ce54015727dad049df4b8e4cde

Fixed issue 2313: [Clean to recycle bin] after Clean-up dry run removes unexpected files
Fixes a regression introduced in 749f359919983821fe05795b8b878a72f688a61d where dryRun differs from no dryRun mode.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/Commands/CleanupCommand.cpp
@@ -110,6 +110,49 @@ static bool GetSubmodulePathList(SubmodulePayload &payload)
 	return true;
 }
 
+static bool GetFilesToCleanUp(CTGitPathList& delList, const CString& baseCmd, CGit *pGit, const CString& path, const boolean quotepath, CSysProgressDlg& sysProgressDlg)
+{
+	CString cmd(baseCmd);
+	if (!path.IsEmpty())
+		cmd += _T(" \"") + path + _T("\"");
+
+	CString cmdout, cmdouterr;
+	if (pGit->Run(cmd, &cmdout, &cmdouterr, CP_UTF8))
+	{
+		MessageBox(nullptr, cmdouterr, _T("TortoiseGit"), MB_ICONERROR);
+		return false;
+	}
+
+	if (sysProgressDlg.HasUserCancelled())
+	{
+		CMessageBox::Show(nullptr, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
+		return false;
+	}
+
+	int pos = 0;
+	CString token = cmdout.Tokenize(_T("\n"), pos);
+	while (!token.IsEmpty())
+	{
+		if (token.Mid(0, 13) == _T("Would remove "))
+		{
+			CString tempPath = token.Mid(13).TrimRight();
+			if (quotepath)
+				tempPath = UnescapeQuotePath(tempPath.Trim(_T('"')));
+			delList.AddPath(pGit->CombinePath(tempPath));
+		}
+
+		token = cmdout.Tokenize(_T("\n"), pos);
+	}
+
+	if (sysProgressDlg.HasUserCancelled())
+	{
+		CMessageBox::Show(nullptr, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
+		return false;
+	}
+
+	return true;
+}
+
 static bool DoCleanUp(const CTGitPathList& pathList, int cleanType, bool bDir, bool bSubmodules, bool bDryRun, bool bNoRecycleBin)
 {
 	CString cmd;
@@ -212,55 +255,24 @@ static bool DoCleanUp(const CTGitPathList& pathList, int cleanType, bool bDir, b
 		bool quotepath = g_Git.GetConfigValueBool(_T("core.quotepath"));
 
 		CTGitPathList delList;
-		for (size_t i = 0; i <= submoduleList.size(); ++i)
+		for (int i = 0; i < pathList.GetCount(); ++i)
+		{
+			CString path;
+			if (pathList[i].IsDirectory())
+				path = pathList[i].GetGitPathString();
+			else
+				path = pathList[i].GetContainingDirectory().GetGitPathString();
+
+			if (!GetFilesToCleanUp(delList, cmd, &g_Git, path, quotepath, sysProgressDlg))
+				return false;
+		}
+
+		for (CString dir : submoduleList)
 		{
 			CGit git;
-			CGit *pGit;
-			if (i == 0)
-				pGit = &g_Git;
-			else
-			{
-				git.m_CurrentDir = submoduleList[i - 1];
-				pGit = &git;
-			}
-			CString cmdout, cmdouterr;
-			if (pGit->Run(cmd, &cmdout, &cmdouterr, CP_UTF8))
-			{
-				MessageBox(nullptr, cmdouterr, _T("TortoiseGit"), MB_ICONERROR);
+			git.m_CurrentDir = dir;
+			if (!GetFilesToCleanUp(delList, cmd, &git, _T(""), quotepath, sysProgressDlg))
 				return false;
-			}
-
-			if (sysProgressDlg.HasUserCancelled())
-			{
-				CMessageBox::Show(nullptr, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
-				return false;
-			}
-
-			int pos = 0;
-			CString token = cmdout.Tokenize(_T("\n"), pos);
-			while (!token.IsEmpty())
-			{
-				if (token.Mid(0, 13) == _T("Would remove "))
-				{
-					CString tempPath = token.Mid(13).TrimRight();
-					if (quotepath)
-					{
-						tempPath = UnescapeQuotePath(tempPath.Trim(_T('"')));
-					}
-					if (i == 0)
-						delList.AddPath(CTGitPath(tempPath));
-					else
-						delList.AddPath(CTGitPath(submoduleList[i - 1] + "/" + tempPath));
-				}
-
-				token = cmdout.Tokenize(_T("\n"), pos);
-			}
-
-			if (sysProgressDlg.HasUserCancelled())
-			{
-				CMessageBox::Show(nullptr, IDS_SVN_USERCANCELLED, IDS_APPNAME, MB_OK);
-				return false;
-			}
 		}
 
 		delList.DeleteAllFiles(true, false);