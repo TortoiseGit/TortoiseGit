TortoiseGit/TortoiseGit

commit 25bd4c961830590ffdb51202e935fd168b7b2484

Pull/Fetch Dlg: Used git config to determine default remote and remote branch. Also enabled 'branch' option when pulling from known remote. Also started implementing rebase option (rebase instead of merge) when pulling. Disabled this though, because I think the rebase dialog should be used for this. It needs some work.

src/TortoiseProc/PullFetchDlg.cpp
@@ -6,13 +6,14 @@
 #include "PullFetchDlg.h"
 #include "Git.h"
 #include "AppUtils.h"
-
+#include "BrowseRefsDlg.h"
 // CPullFetchDlg dialog
 
 IMPLEMENT_DYNAMIC(CPullFetchDlg, CResizableStandAloneDialog)
 
 CPullFetchDlg::CPullFetchDlg(CWnd* pParent /*=NULL*/)
-	: CResizableStandAloneDialog(CPullFetchDlg::IDD, pParent)
+	: CResizableStandAloneDialog(CPullFetchDlg::IDD, pParent),
+	  m_bRebase(false)
 {
 	m_IsPull=TRUE;
     m_bAutoLoad = CAppUtils::IsSSHPutty();
@@ -31,6 +32,7 @@ void CPullFetchDlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_REMOTE_BRANCH, this->m_RemoteBranch);
     DDX_Control(pDX,IDC_REMOTE_MANAGE, this->m_RemoteManage);
     DDX_Check(pDX,IDC_PUTTYKEY_AUTOLOAD,m_bAutoLoad);
+    DDX_Check(pDX,IDC_CHECK_REBASE,m_bRebase);
 
 }
 
@@ -40,6 +42,7 @@ BEGIN_MESSAGE_MAP(CPullFetchDlg,CResizableStandAloneDialog )
 	ON_BN_CLICKED(IDC_OTHER_RD, &CPullFetchDlg::OnBnClickedRd)
 	ON_BN_CLICKED(IDOK, &CPullFetchDlg::OnBnClickedOk)
     ON_STN_CLICKED(IDC_REMOTE_MANAGE, &CPullFetchDlg::OnStnClickedRemoteManage)
+	ON_BN_CLICKED(IDC_BUTTON_BROWSE_REF, &CPullFetchDlg::OnBnClickedButtonBrowseRef)
 END_MESSAGE_MAP()
 
 BOOL CPullFetchDlg::OnInitDialog()
@@ -54,6 +57,7 @@ BOOL CPullFetchDlg::OnInitDialog()
 	AddAnchor(IDCANCEL,BOTTOM_RIGHT);
     AddAnchor(IDC_GROUPT_REMOTE,TOP_LEFT,BOTTOM_RIGHT);
     AddAnchor(IDC_PUTTYKEY_AUTOLOAD,BOTTOM_LEFT);
+	AddAnchor(IDC_CHECK_REBASE,BOTTOM_LEFT);
     AddAnchor(IDC_REMOTE_MANAGE,BOTTOM_LEFT);
 	AddAnchor(IDHELP, BOTTOM_RIGHT);
 
@@ -64,7 +68,12 @@ BOOL CPullFetchDlg::OnInitDialog()
 	CheckRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD,IDC_REMOTE_RD);
 	m_Remote.EnableWindow(TRUE);
 	m_Other.EnableWindow(FALSE);
-	m_RemoteBranch.EnableWindow(FALSE);
+	if(!m_IsPull)
+		m_RemoteBranch.EnableWindow(FALSE);
+
+//	if(!m_IsPull)
+		//Todo: implement rebase option sometime with rebase dialog
+		GetDlgItem(IDC_CHECK_REBASE)->ShowWindow(SW_HIDE);
 
 	m_Other.SetURLHistory(TRUE);
 	m_Other.LoadHistory(_T("Software\\TortoiseGit\\History\\PullURLS"), _T("url"));
@@ -90,12 +99,26 @@ BOOL CPullFetchDlg::OnInitDialog()
 	m_RemoteReg = remote;
 	int sel=0;
 
+	//Select pull-remote from current branch
+	CString currentBranch = g_Git.GetSymbolicRef();
+	CString configName;
+	configName.Format(L"branch.%s.remote", currentBranch);
+	CString pullRemote = g_Git.GetConfigValue(configName);
+
+	//Select pull-branch from current branch
+	configName.Format(L"branch.%s.merge", currentBranch);
+	CString pullBranch = CGit::StripRefName(g_Git.GetConfigValue(configName));
+	m_RemoteBranch.AddString(pullBranch);
+
+	if(pullRemote.IsEmpty())
+		pullRemote = remote;
+
 	if(!g_Git.GetRemoteList(list))
 	{	
 		for(unsigned int i=0;i<list.size();i++)
 		{
 			m_Remote.AddString(list[i]);
-			if(list[i] == remote)
+			if(list[i] == pullRemote)
 				sel = i;
 		}
 	}
@@ -115,14 +138,15 @@ void CPullFetchDlg::OnBnClickedRd()
 	{
 		m_Remote.EnableWindow(TRUE);
 		m_Other.EnableWindow(FALSE);
-		m_RemoteBranch.EnableWindow(FALSE);
-
+		if(!m_IsPull)
+			m_RemoteBranch.EnableWindow(FALSE);
 	}
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_OTHER_RD)
 	{
 		m_Remote.EnableWindow(FALSE);
 		m_Other.EnableWindow(TRUE);;
-		m_RemoteBranch.EnableWindow(TRUE);
+		if(!m_IsPull)
+			m_RemoteBranch.EnableWindow(TRUE);
 	}
 	
 
@@ -134,7 +158,10 @@ void CPullFetchDlg::OnBnClickedOk()
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_REMOTE_RD)
 	{
 		m_RemoteURL=m_Remote.GetString();
-		m_RemoteBranchName.Empty();
+		if(!m_IsPull)
+			m_RemoteBranchName.Empty();
+		else
+			m_RemoteBranchName=m_RemoteBranch.GetString();
 		
 	}
 	if( GetCheckedRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD) == IDC_OTHER_RD)
@@ -155,4 +182,26 @@ void CPullFetchDlg::OnStnClickedRemoteManage()
 {
     // TODO: Add your control notification handler code here
     CAppUtils::LaunchRemoteSetting();
+}
+
+void CPullFetchDlg::OnBnClickedButtonBrowseRef()
+{
+	CString initialRef;
+	initialRef.Format(L"refs/remotes/%s/%s", m_Remote.GetString(), m_RemoteBranch.GetString());
+	CString selectedRef = CBrowseRefsDlg::PickRef(false, initialRef, gPickRef_Remote);
+	if(selectedRef.Left(13) != "refs/remotes/")
+		return;
+
+	selectedRef = selectedRef.Mid(13);
+	int ixSlash = selectedRef.Find('/');
+
+	CString remoteName   = selectedRef.Left(ixSlash);
+	CString remoteBranch = selectedRef.Mid(ixSlash + 1);
+	
+	int ixFound = m_Remote.FindStringExact(0, remoteName);
+	if(ixFound >= 0)
+		m_Remote.SetCurSel(ixFound);
+	m_RemoteBranch.AddString(remoteBranch);
+
+	CheckRadioButton(IDC_REMOTE_RD,IDC_OTHER_RD,IDC_REMOTE_RD);
 }

src/TortoiseProc/Commands/PullCommand.cpp
@@ -44,7 +44,10 @@ bool PullCommand::Execute()
 		
 		CString cmd;
 		CString hashOld = g_Git.GetHash(L"HEAD");
-		cmd.Format(_T("git.exe pull \"%s\" %s"),url,dlg.m_RemoteBranchName);
+		CString cmdRebase;
+		if(dlg.m_bRebase)
+			cmdRebase = "--rebase ";
+		cmd.Format(_T("git.exe pull %s\"%s\" %s"),cmdRebase, url, dlg.m_RemoteBranchName);
 		CProgressDlg progress;
 		progress.m_GitCmd = cmd;
 		progress.m_bAutoCloseOnSuccess = true;

src/TortoiseProc/PullFetchDlg.h
@@ -29,6 +29,7 @@ protected:
 public:
 	BOOL        m_IsPull;
     BOOL        m_bAutoLoad;
+	BOOL		m_bRebase;
     BOOL        m_bAutoLoadEnable;
     CHyperLink  m_RemoteManage;
 
@@ -37,4 +38,5 @@ public:
 	CString m_RemoteURL;
 	CString m_RemoteBranchName;
     afx_msg void OnStnClickedRemoteManage();
+	afx_msg void OnBnClickedButtonBrowseRef();
 };

commit 37d175f9425992363bb5acfaa219bb7af210b64a

Fixed issue #874: Add --rebase option to pull dialog
"--rebase" option is introduced by 25bd4c961830590ffdb51202e935fd168b7b2484,
but further work is needed.
So, the "Launch Rebase After Fetch" checkbox is hidden from Pull dialog.

For the conflict reason, the Rebase dialog uses cherry pick (instead of "--rebase" option) to perform rebase function.
And CLI option "--rebase" cannot be used. (never and ever)

Therefor, if "Launch Rebase After Fetch" checkbox is checked in Pull dialog,
then "git.exe pull --rebase" is adapted to "Fetch & Rebase".

Signed-off-by: Yue Lin Ho <b8732003@student.nsysu.edu.tw>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -68,6 +68,8 @@
 #include "ProgressCommands/FetchProgressCommand.h"
 #include "ProgressCommands/SendMailProgressCommand.h"
 
+static bool DoFetch(const CString& url, const bool fetchAllRemotes, const bool loadPuttyAgent, const int prune, const bool bDepth, const int nDepth, const int fetchTags, const CString& remoteBranch, const boolean runRebase);
+
 CAppUtils::CAppUtils(void)
 {
 }
@@ -2186,6 +2188,18 @@ bool CAppUtils::Pull(bool showPush)
 	dlg.m_IsPull = TRUE;
 	if (dlg.DoModal() == IDOK)
 	{
+		// "git.exe pull --rebase" is not supported, never and ever. So, adapting it to Fetch & Rebase.
+		if (dlg.m_bRebase)
+			return DoFetch(dlg.m_RemoteURL,
+							FALSE, // Fetch all remotes
+							dlg.m_bAutoLoad == BST_CHECKED,
+							dlg.m_bPrune,
+							dlg.m_bDepth == BST_CHECKED,
+							dlg.m_nDepth,
+							dlg.m_bFetchTags,
+							dlg.m_RemoteBranchName,
+							TRUE); // Rebase after fetching
+
 		CString url = dlg.m_RemoteURL;
 
 		if (dlg.m_bAutoLoad)
@@ -2209,9 +2223,6 @@ bool CAppUtils::Pull(bool showPush)
 		CString depth;
 		CString notags;
 		CString prune;
-
-		if (dlg.m_bRebase)
-			cmdRebase = "--rebase ";
 
 		if (!dlg.m_bFetchTags)
 			notags = _T("--no-tags");

src/TortoiseProc/PullFetchDlg.cpp
@@ -136,15 +136,7 @@ BOOL CPullFetchDlg::OnInitDialog()
 	if(!CAppUtils::IsSSHPutty())
 		m_bAutoLoad = false;
 
-	if(m_bAllowRebase)
-	{
-		this->m_bRebase = m_regRebase;
-	}
-	else
-	{
-		GetDlgItem(IDC_CHECK_REBASE)->ShowWindow(SW_HIDE);
-		this->m_bRebase = FALSE;
-	}
+	m_bRebase = m_bAllowRebase && m_regRebase;
 
 	this->UpdateData(FALSE);
 
@@ -163,7 +155,6 @@ BOOL CPullFetchDlg::OnInitDialog()
 
 	if(m_IsPull)
 	{
-		GetDlgItem(IDC_CHECK_REBASE)->ShowWindow(SW_HIDE);
 		// check tags checkbox and make it a normal checkbox
 		m_bFetchTags = 1;
 		m_bFFonly = m_regFFonly;