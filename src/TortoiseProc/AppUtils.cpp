TortoiseGit/TortoiseGit

commit dff41c2d0fca2d8b70d1f73582492e85371fd54e

Allow to resolve submodule conflicts and update list
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -1649,6 +1649,11 @@ bool CAppUtils::ConflictEdit(const CTGitPath& path, bool /*bAlternativeTool = fa
 		CSubmoduleResolveConflictDlg resolveSubmoduleConflictDialog;
 		resolveSubmoduleConflictDialog.SetDiff(merge.GetGitPathString(), revertTheirMy, baseHash, baseSubject, baseOK, localHash, mineSubject, mineOK, changeTypeMine, remoteHash, theirsSubject, theirsOK, changeTypeTheirs);
 		resolveSubmoduleConflictDialog.DoModal();
+		if (resolveSubmoduleConflictDialog.m_bResolved && resolveMsgHwnd)
+		{
+			static UINT WM_REVERTMSG = RegisterWindowMessage(_T("GITSLNM_NEEDSREFRESH"));
+			::PostMessage(resolveMsgHwnd, WM_REVERTMSG, NULL, NULL);
+		}
 
 		return TRUE;
 	}
@@ -3481,8 +3486,8 @@ void CAppUtils::ExploreTo(HWND hwnd, CString path)
 int CAppUtils::ResolveConflict(CTGitPath& path, resolve_with resolveWith)
 {
 	bool b_local = false, b_remote = false;
+	BYTE_VECTOR vector;
 	{
-		BYTE_VECTOR vector;
 		CString cmd;
 		cmd.Format(_T("git.exe ls-files -u -t -z -- \"%s\""), path.GetGitPathString());
 		if (g_Git.Run(cmd, &vector))
@@ -3509,6 +3514,57 @@ int CAppUtils::ResolveConflict(CTGitPath& path, resolve_with resolveWith)
 		}
 	}
 
+	if (path.IsDirectory()) // is submodule conflict
+	{
+		CString err = _T("We're sorry, but you hit a very rare conflict condition with a submodule which cannot be resolved by TortoiseGit. You have to use the command line git for this.");
+		if (b_local && b_remote)
+		{
+			if (!path.HasAdminDir()) // check if submodule is initialized
+			{
+				err += _T("\n\nYou have to checkout the submodule manually into \"") + path.GetGitPathString() + _T("\" and then reset HEAD to the right commit (see resolve submodule conflict dialog for this).");
+				MessageBox(nullptr, err, _T("TortoiseGit"), MB_ICONERROR);
+				return -1;
+			}
+			CGit subgit;
+			subgit.m_CurrentDir = g_Git.CombinePath(path);
+			CGitHash submoduleHead;
+			if (subgit.GetHash(submoduleHead, _T("HEAD")))
+			{
+				MessageBox(nullptr, err, _T("TortoiseGit"), MB_ICONERROR);
+				return -1;
+			}
+			CString baseHash, localHash, remoteHash;
+			ParseHashesFromLsFile(vector, baseHash, localHash, remoteHash);
+			if (resolveWith == RESOLVE_WITH_THEIRS && submoduleHead.ToString() != remoteHash)
+			{
+				CString origPath = g_Git.m_CurrentDir;
+				g_Git.m_CurrentDir = g_Git.CombinePath(path);
+				if (!GitReset(&remoteHash))
+				{
+					g_Git.m_CurrentDir = origPath;
+					return -1;
+				}
+				g_Git.m_CurrentDir = origPath;
+			}
+			else if (resolveWith == RESOLVE_WITH_MINE && submoduleHead.ToString() != localHash)
+			{
+				CString origPath = g_Git.m_CurrentDir;
+				g_Git.m_CurrentDir = g_Git.CombinePath(path);
+				if (!GitReset(&localHash))
+				{
+					g_Git.m_CurrentDir = origPath;
+					return -1;
+				}
+				g_Git.m_CurrentDir = origPath;
+			}
+		}
+		else
+		{
+			MessageBox(nullptr, err, _T("TortoiseGit"), MB_ICONERROR);
+			return -1;
+		}
+	}
+
 	if (resolveWith == RESOLVE_WITH_THEIRS)
 	{
 		CString gitcmd, output;
@@ -3524,7 +3580,7 @@ int CAppUtils::ResolveConflict(CTGitPath& path, resolve_with resolveWith)
 			return -1;
 		}
 	}
-	else if (resolveWith == RESOLVE_WITH_MINE)
+	else if (resolveWith == RESOLVE_WITH_THEIRS)
 	{
 		CString gitcmd, output;
 		if (b_local && b_remote)

src/TortoiseProc/SubmoduleResolveConflictDlg.cpp
@@ -33,6 +33,7 @@ CSubmoduleResolveConflictDlg::CSubmoduleResolveConflictDlg(CWnd* pParent /*=NULL
 	, m_nChangeTypeMine(CGitDiff::Unknown)
 	, m_nChangeTypeTheirs(CGitDiff::Unknown)
 	, m_bRevertTheirMy(false)
+	, m_bResolved(false)
 {
 }
 
@@ -210,13 +211,18 @@ void CSubmoduleResolveConflictDlg::OnBnClickedLog3()
 	ShowLog(m_sTheirsHash);
 }
 
-static void Resolve(const CString& path, bool useMine)
+void CSubmoduleResolveConflictDlg::Resolve(const CString& path, bool useMine)
 {
-	if (CMessageBox::Show(nullptr, IDS_PROC_RESOLVE, IDS_APPNAME, MB_ICONQUESTION | MB_YESNO) != IDYES)
+	if (CMessageBox::Show(GetSafeHwnd(), IDS_PROC_RESOLVE, IDS_APPNAME, MB_ICONQUESTION | MB_YESNO) != IDYES)
 		return;
 
 	CTGitPath gitpath(path);
-	CAppUtils::ResolveConflict(gitpath, useMine ? CAppUtils::RESOLVE_WITH_MINE : CAppUtils::RESOLVE_WITH_THEIRS);
+	gitpath.m_Action = CTGitPath::LOGACTIONS_UNMERGED;
+	if (CAppUtils::ResolveConflict(gitpath, useMine ? CAppUtils::RESOLVE_WITH_MINE : CAppUtils::RESOLVE_WITH_THEIRS) == 0)
+	{
+		m_bResolved = true;
+		EndDialog(0);
+	}
 }
 
 void CSubmoduleResolveConflictDlg::OnBnClickedButtonUpdate2()

src/TortoiseProc/SubmoduleResolveConflictDlg.h
@@ -33,10 +33,14 @@ public:
 
 	void SetDiff(const CString& path, bool revertTheirMy, const CString& baseHash, const CString& baseSubject, bool baseOK, const CString& mineHash, const CString& mineSubject, bool mineOK, CGitDiff::ChangeType mineChangeType, const CString& theirsHash, const CString& theirsSubject, bool theirsOK, CGitDiff::ChangeType theirsChangeType);
 
+	bool m_bResolved;
+
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	virtual BOOL OnInitDialog();
 	virtual HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+
+	void Resolve(const CString& path, bool useMine);
 
 	afx_msg void OnBnClickedLog();
 	afx_msg void OnBnClickedLog2();

commit 257a7bbc276b445fefd969afff8637a0e1de481d

Fix typo
Regression introduced in commit dff41c2d0fca2d8b70d1f73582492e85371fd54e.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -3579,7 +3579,7 @@ int CAppUtils::ResolveConflict(CTGitPath& path, resolve_with resolveWith)
 			return -1;
 		}
 	}
-	else if (resolveWith == RESOLVE_WITH_THEIRS)
+	else if (resolveWith == RESOLVE_WITH_MINE)
 	{
 		CString gitcmd, output;
 		if (b_local && b_remote)