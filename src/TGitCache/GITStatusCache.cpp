2be815e7bdaa1dc9411885feb3c06b91f5a1074b
Sven Strickroth <email@cs-ware.de>
2014-12-29 17:36:36 +0100

    Extend no-watch paths
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TGitCache/GITStatusCache.cpp
+++ src/TGitCache/GITStatusCache.cpp
@@ -247,6 +247,28 @@ CGitStatusCache::CGitStatusCache(void)
 	SecureZeroMemory(path, sizeof(path));
 	SHGetFolderPath(NULL, CSIDL_WINDOWS, NULL, 0, path);
 	m_NoWatchPaths[CTGitPath(CString(path))] = forever;
+	if (SysInfo::Instance().IsVistaOrLater())
+	{
+		CAutoLibrary hShell = AtlLoadSystemLibraryUsingFullPath(_T("shell32.dll"));
+		if (hShell)
+		{
+			typedef HRESULT STDAPICALLTYPE SHGetKnownFolderPathFN(__in REFKNOWNFOLDERID rfid, __in DWORD dwFlags, __in_opt HANDLE hToken, __deref_out PWSTR *ppszPath);
+			SHGetKnownFolderPathFN *pfnSHGetKnownFolderPath = (SHGetKnownFolderPathFN*)GetProcAddress(hShell, "SHGetKnownFolderPath");
+			if (pfnSHGetKnownFolderPath)
+			{
+				KNOWNFOLDERID folderids[] = { FOLDERID_Cookies, FOLDERID_History, FOLDERID_InternetCache, FOLDERID_Windows, FOLDERID_CDBurning, FOLDERID_Fonts, FOLDERID_RecycleBinFolder }; //FOLDERID_SearchHistory
+				for (KNOWNFOLDERID folderid : folderids)
+				{
+					PWSTR pszPath = NULL;
+					if (SHGetKnownFolderPath(folderid, KF_FLAG_CREATE, NULL, &pszPath) != S_OK)
+						continue;
+
+					m_NoWatchPaths[CTGitPath(CString(pszPath))] = forever;
+					CoTaskMemFree(pszPath);
+				}
+			}
+		}
+	}
 	m_bClearMemory = false;
 	m_mostRecentExpiresAt = 0;
 }
