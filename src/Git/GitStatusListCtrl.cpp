fa02b981edaa9068184c34225e5c0ea869b1b4fd
Sven Strickroth <email@cs-ware.de>
2015-01-01 23:38:50 +0100

    Correctly check if item was checked
    Number of checked files could be < 0. Fixes a regression of 9f87ca0d8a2e06718b023b3e138d4c29f193d663.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/Git/GitStatusListCtrl.cpp
+++ src/Git/GitStatusListCtrl.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2014 - TortoiseGit
+// Copyright (C) 2008-2015 - TortoiseGit
 // Copyright (C) 2003-2008, 2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -2257,7 +2257,7 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 										}
 										else
 										{
-											if (GetCheck(index))
+											if (GetCheck(nItem))
 												m_nSelected--;
 											RemoveListEntry(nItem);
 										}
