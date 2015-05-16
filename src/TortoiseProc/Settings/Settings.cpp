401e3e2f3c9f6194ffd20927befa8c04fb3bb705
Sven Strickroth <email@cs-ware.de>
2015-05-16 22:13:37 +0200

    Deduplicate code
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Settings/Settings.cpp
+++ src/TortoiseProc/Settings/Settings.cpp
@@ -213,68 +213,8 @@ void CSettings::HandleRestart()
 		DWORD_PTR res = 0;
 		::SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 0, SMTO_ABORTIFHUNG, 20, &res);
 		// tell the cache to refresh everything
-		HANDLE hPipe = CreateFile(
-			GetCacheCommandPipeName(),		// pipe name
-			GENERIC_READ |					// read and write access
-			GENERIC_WRITE,
-			0,								// no sharing
-			NULL,							// default security attributes
-			OPEN_EXISTING,					// opens existing pipe
-			FILE_FLAG_OVERLAPPED,			// default attributes
-			NULL);							// no template file
-
-
-		if (hPipe != INVALID_HANDLE_VALUE)
-		{
-			// The pipe connected; change to message-read mode.
-			DWORD dwMode;
-
-			dwMode = PIPE_READMODE_MESSAGE;
-			if (SetNamedPipeHandleState(
-				hPipe,    // pipe handle
-				&dwMode,  // new pipe mode
-				NULL,     // don't set maximum bytes
-				NULL))    // don't set maximum time
-			{
-				DWORD cbWritten;
-				TGITCacheCommand cmd;
-				SecureZeroMemory(&cmd, sizeof(TGITCacheCommand));
-				cmd.command = TGITCACHECOMMAND_REFRESHALL;
-				BOOL fSuccess = WriteFile(
-					hPipe,			// handle to pipe
-					&cmd,			// buffer to write from
-					sizeof(cmd),	// number of bytes to write
-					&cbWritten,		// number of bytes written
-					NULL);			// not overlapped I/O
-
-				if (! fSuccess || sizeof(cmd) != cbWritten)
-				{
-					DisconnectNamedPipe(hPipe);
-					CloseHandle(hPipe);
-					hPipe = INVALID_HANDLE_VALUE;
-				}
-				if (hPipe != INVALID_HANDLE_VALUE)
-				{
-					// now tell the cache we don't need it's command thread anymore
-					SecureZeroMemory(&cmd, sizeof(TGITCacheCommand));
-					cmd.command = TGITCACHECOMMAND_END;
-					WriteFile(
-						hPipe,			// handle to pipe
-						&cmd,			// buffer to write from
-						sizeof(cmd),	// number of bytes to write
-						&cbWritten,		// number of bytes written
-						NULL);			// not overlapped I/O
-					DisconnectNamedPipe(hPipe);
-					CloseHandle(hPipe);
-					hPipe = INVALID_HANDLE_VALUE;
-				}
-			}
-			else
-			{
-				ATLTRACE("SetNamedPipeHandleState failed");
-				CloseHandle(hPipe);
-			}
-		}
+		SendCacheCommand(TGITCACHECOMMAND_REFRESHALL);
+		SendCacheCommand(TGITCACHECOMMAND_END);
 	}
 }
 
