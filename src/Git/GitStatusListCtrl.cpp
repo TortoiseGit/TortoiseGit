TortoiseGit/TortoiseGit

commit 15cddb9388e854b0f4dc3e0615c11244f5c2d0db

Introduce MISSING file status
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -1058,6 +1058,8 @@ void CGitStatusListCtrl::AddEntry(CTGitPath * GitPath, WORD /*langID*/, int list
 		m_nShownAdded++;
 		break;
 	case CTGitPath::LOGACTIONS_DELETED:
+	case CTGitPath::LOGACTIONS_MISSING:
+	case CTGitPath::LOGACTIONS_DELETED | CTGitPath::LOGACTIONS_MISSING:
 		m_nShownDeleted++;
 		break;
 	case CTGitPath::LOGACTIONS_REPLACED:
@@ -1126,7 +1128,7 @@ void CGitStatusListCtrl::AddEntry(CTGitPath * GitPath, WORD /*langID*/, int list
 	{
 		CString modificationDate;
 		__int64 filetime = GitPath->GetLastWriteTime();
-		if (filetime && (GitPath->m_Action != CTGitPath::LOGACTIONS_DELETED))
+		if (filetime && !(GitPath->m_Action & CTGitPath::LOGACTIONS_DELETED))
 		{
 			FILETIME* f = (FILETIME*)(__int64*)&filetime;
 			modificationDate = CLoglistUtils::FormatDateAndTime(CTime(g_Git.filetime_to_time_t(f)), DATE_SHORTDATE, true, relativeTimes);
@@ -4236,7 +4238,7 @@ void CGitStatusListCtrl::FilesExport()
 	{
 		CTGitPath *fd = (CTGitPath*)GetItemData(index);
 		// we cannot export directories or folders
-		if (fd->m_Action == CTGitPath::LOGACTIONS_DELETED || fd->IsDirectory())
+		if ((fd->m_Action & CTGitPath::LOGACTIONS_DELETED) || fd->IsDirectory())
 			continue;
 
 		CAppUtils::CreateMultipleDirectory(exportDir + _T("\\") + fd->GetContainingDirectory().GetWinPathString());

Languages/Tortoise.pot
@@ -6215,6 +6215,10 @@ msgstr ""
 msgid "Misc"
 msgstr ""
 
+#. Resource IDs: (20120)
+msgid "Missing"
+msgstr ""
+
 #. Resource IDs: (1422, 62179)
 msgid "Mixed"
 msgstr ""

src/Git/Git.cpp
@@ -3298,6 +3298,8 @@ int CGit::GetWorkingTreeChanges(CTGitPathList& result, bool amend, CTGitPathList
 			CTGitPath* p = result.LookForGitPath(deletelist[j].GetGitPathString());
 			if (!p)
 				result.AddPath(deletelist[j]);
+			else
+				p->m_Action |= CTGitPath::LOGACTIONS_MISSING;
 		}
 	}
 

src/Git/TGitPath.cpp
@@ -1311,7 +1311,7 @@ int CTGitPathList::ParserFromLog(BYTE_VECTOR &log, bool parseDeletes /*false*/)
 				{
 					path.m_StatAdd=_T("0");
 					path.m_StatDel=_T("0");
-					path.m_Action |= CTGitPath::LOGACTIONS_DELETED;
+					path.m_Action |= CTGitPath::LOGACTIONS_DELETED | CTGitPath::LOGACTIONS_MISSING;
 				}
 				else
 				{
@@ -1680,6 +1680,8 @@ CString CTGitPath::GetActionName(int action)
 		return MAKEINTRESOURCE(IDS_PATHACTIONS_CONFLICT);
 	if(action  & CTGitPath::LOGACTIONS_ADDED)
 		return MAKEINTRESOURCE(IDS_PATHACTIONS_ADD);
+	if (action & CTGitPath::LOGACTIONS_MISSING)
+		return MAKEINTRESOURCE(IDS_PATHACTIONS_MISSING);
 	if(action  & CTGitPath::LOGACTIONS_DELETED)
 		return MAKEINTRESOURCE(IDS_PATHACTIONS_DELETE);
 	if(action  & CTGitPath::LOGACTIONS_MERGED )

src/Git/TGitPath.h
@@ -46,6 +46,7 @@ public:
 		LOGACTIONS_MERGED   = 0x00000080,
 		LOGACTIONS_ASSUMEVALID = 0x00000200,
 		LOGACTIONS_SKIPWORKTREE = 0x00000400,
+		LOGACTIONS_MISSING  = 0x00001000,
 		LOGACTIONS_UNVER	= 0x80000000,
 		LOGACTIONS_IGNORE	= 0x40000000,
 

src/Resources/LoglistCommonResource.h
@@ -137,3 +137,4 @@
 #define IDS_LOG_POPUP_EXPORT            20117
 #define IDS_LOG_POPUP_REVERTTOPARENT    20118
 #define IDS_CONFIRMRESETCOLUMNORDER     20119
+#define IDS_PATHACTIONS_MISSING         20120

src/Resources/TortoiseLoglistCommon.rc2
@@ -122,6 +122,7 @@ BEGIN
     IDS_PATHACTIONS_SKIP    "Skip"
     IDS_PATHACTIONS_UNKNOWN "Unknown"
     IDS_PATHACTIONS_IGNORED "Ignored"
+    IDS_PATHACTIONS_MISSING "Missing"
     IDS_PATHACTIONS_ASSUMEUNCHANGED "Assume valid/unchanged"
     IDS_PATHACTIONS_SKIPWORKTREE "Skip worktree"
 END

src/TortoiseProc/CommitDlg.cpp
@@ -790,7 +790,7 @@ void CCommitDlg::OnOK()
 					mgtReAddAfterCommit.AddFile(*entry);
 					mgtReDelAfterCommit.AddFile(entry->GetGitOldPathString());
 				}
-				else if (entry->m_Action & CTGitPath::LOGACTIONS_DELETED)
+				else if (entry->m_Action & CTGitPath::LOGACTIONS_DELETED && !(entry->m_Action & CTGitPath::LOGACTIONS_MISSING))
 					mgtReDelAfterCommit.AddFile(entry->GetGitPathString());
 
 				if (sysProgressDlg.HasUserCancelled())
@@ -853,7 +853,7 @@ void CCommitDlg::OnOK()
 				else if (!(entry->m_Action & CTGitPath::LOGACTIONS_UNVER))
 				{
 					mgtReset.AddFile(entry->GetGitPathString());
-					if (entry->m_Action & CTGitPath::LOGACTIONS_DELETED)
+					if (entry->m_Action & CTGitPath::LOGACTIONS_DELETED && !(entry->m_Action & CTGitPath::LOGACTIONS_MISSING))
 						mgtReDelAfterCommit.AddFile(entry->GetGitPathString());
 				}
 			}

commit b41012b904bf1311beee05aed6ec9e30412f3575

Warn if files exist in index but not in working tree
Fixes a regression introduced in 15cddb9388e854b0f4dc3e0615c11244f5c2d0db.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -3940,7 +3940,7 @@ int CGitStatusListCtrl::UpdateFileList(CTGitPathList *list)
 		CTGitPath * gitpatch=(CTGitPath*)&m_StatusFileList[i];
 		gitpatch->m_Checked = TRUE;
 
-		if ((gitpatch->m_Action == CTGitPath::LOGACTIONS_ADDED || gitpatch->m_Action == CTGitPath::LOGACTIONS_REPLACED) && !gitpatch->Exists())
+		if ((gitpatch->m_Action & (CTGitPath::LOGACTIONS_ADDED | CTGitPath::LOGACTIONS_REPLACED)) && !gitpatch->Exists())
 		{
 			if (!bDeleteChecked)
 			{