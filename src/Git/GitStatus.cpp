TortoiseGit/TortoiseGit

commit cdf0105b6877ee708a6fc3565250cfa51edb82ac

TGitCache: optimized overlays for locally deleted and also for ignored files
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatus.cpp
@@ -238,7 +238,9 @@ git_wc_status_kind GitStatus::GetAllStatus(const CTGitPath& path, git_depth_t de
 	if(isDir)
 	{
 		err = GetDirStatus(sProjectRoot,sSubPath,&statuskind, isfull,bIsRecursive,isfull,NULL, NULL);
-
+		// folders must not be displayed as added or deleted only as modified (this is for Shell Overlay-Modes)
+		if (statuskind == git_wc_status_deleted || statuskind == git_wc_status_added)
+			statuskind = git_wc_status_modified;
 	}
 	else
 	{
@@ -1044,7 +1046,11 @@ int GitStatus::LoadIgnoreFile(const CString &gitdir,const CString &subpaths)
 }
 int GitStatus::IsUnderVersionControl(const CString &gitdir, const CString &path, bool isDir,bool *isVersion)
 {
-	return g_IndexFileMap.IsUnderVersionControl(gitdir, path, isDir, isVersion);
+	if (g_IndexFileMap.IsUnderVersionControl(gitdir, path, isDir, isVersion))
+		return 1;
+	if (!*isVersion)
+		return g_HeadFileMap.IsUnderVersionControl(gitdir, path, isDir, isVersion);
+	return 0;
 }
 
 __int64 GitStatus::GetIndexFileTime(const CString &gitdir)
@@ -1203,7 +1209,7 @@ int GitStatus::EnumDirStatus(const CString &gitdir,const CString &subpath,git_wc
 				}
 				else if(pos <0 && posintree>=0) /* check if file delete in index */
 				{
-					*status = git_wc_status_modified;
+					*status = git_wc_status_deleted;
 					if(callback)
 						callback(gitdir+_T("/")+casepath, *status, bIsDir,pData);
 
@@ -1285,7 +1291,7 @@ int GitStatus::EnumDirStatus(const CString &gitdir,const CString &subpath,git_wc
 						oldstring = filename;
 						if(SearchInSortVector(filelist, filename.GetBuffer(), filename.GetLength())<0)
 						{
-							*status = git_wc_status_modified;
+							*status = git_wc_status_deleted;
 							if(callback)
 								callback(gitdir+_T("/")+(*it).m_FileName, *status, false,pData);
 						}
@@ -1439,7 +1445,7 @@ int GitStatus::GetDirStatus(const CString &gitdir,const CString &subpath,git_wc_
 
 								if(pos < 0)
 								{
-									*status = max(git_wc_status_modified, *status); // added file found
+									*status = max(git_wc_status_added, *status); // added file found
 									if(callback)
 									{
 										int dirpos = (*it).m_FileName.Find(_T('/'), path.GetLength());
@@ -1474,7 +1480,7 @@ int GitStatus::GetDirStatus(const CString &gitdir,const CString &subpath,git_wc_
 								pos = SearchInSortVector(*treeptr, lowcasepathBuffer, lowcasepath.GetLength());
 								if(pos <0)
 								{
-									*status = max(git_wc_status_modified, *status); // added file found
+									*status = max(git_wc_status_added, *status); // added file found
 
 								}
 								else
@@ -1489,7 +1495,7 @@ int GitStatus::GetDirStatus(const CString &gitdir,const CString &subpath,git_wc_
 										if( SearchInSortVector(*indexptr,(*hit).m_FileName.GetBuffer(),-1) < 0)
 										{
 											(*hit).m_FileName.ReleaseBuffer();
-											*status = max(git_wc_status_modified, *status); // deleted file found
+											*status = max(git_wc_status_deleted, *status); // deleted file found
 											break;
 										}
 										(*hit).m_FileName.ReleaseBuffer();
@@ -1535,9 +1541,6 @@ int GitStatus::GetDirStatus(const CString &gitdir,const CString &subpath,git_wc_
 							git_wc_status_kind filestatus = git_wc_status_none;
 
 							GetFileStatus(gitdir,(*it).m_FileName, &filestatus,IsFul, IsRecursive,IsIgnore, callback,pData);
-
-							if (filestatus > git_wc_status_normal && filestatus != git_wc_status_conflicted)
-								*status = git_wc_status_modified; // folders can only be modified or conflicted
 						}
 					}
 				}

src/Git/GitIndex.cpp
@@ -1364,6 +1364,61 @@ bool CGitHeadFileMap::CheckHeadUpdate(const CString &gitdir)
 	return false;
 }
 
+int CGitHeadFileMap::IsUnderVersionControl(const CString &gitdir, const CString &path, bool isDir, bool *isVersion)
+{
+	try
+	{
+		if (path.IsEmpty())
+		{
+			*isVersion = true;
+			return 0;
+		}
+
+		CString subpath = path;
+		subpath.Replace(_T('\\'), _T('/'));
+		if(isDir)
+			subpath += _T('/');
+
+		subpath.MakeLower();
+
+		CheckHeadUpdate(gitdir);
+
+		SHARED_TREE_PTR treeptr;
+		treeptr = SafeGet(gitdir);
+
+		if (treeptr->m_Head != treeptr->m_TreeHash)
+		{
+			treeptr->ReadHeadHash(gitdir);
+
+			// Init Repository
+			if (treeptr->m_HeadFile.IsEmpty())
+			{
+				*isVersion = false;
+				return 0;
+			}
+			else if (treeptr->ReadTree())
+			{
+				treeptr->m_LastModifyTimeHead = 0;
+				*isVersion = false;
+				return 1;
+			}
+			SafeSet(gitdir, treeptr);
+		}
+
+		if(isDir)
+			*isVersion = (SearchInSortVector(*treeptr, subpath.GetBuffer(), subpath.GetLength()) >= 0);
+		else
+			*isVersion = (SearchInSortVector(*treeptr, subpath.GetBuffer(), -1) >= 0);
+		subpath.ReleaseBuffer();
+	}
+	catch(...)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
 int CGitHeadFileMap::GetHeadHash(const CString &gitdir, CGitHash &hash)
 {
 	SHARED_TREE_PTR ptr;

src/Git/gitindex.h
@@ -391,6 +391,7 @@ public:
 						bool isLoaded=false);
 	bool CheckHeadUpdate(const CString &gitdir);
 	int GetHeadHash(const CString &gitdir, CGitHash &hash);
+	int IsUnderVersionControl(const CString &gitdir, const CString &path, bool isDir, bool *isVersion);
 
 
 	bool IsHashChanged(const CString &gitdir)

src/TGitCache/CachedDirectory.cpp
@@ -214,6 +214,12 @@ CStatusCacheEntry CCachedDirectory::GetStatusFromCache(const CTGitPath& path, bo
 	}
 	else
 	{
+		//All file ignored if under ignore directory
+		if (m_ownStatus.GetEffectiveStatus() == git_wc_status_ignored)
+			return CStatusCacheEntry(git_wc_status_ignored);
+		if (m_ownStatus.GetEffectiveStatus() == git_wc_status_unversioned)
+			return CStatusCacheEntry(git_wc_status_unversioned);
+
 		// Look up a file in our own cache
 		AutoLocker lock(m_critSec);
 		CString strCacheKey = GetCacheKey(path);
@@ -234,12 +240,6 @@ CStatusCacheEntry CCachedDirectory::GetStatusFromCache(const CTGitPath& path, bo
 					}
 				}
 			}
-		}
-		else
-		{
-			//All file ignored if under ignore directory
-			if( m_currentFullStatus == git_wc_status_ignored)
-				return CStatusCacheEntry(git_wc_status_ignored);
 		}
 
 		CGitStatusCache::Instance().AddFolderForCrawling(path);
@@ -589,14 +589,17 @@ BOOL CCachedDirectory::GetStatusCallback(const CString & path, git_wc_status_kin
 					// Add any versioned directory, which is not our 'self' entry, to the list for having its status updated
 //OutputDebugStringA("AddFolderCrawl: ");OutputDebugStringW(svnPath.GetWinPathString());OutputDebugStringA("\r\n");
 					if(status >= git_wc_status_normal)
-						if(status != git_wc_status_missing)
-							if(status != git_wc_status_deleted)
-								CGitStatusCache::Instance().AddFolderForCrawling(gitPath);
+						CGitStatusCache::Instance().AddFolderForCrawling(gitPath);
 				}
 
 				// Make sure we know about this child directory
 				// This initial status value is likely to be overwritten from below at some point
 				git_wc_status_kind s = GitStatus::GetMoreImportant(status2->text_status, status2->prop_status);
+
+				// folders must not be displayed as added or deleted only as modified
+				if (s == git_wc_status_deleted || s == git_wc_status_added)
+					s = git_wc_status_modified;
+
 				CCachedDirectory * cdir = CGitStatusCache::Instance().GetDirectoryCacheEntryNoCreate(gitPath);
 				if (cdir)
 				{
@@ -894,10 +897,12 @@ void CCachedDirectory::UpdateChildDirectoryStatus(const CTGitPath& childDir, git
 CStatusCacheEntry CCachedDirectory::GetOwnStatus(bool bRecursive)
 {
 	// Don't return recursive status if we're unversioned ourselves.
-	if(bRecursive && m_ownStatus.GetEffectiveStatus() > git_wc_status_unversioned)
+	if(bRecursive && m_ownStatus.GetEffectiveStatus() > git_wc_status_unversioned && m_ownStatus.GetEffectiveStatus() != git_wc_status_ignored)
 	{
 		CStatusCacheEntry recursiveStatus(m_ownStatus);
 		UpdateCurrentStatus();
+		if (m_currentFullStatus == git_wc_status_deleted || m_currentFullStatus == git_wc_status_added)
+			m_currentFullStatus = git_wc_status_modified;
 		recursiveStatus.ForceStatus(m_currentFullStatus);
 		return recursiveStatus;
 	}

commit 7198f294cdd7a49ca77882cc77b1ac0c740b7fde

Shell(Extended): Fix mismatch of directory and file status
Only staged changes were considered for the calculation of the overlay icons for directories.

Regression introduced in cdf0105b6877ee708a6fc3565250cfa51edb82ac.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatus.cpp
@@ -785,6 +785,14 @@ int GitStatus::GetDirStatus(const CString &gitdir, const CString &subpath, git_w
 							bool assumeValid = false;
 							bool skipWorktree = false;
 							GetFileStatus(gitdir, (*it).m_FileName, &filestatus, IsFul, IsRecursive, IsIgnore, callback, pData, &assumeValid, &skipWorktree);
+							switch (filestatus)
+							{
+							case git_wc_status_added:
+							case git_wc_status_modified:
+							case git_wc_status_deleted:
+							case git_wc_status_conflicted:
+								*status = GetMoreImportant(filestatus, *status);
+							}
 						}
 					}
 				}