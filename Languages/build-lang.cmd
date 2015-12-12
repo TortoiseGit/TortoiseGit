7d6a21762fd82403dc80f271eeeb79c55d49ce5b
Sven Strickroth <email@cs-ware.de>
2015-12-13 00:16:23 +0100

    Canonicalize translations
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- Languages/build-lang.cmd
+++ Languages/build-lang.cmd
@@ -7,20 +7,20 @@ if %OUTDIR:~-1%==\ set OUTDIR=%OUTDIR:~0,-1%
 set CLEAN=
 if [%2]==[clean] set CLEAN="clean"
 
-call :nmake 1026 bg_BG
+call :nmake 1026 bg
 call :nmake 1027 ca
 call :nmake 1028 zh_TW
 call :nmake 1029 cs
 call :nmake 1031 de
-call :nmake 1034 es_ES
+call :nmake 1034 es
 call :nmake 1035 fi
 call :nmake 1036 fr
 call :nmake 1038 hu
-call :nmake 1040 it_IT
+call :nmake 1040 it
 call :nmake 1041 ja
-call :nmake 1042 ko_KR
-call :nmake 1043 nl_NL
-call :nmake 1045 pl_PL
+call :nmake 1042 ko
+call :nmake 1043 nl
+call :nmake 1045 pl
 call :nmake 1046 pt_BR
 call :nmake 1048 ro
 call :nmake 1049 ru
