TortoiseGit/TortoiseGit

commit 7844a7a63ce591736af3bee856f4a72b6f101d52

Refactoring: Reduce indention
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/MiscUI/IconBitmapUtils.cpp
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2016 - TortoiseGit
 // Copyright (C) 2009-2012, 2014 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -28,8 +29,8 @@ IconBitmapUtils::IconBitmapUtils()
 
 IconBitmapUtils::~IconBitmapUtils()
 {
-	for (const auto& bitmap : bitmaps)
-		::DeleteObject(bitmap.second);
+    for (const auto& bitmap : bitmaps)
+        ::DeleteObject(bitmap.second);
     bitmaps.clear();
 }
 
@@ -42,6 +43,7 @@ HBITMAP IconBitmapUtils::IconToBitmap(HINSTANCE hInst, UINT uIcon)
     HICON hIcon = (HICON)LoadImage(hInst, MAKEINTRESOURCE(uIcon), IMAGE_ICON, 12, 12, LR_DEFAULTCOLOR);
     if (!hIcon)
         return NULL;
+    SCOPE_EXIT { DestroyIcon(hIcon); };
 
     RECT rect;
 
@@ -52,46 +54,28 @@ HBITMAP IconBitmapUtils::IconToBitmap(HINSTANCE hInst, UINT uIcon)
 
     HWND desktop = ::GetDesktopWindow();
     if (desktop == NULL)
-    {
-        DestroyIcon(hIcon);
         return NULL;
-    }
 
     HDC screen_dev = ::GetDC(desktop);
     if (screen_dev == NULL)
-    {
-        DestroyIcon(hIcon);
         return NULL;
-    }
+    SCOPE_EXIT { ::ReleaseDC(desktop, screen_dev); };
 
     // Create a compatible DC
     HDC dst_hdc = ::CreateCompatibleDC(screen_dev);
     if (dst_hdc == NULL)
-    {
-        DestroyIcon(hIcon);
-        ::ReleaseDC(desktop, screen_dev);
         return NULL;
-    }
+    SCOPE_EXIT { ::DeleteDC(dst_hdc); };
 
     // Create a new bitmap of icon size
     HBITMAP bmp = ::CreateCompatibleBitmap(screen_dev, rect.right, rect.bottom);
     if (bmp == NULL)
-    {
-        DestroyIcon(hIcon);
-        ::DeleteDC(dst_hdc);
-        ::ReleaseDC(desktop, screen_dev);
         return NULL;
-    }
 
     // Select it into the compatible DC
     HBITMAP old_dst_bmp = (HBITMAP)::SelectObject(dst_hdc, bmp);
     if (old_dst_bmp == NULL)
-    {
-        DestroyIcon(hIcon);
-        ::DeleteDC(dst_hdc);
-        ::ReleaseDC(desktop, screen_dev);
         return NULL;
-    }
 
     // Fill the background of the compatible DC with the white color
     // that is taken by menu routines as transparent
@@ -103,11 +87,8 @@ HBITMAP IconBitmapUtils::IconToBitmap(HINSTANCE hInst, UINT uIcon)
 
     // Restore settings
     ::SelectObject(dst_hdc, old_dst_bmp);
-    ::DeleteDC(dst_hdc);
-    ::ReleaseDC(desktop, screen_dev);
-    DestroyIcon(hIcon);
-    if (bmp)
-        bitmaps.insert(bitmap_it, std::make_pair(uIcon, bmp));
+
+    bitmaps.insert(bitmap_it, std::make_pair(uIcon, bmp));
     return bmp;
 }
 
@@ -140,44 +121,42 @@ HBITMAP IconBitmapUtils::IconToBitmapPARGB32(HICON hIcon)
 
     RECT rcIcon;
     SetRect(&rcIcon, 0, 0, sizIcon.cx, sizIcon.cy);
-    HBITMAP hBmp = NULL;
 
     HDC hdcDest = CreateCompatibleDC(NULL);
-    if (hdcDest)
+    if (!hdcDest)
+        return nullptr;
+    SCOPE_EXIT { DeleteDC(hdcDest); };
+
+    HBITMAP hBmp = nullptr;
+    if (FAILED(Create32BitHBITMAP(hdcDest, &sizIcon, NULL, &hBmp)))
+        return nullptr;
+
+    HBITMAP hbmpOld = (HBITMAP)SelectObject(hdcDest, hBmp);
+    if (!hbmpOld)
+        return hBmp;
+
+    BLENDFUNCTION bfAlpha = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
+    BP_PAINTPARAMS paintParams = { 0 };
+    paintParams.cbSize = sizeof(paintParams);
+    paintParams.dwFlags = BPPF_ERASE;
+    paintParams.pBlendFunction = &bfAlpha;
+
+    HDC hdcBuffer;
+    HPAINTBUFFER hPaintBuffer = BeginBufferedPaint(hdcDest, &rcIcon, BPBF_DIB, &paintParams, &hdcBuffer);
+    if (hPaintBuffer)
     {
-        if (SUCCEEDED(Create32BitHBITMAP(hdcDest, &sizIcon, NULL, &hBmp)))
+        if (DrawIconEx(hdcBuffer, 0, 0, hIcon, sizIcon.cx, sizIcon.cy, 0, NULL, DI_NORMAL))
         {
-            HBITMAP hbmpOld = (HBITMAP)SelectObject(hdcDest, hBmp);
-            if (hbmpOld)
-            {
-                BLENDFUNCTION bfAlpha = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
-                BP_PAINTPARAMS paintParams = {0};
-                paintParams.cbSize = sizeof(paintParams);
-                paintParams.dwFlags = BPPF_ERASE;
-                paintParams.pBlendFunction = &bfAlpha;
-
-                HDC hdcBuffer;
-                HPAINTBUFFER hPaintBuffer = BeginBufferedPaint(hdcDest, &rcIcon, BPBF_DIB, &paintParams, &hdcBuffer);
-                if (hPaintBuffer)
-                {
-                    if (DrawIconEx(hdcBuffer, 0, 0, hIcon, sizIcon.cx, sizIcon.cy, 0, NULL, DI_NORMAL))
-                    {
-                        // If icon did not have an alpha channel we need to convert buffer to PARGB
-                        ConvertBufferToPARGB32(hPaintBuffer, hdcDest, hIcon, sizIcon);
-                    }
-
-                    // This will write the buffer contents to the destination bitmap
-                    EndBufferedPaint(hPaintBuffer, TRUE);
-                }
-
-                SelectObject(hdcDest, hbmpOld);
-            }
+            // If icon did not have an alpha channel we need to convert buffer to PARGB
+            ConvertBufferToPARGB32(hPaintBuffer, hdcDest, hIcon, sizIcon);
         }
 
-        DeleteDC(hdcDest);
+        // This will write the buffer contents to the destination bitmap
+        EndBufferedPaint(hPaintBuffer, TRUE);
     }
 
-    return hBmp;
+    SelectObject(hdcDest, hbmpOld);
+	return hBmp;
 }
 
 HRESULT IconBitmapUtils::Create32BitHBITMAP(HDC hdc, const SIZE *psize, __deref_opt_out void **ppvBits, __out HBITMAP* phBmp) const
@@ -216,26 +195,25 @@ HRESULT IconBitmapUtils::ConvertBufferToPARGB32(HPAINTBUFFER hPaintBuffer, HDC h
     RGBQUAD *prgbQuad;
     int cxRow;
     HRESULT hr = GetBufferedPaintBits(hPaintBuffer, &prgbQuad, &cxRow);
-    if (SUCCEEDED(hr))
+    if (FAILED(hr))
+        return hr;
+
+    Gdiplus::ARGB *pargb = reinterpret_cast<Gdiplus::ARGB *>(prgbQuad);
+    if (HasAlpha(pargb, sizIcon, cxRow))
+        return S_OK;
+
+    ICONINFO info;
+    if (!GetIconInfo(hicon, &info))
+        return S_OK;
+    SCOPE_EXIT
     {
-        Gdiplus::ARGB *pargb = reinterpret_cast<Gdiplus::ARGB *>(prgbQuad);
-        if (!HasAlpha(pargb, sizIcon, cxRow))
-        {
-            ICONINFO info;
-            if (GetIconInfo(hicon, &info))
-            {
-                if (info.hbmMask)
-                {
-                    hr = ConvertToPARGB32(hdc, pargb, info.hbmMask, sizIcon, cxRow);
-                }
+        DeleteObject(info.hbmColor);
+        DeleteObject(info.hbmMask);
+    };
+    if (info.hbmMask)
+        return ConvertToPARGB32(hdc, pargb, info.hbmMask, sizIcon, cxRow);
 
-                DeleteObject(info.hbmColor);
-                DeleteObject(info.hbmMask);
-            }
-        }
-    }
-
-    return hr;
+    return S_OK;
 }
 
 bool IconBitmapUtils::HasAlpha(__in Gdiplus::ARGB *pargb, SIZE& sizImage, int cxRow) const
@@ -272,34 +250,31 @@ HRESULT IconBitmapUtils::ConvertToPARGB32(HDC hdc, __inout Gdiplus::ARGB *pargb,
     void *pvBits = HeapAlloc(hHeap, 0, bmi.bmiHeader.biWidth * 4 * bmi.bmiHeader.biHeight);
     if (pvBits == 0)
         return E_OUTOFMEMORY;
+    SCOPE_EXIT { HeapFree(hHeap, 0, pvBits); };
 
-    HRESULT hr = E_UNEXPECTED;
-    if (GetDIBits(hdc, hbmp, 0, bmi.bmiHeader.biHeight, pvBits, &bmi, DIB_RGB_COLORS) == bmi.bmiHeader.biHeight)
+    if (!GetDIBits(hdc, hbmp, 0, bmi.bmiHeader.biHeight, pvBits, &bmi, DIB_RGB_COLORS) == bmi.bmiHeader.biHeight)
+        return E_UNEXPECTED;
+
+    ULONG cxDelta = cxRow - bmi.bmiHeader.biWidth;
+    Gdiplus::ARGB *pargbMask = static_cast<Gdiplus::ARGB *>(pvBits);
+
+    for (ULONG y = bmi.bmiHeader.biHeight; y; --y)
     {
-        ULONG cxDelta = cxRow - bmi.bmiHeader.biWidth;
-        Gdiplus::ARGB *pargbMask = static_cast<Gdiplus::ARGB *>(pvBits);
-
-        for (ULONG y = bmi.bmiHeader.biHeight; y; --y)
+        for (ULONG x = bmi.bmiHeader.biWidth; x; --x)
         {
-            for (ULONG x = bmi.bmiHeader.biWidth; x; --x)
+            if (*pargbMask++)
             {
-                if (*pargbMask++)
-                {
-                    // transparent pixel
-                    *pargb++ = 0;
-                }
-                else
-                {
-                    // opaque pixel
-                    *pargb++ |= 0xFF000000;
-                }
+                // transparent pixel
+                *pargb++ = 0;
             }
-            pargb += cxDelta;
+            else
+            {
+                // opaque pixel
+                *pargb++ |= 0xFF000000;
+            }
         }
-
-        hr = S_OK;
+        pargb += cxDelta;
     }
-    HeapFree(hHeap, 0, pvBits);
 
-    return hr;
+    return S_OK;
 }

src/TortoiseShell/ItemIDList.cpp
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2016 - TortoiseGit
 // Copyright (C) 2003-2006, 2009, 2011-2013, 2015-2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -88,67 +89,50 @@ tstring ItemIDList::toString(bool resolveLibraries /*= true*/)
 	CComPtr<IShellFolder> parentFolder;
 	tstring ret;
 
-	HRESULT hr = ::SHGetDesktopFolder(&shellFolder);
-	if (FAILED(hr))
+	if (FAILED(::SHGetDesktopFolder(&shellFolder)))
 		return ret;
-	if (parent_)
-	{
-		hr = shellFolder->BindToObject(parent_, 0, IID_IShellFolder, (void**) &parentFolder);
-		if (FAILED(hr))
-			parentFolder = shellFolder;
-	}
-	else
-	{
+	if (!parent_ || FAILED(shellFolder->BindToObject(parent_, 0, IID_IShellFolder, (void**)&parentFolder)))
 		parentFolder = shellFolder;
-	}
 
 	STRRET name;
-	TCHAR * szDisplayName = NULL;
+	TCHAR* szDisplayName = nullptr;
 	if ((parentFolder != 0)&&(item_ != 0))
 	{
-		hr = parentFolder->GetDisplayNameOf(item_, SHGDN_NORMAL | SHGDN_FORPARSING, &name);
-		if (FAILED(hr))
+		if (FAILED(parentFolder->GetDisplayNameOf(item_, SHGDN_NORMAL | SHGDN_FORPARSING, &name)))
 			return ret;
-		hr = StrRetToStr (&name, item_, &szDisplayName);
-		if (FAILED(hr))
+		if (FAILED(StrRetToStr(&name, item_, &szDisplayName)))
 			return ret;
 	}
-	if (szDisplayName == NULL)
-	{
+	if (!szDisplayName)
 		return ret;
-	}
 	ret = szDisplayName;
 	CoTaskMemFree(szDisplayName);
-	if ((resolveLibraries) &&
-		(_tcsncmp(ret.c_str(), _T("::{"), 3)==0))
+
+	if (!((resolveLibraries) && (_tcsncmp(ret.c_str(), _T("::{"), 3) == 0)))
+		return ret;
+
+	CComPtr<IShellLibrary> plib;
+	if (FAILED(plib.CoCreateInstance(CLSID_ShellLibrary, nullptr, CLSCTX_INPROC_SERVER)))
+		return ret;
+
+	CComPtr<IShellItem> psiLibrary;
+	if (FAILED(SHCreateItemFromParsingName(ret.c_str(), nullptr, IID_PPV_ARGS(&psiLibrary))))
+		return ret;
+
+	if (FAILED(plib->LoadLibraryFromItem(psiLibrary, STGM_READ | STGM_SHARE_DENY_NONE)))
+		return ret;
+
+	CComPtr<IShellItem> psiSaveLocation;
+	if (FAILED(plib->GetDefaultSaveFolder(DSFT_DETECT, IID_PPV_ARGS(&psiSaveLocation))))
+		return ret;
+
+	PWSTR pszName = nullptr;
+	if (SUCCEEDED(psiSaveLocation->GetDisplayName(SIGDN_FILESYSPATH, &pszName)))
 	{
-		CComPtr<IShellLibrary> plib;
-		hr = plib.CoCreateInstance(CLSID_ShellLibrary, nullptr, CLSCTX_INPROC_SERVER);
-		if (SUCCEEDED(hr))
-		{
-			CComPtr<IShellItem> psiLibrary;
-			hr = SHCreateItemFromParsingName(ret.c_str(), nullptr, IID_PPV_ARGS(&psiLibrary));
-			if (SUCCEEDED(hr))
-			{
-				hr = plib->LoadLibraryFromItem(psiLibrary, STGM_READ | STGM_SHARE_DENY_NONE);
-				if (SUCCEEDED(hr))
-				{
-					CComPtr<IShellItem> psiSaveLocation;
-					hr = plib->GetDefaultSaveFolder(DSFT_DETECT, IID_PPV_ARGS(&psiSaveLocation));
-					if (SUCCEEDED(hr))
-					{
-						PWSTR pszName = nullptr;
-						hr = psiSaveLocation->GetDisplayName(SIGDN_FILESYSPATH, &pszName);
-						if (SUCCEEDED(hr))
-						{
-							ret = pszName;
-							CoTaskMemFree(pszName);
-						}
-					}
-				}
-			}
-		}
+		ret = pszName;
+		CoTaskMemFree(pszName);
 	}
+
 	return ret;
 }
 

src/Utils/CommonAppUtils.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2013, 2015 - TortoiseGit
+// Copyright (C) 2008-2013, 2015-2016 - TortoiseGit
 // Copyright (C) 2003-2008,2010 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -39,10 +39,8 @@ bool CCommonAppUtils::LaunchApplication(const CString& sCommandLine, UINT idErrM
 	if (uac)
 	{
 		CString file, param;
-		SHELLEXECUTEINFO shellinfo;
-		memset(&shellinfo, 0, sizeof(shellinfo));
+		SHELLEXECUTEINFO shellinfo = { 0 };
 		shellinfo.cbSize = sizeof(shellinfo);
-		shellinfo.hwnd = NULL;
 		shellinfo.lpVerb = _T("runas");
 		shellinfo.nShow = SW_SHOWNORMAL;
 		shellinfo.fMask = SEE_MASK_NOCLOSEPROCESS;
@@ -95,43 +93,37 @@ bool CCommonAppUtils::LaunchApplication(const CString& sCommandLine, UINT idErrM
 		}
 
 		if (bWaitForStartup)
-		{
 			WaitForInputIdle(shellinfo.hProcess, 10000);
-		}
 
 		CloseHandle(shellinfo.hProcess);
+
+		return true;
 	}
-	else
+
+	STARTUPINFO startup = { 0 };
+	PROCESS_INFORMATION process = { 0 };
+	startup.cb = sizeof(startup);
+
+	CString cleanCommandLine(sCommandLine);
+	if (CreateProcess(nullptr, const_cast<TCHAR*>((LPCTSTR)cleanCommandLine), nullptr, nullptr, FALSE, 0, nullptr, theCWD, &startup, &process) == 0)
 	{
-		STARTUPINFO startup;
-		PROCESS_INFORMATION process;
-		memset(&startup, 0, sizeof(startup));
-		startup.cb = sizeof(startup);
-		memset(&process, 0, sizeof(process));
-
-		CString cleanCommandLine(sCommandLine);
-
-		if (CreateProcess(NULL, const_cast<TCHAR*>((LPCTSTR)cleanCommandLine), NULL, NULL, FALSE, 0, 0, theCWD, &startup, &process)==0)
+		if (idErrMessageFormat)
 		{
-			if(idErrMessageFormat != 0)
-			{
-				CString temp;
-				temp.Format(idErrMessageFormat, (LPCTSTR)CFormatMessageWrapper());
-				MessageBox(NULL, temp, _T("TortoiseGit"), MB_OK | MB_ICONINFORMATION);
-			}
-			return false;
+			CString temp;
+			temp.Format(idErrMessageFormat, (LPCTSTR)CFormatMessageWrapper());
+			MessageBox(nullptr, temp, _T("TortoiseGit"), MB_OK | MB_ICONINFORMATION);
 		}
-
-		AllowSetForegroundWindow(process.dwProcessId);
-
-		if (bWaitForStartup)
-		{
-			WaitForInputIdle(process.hProcess, 10000);
-		}
-
-		CloseHandle(process.hThread);
-		CloseHandle(process.hProcess);
+		return false;
 	}
+
+	AllowSetForegroundWindow(process.dwProcessId);
+
+	if (bWaitForStartup)
+		WaitForInputIdle(process.hProcess, 10000);
+
+	CloseHandle(process.hThread);
+	CloseHandle(process.hProcess);
+
 	return true;
 }
 
@@ -163,14 +155,13 @@ bool CCommonAppUtils::IsAdminLogin()
 	// Initialize SID.
 	if (!AllocateAndInitializeSid(&ntAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &administratorsGroup))
 		return false;
+	SCOPE_EXIT { FreeSid(administratorsGroup); };
 
 	// Check whether the token is present in admin group.
 	BOOL isInAdminGroup = FALSE;
 	if (!CheckTokenMembership(NULL, administratorsGroup, &isInAdminGroup))
-		isInAdminGroup = FALSE;
+		return false;
 
-	// Free SID and return.
-	FreeSid(administratorsGroup);
 	return !!isInAdminGroup;
 }
 
@@ -184,47 +175,41 @@ bool CCommonAppUtils::SetListCtrlBackgroundImage(HWND hListCtrl, UINT nID, int w
 	HICON hIcon = (HICON)LoadImage(AfxGetResourceHandle(), MAKEINTRESOURCE(nID), IMAGE_ICON, width, height, LR_DEFAULTCOLOR);
 	if (!hIcon)
 		return false;
+	SCOPE_EXIT { DestroyIcon(hIcon); };
 
 	RECT rect = {0};
 	rect.right = width;
 	rect.bottom = height;
-	HBITMAP bmp = NULL;
 
 	HWND desktop = ::GetDesktopWindow();
-	if (desktop)
-	{
-		HDC screen_dev = ::GetDC(desktop);
-		if (screen_dev)
-		{
-			// Create a compatible DC
-			HDC dst_hdc = ::CreateCompatibleDC(screen_dev);
-			if (dst_hdc)
-			{
-				// Create a new bitmap of icon size
-				bmp = ::CreateCompatibleBitmap(screen_dev, rect.right, rect.bottom);
-				if (bmp)
-				{
-					// Select it into the compatible DC
-					HBITMAP old_dst_bmp = (HBITMAP)::SelectObject(dst_hdc, bmp);
-					// Fill the background of the compatible DC with the given color
-					::SetBkColor(dst_hdc, bkColor);
-					::ExtTextOut(dst_hdc, 0, 0, ETO_OPAQUE, &rect, NULL, 0, NULL);
-
-					// Draw the icon into the compatible DC
-					::DrawIconEx(dst_hdc, 0, 0, hIcon, rect.right, rect.bottom, 0, NULL, DI_NORMAL);
-					::SelectObject(dst_hdc, old_dst_bmp);
-				}
-				::DeleteDC(dst_hdc);
-			}
-			::ReleaseDC(desktop, screen_dev);
-		}
-	}
-
-	// Restore settings
-	DestroyIcon(hIcon);
-
-	if (bmp == NULL)
+	if (!desktop)
 		return false;
+
+	HDC screen_dev = ::GetDC(desktop);
+	if (!screen_dev)
+		return false;
+	SCOPE_EXIT { ::ReleaseDC(desktop, screen_dev); };
+
+	// Create a compatible DC
+	HDC dst_hdc = ::CreateCompatibleDC(screen_dev);
+	if (!dst_hdc)
+		return false;
+	SCOPE_EXIT { ::DeleteDC(dst_hdc); };
+
+	// Create a new bitmap of icon size
+	HBITMAP bmp = ::CreateCompatibleBitmap(screen_dev, rect.right, rect.bottom);
+	if (!bmp)
+		return false;
+
+	// Select it into the compatible DC
+	HBITMAP old_dst_bmp = (HBITMAP)::SelectObject(dst_hdc, bmp);
+	// Fill the background of the compatible DC with the given color
+	::SetBkColor(dst_hdc, bkColor);
+	::ExtTextOut(dst_hdc, 0, 0, ETO_OPAQUE, &rect, NULL, 0, NULL);
+
+	// Draw the icon into the compatible DC
+	::DrawIconEx(dst_hdc, 0, 0, hIcon, rect.right, rect.bottom, 0, NULL, DI_NORMAL);
+	::SelectObject(dst_hdc, old_dst_bmp);
 
 	LVBKIMAGE lv;
 	lv.ulFlags = LVBKIF_TYPE_WATERMARK;
@@ -286,17 +271,12 @@ bool CCommonAppUtils::FileOpenSave(CString& path, int * filterindex, UINT title,
 	else
 		ofn.Flags = OFN_OVERWRITEPROMPT | OFN_EXPLORER;
 
-
 	// Display the Open dialog box.
 	bool bRet = false;
 	if (bOpen)
-	{
 		bRet = !!GetOpenFileName(&ofn);
-	}
 	else
-	{
 		bRet = !!GetSaveFileName(&ofn);
-	}
 	SetCurrentDirectory(sOrigCWD);
 	if (bRet)
 	{

src/Utils/MiscUI/BrowseFolder.cpp
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2016 - TortoiseGit
 // Copyright (C) 2003-2014, 2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -61,7 +62,6 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, LPTSTR path, size_t pathl
 }
 CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CString& sDefaultPath /* = CString() */)
 {
-	retVal ret = OK;		//assume OK
 	m_sDefaultPath = sDefaultPath;
 	if (m_sDefaultPath.IsEmpty() && !path.IsEmpty())
 	{
@@ -82,115 +82,88 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
 		m_sDefaultPath = path;
 	}
 
-	HRESULT hr;
-
 	// Create a new common open file dialog
 	CComPtr<IFileOpenDialog> pfd;
-	hr = pfd.CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER);
-	if (SUCCEEDED(hr))
+	if (FAILED(pfd.CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER)))
 	{
-		// Set the dialog as a folder picker
-		DWORD dwOptions;
-		if (SUCCEEDED(hr = pfd->GetOptions(&dwOptions)))
-		{
-			hr = pfd->SetOptions(dwOptions | FOS_PICKFOLDERS | FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST);
-		}
-
-		// Set a title
-		if (SUCCEEDED(hr))
-		{
-			TCHAR * nl = _tcschr(m_title, '\n');
-			if (nl)
-				*nl = 0;
-			pfd->SetTitle(m_title);
-		}
-
-		// set the default folder
-		if (SUCCEEDED(hr))
-		{
-			CComPtr<IShellItem> psiDefault;
-			hr = SHCreateItemFromParsingName(m_sDefaultPath, nullptr, IID_PPV_ARGS(&psiDefault));
-			if (SUCCEEDED(hr))
-			{
-				hr = pfd->SetFolder(psiDefault);
-			}
-		}
-
-		if (m_CheckText[0] != 0)
-		{
-			CComPtr<IFileDialogCustomize> pfdCustomize;
-			hr = pfd.QueryInterface(&pfdCustomize);
-			if (SUCCEEDED(hr))
-			{
-				pfdCustomize->StartVisualGroup(100, L"");
-				pfdCustomize->AddCheckButton(101, m_CheckText, FALSE);
-				if (m_CheckText2[0] != 0)
-				{
-					pfdCustomize->AddCheckButton(102, m_CheckText2, FALSE);
-				}
-				pfdCustomize->EndVisualGroup();
-			}
-		}
-
-		// Show the open file dialog
-		if (SUCCEEDED(hr) && SUCCEEDED(hr = pfd->Show(parent)))
-		{
-			// Get the selection from the user
-			CComPtr<IShellItem> psiResult;
-			hr = pfd->GetResult(&psiResult);
-			if (SUCCEEDED(hr))
-			{
-				PWSTR pszPath = NULL;
-				hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszPath);
-				if (SUCCEEDED(hr))
-				{
-					path = pszPath;
-					CoTaskMemFree(pszPath);
-				}
-
-				CComPtr<IFileDialogCustomize> pfdCustomize;
-				hr = pfd.QueryInterface(&pfdCustomize);
-				if (SUCCEEDED(hr))
-				{
-					pfdCustomize->GetCheckButtonState(101, &m_bCheck);
-					pfdCustomize->GetCheckButtonState(102, &m_bCheck2);
-				}
-			}
-			else
-				ret = CANCEL;
-		}
-		else
-			ret = CANCEL;
-	}
-	else
-	{
-		BROWSEINFO browseInfo		= {};
-		browseInfo.hwndOwner		= parent;
-		browseInfo.pidlRoot			= m_root;
-		browseInfo.pszDisplayName	= m_displayName;
-		browseInfo.lpszTitle		= m_title;
-		browseInfo.ulFlags			= m_style;
-		browseInfo.lParam			= (LPARAM)this;
-		browseInfo.lpfn				= BrowseCallBackProc;
+		BROWSEINFO browseInfo = {};
+		browseInfo.hwndOwner = parent;
+		browseInfo.pidlRoot = m_root;
+		browseInfo.pszDisplayName = m_displayName;
+		browseInfo.lpszTitle = m_title;
+		browseInfo.ulFlags = m_style;
+		browseInfo.lParam = (LPARAM)this;
+		browseInfo.lpfn = BrowseCallBackProc;
 
 		PCIDLIST_ABSOLUTE itemIDList = SHBrowseForFolder(&browseInfo);
-
 		//is the dialog canceled?
 		if (!itemIDList)
-			ret = CANCEL;
+			return CANCEL;
+		SCOPE_EXIT{ CoTaskMemFree((LPVOID)itemIDList); };
 
-		if (ret != CANCEL)
-		{
-			if (!SHGetPathFromIDList(itemIDList, path.GetBuffer(MAX_PATH)))		// MAX_PATH ok. Explorer can't handle paths longer than MAX_PATH.
-				ret = NOPATH;
+		if (!SHGetPathFromIDList(itemIDList, CStrBuf(path, MAX_PATH)))		// MAX_PATH ok. Explorer can't handle paths longer than MAX_PATH.
+			return NOPATH;
 
-			path.ReleaseBuffer();
-
-			CoTaskMemFree((LPVOID)itemIDList);
-		}
+		return OK;
 	}
 
-	return ret;
+	// Set the dialog as a folder picker
+	DWORD dwOptions;
+	if (FAILED(pfd->GetOptions(&dwOptions)))
+		return CANCEL;
+	if (FAILED(pfd->SetOptions(dwOptions | FOS_PICKFOLDERS | FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST)))
+		return CANCEL;
+
+	// Set a title
+	TCHAR* nl = _tcschr(m_title, '\n');
+	if (nl)
+		*nl = 0;
+	pfd->SetTitle(m_title);
+
+	// set the default folder
+	CComPtr<IShellItem> psiDefault;
+	if (FAILED(SHCreateItemFromParsingName(m_sDefaultPath, nullptr, IID_PPV_ARGS(&psiDefault))))
+		return CANCEL;
+	if (FAILED(pfd->SetFolder(psiDefault)))
+		return CANCEL;
+
+	if (m_CheckText[0] != 0)
+	{
+		CComPtr<IFileDialogCustomize> pfdCustomize;
+		if (FAILED(pfd.QueryInterface(&pfdCustomize)))
+			return CANCEL;
+
+		pfdCustomize->StartVisualGroup(100, L"");
+		pfdCustomize->AddCheckButton(101, m_CheckText, FALSE);
+		if (m_CheckText2[0] != 0)
+			pfdCustomize->AddCheckButton(102, m_CheckText2, FALSE);
+		pfdCustomize->EndVisualGroup();
+	}
+
+	// Show the open file dialog
+	if (FAILED(pfd->Show(parent)))
+		return CANCEL;
+
+	// Get the selection from the user
+	CComPtr<IShellItem> psiResult;
+	if (FAILED(pfd->GetResult(&psiResult)))
+		return CANCEL;
+
+	PWSTR pszPath = nullptr;
+	if (SUCCEEDED(psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszPath)))
+	{
+		path = pszPath;
+		CoTaskMemFree(pszPath);
+	}
+
+	CComPtr<IFileDialogCustomize> pfdCustomize;
+	if (SUCCEEDED(pfd.QueryInterface(&pfdCustomize)))
+	{
+		pfdCustomize->GetCheckButtonState(101, &m_bCheck);
+		pfdCustomize->GetCheckButtonState(102, &m_bCheck2);
+	}
+
+	return OK;
 }
 
 void CBrowseFolder::SetInfo(LPCTSTR title)

commit 3fd7fb7a08511b631d17044dfc728e60a2b5cada

Fix typo
Introduced in 7844a7a63ce591736af3bee856f4a72b6f101d52.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/MiscUI/IconBitmapUtils.cpp
@@ -252,7 +252,7 @@ HRESULT IconBitmapUtils::ConvertToPARGB32(HDC hdc, __inout Gdiplus::ARGB *pargb,
         return E_OUTOFMEMORY;
     SCOPE_EXIT { HeapFree(hHeap, 0, pvBits); };
 
-    if (!GetDIBits(hdc, hbmp, 0, bmi.bmiHeader.biHeight, pvBits, &bmi, DIB_RGB_COLORS) == bmi.bmiHeader.biHeight)
+    if (GetDIBits(hdc, hbmp, 0, bmi.bmiHeader.biHeight, pvBits, &bmi, DIB_RGB_COLORS) != bmi.bmiHeader.biHeight)
         return E_UNEXPECTED;
 
     ULONG cxDelta = cxRow - bmi.bmiHeader.biWidth;