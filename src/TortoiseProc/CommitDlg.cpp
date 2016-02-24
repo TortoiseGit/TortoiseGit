b8a248b859d4622b80a0c49a84c9028942c75c02
Sup Yut Sum <ch3cooli@gmail.com>
2016-02-24 20:16:19 +0800

    Disable commit button when commit message is empty or full of whitespaces
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

--- src/TortoiseProc/CommitDlg.cpp
+++ src/TortoiseProc/CommitDlg.cpp
@@ -2244,7 +2244,8 @@ LRESULT CCommitDlg::OnUpdateOKButton(WPARAM, LPARAM)
 	if (m_bBlock)
 		return 0;
 
-	bool bValidLogSize = !m_cLogMessage.GetText().IsEmpty() && m_cLogMessage.GetText().GetLength() >= m_ProjectProperties.nMinLogSize;
+	CString text = m_cLogMessage.GetText().Trim();
+	bool bValidLogSize = !text.IsEmpty() && text.GetLength() >= m_ProjectProperties.nMinLogSize;
 	bool bAmendOrSelectFilesOrMerge = m_ListCtrl.GetSelected() > 0 || (m_bCommitAmend && m_bAmendDiffToLastCommit) || CTGitPath(g_Git.m_CurrentDir).IsMergeActive();
 
 	DialogEnableWindow(IDOK, bValidLogSize && (m_bCommitMessageOnly || bAmendOrSelectFilesOrMerge));
