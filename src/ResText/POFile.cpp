1884a7016cabd6f689b95cdfe7f96cd53ef8f27b
Sven Strickroth <email@cs-ware.de>
2016-05-13 01:12:17 +0200

    Make sure the string is not used as a format string but provide our own
    Based on TortoiseSVN revision 27315.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/ResText/POFile.cpp
+++ src/ResText/POFile.cpp
@@ -1,7 +1,7 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2012-2013 - TortoiseGit
-// Copyright (C) 2003-2008, 2011-2015 - TortoiseSVN
+// Copyright (C) 2003-2008, 2011-2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -161,7 +161,7 @@ BOOL CPOFile::ParseFile(LPCTSTR szPath, BOOL bUpdateExisting, bool bAdjustEOLs)
 			entry.push_back(line.get());
 		}
 	} while (File.gcount() > 0);
-	printf(File.getloc().name().c_str());
+	printf("%s", File.getloc().name().c_str());
 	File.close();
 	RESOURCEENTRY emptyentry = {0};
 	(*this)[std::wstring(L"")] = emptyentry;
