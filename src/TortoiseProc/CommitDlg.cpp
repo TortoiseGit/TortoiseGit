TortoiseGit/TortoiseGit

commit 1275ee25a7dd6edb1da7f988516211b52197f5b1

Fixed issue #2798: Commit message autocompletion doesn't work with UTF-8 encoded files
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/CommitDlg.cpp
@@ -40,6 +40,7 @@
 #include "LogDlg.h"
 #include "BstrSafeVector.h"
 #include "StringUtils.h"
+#include "FileTextLines.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -1833,24 +1834,59 @@ void CCommitDlg::ScanFile(const CString& sFilePath, const CString& sRegex, const
 			return;
 		}
 		// allocate memory to hold file contents
-		auto buffer = std::make_unique<char[]>(size);
-		DWORD readbytes;
-		if (!ReadFile(hFile, buffer.get(), size, &readbytes, nullptr))
-			return;
-		int opts = 0;
-		IsTextUnicode(buffer.get(), readbytes, &opts);
-		if (opts & IS_TEXT_UNICODE_NULL_BYTES)
-			return;
-		if (opts & IS_TEXT_UNICODE_UNICODE_MASK)
-			sFileContent = std::wstring((wchar_t*)buffer.get(), readbytes / sizeof(WCHAR));
-		if ((opts & IS_TEXT_UNICODE_NOT_UNICODE_MASK) || (opts == 0))
+		CBuffer oFile;
+		try
 		{
-			const int ret = MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, (LPCSTR)buffer.get(), readbytes, nullptr, 0);
-			auto pWideBuf = std::make_unique<wchar_t[]>(ret);
-			const int ret2 = MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, (LPCSTR)buffer.get(), readbytes, pWideBuf.get(), ret);
-			if (ret2 == ret)
-				sFileContent = std::wstring(pWideBuf.get(), ret);
+			oFile.SetLength(size);
 		}
+		catch (CMemoryException*)
+		{
+			return;
+		}
+		DWORD readbytes;
+		if (!ReadFile(hFile, oFile, size, &readbytes, nullptr))
+			return;
+		oFile.SetLength(readbytes);
+		CFileTextLines filetextlines;
+		CFileTextLines::UnicodeType type = filetextlines.CheckUnicodeType(oFile, readbytes);
+		try
+		{
+			CBaseFilter* pFilter = nullptr;
+			switch (type)
+			{
+			case CFileTextLines::BINARY:
+				return;
+			case CFileTextLines::UTF8:
+			case CFileTextLines::UTF8BOM:
+				pFilter = new CUtf8Filter(NULL);
+				break;
+			default:
+			case CFileTextLines::ASCII:
+				pFilter = new CAsciiFilter(NULL);
+				break;
+			case CFileTextLines::UTF16_BE:
+			case CFileTextLines::UTF16_BEBOM:
+				pFilter = new CUtf16beFilter(NULL);
+				break;
+			case CFileTextLines::UTF16_LE:
+			case CFileTextLines::UTF16_LEBOM:
+				pFilter = new CUtf16leFilter(NULL);
+				break;
+			case CFileTextLines::UTF32_BE:
+				pFilter = new CUtf32beFilter(NULL);
+				break;
+			case CFileTextLines::UTF32_LE:
+				pFilter = new CUtf32leFilter(NULL);
+				break;
+			}
+			pFilter->Decode(oFile);
+			delete pFilter;
+		}
+		catch (CMemoryException*)
+		{
+			return;
+		}
+		sFileContent = std::wstring((wchar_t*)oFile, oFile.GetLength());
 	}
 	if (sFileContent.empty() || !m_bRunThread)
 		return;

src/TortoiseProc/TortoiseProc.vcxproj
@@ -40,7 +40,7 @@
   <PropertyGroup Label="UserMacros" />
   <ItemDefinitionGroup>
     <ClCompile>
-      <AdditionalIncludeDirectories>$(ProjectDir);..\Resources;$InputDir;..\..\ext\ResizableLib;..\Git;..\Utils;..\..\ext\hunspell;..\..\ext\scintilla\include;..\Utils\TreePropSheet;..\Utils\MiscUI;..\TortoiseShell;..\..\ext\gitdll;..\..\ext\libgit2\include;..\..\ext\zlib;..\..\ext\OGDF;..\AsyncFramework;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>$(ProjectDir);..\Resources;$InputDir;..\..\ext\ResizableLib;..\Git;..\Utils;..\..\ext\hunspell;..\..\ext\scintilla\include;..\Utils\TreePropSheet;..\Utils\MiscUI;..\TortoiseShell;..\..\ext\gitdll;..\..\ext\libgit2\include;..\..\ext\zlib;..\..\ext\OGDF;..\AsyncFramework;..\TortoiseMerge;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>HUNSPELL_STATIC;THESAURUS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ExceptionHandling>Async</ExceptionHandling>
     </ClCompile>
@@ -62,6 +62,7 @@
     <ClCompile Include="..\Git\GitStatus.cpp" />
     <ClCompile Include="..\Git\MassiveGitTaskBase.cpp" />
     <ClCompile Include="..\Git\TGitPath.cpp" />
+    <ClCompile Include="..\TortoiseMerge\FileTextLines.cpp" />
     <ClCompile Include="..\Utils\ACEdit.cpp" />
     <ClCompile Include="..\Utils\ACListWnd.cpp" />
     <ClCompile Include="..\Utils\BugTraqAssociations.cpp" />
@@ -319,6 +320,7 @@
     <ClInclude Include="..\Git\MassiveGitTaskBase.h" />
     <ClInclude Include="..\Git\TGitPath.h" />
     <ClInclude Include="..\Resources\LoglistCommonResource.h" />
+    <ClInclude Include="..\TortoiseMerge\FileTextLines.h" />
     <ClInclude Include="..\Utils\ACEdit.h" />
     <ClInclude Include="..\Utils\ACListWnd.h" />
     <ClInclude Include="..\Utils\BugTraqAssociations.h" />

src/TortoiseProc/TortoiseProc.vcxproj.filters
@@ -159,6 +159,9 @@
     <Filter Include="Utils\TreePropSheet">
       <UniqueIdentifier>{e772451b-ebd6-46da-b8e2-f4794840702a}</UniqueIdentifier>
     </Filter>
+    <Filter Include="TortoiseGitMerge">
+      <UniqueIdentifier>{69ffded7-2494-4d2b-b7d8-4681289c8b1e}</UniqueIdentifier>
+    </Filter>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="AddRemoteDlg.cpp">
@@ -907,6 +910,9 @@
     </ClCompile>
     <ClCompile Include="..\Git\GitDataObject.cpp">
       <Filter>Git</Filter>
+    </ClCompile>
+    <ClCompile Include="..\TortoiseMerge\FileTextLines.cpp">
+      <Filter>TortoiseGitMerge</Filter>
     </ClCompile>
   </ItemGroup>
   <ItemGroup>
@@ -1692,6 +1698,9 @@
     </ClInclude>
     <ClInclude Include="..\Git\GitDataObject.h">
       <Filter>Git</Filter>
+    </ClInclude>
+    <ClInclude Include="..\TortoiseMerge\FileTextLines.h">
+      <Filter>TortoiseGitMerge</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

commit e88ea6d841b5280397b98fdd4da7faa6f3a36a51

Use correct length for buffer
This fixes a regression introduced in commit 1275ee25a7dd6edb1da7f988516211b52197f5b1.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/CommitDlg.cpp
@@ -1886,7 +1886,7 @@ void CCommitDlg::ScanFile(const CString& sFilePath, const CString& sRegex, const
 		{
 			return;
 		}
-		sFileContent = std::wstring((wchar_t*)oFile, oFile.GetLength());
+		sFileContent = std::wstring((wchar_t*)oFile, oFile.GetLength() / sizeof(wchar_t));
 	}
 	if (sFileContent.empty() || !m_bRunThread)
 		return;