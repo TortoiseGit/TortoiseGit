TortoiseGit/TortoiseGit

commit 46b96924698a2160a3010abdfa49a940b3f5e03d

Provide more copy options
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/GitLogListBase.h
@@ -318,7 +318,6 @@ public:
 	ID_COMPARE = 1, // compare revision with WC
 	ID_SAVEAS,
 	ID_COMPARETWO, // compare two revisions
-	ID_COPY,
 	ID_REVERTREV,
 	ID_MERGEREV,
 	ID_GNUDIFF1, // compare with WC, unified
@@ -332,7 +331,14 @@ public:
 	ID_DIFF,
 	ID_OPENWITH,
 	ID_COPYCLIPBOARD,
-	ID_COPYHASH,
+	ID_COPYCLIPBOARDFULL,
+	ID_COPYCLIPBOARDFULLNOPATHS,
+	ID_COPYCLIPBOARDHASH,
+	ID_COPYCLIPBOARDAUTHORSFULL,
+	ID_COPYCLIPBOARDAUTHORSNAME,
+	ID_COPYCLIPBOARDAUTHORSEMAIL,
+	ID_COPYCLIPBOARDSUBJECTS,
+	ID_COPYCLIPBOARDMESSAGES,
 	ID_REVERTTOREV,
 	ID_BLAMECOMPARE,
 	ID_BLAMEDIFF,
@@ -365,7 +371,6 @@ public:
 	ID_PULL,
 	ID_FETCH,
 	ID_SHOWBRANCHES,
-	ID_COPYCLIPBOARDMESSAGES,
 	ID_BISECTSTART,
 	ID_LOG_VIEWRANGE,
 	ID_LOG_VIEWRANGE_REACHABLEFROMONLYONE,
@@ -378,13 +383,6 @@ public:
 	ID_BISECTSKIP,
 	ID_SVNDCOMMIT,
 	ID_COMPARETWOCOMMITCHANGES,
-	};
-	enum
-	{
-	ID_COPY_ALL,
-	ID_COPY_MESSAGE,
-	ID_COPY_SUBJECT,
-	ID_COPY_HASH,
 	};
 	enum FilterShow
 	{
@@ -408,7 +406,7 @@ public:
 	inline unsigned __int64 GetContextMenuBit(int i){ return ((unsigned __int64 )0x1)<<i ;}
 	static CString GetRebaseActionName(int action);
 	void InsertGitColumn();
-	void CopySelectionToClipBoard(int toCopy = ID_COPY_ALL);
+	void CopySelectionToClipBoard(int toCopy = ID_COPYCLIPBOARDFULL);
 	void DiffSelectedRevWithPrevious();
 	bool IsSelectionContinuous();
 protected:

Languages/Tortoise.pot
@@ -2073,12 +2073,24 @@ msgstr ""
 msgid "Author Email"
 msgstr ""
 
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_AUTHORSEMAIL)
+msgid "Author email addresses"
+msgstr ""
+
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_AUTHORSNAME)
+msgid "Author names"
+msgstr ""
+
 #. Resource IDs: (Dialog IDD_ABOUT: Control id 65535)
 msgid "Author:\t\t\t\tStefan Kueng"
 msgstr ""
 
 #. Resource IDs: (IDS_LOG_FILTER_AUTHORS)
 msgid "Authors"
+msgstr ""
+
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_AUTHORSFULL)
+msgid "Authors (full)"
 msgstr ""
 
 #. Resource IDs: (Dialog IDD_STATGRAPH: Control id 1265)
@@ -3152,10 +3164,6 @@ msgstr ""
 
 #. Resource IDs: (IDS_PROC_COPYRENAME)
 msgid "Copy and rename"
-msgstr ""
-
-#. Resource IDs: (IDS_LOG_POPUP_COPYTOCLIPBOARDMESSAGES)
-msgid "Copy log messages to clipboard"
 msgstr ""
 
 #. Resource IDs: (IDS_BLAME_POPUP_COPYLOGTOCLIPBOARD)
@@ -4935,6 +4943,14 @@ msgstr ""
 msgid "Full Screen"
 msgstr ""
 
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_FULLNOPATHS)
+msgid "Full commit data without changed paths"
+msgstr ""
+
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_FULL)
+msgid "Full data"
+msgstr ""
+
 #. Resource IDs: (Dialog IDD_FIND: Control id 20086)
 msgid "Full text search"
 msgstr ""
@@ -6345,7 +6361,7 @@ msgstr ""
 msgid "Message part &expression:"
 msgstr ""
 
-#. Resource IDs: (Dialog IDD_MERGE: Control id 1742, IDS_LOG_FILTER_MESSAGES)
+#. Resource IDs: (Dialog IDD_MERGE: Control id 1742, IDS_LOG_FILTER_MESSAGES, IDS_LOG_POPUP_CLIPBOARD_MSGS)
 msgid "Messages"
 msgstr ""
 
@@ -8920,7 +8936,7 @@ msgstr ""
 msgid "Show long format even if a tag is exactly on that commit"
 msgstr ""
 
-#. Resource IDs: (20308)
+#. Resource IDs: (25308)
 msgid "Show next change of selected commit\nShow next"
 msgstr ""
 
@@ -8952,7 +8968,7 @@ msgstr ""
 msgid "Show original line number"
 msgstr ""
 
-#. Resource IDs: (20309)
+#. Resource IDs: (25309)
 msgid "Show previous change of selected commit\nShow previous"
 msgstr ""
 
@@ -9297,6 +9313,10 @@ msgstr ""
 
 #. Resource IDs: (Dialog IDD_DIFFSUBMODULE: Control id 1714, Dialog IDD_DIFFSUBMODULE: Control id 1715, Dialog IDD_RESOLVESUBMODULECONFLICT: Control id 1714, Dialog IDD_RESOLVESUBMODULECONFLICT: Control id 1715, Dialog IDD_RESOLVESUBMODULECONFLICT: Control id 1717, Dialog IDD_SENDMAIL: Control id 65535)
 msgid "Subject:"
+msgstr ""
+
+#. Resource IDs: (IDS_LOG_POPUP_CLIPBOARD_SUBJECTS)
+msgid "Subjects"
 msgstr ""
 
 #. Resource IDs: (Dialog IDD_SYNC: Control id 1639)

src/Resources/LoglistCommonResource.h
@@ -30,7 +30,6 @@
 #define IDS_LOG_POPUP_OPEN				20014
 #define IDS_LOG_POPUP_OPENWITH			20015
 #define IDS_LOG_POPUP_BLAME				20016
-#define IDS_LOG_POPUP_COPYTOCLIPBOARDMESSAGES 20017
 #define IDS_LOG_POPUP_VIEWREV			20019
 #define IDS_LOG_POPUP_VIEWPATHREV		20020
 #define IDS_LOG_POPUP_REVERTTOREV 		20021
@@ -139,3 +138,11 @@
 #define IDS_LOG_POPUP_REVERTTOPARENT    20118
 #define IDS_CONFIRMRESETCOLUMNORDER     20119
 #define IDS_PATHACTIONS_MISSING         20120
+
+#define IDS_LOG_POPUP_CLIPBOARD_FULL    20121
+#define IDS_LOG_POPUP_CLIPBOARD_FULLNOPATHS 20122
+#define IDS_LOG_POPUP_CLIPBOARD_AUTHORSFULL 20123
+#define IDS_LOG_POPUP_CLIPBOARD_AUTHORSNAME 20124
+#define IDS_LOG_POPUP_CLIPBOARD_AUTHORSEMAIL 20125
+#define IDS_LOG_POPUP_CLIPBOARD_SUBJECTS 20126
+#define IDS_LOG_POPUP_CLIPBOARD_MSGS    20127

src/Resources/TortoiseLoglistCommon.rc2
@@ -42,7 +42,13 @@ STRINGTABLE
 BEGIN
     IDS_LOG_POPUP_REVERTREVS "&Revert changes by these commits"
     IDS_LOG_POPUP_COPYTOCLIPBOARD "Copy to clipboard"
-    IDS_LOG_POPUP_COPYTOCLIPBOARDMESSAGES "Copy log messages to clipboard"
+    IDS_LOG_POPUP_CLIPBOARD_FULL "Full data"
+    IDS_LOG_POPUP_CLIPBOARD_FULLNOPATHS "Full commit data without changed paths"
+    IDS_LOG_POPUP_CLIPBOARD_AUTHORSFULL "Authors (full)"
+    IDS_LOG_POPUP_CLIPBOARD_AUTHORSNAME "Author names"
+    IDS_LOG_POPUP_CLIPBOARD_AUTHORSEMAIL "Author email addresses"
+    IDS_LOG_POPUP_CLIPBOARD_SUBJECTS "Subjects"
+    IDS_LOG_POPUP_CLIPBOARD_MSGS "Messages"
     IDS_LOG_POPUP_REVERTTOREV "R&evert to this revision"
     IDS_LOG_POPUP_REVERTTOPARENT "Revert to parent revision"
     IDS_LOG_POPUP_EXPORT "Export selection to..."

src/TortoiseGitBlame/LogListBlameAction.cpp
@@ -37,7 +37,6 @@ void CGitBlameLogList::hideUnimplementedCommands()
 		GetContextMenuBit(ID_GNUDIFF1) |
 		GetContextMenuBit(ID_BLAMEPREVIOUS) |
 		GetContextMenuBit(ID_COPYCLIPBOARD) |
-		GetContextMenuBit(ID_COPYHASH) |
 		GetContextMenuBit(ID_EXPORT) |
 		GetContextMenuBit(ID_CREATE_BRANCH) |
 		GetContextMenuBit(ID_CREATE_TAG) |
@@ -127,15 +126,15 @@ void CGitBlameLogList::ContextMenuAction(int cmd, int /*FirstSelect*/, int /*Las
 				}
 			}
 			break;
-		case ID_COPYCLIPBOARD:
-			{
-				CopySelectionToClipBoard();
-			}
-			break;
-		case ID_COPYHASH:
-			{
-				CopySelectionToClipBoard(ID_COPY_HASH);
-			}
+		case ID_COPYCLIPBOARDFULL:
+		case ID_COPYCLIPBOARDFULLNOPATHS:
+		case ID_COPYCLIPBOARDHASH:
+		case ID_COPYCLIPBOARDAUTHORSFULL:
+		case ID_COPYCLIPBOARDAUTHORSNAME:
+		case ID_COPYCLIPBOARDAUTHORSEMAIL:
+		case ID_COPYCLIPBOARDSUBJECTS:
+		case ID_COPYCLIPBOARDMESSAGES:
+			CopySelectionToClipBoard(cmd & 0xFFFF);
 			break;
 		case ID_EXPORT:
 			RunTortoiseGitProcWithCurrentRev(L"export", pRev);

src/TortoiseGitBlame/Resource.h
@@ -48,16 +48,16 @@
 #define IDC_LOG                         1003
 #define IDC_STATIC_AUTHORS              1829
 #define ID_INDICATOR_ENCODING           2000
-#define IDS_STATUS_PANE1                20122
-#define IDS_STATUS_PANE2                20123
-#define IDS_TOOLBAR_STANDARD            20124
-#define IDS_TOOLBAR_CUSTOMIZE           20125
-#define IDS_OUTPUT_WND                  20157
-#define IDS_PROPERTIES_WND              20158
-#define IDS_EDIT_MENU                   20306
-#define IDS_GIT_LOG_TAB                 20307
-#define ID_VIEW_NEXT                    20308
-#define ID_VIEW_PREV                    20309
+#define IDS_STATUS_PANE1                25122
+#define IDS_STATUS_PANE2                25123
+#define IDS_TOOLBAR_STANDARD            25124
+#define IDS_TOOLBAR_CUSTOMIZE           25125
+#define IDS_OUTPUT_WND                  25157
+#define IDS_PROPERTIES_WND              25158
+#define IDS_EDIT_MENU                   25306
+#define IDS_GIT_LOG_TAB                 25307
+#define ID_VIEW_NEXT                    25308
+#define ID_VIEW_PREV                    25309
 #define ID_EDIT_ENCODING                32771
 #define ID_EDIT_ENCODE                  32772
 #define ID_ENCODE_AUTO                  32774

src/TortoiseGitBlame/TortoiseGitBlameView.cpp
@@ -1,6 +1,6 @@
 // TortoiseGitBlame - a Viewer for Git Blames
 
-// Copyright (C) 2008-2016 - TortoiseGit
+// Copyright (C) 2008-2017 - TortoiseGit
 // Copyright (C) 2003-2008, 2014 - TortoiseSVN
 
 // Copyright (C)2003 Don HO <donho@altern.org>
@@ -527,11 +527,11 @@ void CTortoiseGitBlameView::ContextMenuAction(int cmd, GitRev *pRev, GIT_REV_LIS
 		break;
 
 	case ID_COPYHASHTOCLIPBOARD:
-		this->GetLogList()->CopySelectionToClipBoard(CGitLogListBase::ID_COPY_HASH);
+		this->GetLogList()->CopySelectionToClipBoard(CGitLogListBase::ID_COPYCLIPBOARDHASH);
 		break;
 
 	case ID_COPYLOGTOCLIPBOARD:
-		this->GetLogList()->CopySelectionToClipBoard();
+		this->GetLogList()->CopySelectionToClipBoard(CGitLogListBase::ID_COPYCLIPBOARDFULL);
 		break;
 	}
 }

src/TortoiseProc/GitLogListAction.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2016 - TortoiseGit
+// Copyright (C) 2008-2017 - TortoiseGit
 // Copyright (C) 2005-2007 Marco Costalba
 
 // This program is free software; you can redistribute it and/or
@@ -490,22 +490,16 @@ void CGitLogList::ContextMenuAction(int cmd,int FirstSelect, int LastSelect, CMe
 				CAppUtils::RunTortoiseGitProc(cmdline);
 			}
 			break;
-		case ID_COPYCLIPBOARD:
-			{
-				CopySelectionToClipBoard();
-			}
-			break;
+		case ID_COPYCLIPBOARDFULL:
+		case ID_COPYCLIPBOARDFULLNOPATHS:
+		case ID_COPYCLIPBOARDHASH:
+		case ID_COPYCLIPBOARDAUTHORSFULL:
+		case ID_COPYCLIPBOARDAUTHORSNAME:
+		case ID_COPYCLIPBOARDAUTHORSEMAIL:
+		case ID_COPYCLIPBOARDSUBJECTS:
 		case ID_COPYCLIPBOARDMESSAGES:
 			{
-				if ((GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0)
-					CopySelectionToClipBoard(ID_COPY_SUBJECT);
-				else
-					CopySelectionToClipBoard(ID_COPY_MESSAGE);
-			}
-			break;
-		case ID_COPYHASH:
-			{
-				CopySelectionToClipBoard(ID_COPY_HASH);
+				CopySelectionToClipBoard(cmd & 0xFFFF);
 			}
 			break;
 		case ID_EXPORT:

commit b37eb1791001e376f93127a2e5559aed4ab7208c

Make sure IDs fit into bitmap
Commit 46b96924698a2160a3010abdfa49a940b3f5e03d caused a regression as e.g. ID_COMPARETWOCOMMITCHANGES did not match any more.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/GitLogListBase.h
@@ -331,14 +331,6 @@ public:
 	ID_DIFF,
 	ID_OPENWITH,
 	ID_COPYCLIPBOARD,
-	ID_COPYCLIPBOARDFULL,
-	ID_COPYCLIPBOARDFULLNOPATHS,
-	ID_COPYCLIPBOARDHASH,
-	ID_COPYCLIPBOARDAUTHORSFULL,
-	ID_COPYCLIPBOARDAUTHORSNAME,
-	ID_COPYCLIPBOARDAUTHORSEMAIL,
-	ID_COPYCLIPBOARDSUBJECTS,
-	ID_COPYCLIPBOARDMESSAGES,
 	ID_REVERTTOREV,
 	ID_BLAMECOMPARE,
 	ID_BLAMEDIFF,
@@ -383,6 +375,15 @@ public:
 	ID_BISECTSKIP,
 	ID_SVNDCOMMIT,
 	ID_COMPARETWOCOMMITCHANGES,
+	// the following can be >= 64 as those are not used for GetContextMenuBit(), all others must be < 64 in order to fit into __int64
+	ID_COPYCLIPBOARDFULL,
+	ID_COPYCLIPBOARDFULLNOPATHS,
+	ID_COPYCLIPBOARDHASH,
+	ID_COPYCLIPBOARDAUTHORSFULL,
+	ID_COPYCLIPBOARDAUTHORSNAME,
+	ID_COPYCLIPBOARDAUTHORSEMAIL,
+	ID_COPYCLIPBOARDSUBJECTS,
+	ID_COPYCLIPBOARDMESSAGES,
 	};
 	enum FilterShow
 	{