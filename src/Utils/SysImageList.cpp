TortoiseGit/TortoiseGit

commit 308f806f53f1a5ae312e02552c8c8440f8e4cc13

Don't mess with a system managed system image list
https://blogs.msdn.microsoft.com/oldnewthing/20170224-00/?p=95565

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/SysImageList.cpp
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2017 - TortoiseGit
 // Copyright (C) 2003-2006, 2008-2010 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -29,18 +30,21 @@ CSysImageList::CSysImageList()
 	SHFILEINFO ssfi;
 	TCHAR windir[MAX_PATH] = {0};
 	GetWindowsDirectory(windir, _countof(windir));  // MAX_PATH ok.
-	HIMAGELIST hSystemImageList =
+	hSystemImageList =
 		(HIMAGELIST)SHGetFileInfo(
 			windir,
 			0,
 			&ssfi, sizeof ssfi,
 			SHGFI_SYSICONINDEX | SHGFI_SMALLICON);
-	Attach(hSystemImageList);
+
+	int cx, cy;
+	ImageList_GetIconSize(hSystemImageList, &cx, &cy);
+	auto emptyImageList = ImageList_Create(cx, cy, ILC_COLOR32, ImageList_GetImageCount(hSystemImageList), 10);
+	Attach(emptyImageList);
 }
 
 CSysImageList::~CSysImageList()
 {
-	Detach();
 }
 
 // Singleton specific operations
@@ -65,27 +69,27 @@ int CSysImageList::AddIcon(const HICON hIcon)
 	return this->Add(hIcon);
 }
 
-int CSysImageList::GetDirIconIndex() const
+int CSysImageList::GetDirIconIndex()
 {
 	return GetFileIcon(L"Doesn't matter", FILE_ATTRIBUTE_DIRECTORY, 0);
 }
 
-int CSysImageList::GetDirOpenIconIndex() const
+int CSysImageList::GetDirOpenIconIndex()
 {
 	return GetFileIcon(L"Doesn't matter", FILE_ATTRIBUTE_DIRECTORY, SHGFI_OPENICON);
 }
 
-int CSysImageList::GetDefaultIconIndex() const
+int CSysImageList::GetDefaultIconIndex()
 {
 	return GetFileIcon(L"", FILE_ATTRIBUTE_NORMAL, 0);
 }
 
-int CSysImageList::GetFileIconIndex(const CString& file) const
+int CSysImageList::GetFileIconIndex(const CString& file)
 {
 	return GetFileIcon(file, FILE_ATTRIBUTE_NORMAL, 0);
 }
 
-int CSysImageList::GetPathIconIndex(const CTGitPath& filePath) const
+int CSysImageList::GetPathIconIndex(const CTGitPath& filePath)
 {
 	CString strExtension = filePath.GetFileExtension();
 	strExtension.MakeUpper();
@@ -100,7 +104,7 @@ int CSysImageList::GetPathIconIndex(const CTGitPath& filePath) const
 	return it->second;
 }
 
-int CSysImageList::GetFileIcon( LPCTSTR file, DWORD attributes, UINT extraFlags ) const
+int CSysImageList::GetFileIcon(LPCTSTR file, DWORD attributes, UINT extraFlags)
 {
 	SHFILEINFO sfi = { 0 };
 
@@ -110,5 +114,5 @@ int CSysImageList::GetFileIcon( LPCTSTR file, DWORD attributes, UINT extraFlags 
 		&sfi, sizeof sfi,
 		SHGFI_SYSICONINDEX | SHGFI_SMALLICON | SHGFI_USEFILEATTRIBUTES | extraFlags);
 
-	return sfi.iIcon;
+	return AddIcon(ImageList_ExtractIcon(nullptr, hSystemImageList, sfi.iIcon));
 }

src/Utils/SysImageList.h
@@ -1,5 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2017 - TortoiseGit
 // Copyright (C) 2003-2006,2009-2010,2015 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -56,26 +57,26 @@ public:
 	/**
 	 * Returns the icon index for a directory.
 	 */
-	int GetDirIconIndex() const;
+	int GetDirIconIndex();
 	/**
 	 * Returns the icon index for a directory that's open (e.g. for a tree control)
 	 */
-	int GetDirOpenIconIndex() const;
+	int GetDirOpenIconIndex();
 	/**
 	 * Returns the icon index for a file which has no special icon associated.
 	 */
-	int GetDefaultIconIndex() const;
+	int GetDefaultIconIndex();
 	/**
 	 * Returns the icon index for the specified \a file. Only the file extension
 	 * is used to determine the file's icon.
 	 */
-	int GetFileIconIndex(const CString& file) const;
+	int GetFileIconIndex(const CString& file);
 
 	/**
 	 * Get the index for a Git-style path file.
 	 * Uses a cache to speed things up
 	 */
-	int GetPathIconIndex(const CTGitPath& file) const;
+	int GetPathIconIndex(const CTGitPath& file);
 
 	/**
 	 * Adds an icon to the image list and returns the index of the
@@ -86,10 +87,12 @@ public:
 private:
 	static CSysImageList *instance;
 
+	HIMAGELIST hSystemImageList;
+
 	typedef std::map<CString, int> IconIndexMap;
 	mutable IconIndexMap m_indexCache;
 
-	int GetFileIcon(LPCTSTR file, DWORD attributes, UINT extraFlags) const;
+	int GetFileIcon(LPCTSTR file, DWORD attributes, UINT extraFlags);
 };
 
 

commit 798a9f625dcb70a59c88ce02954787f477e150f4

Create the image list with a mask, otherwise the overlays won't work
Fixes a regression introduced in commit 308f806f53f1a5ae312e02552c8c8440f8e4cc13.

Based on TortoiseSVN rev. 27747.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/SysImageList.cpp
@@ -39,7 +39,7 @@ CSysImageList::CSysImageList()
 
 	int cx, cy;
 	ImageList_GetIconSize(hSystemImageList, &cx, &cy);
-	auto emptyImageList = ImageList_Create(cx, cy, ILC_COLOR32, ImageList_GetImageCount(hSystemImageList), 10);
+	auto emptyImageList = ImageList_Create(cx, cy, ILC_COLOR32 | ILC_MASK, ImageList_GetImageCount(hSystemImageList), 10);
 	Attach(emptyImageList);
 }
 