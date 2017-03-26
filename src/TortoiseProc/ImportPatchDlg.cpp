724a612bd43e1066d6c6c5638566e9db5dbb92f1
Sven Strickroth <email@cs-ware.de>
2017-03-26 15:14:59 +0200

    Apply Serial Patch requires username to be set
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/ImportPatchDlg.cpp
+++ src/TortoiseProc/ImportPatchDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2016 - TortoiseGit
+// Copyright (C) 2008-2017 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -504,6 +504,9 @@ void CImportPatchDlg::OnBnClickedOk()
 	this->UpdateData();
 
 	SaveSplitterPos();
+
+	if (!CAppUtils::CheckUserData())
+		return;
 
 	if(IsFinish())
 	{
