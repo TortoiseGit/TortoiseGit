2df3839fe765999e636345534c3d874a109829f0
Yue Lin Ho <b8732003@student.nsysu.edu.tw>
2017-07-01 21:15:55 +0800

    Can pick a tag ref (full name) to push from push dialog
    Signed-off-by: Yue Lin Ho <b8732003@student.nsysu.edu.tw>
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/PushDlg.cpp
+++ src/TortoiseProc/PushDlg.cpp
@@ -481,7 +481,7 @@ void CPushDlg::OnBnClickedButtonBrowseSourceBranch()
 	{
 	case 0: /* Browse Refence*/
 		{
-			if(CBrowseRefsDlg::PickRefForCombo(&m_BranchSource, gPickRef_Head))
+			if (CBrowseRefsDlg::PickRefForCombo(&m_BranchSource, gPickRef_Head | gPickRef_Tag))
 				OnCbnSelchangeBranchSource();
 		}
 		break;
