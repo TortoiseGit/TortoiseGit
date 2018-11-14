TortoiseGit/TortoiseGit

commit 054af0f034e92afe78818943d678f380704a3449

Fixed issue #3297: Missing URI-encode of `%BUGID%` part by call of issue tracker URL
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ProjectProperties.cpp
@@ -1,4 +1,4 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2011, 2013-2018 - TortoiseGit
 
@@ -432,7 +432,13 @@ CString ProjectProperties::GetBugIDUrl(const CString& sBugID)
 	if (!sMessage.IsEmpty() || !sCheckRe.IsEmpty())
 	{
 		ret = sUrl;
-		ret.Replace(L"%BUGID%", sBugID);
+		CString parameter;
+		DWORD size = INTERNET_MAX_URL_LENGTH;
+		UrlEscape(sBugID, CStrBuf(parameter, size + 1), &size, URL_ESCAPE_SEGMENT_ONLY | URL_ESCAPE_PERCENT | URL_ESCAPE_AS_UTF8);
+		// UrlEscape does not escape + and =, starting with Win8 the URL_ESCAPE_ASCII_URI_COMPONENT flag could be used and the following two lines would not be necessary
+		parameter.Replace(L"+", L"%2B");
+		parameter.Replace(L"=", L"%3D");
+		ret.Replace(L"%BUGID%", parameter);
 	}
 	return ret;
 }

test/UnitTests/ProjectPropertiesTest.cpp
@@ -1,6 +1,6 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2015-2016 - TortoiseGit
+// Copyright (C) 2015-2016, 2018 - TortoiseGit
 // Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -59,4 +59,14 @@ TEST(ProjectPropertiesTest, ParseBugIDs)
 	ASSERT_STREQ(L"000815", sRet);
 	ASSERT_TRUE(props.HasBugID(L"test test [[000815]]] some stupid programming error fixed"));
 	ASSERT_FALSE(props.HasBugID(L"test test [000815]] some stupid programming error fixed"));
+}
+
+TEST(ProjectPropertiesTest, GetBugIDUrl)
+{
+	ProjectProperties props;
+	ASSERT_STREQ(L"", props.GetBugIDUrl(L"something"));
+
+	props.sCheckRe = L"don't care";
+	props.sUrl = L"http://tortoisesvn.tigris.org/issues/show_bug.cgi?id=%BUGID%&bla=%25#anchor";
+	ASSERT_STREQ(L"http://tortoisesvn.tigris.org/issues/show_bug.cgi?id=%C3%84bf%25def%23g%3Chi%20j%2Fkl%2Bmn%26o%3D,%3F&bla=%25#anchor", props.GetBugIDUrl(L"Äbf%def#g<hi j/kl+mn&o=,?"));
 }

commit e9ffaca62e03208be9334ca6c96d02d22664dcea

Fix build
Error introduced in commit 054af0f034e92afe78818943d678f380704a3449.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/ProjectProperties.cpp
@@ -22,6 +22,7 @@
 #include "Git.h"
 #include "UnicodeUtils.h"
 #include "TempFile.h"
+#include <WinInet.h>
 
 struct num_compare
 {