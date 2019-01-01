TortoiseGit/TortoiseGit

commit df96f22f90dcca60c46e292b45127071c8b30d36

Try not to leak passwords in memory
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/WindowsCredentialsStore.cpp
@@ -1,6 +1,6 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2016 - TortoiseGit
+// Copyright (C) 2016, 2018 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -23,25 +23,29 @@
 
 #pragma comment(lib, "Advapi32.lib")
 
-int CWindowsCredentialsStore::GetCredential(const CString& entryName, CString& username, CString& password)
+int CWindowsCredentialsStore::GetCredential(const CString& entryName, CCredentials& credentials)
 {
 	PCREDENTIAL cred = nullptr;
-	if (CredRead(entryName, 1, 0, &cred) != TRUE || !cred)
+	if (CredRead(entryName, CRED_TYPE_GENERIC, 0, &cred) != TRUE || !cred)
 		return -1;
 
-	username = cred->UserName;
-	password = CString((TCHAR*)cred->CredentialBlob, cred->CredentialBlobSize / sizeof(TCHAR));
+	credentials.m_username = cred->UserName;
+	wcsncpy_s(credentials.m_password, _countof(credentials.m_password), (TCHAR*)cred->CredentialBlob, cred->CredentialBlobSize / sizeof(TCHAR));
+	SecureZeroMemory(cred->CredentialBlob, cred->CredentialBlobSize);
+	CredFree(&cred);
 	return 0;
 }
 
-int CWindowsCredentialsStore::SaveCredential(const CString& entryName, const CString& username, const CString& password)
+int CWindowsCredentialsStore::SaveCredential(const CString& entryName, const CString& username, const TCHAR* password)
 {
+	ATLASSERT(password);
+
 	CREDENTIAL cred = { 0 };
 	cred.Type = CRED_TYPE_GENERIC;
 	cred.TargetName = const_cast<LPTSTR>(static_cast<LPCTSTR>(entryName));
 	cred.UserName = const_cast<LPTSTR>(static_cast<LPCTSTR>(username));
-	cred.CredentialBlob = (LPBYTE)static_cast<LPCTSTR>(password);
-	cred.CredentialBlobSize = password.GetLength() * sizeof(TCHAR);
+	cred.CredentialBlob = (LPBYTE)password;
+	cred.CredentialBlobSize = (int)wcslen(password) * sizeof(TCHAR);
 	cred.Persist = CRED_PERSIST_LOCAL_MACHINE;
 	return CredWrite(&cred, 0) == TRUE ? 0 : -1;
 }
@@ -49,4 +53,14 @@ int CWindowsCredentialsStore::SaveCredential(const CString& entryName, const CSt
 int CWindowsCredentialsStore::DeleteCredential(const CString& entryName)
 {
 	return CredDelete(entryName, CRED_TYPE_GENERIC, 0) == TRUE ? 0 : -1;
+}
+
+CCredentials::CCredentials()
+{
+	SecureZeroMemory(&m_password, sizeof(m_password));
+}
+
+CCredentials ::~CCredentials()
+{
+	SecureZeroMemory(&m_password, sizeof(m_password));
 }

src/Resources/TortoiseProcENG.rc
@@ -437,9 +437,9 @@ BEGIN
                     "Button",BS_AUTOCHECKBOX | WS_TABSTOP,8,84,277,10
     GROUPBOX        "Credentials",IDC_STATIC,17,97,268,51
     LTEXT           "Login:",IDC_STATIC_SMTPLOGIN,36,110,33,8
-    EDITTEXT        IDC_SMTP_USER,77,107,199,14,ES_AUTOHSCROLL
-    LTEXT           "Password:",IDC_STATIC_SMTPPASSWORD,35,129,34,8
-    EDITTEXT        IDC_SMTP_PASSWORD,77,127,199,14,ES_PASSWORD | ES_AUTOHSCROLL
+    EDITTEXT        IDC_SMTP_USER,77,107,199,14,ES_AUTOHSCROLL | ES_READONLY
+    PUSHBUTTON      "&Store credentials...",IDC_STORECREDENTIALS,77,125,98,14
+    PUSHBUTTON      "&Clear",IDC_CLEARCREDENTIALS,180,125,98,14
 END
 
 IDD_SVNPROGRESS DIALOGEX 0, 0, 434, 165

src/SshAskPass/SshAskPass.cpp
@@ -30,7 +30,7 @@
 #pragma comment(lib, "comctl32.lib")
 #pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-#define MAX_LOADSTRING 100
+#define MAX_LOADSTRING 256
 
 // Global Variables:
 HINSTANCE hInst;								// current instance
@@ -80,7 +80,15 @@ int APIENTRY _tWinMain(HINSTANCE	/*hInstance*/,
 
 	if (DialogBox(hInst, MAKEINTRESOURCE(IDD_ASK_PASSWORD), nullptr, PasswdDlg) == IDOK)
 	{
-		printf("%s\n", CUnicodeUtils::StdGetUTF8(g_PassWord).c_str());
+		auto len = (int)_tcslen(g_PassWord);
+		auto size = len * 4 + 1;
+		auto buf = new char[size];
+		auto ret = WideCharToMultiByte(CP_UTF8, 0, g_PassWord, len, buf, size - 1, nullptr, nullptr);
+		buf[ret] = '\0';
+		printf("%s\n", buf);
+		SecureZeroMemory(buf, size);
+		delete[] buf;
+		SecureZeroMemory(&g_PassWord, sizeof(g_PassWord));
 		return 0;
 	}
 	wprintf(L"\n");
@@ -141,11 +149,18 @@ INT_PTR CALLBACK PasswdDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM /*lPar
 
 		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
 		{
-			if( LOWORD(wParam) == IDOK )
-			{
-				HWND password = ::GetDlgItem(hDlg,IDC_PASSWORD);
-				::GetWindowText(password,g_PassWord,MAX_LOADSTRING);
-			}
+			HWND password = ::GetDlgItem(hDlg, IDC_PASSWORD);
+			if (LOWORD(wParam) == IDOK)
+				::GetWindowText(password, g_PassWord, MAX_LOADSTRING);
+
+			// overwrite textfield contents with garbage in order to wipe the cache
+			TCHAR gargabe[MAX_LOADSTRING];
+			wmemset(gargabe, L'*', _countof(gargabe));
+			gargabe[_countof(gargabe) - 1] = L'\0';
+			::SetWindowText(password, gargabe);
+			gargabe[0] = L'\0';
+			::SetWindowText(password, gargabe);
+
 			EndDialog(hDlg, LOWORD(wParam));
 			return (INT_PTR)TRUE;
 		}

src/TortoisePlink/LoginDialog.cpp
@@ -1,5 +1,6 @@
-// TortoiseSVN - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2018 - TortoiseGit
 // Copyright (C) 2003, 2013, 2018 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -29,20 +30,20 @@ class LoginDialog
 {
 public:
    LoginDialog(const std::string& prompt);
+   ~LoginDialog();
 
-   static bool DoLoginDialog(std::string& password, const std::string& prompt);
+   static bool DoLoginDialog(char* password, int maxlen, const char* prompt);
 
 private:
    bool myOK;
    HWND _hdlg;
 
-   std::string  myPassword;
+   char myPassword[MAX_LENGTH_PASSWORD];
    std::string  myPrompt;
 
    void CreateModule(void);
    void RetrieveValues();
-
-   std::string GetPassword();
+   void PurgeValues();
 
    friend BOOL CALLBACK LoginDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
@@ -52,11 +53,7 @@ BOOL DoLoginDialog(char* password, int maxlen, const char* prompt)
 {
    g_hmodThisDll = GetModuleHandle(0);
    g_hwndMain = GetParentHwnd();
-   std::string passwordstr;
-   BOOL res = LoginDialog::DoLoginDialog(passwordstr, prompt);
-   if (res)
-      strncpy(password, passwordstr.c_str(), maxlen);
-   return res;
+   return LoginDialog::DoLoginDialog(password, maxlen, prompt);
 }
 
 
@@ -70,6 +67,7 @@ BOOL CALLBACK LoginDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPa
       // Set prompt text
       SendDlgItemMessage(hwndDlg, IDC_LOGIN_PROMPT, WM_SETTEXT,
                          pDlg->myPrompt.length(), (LPARAM) pDlg->myPrompt.c_str());
+      SendDlgItemMessage(hwndDlg, IDC_LOGIN_PASSWORD, EM_SETLIMITTEXT, MAX_LENGTH_PASSWORD - 1, 0);
       // Make sure edit control has the focus
       //SendDlgItemMessage(hwndDlg, IDC_LOGIN_PASSWORD, WM_SETFOCUS, 0, 0);
       if (GetDlgCtrlID((HWND) wParam) != IDC_LOGIN_PASSWORD)
@@ -83,6 +81,7 @@ BOOL CALLBACK LoginDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPa
    {
       LoginDialog* pDlg = (LoginDialog*) GetWindowLongPtr(hwndDlg, GWLP_USERDATA);
       pDlg->myOK = false;
+      pDlg->PurgeValues();
       EndDialog(hwndDlg, IDCANCEL);
       return 1;
    }
@@ -91,6 +90,7 @@ BOOL CALLBACK LoginDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPa
       LoginDialog* pDlg = (LoginDialog*) GetWindowLongPtr(hwndDlg, GWLP_USERDATA);
       pDlg->myOK = true;
       pDlg->RetrieveValues();
+      pDlg->PurgeValues();
       EndDialog(hwndDlg, IDOK);
       return 1;
    }
@@ -100,6 +100,12 @@ BOOL CALLBACK LoginDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPa
 LoginDialog::LoginDialog(const std::string& prompt)
 {
    myPrompt = prompt;
+   SecureZeroMemory(&myPassword, sizeof(myPassword));
+}
+
+LoginDialog::~LoginDialog()
+{
+   SecureZeroMemory(&myPassword, sizeof(myPassword));
 }
 
 void LoginDialog::CreateModule(void)
@@ -109,34 +115,37 @@ void LoginDialog::CreateModule(void)
 }
 
 
-bool LoginDialog::DoLoginDialog(std::string& password, const std::string& prompt)
+bool LoginDialog::DoLoginDialog(char* password, int maxlen, const char* prompt)
 {
    LoginDialog *pDlg = new LoginDialog(prompt);
 
    pDlg->CreateModule();
 
    bool ret = pDlg->myOK;
-   password = pDlg->myPassword;
+
+   if (ret)
+      strncpy_s(password, maxlen, pDlg->myPassword, sizeof(pDlg->myPassword));
 
    delete pDlg;
 
    return ret;
 }
 
-
-std::string LoginDialog::GetPassword()
-{
-   char szTxt[256];
-   SendDlgItemMessage(_hdlg, IDC_LOGIN_PASSWORD, WM_GETTEXT, sizeof(szTxt), (LPARAM)szTxt);
-   std::string strText = szTxt;
-   return strText;
-}
-
 void LoginDialog::RetrieveValues()
 {
-   myPassword = GetPassword();
+   SendDlgItemMessage(_hdlg, IDC_LOGIN_PASSWORD, WM_GETTEXT, sizeof(myPassword), (LPARAM)myPassword);
 }
 
+void LoginDialog::PurgeValues()
+{
+   // overwrite textfield contents with garbage in order to wipe the cache
+   char gargabe[MAX_LENGTH_PASSWORD];
+   memset(gargabe, L'*', sizeof(gargabe));
+   gargabe[sizeof(gargabe) - 1] = '\0';
+   SendDlgItemMessage(_hdlg, IDC_LOGIN_PASSWORD, WM_SETTEXT, 0, (LPARAM)gargabe);
+   gargabe[0] = '\0';
+   SendDlgItemMessage(_hdlg, IDC_LOGIN_PASSWORD, WM_SETTEXT, 0, (LPARAM)gargabe);
+}
 
 BOOL IsWinNT()
 {

src/TortoisePlink/LoginDialog.h
@@ -1,5 +1,6 @@
-// TortoiseCVS - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
+// Copyright (C) 2018 - TortoiseGit
 // Copyright (C) 2000 - Francis Irving
 // <francis@flourish.org> - May 2000
 
@@ -21,6 +22,8 @@
 #define LOGIN_DIALOG_H
 
 #include <windows.h>
+
+#define MAX_LENGTH_PASSWORD 256
 
 #ifdef __cplusplus
 extern "C" {

src/TortoiseProc/AppUtils.cpp
@@ -3534,13 +3534,9 @@ int CAppUtils::Git2GetUserPassword(git_cred **out, const char *url, const char *
 	if (username_from_url)
 		dlg.m_UserName = CUnicodeUtils::GetUnicode(username_from_url, CP_UTF8);
 
-	CStringA username, password;
 	if (dlg.DoModal() == IDOK)
-	{
-		username = CUnicodeUtils::GetMulti(dlg.m_UserName, CP_UTF8);
-		password = CUnicodeUtils::GetMulti(dlg.m_Password, CP_UTF8);
-		return git_cred_userpass_plaintext_new(out, username, password);
-	}
+		return git_cred_userpass_plaintext_new(out, CUnicodeUtils::GetMulti(dlg.m_UserName, CP_UTF8), dlg.m_passwordA);
+
 	giterr_set_str(GITERR_NONE, "User cancelled.");
 	return GIT_EUSER;
 }

src/TortoiseProc/SendMail.cpp
@@ -1,4 +1,4 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2008-2013, 2015-2016, 2018 - TortoiseGit
 
@@ -118,9 +118,9 @@ int CSendMail::SendMail(const CString& FromName, const CString& FromMail, const 
 			CString recipients(To);
 			if (!CC.IsEmpty())
 				recipients += L";" + CC;
-			CString username, password;
-			CWindowsCredentialsStore::GetCredential(L"TortoiseGit:SMTP-Credentials", username, password);
-			if (mail.SendEmail((CString)CRegString(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Address", L""), username, password, (BOOL)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\AuthenticationRequired", FALSE), sender, recipients, subject, body, &attachments, CC, (DWORD)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Port", 25), sender, To, (DWORD)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Encryption", 0)) == TRUE)
+			CCredentials credentials;
+			CWindowsCredentialsStore::GetCredential(L"TortoiseGit:SMTP-Credentials", credentials);
+			if (mail.SendEmail((CString)CRegString(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Address", L""), &credentials, (BOOL)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\AuthenticationRequired", FALSE), sender, recipients, subject, body, &attachments, CC, (DWORD)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Port", 25), sender, To, (DWORD)CRegDWORD(L"Software\\TortoiseGit\\TortoiseProc\\SendMail\\Encryption", 0)) == TRUE)
 				return 0;
 			else
 			{

src/TortoiseProc/Settings/SettingSMTP.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009, 2013-2016 - TortoiseGit
+// Copyright (C) 2009, 2013-2016, 2018 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -25,6 +25,7 @@
 #include "SendMail.h"
 #include "MailMsg.h"
 #include "WindowsCredentialsStore.h"
+#include "UserPassword.h"
 
 // CSettingSMTP dialog
 
@@ -43,8 +44,6 @@ CSettingSMTP::CSettingSMTP()
 	m_Port = m_regPort;
 	m_dwSMTPEnrcyption = m_regEncryption;
 	m_bAuth = m_regAuthenticate;
-
-	CWindowsCredentialsStore::GetCredential(L"TortoiseGit:SMTP-Credentials", m_User, m_Password);
 }
 
 CSettingSMTP::~CSettingSMTP()
@@ -59,8 +58,6 @@ void CSettingSMTP::DoDataExchange(CDataExchange* pDX)
 	DDX_Check(pDX, IDC_SMTP_AUTH, m_bAuth);
 	DDX_Text(pDX, IDC_SMTP_SERVER, m_Server);
 	DDX_Text(pDX, IDC_SMTP_PORT, m_Port);
-	DDX_Text(pDX, IDC_SMTP_USER, m_User);
-	DDX_Text(pDX, IDC_SMTP_PASSWORD, m_Password);
 }
 
 BEGIN_MESSAGE_MAP(CSettingSMTP, CPropertyPage)
@@ -71,8 +68,8 @@ BEGIN_MESSAGE_MAP(CSettingSMTP, CPropertyPage)
 	ON_EN_CHANGE(IDC_SMTP_SERVER, OnModified)
 	ON_EN_CHANGE(IDC_SMTP_PORT, OnModified)
 	ON_EN_CHANGE(IDC_SEND_ADDRESS, OnModified)
-	ON_EN_CHANGE(IDC_SMTP_USER, OnModified)
-	ON_EN_CHANGE(IDC_SMTP_PASSWORD, OnModified)
+	ON_BN_CLICKED(IDC_STORECREDENTIALS, OnBnClickedStoreCredentials)
+	ON_BN_CLICKED(IDC_CLEARCREDENTIALS, OnBnClickedClearCredentials)
 END_MESSAGE_MAP()
 
 BOOL CSettingSMTP::OnInitDialog()
@@ -110,7 +107,7 @@ BOOL CSettingSMTP::OnInitDialog()
 	this->UpdateData(FALSE);
 
 	OnModifiedDeliveryCombo();
-	OnBnClickedSmtpAuth();
+	UpdateCredentialsButtons();
 
 	return TRUE;
 }
@@ -130,15 +127,14 @@ void CSettingSMTP::OnModifiedDeliveryCombo()
 {
 	m_dwDeliveryType = (DWORD)m_SMTPDeliveryTypeCombo.GetItemData(m_SMTPDeliveryTypeCombo.GetCurSel());
 
-	GetDlgItem(IDC_SMTP_PASSWORD)->EnableWindow(m_dwDeliveryType >= 2 && m_bAuth);
 	GetDlgItem(IDC_SMTP_USER)->EnableWindow(m_dwDeliveryType >= 2 && m_bAuth);
+	GetDlgItem(IDC_STORECREDENTIALS)->EnableWindow(m_dwDeliveryType >= 2 && m_bAuth);
 	GetDlgItem(IDC_SMTP_AUTH)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_SMTPENCRYPTIONCOMBO)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_SMTP_PORT)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_SMTP_SERVER)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_STATIC_SMTPENCRYPTION)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_STATIC_SMTPLOGIN)->EnableWindow(m_dwDeliveryType >= 2 && m_bAuth);
-	GetDlgItem(IDC_STATIC_SMTPPASSWORD)->EnableWindow(m_dwDeliveryType >= 2 && m_bAuth);
 	GetDlgItem(IDC_STATIC_SMTPPORT)->EnableWindow(m_dwDeliveryType >= 2);
 	GetDlgItem(IDC_STATIC_SMTPSERVER)->EnableWindow(m_dwDeliveryType >= 2);
 
@@ -155,10 +151,6 @@ BOOL CSettingSMTP::OnApply()
 	m_regPort = m_Port;
 	m_regAuthenticate = m_bAuth;
 	m_regEncryption = m_dwSMTPEnrcyption;
-	if (m_User.IsEmpty() && m_Password.IsEmpty())
-		CWindowsCredentialsStore::DeleteCredential(L"TortoiseGit:SMTP-Credentials");
-	else
-		CWindowsCredentialsStore::SaveCredential(L"TortoiseGit:SMTP-Credentials", m_User, m_Password);
 
 	SetModified(FALSE);
 
@@ -170,8 +162,34 @@ void CSettingSMTP::OnBnClickedSmtpAuth()
 {
 	UpdateData();
 	GetDlgItem(IDC_SMTP_USER)->EnableWindow(m_bAuth);
-	GetDlgItem(IDC_SMTP_PASSWORD)->EnableWindow(m_bAuth);
 	GetDlgItem(IDC_STATIC_SMTPLOGIN)->EnableWindow(m_bAuth);
-	GetDlgItem(IDC_STATIC_SMTPPASSWORD)->EnableWindow(m_bAuth);
+	GetDlgItem(IDC_STORECREDENTIALS)->EnableWindow(m_bAuth);
 	SetModified();
+}
+
+void CSettingSMTP::OnBnClickedStoreCredentials()
+{
+	UpdateData();
+	CUserPassword dlg;
+	GetDlgItem(IDC_SMTP_USER)->GetWindowText(dlg.m_UserName);
+	if (dlg.DoModal() == IDOK)
+		CWindowsCredentialsStore::SaveCredential(L"TortoiseGit:SMTP-Credentials", dlg.m_UserName, dlg.m_password);
+
+	UpdateCredentialsButtons();
+	UpdateData(FALSE);
+}
+
+void CSettingSMTP::OnBnClickedClearCredentials()
+{
+	CWindowsCredentialsStore::DeleteCredential(L"TortoiseGit:SMTP-Credentials");
+	GetDlgItem(IDC_SMTP_USER)->SetWindowText(L"");
+	GetDlgItem(IDC_CLEARCREDENTIALS)->EnableWindow(FALSE);
+}
+
+void CSettingSMTP::UpdateCredentialsButtons()
+{
+	CCredentials credentials;
+	CWindowsCredentialsStore::GetCredential(L"TortoiseGit:SMTP-Credentials", credentials);
+	GetDlgItem(IDC_SMTP_USER)->SetWindowText(credentials.m_username);
+	GetDlgItem(IDC_CLEARCREDENTIALS)->EnableWindow(!credentials.m_username.IsEmpty());
 }

commit dc3d7039cf7a737c844e83bd0f59db15b67201ac

Fix possible crash
Introduced by a typo in commit df96f22f90dcca60c46e292b45127071c8b30d36.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/WindowsCredentialsStore.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2016, 2018 - TortoiseGit
+// Copyright (C) 2016, 2018-2019 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -32,7 +32,7 @@ int CWindowsCredentialsStore::GetCredential(const CString& entryName, CCredentia
 	credentials.m_username = cred->UserName;
 	wcsncpy_s(credentials.m_password, _countof(credentials.m_password), (TCHAR*)cred->CredentialBlob, cred->CredentialBlobSize / sizeof(TCHAR));
 	SecureZeroMemory(cred->CredentialBlob, cred->CredentialBlobSize);
-	CredFree(&cred);
+	CredFree(cred);
 	return 0;
 }
 