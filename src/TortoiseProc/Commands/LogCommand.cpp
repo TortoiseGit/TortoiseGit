413e7bad790fcc0ad578a149d315d5fb766054be
Sven Strickroth <email@cs-ware.de>
2019-02-03 14:49:29 +0100

    Improve error message for !IsWorkingTreeOrBareRepo case
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Commands/LogCommand.cpp
+++ src/TortoiseProc/Commands/LogCommand.cpp
@@ -1,7 +1,7 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2007-2008,2011 - TortoiseSVN
-// Copyright (C) 2008-2016, 2018 - TortoiseGit
+// Copyright (C) 2008-2016, 2018-2019 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -28,7 +28,7 @@ bool LogCommand::Execute()
 {
 	if (!GitAdminDir::IsWorkingTreeOrBareRepo(g_Git.m_CurrentDir))
 	{
-		CMessageBox::Show(GetExplorerHWND(), IDS_NOWORKINGCOPY, IDS_APPNAME, MB_ICONERROR);
+		CMessageBox::Show(GetExplorerHWND(), IDS_NOGITREPO, IDS_APPNAME, MB_ICONERROR);
 		return false;
 	}
 
