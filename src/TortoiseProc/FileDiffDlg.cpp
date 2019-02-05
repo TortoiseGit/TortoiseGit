18f758b148843831447005a403fe7c5f6aeb537e
Sven Strickroth <email@cs-ware.de>
2019-02-05 19:11:50 +0100

    FileDiffDlg: Don't allow closing while reference loading thread still might be working
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/FileDiffDlg.cpp
+++ src/TortoiseProc/FileDiffDlg.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2018 - TortoiseGit
+// Copyright (C) 2008-2019 - TortoiseGit
 // Copyright (C) 2003-2008, 2018 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -949,7 +949,7 @@ BOOL CFileDiffDlg::PreTranslateMessage(MSG* pMsg)
 
 void CFileDiffDlg::OnCancel()
 {
-	if (m_bThreadRunning)
+	if (m_bThreadRunning || m_bLoadingRef)
 		return;
 	__super::OnCancel();
 }
