TortoiseGit/TortoiseGit

commit a52b72da377b9bd79a2ebfccac7a6eb0195e3d66

Extend the hook scripts with the ability to use information from the project properties instead of the registry alone
This way a hook script can be configured inside a repository using the .tgitconfig file without every user having to configure it manually.

Signed-off-by: Stefan Küng <stefankueng@gmail.com>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/Hooks.cpp
@@ -26,6 +26,7 @@
 #include "Git.h"
 
 CHooks* CHooks::m_pInstance;
+CTGitPath CHooks::m_RootPath;
 
 CHooks::CHooks()
 {
@@ -41,7 +42,7 @@ bool CHooks::Create()
 		m_pInstance = new CHooks();
 	CRegString reghooks(L"Software\\TortoiseGit\\hooks");
 	CString strhooks = reghooks;
-	ParseHookString(strhooks);
+	ParseHookString(strhooks, false);
 	return true;
 }
 
@@ -60,6 +61,8 @@ bool CHooks::Save()
 	CString strhooks;
 	for (hookiterator it = begin(); it != end(); ++it)
 	{
+		if (it->second.bLocal)
+			continue;
 		strhooks += GetHookTypeString(it->first.htype);
 		strhooks += '\n';
 		if (!it->second.bEnabled)
@@ -77,6 +80,57 @@ bool CHooks::Save()
 	reghooks = strhooks;
 	if (reghooks.GetLastError())
 		return false;
+
+	if (g_Git.m_CurrentDir.IsEmpty() || GitAdminDir::IsBareRepo(g_Git.m_CurrentDir))
+		return true;
+
+	// load the .tgitconfig file
+	CAutoConfig gitconfig(true);
+	git_config_add_file_ondisk(gitconfig, CGit::GetGitPathStringA(g_Git.CombinePath(L".tgitconfig")), GIT_CONFIG_LEVEL_LOCAL, nullptr, FALSE); // this needs to have the second highest priority
+
+	// delete all existing local hooks
+	for (const auto& hook : { PROJECTPROPNAME_STARTCOMMITHOOK, PROJECTPROPNAME_PRECOMMITHOOK, PROJECTPROPNAME_POSTCOMMITHOOK, PROJECTPROPNAME_PREPUSHHOOK, PROJECTPROPNAME_POSTPUSHHOOK, PROJECTPROPNAME_PREREBASEHOOK })
+	{
+		CStringA hookA = CUnicodeUtils::GetUTF8(hook);
+		for (const auto& val : { "cmdline", "wait", "show" })
+		{
+			git_config_delete_entry(gitconfig, hookA + val);
+		}
+	}
+
+	// now save the local hooks to .tgitconfig
+	for (const auto& hook : CHooks::Instance())
+	{
+		if (!hook.second.bLocal)
+			continue;
+
+		CStringA sHookPropName;
+		switch (hook.first.htype)
+		{
+		case start_commit_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_STARTCOMMITHOOK);
+			break;
+		case pre_commit_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_PRECOMMITHOOK);
+			break;
+		case post_commit_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_POSTCOMMITHOOK);
+			break;
+		case pre_push_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_PREPUSHHOOK);
+			break;
+		case post_push_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_POSTPUSHHOOK);
+			break;
+		case pre_rebase_hook:
+			sHookPropName = CUnicodeUtils::GetUTF8(PROJECTPROPNAME_PREREBASEHOOK);
+			break;
+		}
+		git_config_set_string(gitconfig, sHookPropName + "cmdline", CUnicodeUtils::GetUTF8(hook.second.commandline));
+		git_config_set_bool(gitconfig, sHookPropName + "wait", hook.second.bWait);
+		git_config_set_bool(gitconfig, sHookPropName + "show", hook.second.bShow);
+	}
+
 	return true;
 }
 
@@ -85,7 +139,7 @@ bool CHooks::Remove(const hookkey &key)
 	return (erase(key) > 0);
 }
 
-void CHooks::Add(hooktype ht, const CTGitPath& Path, LPCTSTR szCmd, bool bWait, bool bShow, bool bEnabled)
+void CHooks::Add(hooktype ht, const CTGitPath& Path, LPCTSTR szCmd, bool bWait, bool bShow, bool bEnabled, bool bLocal)
 {
 	hookkey key;
 	key.htype = ht;
@@ -99,6 +153,7 @@ void CHooks::Add(hooktype ht, const CTGitPath& Path, LPCTSTR szCmd, bool bWait, 
 	cmd.bWait = bWait;
 	cmd.bShow = bShow;
 	cmd.bEnabled = bEnabled;
+	cmd.bLocal = bLocal;
 	insert(std::pair<hookkey, hookcmd>(key, cmd));
 }
 
@@ -151,6 +206,26 @@ hooktype CHooks::GetHookType(const CString& s)
 	return unknown_hook;
 }
 
+void CHooks::SetProjectProperties(const CTGitPath& Path, const ProjectProperties& pp)
+{
+	m_RootPath = Path;
+
+	CString sHookString;
+	if (!pp.sPreCommitHook.IsEmpty())
+		sHookString += GetHookTypeString(pre_commit_hook) + L"\n?\n" + pp.sPreCommitHook + L"\n";
+	if (!pp.sStartCommitHook.IsEmpty())
+		sHookString += GetHookTypeString(start_commit_hook) + L"\n?\n" + pp.sStartCommitHook + L"\n";
+	if (!pp.sPostCommitHook.IsEmpty())
+		sHookString += GetHookTypeString(post_commit_hook) + L"\n?\n" + pp.sPostCommitHook + L"\n";
+	if (!pp.sPrePushHook.IsEmpty())
+		sHookString += GetHookTypeString(pre_push_hook) + L"\n?\n" + pp.sPrePushHook + L"\n";
+	if (!pp.sPostPushHook.IsEmpty())
+		sHookString += GetHookTypeString(post_push_hook) + L"\n?\n" + pp.sPostPushHook + L"\n";
+	if (!pp.sPreRebaseHook.IsEmpty())
+		sHookString += GetHookTypeString(pre_rebase_hook) + L"\n?\n" + pp.sPreRebaseHook + L"\n";
+	ParseHookString(sHookString, true);
+}
+
 void CHooks::AddParam(CString& sCmd, const CString& param)
 {
 	sCmd += L" \"";
@@ -193,6 +268,7 @@ bool CHooks::StartCommit(const CString& workingTree, const CTGitPathList& pathLi
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddPathParam(sCmd, pathList);
 	CTGitPath temppath = AddMessageFileParam(sCmd, message);
 	AddCWDParam(sCmd, workingTree);
@@ -210,6 +286,7 @@ bool CHooks::PreCommit(const CString& workingTree, const CTGitPathList& pathList
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddPathParam(sCmd, pathList);
 	CTGitPath temppath = AddMessageFileParam(sCmd, message);
 	AddCWDParam(sCmd, workingTree);
@@ -225,6 +302,7 @@ bool CHooks::PostCommit(const CString& workingTree, bool amend, DWORD& exitcode,
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddCWDParam(sCmd, workingTree);
 	if (amend)
 		AddParam(sCmd, L"true");
@@ -240,6 +318,7 @@ bool CHooks::PrePush(const CString& workingTree, DWORD& exitcode, CString& error
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddErrorParam(sCmd, error);
 	AddCWDParam(sCmd, workingTree);
 	exitcode = RunScript(sCmd, workingTree, error, it->second.bWait, it->second.bShow);
@@ -252,6 +331,7 @@ bool CHooks::PostPush(const CString& workingTree, DWORD& exitcode, CString& erro
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddErrorParam(sCmd, error);
 	AddCWDParam(sCmd, workingTree);
 	exitcode = RunScript(sCmd, workingTree, error, it->second.bWait, it->second.bShow);
@@ -264,6 +344,7 @@ bool CHooks::PreRebase(const CString& workingTree, const CString& upstream, cons
 	if (it == end())
 		return false;
 	CString sCmd = it->second.commandline;
+	sCmd.Replace(L"%root%", m_RootPath.GetWinPathString());
 	AddParam(sCmd, upstream);
 	AddParam(sCmd, rebasedBranch);
 	AddErrorParam(sCmd, error);
@@ -300,10 +381,24 @@ const_hookiterator CHooks::FindItem(hooktype t, const CString& workingTree) cons
 		return it;
 	}
 
+	// try the root path
+	key.htype = t;
+	key.path = m_RootPath;
+	it = find(key);
+	if (it != end())
+		return it;
+
+	// look for a script with a path as '*'
+	key.htype = t;
+	key.path = CTGitPath(L"*");
+	it = find(key);
+	if (it != end())
+		return it;
+
 	return end();
 }
 
-void CHooks::ParseHookString(CString strhooks)
+void CHooks::ParseHookString(CString strhooks, bool bLocal)
 {
 	// now fill the map with all the hooks defined in the string
 	// the string consists of multiple lines, where one hook script is defined
@@ -311,12 +406,14 @@ void CHooks::ParseHookString(CString strhooks)
 	// line 1: the hook type
 	// line 2: path to working copy where to apply the hook script,
 	//         if it starts with "!" this hook is disabled (this should provide backward and forward compatibility)
+	//         if it starts with "?" this hook is for the current project folder
 	// line 3: command line to execute
 	// line 4: 'true' or 'false' for waiting for the script to finish
 	// line 5: 'show' or 'hide' on how to start the hook script
 	hookkey key;
 	int pos = 0;
 	hookcmd cmd;
+	cmd.bLocal = bLocal;
 	while ((pos = strhooks.Find(L'\n')) >= 0)
 	{
 		// line 1
@@ -336,7 +433,10 @@ void CHooks::ParseHookString(CString strhooks)
 				strhooks = strhooks.Mid((int)wcslen(L"!"));
 				--pos;
 			}
-			key.path = CTGitPath(strhooks.Left(pos));
+			if (strhooks[0] == L'?' && pos > 0)
+				key.path = CTGitPath(m_RootPath);
+			else
+				key.path = CTGitPath(strhooks.Left(pos));
 			if (pos + 1 < strhooks.GetLength())
 				strhooks = strhooks.Mid(pos + 1);
 			else

Languages/Tortoise.pot
@@ -7628,6 +7628,10 @@ msgstr ""
 msgid "Right image"
 msgstr ""
 
+#. Resource IDs: (Dialog IDD_SETTINGSHOOKCONFIG: Control id 1848)
+msgid "Run for this repository"
+msgstr ""
+
 #. Resource IDs: (Dialog IDD_SETTINGSHOOKCONFIG: Control id 1335)
 msgid "Run when working tree path is under:"
 msgstr ""

doc/source/en/TortoiseGit/tgit_dug/dug_settings_hooks.xml
@@ -105,6 +105,17 @@
 		Use <literal>*</literal> for matching all working trees.
 	</para>
 	<para>
+		If the checkbox <guibutton>Run for this repository</guibutton> is checked then
+		the hook script is attached to the current repository and configured
+		automatically for every clone and checkout (the hook information is stored in
+		the file <filename>.tgitconfig</filename> in the repository root so that it will
+		be automatically shared with all other developers using TortoiseGit &gt;= 2.7.1).
+		In this case, you can specify paths for the command line with the
+		replacement string <literal>%root%</literal> for the path to the
+		working tree folder. The hook script has to be inside the repository and
+		also be checked out of course.
+	</para>
+	<para>
 		Next you must specify the command line to execute, starting with the path
 		to the hook script or executable.
 		This could be a batch file, an executable file or any other file which

src/Resources/TortoiseProcENG.rc
@@ -860,7 +860,7 @@ BEGIN
     PUSHBUTTON      "Add...",IDC_HOOKADDBUTTON,243,195,50,14
 END
 
-IDD_SETTINGSHOOKCONFIG DIALOGEX 0, 0, 395, 133
+IDD_SETTINGSHOOKCONFIG DIALOGEX 0, 0, 395, 148
 STYLE DS_SETFONT | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME
 CAPTION "Configure Hook Scripts"
 FONT 9, "MS Shell Dlg 2", 400, 0, 0x1
@@ -868,17 +868,18 @@ BEGIN
     CONTROL         "&Enable",IDC_ENABLE,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,7,351,10
     LTEXT           "Hook Type:",IDC_HOOKTYPELABEL,7,22,213,8
     COMBOBOX        IDC_HOOKTYPECOMBO,254,19,134,107,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
-    LTEXT           "Run when working tree path is under:",IDC_HOOKWCPATHLABEL,7,37,381,8
-    EDITTEXT        IDC_HOOKPATH,7,48,351,14,ES_AUTOHSCROLL
-    PUSHBUTTON      "...",IDC_HOOKBROWSE,366,48,22,14
-    LTEXT           "Command Line To Execute:",IDC_HOOKCMLABEL,7,71,381,8
-    EDITTEXT        IDC_HOOKCOMMANDLINE,7,84,351,14,ES_AUTOHSCROLL
-    PUSHBUTTON      "...",IDC_HOOKCOMMANDBROWSE,366,84,22,14
-    CONTROL         "Wait for the script to finish",IDC_WAITCHECK,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,102,351,10
-    CONTROL         "Hide the script while running",IDC_HIDECHECK,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,116,198,10
-    DEFPUSHBUTTON   "OK",IDOK,228,112,50,14
-    PUSHBUTTON      "Cancel",IDCANCEL,283,112,50,14
-    PUSHBUTTON      "Help",IDHELP,338,112,50,14
+    CONTROL         "Run for this repository",IDC_LOCALCHECK,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,41,86,10
+    LTEXT           "Run when working tree path is under:",IDC_HOOKWCPATHLABEL,7,52,381,8
+    EDITTEXT        IDC_HOOKPATH,7,63,351,14,ES_AUTOHSCROLL
+    PUSHBUTTON      "...",IDC_HOOKBROWSE,366,63,22,14
+    LTEXT           "Command Line To Execute:",IDC_HOOKCMLABEL,7,86,381,8
+    EDITTEXT        IDC_HOOKCOMMANDLINE,7,99,351,14,ES_AUTOHSCROLL
+    PUSHBUTTON      "...",IDC_HOOKCOMMANDBROWSE,366,99,22,14
+    CONTROL         "Wait for the script to finish",IDC_WAITCHECK,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,117,351,10
+    CONTROL         "Hide the script while running",IDC_HIDECHECK,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,7,131,198,10
+    DEFPUSHBUTTON   "OK",IDOK,228,127,50,14
+    PUSHBUTTON      "Cancel",IDCANCEL,283,127,50,14
+    PUSHBUTTON      "Help",IDHELP,338,127,50,14
 END
 
 IDD_EXPORT DIALOGEX 0, 0, 300, 183
@@ -2585,7 +2586,7 @@ BEGIN
         VERTGUIDE, 358
         VERTGUIDE, 366
         TOPMARGIN, 7
-        BOTTOMMARGIN, 126
+        BOTTOMMARGIN, 141
     END
 
     IDD_EXPORT, DIALOG

src/TortoiseProc/AppUtils.cpp
@@ -2708,6 +2708,9 @@ bool CAppUtils::DoPush(HWND hWnd, bool autoloadKey, bool pack, bool tags, bool a
 {
 	CString error;
 	DWORD exitcode = 0xFFFFFFFF;
+	ProjectProperties pp;
+	pp.ReadProps();
+	CHooks::Instance().SetProjectProperties(g_Git.m_CurrentDir, pp);
 	if (CHooks::Instance().PrePush(g_Git.m_CurrentDir, exitcode, error))
 	{
 		if (exitcode)
@@ -2800,6 +2803,9 @@ bool CAppUtils::DoPush(HWND hWnd, bool autoloadKey, bool pack, bool tags, bool a
 		// need to execute hooks as those might be needed by post action commands
 		DWORD exitcode = 0xFFFFFFFF;
 		CString error;
+		ProjectProperties pp;
+		pp.ReadProps();
+		CHooks::Instance().SetProjectProperties(g_Git.m_CurrentDir, pp);
 		if (CHooks::Instance().PostPush(g_Git.m_CurrentDir, exitcode, error))
 		{
 			if (exitcode)

src/TortoiseProc/CommitDlg.cpp
@@ -657,6 +657,7 @@ void CCommitDlg::OnOK()
 	if (CAppUtils::MessageContainsConflictHints(GetSafeHwnd(), m_sLogMessage))
 		return;
 
+	CHooks::Instance().SetProjectProperties(g_Git.m_CurrentDir, m_ProjectProperties);
 	if (CHooks::Instance().IsHookPresent(pre_commit_hook, g_Git.m_CurrentDir)) {
 		DWORD exitcode = 0xFFFFFFFF;
 		CString error;
@@ -1152,6 +1153,7 @@ void CCommitDlg::OnOK()
 		{
 			DWORD exitcode = 0xFFFFFFFF;
 			CString error;
+			CHooks::Instance().SetProjectProperties(g_Git.m_CurrentDir, m_ProjectProperties);
 			if (CHooks::Instance().PostCommit(g_Git.m_CurrentDir, amend.IsEmpty(), exitcode, error))
 			{
 				if (exitcode)
@@ -2895,6 +2897,7 @@ bool CCommitDlg::RunStartCommitHook()
 {
 	DWORD exitcode = 0xFFFFFFFF;
 	CString error;
+	CHooks::Instance().SetProjectProperties(g_Git.m_CurrentDir, m_ProjectProperties);
 	if (CHooks::Instance().StartCommit(g_Git.m_CurrentDir, m_pathList, m_sLogMessage, exitcode, error))
 	{
 		if (exitcode)

src/TortoiseProc/ProjectProperties.cpp
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2011, 2013-2017 - TortoiseGit
+// Copyright (C) 2003-2011, 2013-2018 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -128,6 +128,13 @@ int ProjectProperties::ReadProps()
 			nMinLogSize = _wtoi(val);
 	}
 
+	FetchHookString(gitconfig, PROJECTPROPNAME_STARTCOMMITHOOK, sStartCommitHook);
+	FetchHookString(gitconfig, PROJECTPROPNAME_PRECOMMITHOOK, sPreCommitHook);
+	FetchHookString(gitconfig, PROJECTPROPNAME_POSTCOMMITHOOK, sPostCommitHook);
+	FetchHookString(gitconfig, PROJECTPROPNAME_PREPUSHHOOK, sPrePushHook);
+	FetchHookString(gitconfig, PROJECTPROPNAME_POSTPUSHHOOK, sPostPushHook);
+	FetchHookString(gitconfig, PROJECTPROPNAME_PREREBASEHOOK, sPreRebaseHook);
+
 	return 0;
 }
 
@@ -224,6 +231,28 @@ void ProjectProperties::AutoUpdateRegex()
 
 		regExNeedUpdate = false;
 	}
+}
+
+void ProjectProperties::FetchHookString(CAutoConfig& gitconfig, const CString& sBase, CString& sHook)
+{
+	sHook.Empty();
+	CString sVal;
+	gitconfig.GetString(sBase + L"cmdline", sVal);
+	if (sVal.IsEmpty())
+		return;
+	sHook += sVal + L'\n';
+	bool boolval = false;
+	gitconfig.GetBool(sBase + L"wait", boolval);
+	if (boolval)
+		sHook += L"true\n";
+	else
+		sHook += L"false\n";
+	boolval = false;
+	gitconfig.GetBool(sBase + L"show", boolval);
+	if (boolval)
+		sHook += L"true";
+	else
+		sHook += L"false";
 }
 
 std::vector<CHARRANGE> ProjectProperties::FindBugIDPositions(const CString& msg)

src/TortoiseProc/ProjectProperties.h
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009, 2011-2014, 2017 - TortoiseGit
+// Copyright (C) 2009, 2011-2014, 2018 - TortoiseGit
 // Copyright (C) 2003-2008,2011-2012 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -41,6 +41,13 @@
 
 #define PROJECTPROPNAME_WEBVIEWER_REV		L"webviewer.revision"
 #define PROJECTPROPNAME_WEBVIEWER_PATHREV	L"webviewer.pathrevision"
+
+#define PROJECTPROPNAME_STARTCOMMITHOOK		L"hook.startcommit."
+#define PROJECTPROPNAME_PRECOMMITHOOK		L"hook.precommit."
+#define PROJECTPROPNAME_POSTCOMMITHOOK		L"hook.postcommit."
+#define PROJECTPROPNAME_PREPUSHHOOK			L"hook.prepush."
+#define PROJECTPROPNAME_POSTPUSHHOOK		L"hook.postpush."
+#define PROJECTPROPNAME_PREREBASEHOOK		L"hook.prerebase."
 
 /**
  * \ingroup TortoiseProc
@@ -195,11 +202,26 @@ public:
 	CString		sProviderUuid;
 	CString		sProviderUuid64;
 
+	/// multi line string containing the data for a start-commit-hook
+	CString		sStartCommitHook;
+	/// multi line string containing the data for a pre-commit-hook
+	CString		sPreCommitHook;
+	/// multi line string containing the data for a post-commit-hook
+	CString		sPostCommitHook;
+	/// multi line string containing the data for a pre-push-hook
+	CString		sPrePushHook;
+	/// multi line string containing the data for a post-push-hook
+	CString		sPostPushHook;
+	/// multi line string containing the data for a pre-rebase-hook
+	CString		sPreRebaseHook;
+
 private:
 	/**
 	 * Constructing regex objects is expensive. Therefore, cache them here.
 	 */
 	void AutoUpdateRegex();
+
+	void FetchHookString(CAutoConfig& gitconfig, const CString& sBase, CString& sHook);
 
 	bool regExNeedUpdate;
 	std::wregex regCheck;

commit 562cf853a79007ad0f0ac4576b305e6bc1e127ca

Do not execute disabled hooks
Regression introduced in commit a52b72da377b9bd79a2ebfccac7a6eb0195e3d66.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/Hooks.cpp
@@ -451,14 +451,14 @@ hookiterator CHooks::FindItem(hooktype t, const CString& workingTree)
 	key.htype = t;
 	key.path = m_RootPath;
 	it = find(key);
-	if (it != end())
+	if (it != end() && it->second.bEnabled)
 		return it;
 
 	// look for a script with a path as '*'
 	key.htype = t;
 	key.path = CTGitPath(L"*");
 	it = find(key);
-	if (it != end())
+	if (it != end() && it->second.bEnabled)
 		return it;
 
 	return end();