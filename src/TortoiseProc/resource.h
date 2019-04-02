96d27cfcd54781e0bb87c5ddc30acfa2677e2be8
Vitaliy Grabchuk <vitaliygrabchuk@gmail.com>
2019-04-02 13:23:46 +0300

    Added "Persistent changelists" checkbox
    Signed-off-by: Vitaliy Grabchuk <vitaliygrabchuk@gmail.com>

--- src/TortoiseProc/resource.h
+++ src/TortoiseProc/resource.h
@@ -1611,6 +1611,7 @@
 #define IDS_LOG_FILTER_BY               1850
 #define IDC_STATICELEVATIONNEEDED       1850
 #define IDS_LOG_FILTER_PATHS            1851
+#define IDC_PERSISTENTCHANGELISTS       1851
 #define IDS_LOG_FILTER_AUTHORS          1852
 #define IDS_LOG_FILTER_MESSAGES         1853
 #define IDS_LOG_FILTER_REVS             1854
@@ -2059,9 +2060,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        363
+#define _APS_NEXT_RESOURCE_VALUE        364
 #define _APS_NEXT_COMMAND_VALUE         32874
-#define _APS_NEXT_CONTROL_VALUE         1851
+#define _APS_NEXT_CONTROL_VALUE         1852
 #define _APS_NEXT_SYMED_VALUE           201
 #endif
 #endif
