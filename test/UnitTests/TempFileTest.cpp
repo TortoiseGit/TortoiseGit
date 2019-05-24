TortoiseGit/TortoiseGit

commit a6a582153effba19759282de3f757f14be463bb3

Fix intellisense error "invalid nontype template argument of type `HANDLE`"
Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>

src/Utils/SmartHandle.h
@@ -19,6 +19,16 @@
 //
 #pragma once
 
+template <typename type>
+struct CDefaultHandleNull
+{
+	static constexpr type DefaultHandle = nullptr;
+};
+
+struct CDefaultHandleInvalid
+{
+	static constexpr HANDLE DefaultHandle = INVALID_HANDLE_VALUE;
+};
 
 /**
  * \ingroup Utils
@@ -26,12 +36,12 @@
  */
 template <typename HandleType,
 	template <class> class CloseFunction,
-	HandleType NULL_VALUE = nullptr>
+	typename NullType = CDefaultHandleNull<HandleType>>
 class CSmartHandle
 {
 public:
 	CSmartHandle()
-		: m_Handle(NULL_VALUE)
+		: m_Handle(NullType::DefaultHandle)
 	{
 	}
 
@@ -70,7 +80,7 @@ public:
 	HandleType Detach()
 	{
 		HandleType p = m_Handle;
-		m_Handle = NULL_VALUE;
+		m_Handle = NullType::DefaultHandle;
 
 		return p;
 	}
@@ -92,7 +102,7 @@ public:
 
 	bool IsValid() const
 	{
-		return m_Handle != NULL_VALUE;
+		return m_Handle != NullType::DefaultHandle;
 	}
 
 
@@ -110,10 +120,10 @@ private:
 protected:
 	bool CleanUp()
 	{
-		if ( m_Handle != NULL_VALUE )
+		if (m_Handle != NullType::DefaultHandle)
 		{
 			const bool b = CloseFunction<HandleType>::Close(m_Handle);
-			m_Handle = NULL_VALUE;
+			m_Handle = NullType::DefaultHandle;
 			return b;
 		}
 		return false;
@@ -187,8 +197,8 @@ typedef CSmartHandle<HANDLE,	CCloseHandle>											CAutoGeneralHandle;
 typedef CSmartHandle<HKEY,		CCloseRegKey>											CAutoRegKey;
 typedef CSmartHandle<PVOID,		CCloseViewOfFile>										CAutoViewOfFile;
 typedef CSmartHandle<HMODULE,	CCloseLibrary>											CAutoLibrary;
-typedef CSmartHandle<HANDLE,	CCloseHandle, INVALID_HANDLE_VALUE>						CAutoFile;
-typedef CSmartHandle<HANDLE,	CCloseFindFile, INVALID_HANDLE_VALUE>					CAutoFindFile;
+typedef CSmartHandle<HANDLE,	CCloseHandle, CDefaultHandleInvalid>					CAutoFile;
+typedef CSmartHandle<HANDLE,	CCloseFindFile, CDefaultHandleInvalid>					CAutoFindFile;
 typedef CSmartHandle<FILE*,		CCloseFILE>												CAutoFILE;
 
 /*

commit bef14476b54833069f99c603c23b5dc9ff441057

Add test for unique filenames generated from CTempFiles
This basically tests CAutoFile::IsValid() which was broken in commit a6a582153effba19759282de3f757f14be463bb3.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

test/UnitTests/TempFileTest.cpp
@@ -1,0 +1,34 @@
+﻿// TortoiseGit - a Windows shell extension for easy version control
+
+// Copyright (C) 2019 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "stdafx.h"
+#include "TempFile.h"
+
+TEST(CTempFiles, UniqueName)
+{
+	auto path1 = CTempFiles::Instance().GetTempFilePath(false, CTGitPath(L"something.cpp"));
+	EXPECT_TRUE(path1.Exists());
+	auto path2 = CTempFiles::Instance().GetTempFilePath(false, CTGitPath(L"something.cpp"));
+	EXPECT_TRUE(path2.Exists());
+
+	EXPECT_STRNE(path1.GetWinPathString(), path2.GetWinPathString());
+
+	path1.Delete(false, false);
+	path2.Delete(false, false);
+}

test/UnitTests/UnitTests.vcxproj
@@ -174,6 +174,7 @@
       <PrecompiledHeader>Create</PrecompiledHeader>
     </ClCompile>
     <ClCompile Include="StringUtilsTest.cpp" />
+    <ClCompile Include="TempFileTest.cpp" />
     <ClCompile Include="TGitPathTest.cpp" />
     <ClCompile Include="UniqueQueueTests.cpp" />
     <ClCompile Include="UnitTests.cpp" />

test/UnitTests/UnitTests.vcxproj.filters
@@ -367,5 +367,8 @@
     <ClCompile Include="PatchTest.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="TempFileTest.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
   </ItemGroup>
 </Project>