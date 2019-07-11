c18572c122305cd2a6a0265f112caf7d3a0ddfad
Sven Strickroth <email@cs-ware.de>
2019-07-11 16:57:39 +0200

    Initialize member
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Commands/Command.h
+++ src/TortoiseProc/Commands/Command.h
@@ -1,6 +1,6 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2009, 2011, 2013-2014, 2016, 2018 - TortoiseGit
+// Copyright (C) 2008-2009, 2011, 2013-2014, 2016, 2018-2019 - TortoiseGit
 // Copyright (C) 2007 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -81,7 +81,7 @@ protected:
 	CTGitPath				orgCmdLinePath;
 
 private:
-	HWND					hwndExplorer;
+	HWND					hwndExplorer = nullptr;
 };
 
 /**
