TortoiseGit/TortoiseGit

commit 9d4942fe4c38154d82983834b3518381aa1b9a94

Cleanup
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/GitDiff.cpp
@@ -132,8 +132,7 @@ int CGitDiff::DiffNull(HWND hWnd, const CTGitPath* pPath, const CString& rev1, b
 	CString tempfile = CTempFiles::Instance().GetTempFilePath(false, *pPath, rev1Hash).GetWinPathString();
 	::SetFileAttributes(tempfile, FILE_ATTRIBUTE_READONLY);
 
-	CAppUtils::DiffFlags flags;
-	flags.bAlternativeTool = bAlternative;
+	auto flags = CAppUtils::DiffFlags().AlternativeTool();
 	if(bIsAdd)
 		CAppUtils::StartExtDiff(tempfile,file1,
 							pPath->GetGitPathString(),
@@ -441,8 +440,6 @@ int CGitDiff::Diff(HWND hWnd, const CTGitPath* pPath, const CTGitPath* pPath2, c
 		title2.Format(IDS_DIFF_WCNAME, static_cast<LPCTSTR>(pPath2->GetGitPathString()));
 	}
 
-	CAppUtils::DiffFlags flags;
-	flags.bAlternativeTool = bAlternativeTool;
 	CAppUtils::StartExtDiff(file2,file1,
 							title2,
 							title1,
@@ -450,7 +447,7 @@ int CGitDiff::Diff(HWND hWnd, const CTGitPath* pPath, const CTGitPath* pPath2, c
 							g_Git.CombinePath(pPath),
 							rev2Hash,
 							rev1Hash,
-							flags, jumpToLine);
+							CAppUtils::DiffFlags().AlternativeTool(), jumpToLine);
 
 	return 0;
 }

src/TortoiseProc/DeleteConflictDlg.cpp
@@ -127,8 +127,6 @@ void CDeleteConflictDlg::OnBnClickedShowdiff()
 {
 	CString base;
 	base.LoadString(IDS_PROC_DIFF_BASE);
-	CAppUtils::DiffFlags flags;
-	flags.bAlternativeTool = !!(GetAsyncKeyState(VK_SHIFT) & 0x8000);
 	CAppUtils::StartExtDiff(m_FileBaseVersion.GetWinPathString(), g_Git.CombinePath(m_File),
 		base,
 		m_bDiffMine ? m_LocalHash.ToString() : m_RemoteHash.ToString(),
@@ -136,5 +134,5 @@ void CDeleteConflictDlg::OnBnClickedShowdiff()
 		g_Git.CombinePath(m_File),
 		m_bDiffMine ? m_LocalHash : m_RemoteHash,
 		m_bDiffMine ? m_RemoteHash : m_LocalHash,
-		flags);
+		CAppUtils::DiffFlags().AlternativeTool(!!(GetAsyncKeyState(VK_SHIFT) & 0x8000)));
 }

src/TortoiseProc/GitLogListAction.cpp
@@ -425,8 +425,7 @@ void CGitLogList::ContextMenuAction(int cmd, int FirstSelect, int LastSelect, CM
 					MessageBox(L"Could not generate patch for commit " + pLastEntry->m_CommitHash.ToString() + L".\n" + err, L"TortoiseGit", MB_ICONERROR);
 					break;
 				}
-				CAppUtils::DiffFlags flags;
-				CAppUtils::StartExtDiff(patch1, patch2, pFirstEntry->m_CommitHash.ToString(), pLastEntry->m_CommitHash.ToString(), pFirstEntry->m_CommitHash.ToString() + L".patch", pLastEntry->m_CommitHash.ToString() + L".patch", pFirstEntry->m_CommitHash, pLastEntry->m_CommitHash, flags);
+				CAppUtils::StartExtDiff(patch1, patch2, pFirstEntry->m_CommitHash.ToString(), pLastEntry->m_CommitHash.ToString(), pFirstEntry->m_CommitHash.ToString() + L".patch", pLastEntry->m_CommitHash.ToString() + L".patch", pFirstEntry->m_CommitHash, pLastEntry->m_CommitHash, CAppUtils::DiffFlags());
 			}
 			break;
 		case ID_LOG_VIEWRANGE:

commit 80a56c37bfaeb4c338f30b127515c75dc655ac09

Don't always use alternative diff tool
Regression introduced in commit 9d4942fe4c38154d82983834b3518381aa1b9a94.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/GitDiff.cpp
@@ -132,7 +132,7 @@ int CGitDiff::DiffNull(HWND hWnd, const CTGitPath* pPath, const CString& rev1, b
 	CString tempfile = CTempFiles::Instance().GetTempFilePath(false, *pPath, rev1Hash).GetWinPathString();
 	::SetFileAttributes(tempfile, FILE_ATTRIBUTE_READONLY);
 
-	auto flags = CAppUtils::DiffFlags().AlternativeTool();
+	auto flags = CAppUtils::DiffFlags().AlternativeTool(bAlternative);
 	if(bIsAdd)
 		CAppUtils::StartExtDiff(tempfile,file1,
 							pPath->GetGitPathString(),
@@ -447,7 +447,7 @@ int CGitDiff::Diff(HWND hWnd, const CTGitPath* pPath, const CTGitPath* pPath2, c
 							g_Git.CombinePath(pPath),
 							rev2Hash,
 							rev1Hash,
-							CAppUtils::DiffFlags().AlternativeTool(), jumpToLine);
+							CAppUtils::DiffFlags().AlternativeTool(bAlternativeTool), jumpToLine);
 
 	return 0;
 }