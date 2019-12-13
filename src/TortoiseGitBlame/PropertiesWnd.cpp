TortoiseGit/TortoiseGit

commit f72870d403408853816711966a420357ea71f3c0

make it compile again
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseGitBlame/PropertiesWnd.cpp
@@ -352,16 +352,16 @@ void CPropertiesWnd::UpdateProperties(GitRev *rev)
 	if(rev)
 	{
 		m_CommitHash->SetValue(rev->m_CommitHash.ToString());
-		m_AuthorName->SetValue(rev->m_AuthorName);
-		m_AuthorDate->SetValue(rev->m_AuthorDate.Format(_T("%Y-%m-%d %H:%M")));
-		m_AuthorEmail->SetValue(rev->m_AuthorEmail);	
+		m_AuthorName->SetValue(rev->GetAuthorName());
+		m_AuthorDate->SetValue(rev->GetAuthorDate().Format(_T("%Y-%m-%d %H:%M")));
+		m_AuthorEmail->SetValue(rev->GetAuthorEmail());	
 
-		m_CommitterName->SetValue(rev->m_CommitterName);
-		m_CommitterEmail->SetValue(rev->m_CommitterEmail);
-		m_CommitterDate->SetValue(rev->m_CommitterDate.Format(_T("%Y-%m-%d %H:%M")));
+		m_CommitterName->SetValue(rev->GetAuthorName());
+		m_CommitterEmail->SetValue(rev->GetCommitterEmail());
+		m_CommitterDate->SetValue(rev->GetCommitterDate().Format(_T("%Y-%m-%d %H:%M")));
 
-		m_Subject->SetValue(rev->m_Subject);
-		m_Body->SetValue(rev->m_Body.Trim());
+		m_Subject->SetValue(rev->GetSubject());
+		m_Body->SetValue(rev->GetBody().Trim());
 
 		RemoveParent();
 
@@ -386,7 +386,7 @@ void CPropertiesWnd::UpdateProperties(GitRev *rev)
 				p= &pLogEntry->m_pLogCache->m_HashMap[rev->m_ParentHash[i]] ;
 			}
 			if(p)
-				parentsubject=p->m_Subject;
+				parentsubject=p->GetSubject();
 
 			str.Format(_T("%d - %s \n %s"),i,rev->m_ParentHash[i].ToString(),parentsubject);
 			

src/TortoiseGitBlame/TortoiseGitBlameView.cpp
@@ -2590,7 +2590,7 @@ void CTortoiseGitBlameView::UpdateInfo(int Encode)
 			this->m_NoListCommit[hash].GetCommitFromHash(hash);
 		}
 		m_ID.push_back(-1); // m_ID is calculated lazy on demand
-		m_Authors.push_back(m_NoListCommit[hash].m_AuthorName);
+		m_Authors.push_back(m_NoListCommit[hash].GetAuthorName());
 
 		m_CommitHash.push_back(hash);
 		pos = current+1;
@@ -2777,10 +2777,10 @@ void CTortoiseGitBlameView::OnMouseHover(UINT nFlags, CPoint point)
 
 			CString str;
 			str.Format(_T("%s\n<b>%s</b>\n%s %s\n%s"),pRev->m_CommitHash.ToString(),
-													   pRev->m_Subject,
-													   pRev->m_AuthorName,
-													   CAppUtils::FormatDateAndTime( pRev->m_AuthorDate, m_DateFormat ), 
-													   pRev->m_Body);
+													   pRev->GetSubject(),
+													   pRev->GetAuthorName(),
+													   CAppUtils::FormatDateAndTime( pRev->GetAuthorDate(), m_DateFormat ), 
+													   pRev->GetBody());
 			m_ToolTip.AddTool(this,str);
 			m_ToolTip.DisplayToolTip(&point);
 	

src/TortoiseShell/SVNPropertyPage.cpp
@@ -298,9 +298,9 @@ void CGitPropertyPage::InitWorkfileView()
 		rev.GetCommit(CString(_T("HEAD")));
 		
 		SetDlgItemText(m_hwnd,IDC_HEAD_HASH,rev.m_CommitHash.ToString());
-		SetDlgItemText(m_hwnd,IDC_HEAD_SUBJECT,rev.m_Subject);
-		SetDlgItemText(m_hwnd,IDC_HEAD_AUTHOR,rev.m_AuthorName);
-		SetDlgItemText(m_hwnd,IDC_HEAD_DATE,rev.m_AuthorDate.Format(_T("%Y-%m-%d %H:%M:%S")));
+		SetDlgItemText(m_hwnd,IDC_HEAD_SUBJECT,rev.GetSubject());
+		SetDlgItemText(m_hwnd,IDC_HEAD_AUTHOR,rev.GetAuthorName());
+		SetDlgItemText(m_hwnd,IDC_HEAD_DATE,rev.GetAuthorDate().Format(_T("%Y-%m-%d %H:%M:%S")));
 
 		if (filenames.size() == 1)
 		{
@@ -351,9 +351,9 @@ void CGitPropertyPage::InitWorkfileView()
 			}
 
 			SetDlgItemText(m_hwnd,IDC_LAST_HASH,rev.m_CommitHash.ToString());
-			SetDlgItemText(m_hwnd,IDC_LAST_SUBJECT,rev.m_Subject);
-			SetDlgItemText(m_hwnd,IDC_LAST_AUTHOR,rev.m_AuthorName);
-			SetDlgItemText(m_hwnd,IDC_LAST_DATE,rev.m_AuthorDate.Format(_T("%Y-%m-%d %H:%M:%S")));
+			SetDlgItemText(m_hwnd,IDC_LAST_SUBJECT,rev.GetSubject());
+			SetDlgItemText(m_hwnd,IDC_LAST_AUTHOR,rev.GetAuthorName());
+			SetDlgItemText(m_hwnd,IDC_LAST_DATE,rev.GetAuthorDate().Format(_T("%Y-%m-%d %H:%M:%S")));
 
 		}else
 		{

commit 8b0d69f576ea46f4cc13b906f10998c308e0dea2

Correctly show committer name instead of author name in properties for committer name
Regression introduced in f72870d403408853816711966a420357ea71f3c0.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseGitBlame/PropertiesWnd.cpp
@@ -251,7 +251,7 @@ void CPropertiesWnd::UpdateProperties(GitRevLoglist* pRev)
 		m_AuthorDate->SetValue(authorDate);
 		m_AuthorEmail->SetValue(pRev->GetAuthorEmail());
 
-		m_CommitterName->SetValue(pRev->GetAuthorName());
+		m_CommitterName->SetValue(pRev->GetCommitterName());
 		m_CommitterEmail->SetValue(pRev->GetCommitterEmail());
 		CString committerDate = pRev->GetCommitterDate().Format(L"%Y-%m-%d %H:%M");
 		m_CommitterDate->SetValue(committerDate);