ba8f50d20e6d675b90a810e7f312f39324128a80
Sven Strickroth <email@cs-ware.de>
2020-01-03 21:26:16 +0100

    Make sure correct type for GetParentHwnd() is used
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoisePlink/Windows/WINPLINK.C
+++ src/TortoisePlink/Windows/WINPLINK.C
@@ -15,6 +15,8 @@
 
 #define WM_AGENT_CALLBACK (WM_APP + 4)
 
+extern HWND GetParentHwnd();
+
 #include <commctrl.h>
 #pragma comment(lib, "comctl32.lib")
 #pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
