TortoiseGit/TortoiseGit

commit b35cbe4f4b67300cdb030eb6ad243d3d53a8d031

Allow to use Mailmap everywhere
This also activates Mailmap for GitWCRev.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitRevRefBrowser.cpp
@@ -23,6 +23,7 @@
 #include "Git.h"
 #include "GitRevRefBrowser.h"
 #include "StringUtils.h"
+#include "GitMailmap.h"
 
 GitRevRefBrowser::GitRevRefBrowser() : GitRev()
 {
@@ -53,13 +54,14 @@ int GitRevRefBrowser::GetGitRevRefMap(MAP_REF_GITREVREFBROWSER& map, int mergefi
 	}
 
 	CString cmd;
-	cmd.Format(L"git.exe for-each-ref%s --format=\"%%(refname)%%04 %%(objectname)%%04 %%(upstream)%%04 %%(subject)%%04 %%(authorname)%%04 %%(authordate:raw)%%04 %%(creator)%%04 %%(creatordate:raw)%%03\"", static_cast<LPCTSTR>(args));
+	cmd.Format(L"git.exe for-each-ref%s --format=\"%%(refname)%%04 %%(objectname)%%04 %%(upstream)%%04 %%(subject)%%04 %%(authorname)%%04 %%(authoremail)%%04 %%(authordate:raw)%%04 %%(creator)%%04 %%(creatordate:raw)%%03\"", static_cast<LPCTSTR>(args));
 	CString allRefs;
 	if (g_Git.Run(cmd, &allRefs, &err, CP_UTF8))
 		return -1;
 
 	int linePos = 0;
 	CString singleRef;
+	CGitMailmap mailmap;
 	while (!(singleRef = allRefs.Tokenize(L"\03", linePos)).IsEmpty())
 	{
 		singleRef.TrimLeft(L"\r\n");
@@ -74,15 +76,20 @@ int GitRevRefBrowser::GetGitRevRefMap(MAP_REF_GITREVREFBROWSER& map, int mergefi
 		ref.m_UpstreamRef = singleRef.Tokenize(L"\04", valuePos).Trim(); if (valuePos < 0) continue;
 		ref.m_Subject = singleRef.Tokenize(L"\04", valuePos).Trim(); if (valuePos < 0) continue;
 		ref.m_AuthorName = singleRef.Tokenize(L"\04", valuePos).Trim(); if (valuePos < 0) continue;
+		CString email = singleRef.Tokenize(L"\04", valuePos).Trim().Trim(L"<>"); if (valuePos < 0) continue;
 		CString date = singleRef.Tokenize(L"\04", valuePos).Trim();
 		ref.m_AuthorDate = StrToInt(date);
 		if (ref.m_AuthorName.IsEmpty())
 		{
 			ref.m_AuthorName = singleRef.Tokenize(L"\04", valuePos).Trim(); if (valuePos < 0) continue;
-			ref.m_AuthorName = ref.m_AuthorName.Left(ref.m_AuthorName.Find(L" <")).Trim();
+			email = ref.m_AuthorName.Mid(ref.m_AuthorName.Find(L" <") + static_cast<int>(wcslen(L" <")));
+			email.Truncate(email.Find(L'>'));
+			ref.m_AuthorName.Truncate(ref.m_AuthorName.Find(L" <"));
 			date = singleRef.Tokenize(L"\04", valuePos).Trim();
 			ref.m_AuthorDate = StrToInt(date);
 		}
+		if (mailmap)
+			ref.m_AuthorName = mailmap.TranslateAuthor(ref.m_AuthorName, email);
 
 		if (CStringUtils::StartsWith(refName, L"refs/heads/"))
 			ref.m_Description = descriptions[refName.Mid(static_cast<int>(wcslen(L"refs/heads/")))];

src/Git/GitMailmap.cpp
@@ -1,0 +1,98 @@
+﻿// TortoiseGit - a Windows shell extension for easy version control
+
+// Copyright (C) 2013-2019 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "stdafx.h"
+#include "GitMailmap.h"
+
+CGitMailmap::CGitMailmap()
+{
+	if (ShouldLoadMailmap())
+		git_read_mailmap(&m_pMailmap);
+}
+
+CGitMailmap::~CGitMailmap()
+{
+	git_free_mailmap(m_pMailmap);
+}
+
+bool CGitMailmap::ShouldLoadMailmap()
+{
+	return CRegDWORD(L"Software\\TortoiseGit\\LogDialog\\UseMailmap", FALSE) == TRUE;
+}
+
+void CGitMailmap::Translate(CString& name, CString& email) const
+{
+	ASSERT(m_pMailmap);
+	struct payload_struct
+	{
+		const CString* name;
+		const char* authorName;
+	} payload = { &name, nullptr };
+	const char* author1 = nullptr;
+	const char* email1 = nullptr;
+	git_lookup_mailmap(m_pMailmap, &email1, &author1, CUnicodeUtils::GetUTF8(email), &payload,
+					   [](void* payload) -> const char* { return reinterpret_cast<payload_struct*>(payload)->authorName = _strdup(CUnicodeUtils::GetUTF8(*reinterpret_cast<payload_struct*>(payload)->name)); });
+	free((void*)payload.authorName);
+	if (email1)
+		email = CUnicodeUtils::GetUnicode(email1);
+	if (author1)
+		name = CUnicodeUtils::GetUnicode(author1);
+}
+
+static bool GetMailmapMapping(GIT_MAILMAP mailmap, const CString& email, const CString& name, bool returnEmail, CString& ret)
+{
+	struct payload_struct
+	{
+		const CString* name;
+		const char* authorName;
+	} payload = { &name, nullptr };
+	const char* author1 = nullptr;
+	const char* email1 = nullptr;
+	if (git_lookup_mailmap(mailmap, &email1, &author1, CUnicodeUtils::GetUTF8(email), &payload,
+						   [](void* payload) -> const char* { return reinterpret_cast<payload_struct*>(payload)->authorName = _strdup(CUnicodeUtils::GetUTF8(*reinterpret_cast<payload_struct*>(payload)->name)); }) == -1)
+		return false;
+	free((void*)payload.authorName);
+	if (returnEmail && email1)
+		ret = CUnicodeUtils::GetUnicode(email1);
+	else if (returnEmail)
+		return false;
+	else if (author1)
+		ret = CUnicodeUtils::GetUnicode(author1);
+	else
+		return false;
+	return true;
+}
+
+const CString CGitMailmap::TranslateAuthor(const CString& name, const CString& email) const
+{
+	ASSERT(m_pMailmap);
+	CString ret;
+	if (GetMailmapMapping(m_pMailmap, email, name, false, ret))
+		return ret;
+	return name;
+}
+
+const CString CGitMailmap::TranslateEmail(const CString& name, const CString& email) const
+{
+	ASSERT(m_pMailmap);
+	CString ret;
+	if (GetMailmapMapping(m_pMailmap, email, name, true, ret))
+		return ret;
+	return email;
+}

src/Git/GitMailmap.h
@@ -1,0 +1,41 @@
+﻿// TortoiseGit - a Windows shell extension for easy version control
+
+// Copyright (C) 2019 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#pragma once
+#include "gitdll.h"
+
+class CGitMailmap {
+public:
+	CGitMailmap();
+	~CGitMailmap();
+
+	CGitMailmap& operator=(CGitMailmap&) = delete;
+	CGitMailmap(const CGitMailmap&) = delete;
+
+	operator bool() const { return m_pMailmap; }
+
+	const CString TranslateAuthor(const CString& name, const CString& email) const;
+	const CString TranslateEmail(const CString& name, const CString& email) const;
+	void Translate(CString& name, CString& email) const;
+
+	static bool ShouldLoadMailmap();
+
+private:
+	GIT_MAILMAP m_pMailmap = nullptr;
+};

src/Git/GitRev.cpp
@@ -299,3 +299,11 @@ int GitRev::GetCommit(const CString& refname)
 	CGitHash hash = CGitHash::FromRaw(sha);
 	return GetCommitFromHash_withoutLock(hash);
 }
+
+void GitRev::ApplyMailmap(const CGitMailmap& mailmap)
+{
+	if (!mailmap)
+		return;
+	mailmap.Translate(m_AuthorName, m_AuthorEmail);
+	mailmap.Translate(m_CommitterName, m_CommitterEmail);
+}

src/Git/GitRev.h
@@ -22,6 +22,7 @@
 #include "AtlTime.h"
 #include "GitHash.h"
 #include "GitDll.h"
+#include "GitMailmap.h"
 
 typedef std::vector<CGitHash> GIT_REV_LIST;
 
@@ -127,6 +128,8 @@ public:
 	int GetCommit(const CString& rev);
 
 	CString GetLastErr() const { return m_sErr; }
+
+	void ApplyMailmap(const CGitMailmap& mailmap = CGitMailmap());
 
 	void DbgPrint();
 

src/Git/GitRevLoglist.cpp
@@ -26,6 +26,8 @@
 
 typedef CComCritSecLock<CComCriticalSection> CAutoLocker;
 
+std::shared_ptr<CGitMailmap> GitRevLoglist::s_Mailmap = nullptr;
+
 GitRevLoglist::GitRevLoglist(void) : GitRev()
 , m_Action(0)
 , m_RebaseAction(0)

src/Git/GitRevLoglist.h
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2018 - TortoiseGit
+// Copyright (C) 2008-2019 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -55,6 +55,8 @@ public:
 
 	// Show version tree Graphic
 	std::vector<int> m_Lanes;
+
+	static std::shared_ptr<CGitMailmap> s_Mailmap;
 
 	volatile LONG m_IsFull;
 	volatile LONG m_IsUpdateing;
@@ -114,6 +116,9 @@ protected:
 		if (!m_IsCommitParsed && m_GitCommit.m_pGitCommit)
 		{
 			ParserFromCommit(&m_GitCommit);
+			auto mailmap = s_Mailmap;
+			if (mailmap)
+				ApplyMailmap(*mailmap);
 			InterlockedExchange(&m_IsCommitParsed, TRUE);
 			git_free_commit(&m_GitCommit);
 			if (m_IsDiffFiles)

src/GitWCRev/status.cpp
@@ -290,9 +290,23 @@ int GetStatus(const TCHAR* path, GitWCRev_t& GitStat)
 		return ERR_GIT_ERR;
 
 	const git_signature* sig = git_commit_author(commit);
-	GitStat.HeadAuthor = sig->name;
-	GitStat.HeadEmail = sig->email;
 	GitStat.HeadTime = sig->when.time;
+	if (CRegStdDWORD(L"Software\\TortoiseGit\\LogDialog\\UseMailmap", FALSE) == TRUE)
+	{
+		CAutoMailmap mailmap;
+		if (git_mailmap_from_repository(mailmap.GetPointer(), repo))
+			return ERR_GIT_ERR;
+		CAutoSignature resolvedSignature;
+		if (git_mailmap_resolve_signature(resolvedSignature.GetPointer(), mailmap, sig))
+			return ERR_GIT_ERR;
+		GitStat.HeadAuthor = (*resolvedSignature).name;
+		GitStat.HeadEmail = (*resolvedSignature).email;
+	}
+	else
+	{
+		GitStat.HeadAuthor = sig->name;
+		GitStat.HeadEmail = sig->email;
+	}
 
 #pragma warning(push)
 #pragma warning(disable: 4510 4512 4610)

commit 812531c7dee9b2c6dc4c5007c13fa394f2f51363

Fix possible crash if there is no author
Issue introduced in commit b35cbe4f4b67300cdb030eb6ad243d3d53a8d031.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitRevRefBrowser.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2019 - TortoiseGit
+// Copyright (C) 2009-2020 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -83,8 +83,8 @@ int GitRevRefBrowser::GetGitRevRefMap(MAP_REF_GITREVREFBROWSER& map, int mergefi
 		{
 			ref.m_AuthorName = singleRef.Tokenize(L"\04", valuePos).Trim(); if (valuePos < 0) continue;
 			email = ref.m_AuthorName.Mid(ref.m_AuthorName.Find(L" <") + static_cast<int>(wcslen(L" <")));
-			email.Truncate(email.Find(L'>'));
-			ref.m_AuthorName.Truncate(ref.m_AuthorName.Find(L" <"));
+			email.Truncate(max(0, email.Find(L'>')));
+			ref.m_AuthorName.Truncate(max(0, ref.m_AuthorName.Find(L" <")));
 			date = singleRef.Tokenize(L"\04", valuePos).Trim();
 			ref.m_AuthorDate = StrToInt(date);
 		}