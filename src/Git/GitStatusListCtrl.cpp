TortoiseGit/TortoiseGit

commit 45f4d3c1433f8989113cffd4c6b51fcd6a2aca32

Provide more information about branch being merged/rebased in case of conflict
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -1695,10 +1695,54 @@ bool ParseHashesFromLsFile(const BYTE_VECTOR& out, CString& hash1, CString& hash
 	return false;
 }
 
-bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/, bool revertTheirMy /*= false*/, HWND resolveMsgHwnd /*= nullptr*/)
+static void GetConflictTitles(CString& baseText, CString& mineText, CString& theirsText, bool rebaseActive)
 {
-	bool bRet = false;
+	baseText.LoadString(IDS_PROC_DIFF_BASE);
+	if (rebaseActive)
+	{
+		CString adminDir;
+		GitAdminDir::GetAdminDirPath(g_Git.m_CurrentDir, adminDir);
+		mineText = L"Rebasing onto";
+		if (!CStringUtils::ReadStringFromTextFile(adminDir + L"tgitrebase.active\\onto", mineText))
+		{
+			CGitHash hash;
+			if (!g_Git.GetHash(hash, L"rebase-apply/onto"))
+				g_Git.GuessRefForHash(mineText, hash);
+		}
+		theirsText = L"Branch being rebased";
+		if (!CStringUtils::ReadStringFromTextFile(adminDir + L"tgitrebase.active\\head-name", theirsText))
+		{
+			if (CStringUtils::ReadStringFromTextFile(adminDir + L"rebase-apply/head-name", theirsText))
+				theirsText = CGit::StripRefName(theirsText);
+		}
+		return;
+	}
 
+	static const struct {
+		const wchar_t*	headref;
+		bool			guessRef;
+		UINT			theirstext;
+	} infotexts[] = { { L"MERGE_HEAD", true, IDS_CONFLICT_INFOTEXT }, { L"CHERRY_PICK_HEAD", false, IDS_CONFLICT_INFOTEXT }, { L"REVERT_HEAD", false, IDS_CONFLICT_REVERT } };
+	mineText = L"HEAD";
+	theirsText.LoadString(IDS_CONFLICT_REFTOBEMERGED);
+	for (const auto& infotext : infotexts)
+	{
+		CGitHash hash;
+		if (!g_Git.GetHash(hash, infotext.headref))
+		{
+			CString guessedRef;
+			if (!infotext.guessRef)
+				guessedRef = hash.ToString();
+			else
+				g_Git.GuessRefForHash(guessedRef, hash);
+			theirsText.FormatMessage(infotext.theirstext, infotext.headref, (LPCTSTR)guessedRef);
+			break;
+		}
+	}
+}
+
+bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/, bool isRebase /*= false*/, HWND resolveMsgHwnd /*= nullptr*/)
+{
 	CTGitPath merge=path;
 	CTGitPath directory = merge.GetDirectory();
 
@@ -1709,6 +1753,9 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 
 	if (g_Git.Run(cmd, &vector))
 		return FALSE;
+
+	CString baseTitle, mineTitle, theirsTitle;
+	GetConflictTitles(baseTitle, mineTitle, theirsTitle, isRebase);
 
 	if (merge.IsDirectory())
 	{
@@ -1764,7 +1811,7 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 			return FALSE;
 
 		CSubmoduleResolveConflictDlg resolveSubmoduleConflictDialog;
-		resolveSubmoduleConflictDialog.SetDiff(merge.GetGitPathString(), revertTheirMy, baseHash, baseSubject, baseOK, localHash, mineSubject, mineOK, changeTypeMine, remoteHash, theirsSubject, theirsOK, changeTypeTheirs);
+		resolveSubmoduleConflictDialog.SetDiff(merge.GetGitPathString(), isRebase, baseTitle, mineTitle, theirsTitle, baseHash, baseSubject, baseOK, localHash, mineSubject, mineOK, changeTypeMine, remoteHash, theirsSubject, theirsOK, changeTypeTheirs);
 		resolveSubmoduleConflictDialog.DoModal();
 		if (resolveSubmoduleConflictDialog.m_bResolved && resolveMsgHwnd)
 		{
@@ -1789,7 +1836,7 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 	CTGitPath mine;
 	CTGitPath base;
 
-	if (revertTheirMy)
+	if (isRebase)
 	{
 		mine.SetFromGit(GetMergeTempFile(L"REMOTE", merge));
 		theirs.SetFromGit(GetMergeTempFile(L"LOCAL", merge));
@@ -1855,10 +1902,10 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 	if(b_local && b_remote )
 	{
 		merge.SetFromWin(g_Git.CombinePath(merge));
-		if( revertTheirMy )
-			bRet = !!CAppUtils::StartExtMerge(bAlternativeTool, base, mine, theirs, merge, L"BASE", L"REMOTE", L"LOCAL", CString(), false, resolveMsgHwnd, true);
-		else
-			bRet = !!CAppUtils::StartExtMerge(bAlternativeTool, base, theirs, mine, merge, L"BASE", L"REMOTE", L"LOCAL", CString(), false, resolveMsgHwnd, true);
+		if (isRebase)
+			return !!CAppUtils::StartExtMerge(bAlternativeTool, base, mine, theirs, merge, baseTitle, mineTitle, theirsTitle, CString(), false, resolveMsgHwnd, true);
+
+		return !!CAppUtils::StartExtMerge(bAlternativeTool, base, theirs, mine, merge, baseTitle, theirsTitle, mineTitle, CString(), false, resolveMsgHwnd, true);
 	}
 	else
 	{
@@ -1867,37 +1914,19 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 		::DeleteFile(base.GetWinPathString());
 
 		CDeleteConflictDlg dlg;
-		if (!revertTheirMy)
+		if (!isRebase)
 		{
 			DescribeConflictFile(b_local, b_base, dlg.m_LocalStatus);
 			DescribeConflictFile(b_remote, b_base, dlg.m_RemoteStatus);
-			CGitHash localHash, remoteHash;
-			if (!g_Git.GetHash(localHash, L"HEAD"))
-				dlg.m_LocalHash = localHash.ToString();
-			if (!g_Git.GetHash(remoteHash, L"MERGE_HEAD"))
-				dlg.m_RemoteHash = remoteHash.ToString();
-			else if (!g_Git.GetHash(remoteHash, L"rebase-apply/original-commit"))
-				dlg.m_RemoteHash = remoteHash.ToString();
-			else if (!g_Git.GetHash(remoteHash, L"CHERRY_PICK_HEAD"))
-				dlg.m_RemoteHash = remoteHash.ToString();
-			else if (!g_Git.GetHash(remoteHash, L"REVERT_HEAD"))
-				dlg.m_RemoteHash = remoteHash.ToString();
+			dlg.m_LocalHash = mineTitle;
+			dlg.m_RemoteHash = theirsTitle;
 		}
 		else
 		{
 			DescribeConflictFile(b_local, b_base, dlg.m_RemoteStatus);
 			DescribeConflictFile(b_remote, b_base, dlg.m_LocalStatus);
-			CGitHash localHash, remoteHash;
-			if (!g_Git.GetHash(remoteHash, L"HEAD"))
-				dlg.m_RemoteHash = remoteHash.ToString();
-			if (!g_Git.GetHash(localHash, L"MERGE_HEAD"))
-				dlg.m_LocalHash = localHash.ToString();
-			else if (!g_Git.GetHash(localHash, L"rebase-apply/original-commit"))
-				dlg.m_LocalHash = localHash.ToString();
-			else if (!g_Git.GetHash(localHash, L"CHERRY_PICK_HEAD"))
-				dlg.m_LocalHash = localHash.ToString();
-			else if (!g_Git.GetHash(localHash, L"REVERT_HEAD"))
-				dlg.m_LocalHash = localHash.ToString();
+			dlg.m_LocalHash = theirsTitle;
+			dlg.m_RemoteHash = mineTitle;
 		}
 		dlg.m_bShowModifiedButton = b_base;
 		dlg.m_File=merge.GetGitPathString();
@@ -1921,10 +1950,8 @@ bool CAppUtils::ConflictEdit(CTGitPath& path, bool bAlternativeTool /*= false*/,
 			}
 			return TRUE;
 		}
-		else
-			return FALSE;
+		return FALSE;
 	}
-	return bRet;
 }
 
 bool CAppUtils::IsSSHPutty()

Languages/Tortoise.pot
@@ -145,6 +145,11 @@ msgstr ""
 msgid "%1!d! minutes ago"
 msgstr ""
 
+#. Resource IDs: (IDS_CONFLICT_INFOTEXT)
+#, c-format
+msgid "%1!s! (%2!s!)"
+msgstr ""
+
 #. Resource IDs: (IDS_REPOBROWSE_INFO)
 #, c-format
 msgid "%1!s!\nShowing %2!d! files, %3!d! submodules and %4!d! folders, %5!d! items in total"
@@ -3623,16 +3628,16 @@ msgstr ""
 msgid "Delete the existing branch or use a different name."
 msgstr ""
 
+#. Resource IDs: (Dialog IDD_RESOLVE_CONFLICT: Control id 4570)
+msgid "Delete/modify merge conflict"
+msgstr ""
+
 #. Resource IDs: (IDS_DELETEWHENEMPTY_TASK3)
 msgid "Delete\nThe file is removed."
 msgstr ""
 
 #. Resource IDs: (Dialog IDD_COMMITDLG: Control id 1709, Dialog IDD_SETTINGSOVERLAYHANDLERS: Control id 1551, IDS_DELETED, IDS_PATHACTIONS_DELETE, IDS_SVNACTION_DELETE, IDS_SVN_SUMMARIZEDELETED)
 msgid "Deleted"
-msgstr ""
-
-#. Resource IDs: (Dialog IDD_RESOLVE_CONFLICT: Control id 4570)
-msgid "Deleted merge conflict"
 msgstr ""
 
 #. Resource IDs: (IDS_MENUDESCDELETEIGNORE)
@@ -6110,10 +6115,6 @@ msgstr ""
 msgid "Local status"
 msgstr ""
 
-#. Resource IDs: (Dialog IDD_RESOLVE_CONFLICT: Control id 65535)
-msgid "Local:"
-msgstr ""
-
 #. Resource IDs: (IDS_EXPORTFILE_TT)
 msgid "Location where the contents of the\nselected revision of the repository will be saved to."
 msgstr ""
@@ -7077,6 +7078,11 @@ msgstr ""
 msgid "Parent 2"
 msgstr ""
 
+#. Resource IDs: (IDS_CONFLICT_REVERT)
+#, c-format
+msgid "Parent of %2!s!"
+msgstr ""
+
 #. Resource IDs: (IDS_PARENTS)
 msgid "Parent(s)"
 msgstr ""
@@ -7774,7 +7780,7 @@ msgstr ""
 msgid "Remote status"
 msgstr ""
 
-#. Resource IDs: (Dialog IDD_DELETEREMOTETAG: Control id 65535, Dialog IDD_RESOLVE_CONFLICT: Control id 65535, Dialog IDD_SETTINREMOTE: Control id 65535)
+#. Resource IDs: (Dialog IDD_DELETEREMOTETAG: Control id 65535, Dialog IDD_SETTINREMOTE: Control id 65535)
 msgid "Remote:"
 msgstr ""
 
@@ -11032,6 +11038,10 @@ msgstr ""
 
 #. Resource IDs: (Dialog IDD_SETTINGSBUGTRAQ_CONFIG: Control id 65535)
 msgid "bugtraq.warningifnoissue"
+msgstr ""
+
+#. Resource IDs: (IDS_CONFLICT_REFTOBEMERGED)
+msgid "changes to-be-integrated"
 msgstr ""
 
 #. Resource IDs: (IDS_STATUSLIST_CONTEXT_COPYCOL)

src/Git/Git.cpp
@@ -2069,6 +2069,34 @@ int CGit::GetMapHashToFriendName(MAP_HASH_NAME &map)
 	return ret;
 }
 
+int CGit::GuessRefForHash(CString& ref, const CGitHash& hash)
+{
+	MAP_HASH_NAME map;
+	if (GetMapHashToFriendName(map))
+		return -1;
+
+	auto it = map.find(hash);
+	if (it == map.cend())
+	{
+		ref = hash.ToString();
+		return 0;
+	}
+
+	const auto& reflist = it->second;
+	for (const auto& reftype : { L"refs/heads/", L"refs/remotes/", L"refs/tags/" })
+	{
+		auto found = std::find_if(reflist.cbegin(), reflist.cend(), [&reftype](const auto& ref) { return CStringUtils::StartsWith(ref, reftype); });
+		if (found == reflist.cend())
+			continue;
+		
+		GetShortName(*found, ref, reftype);
+		return 0;
+	}
+	
+	ref = hash.ToString();
+	return 0;
+}
+
 int CGit::GetBranchDescriptions(MAP_STRING_STRING& map)
 {
 	CAutoConfig config(true);

src/Git/Git.h
@@ -382,6 +382,7 @@ public:
 	int GetRemoteTags(const CString& remote, STRING_VECTOR& list);
 	int DeleteRemoteRefs(const CString& remote, const STRING_VECTOR& list);
 	int GetBranchDescriptions(MAP_STRING_STRING& map);
+	int GuessRefForHash(CString& ref, const CGitHash& hash);
 	int GetMapHashToFriendName(MAP_HASH_NAME &map);
 	static int GetMapHashToFriendName(git_repository* repo, MAP_HASH_NAME &map);
 

src/Resources/TortoiseProcENG.rc
@@ -1483,25 +1483,23 @@ BEGIN
     PUSHBUTTON      "Button3",IDC_REBASE_POST_BUTTON,6,217,75,14,NOT WS_VISIBLE
 END
 
-IDD_RESOLVE_CONFLICT DIALOGEX 0, 0, 285, 129
+IDD_RESOLVE_CONFLICT DIALOGEX 0, 0, 285, 125
 STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU
 CAPTION "Conflict"
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
 BEGIN
-    GROUPBOX        "Deleted merge conflict",IDC_DEL_GROUP,7,7,271,95
-    LTEXT           "",IDC_INFOLABEL,19,21,251,8,SS_PATHELLIPSIS
-    LTEXT           "Local:",IDC_STATIC,19,39,42,8
-    LTEXT           "Static",IDC_LOCAL_STATUS,58,39,167,8
-    EDITTEXT        IDC_FROMHASH,19,51,188,12,ES_AUTOHSCROLL | ES_READONLY | NOT WS_BORDER
-    PUSHBUTTON      "Show log",IDC_LOG,218,51,52,12
-    LTEXT           "Remote:",IDC_STATIC,19,70,40,8
-    LTEXT           "Static",IDC_REMOTE_STATUS,58,70,164,8
-    EDITTEXT        IDC_TOHASH,19,82,188,12,ES_AUTOHSCROLL | ES_READONLY | NOT WS_BORDER
-    PUSHBUTTON      "Show log",IDC_LOG2,218,82,52,12
-    PUSHBUTTON      "OK",IDC_MODIFY,37,108,50,14
-    PUSHBUTTON      "Delete",IDC_DELETE,98,108,50,14
-    DEFPUSHBUTTON   "Abort",IDCANCEL,159,108,50,14
-    PUSHBUTTON      "Help",IDHELP,220,108,50,14
+    GROUPBOX        "Delete/modify merge conflict",IDC_DEL_GROUP,7,7,271,91
+    LTEXT           "",IDC_INFOLABEL,22,21,248,8,SS_WORDELLIPSIS
+    LTEXT           "Static",IDC_LOCAL_STATUS,22,50,167,8
+    EDITTEXT        IDC_FROMHASH,19,35,188,12,ES_AUTOHSCROLL | ES_READONLY | NOT WS_BORDER,WS_EX_CLIENTEDGE
+    PUSHBUTTON      "Show log",IDC_LOG,218,35,52,12
+    LTEXT           "Static",IDC_REMOTE_STATUS,22,81,164,8
+    EDITTEXT        IDC_TOHASH,19,66,188,12,ES_AUTOHSCROLL | ES_READONLY | NOT WS_BORDER,WS_EX_CLIENTEDGE
+    PUSHBUTTON      "Show log",IDC_LOG2,218,66,52,12
+    PUSHBUTTON      "OK",IDC_MODIFY,37,104,50,14
+    PUSHBUTTON      "Delete",IDC_DELETE,98,104,50,14
+    DEFPUSHBUTTON   "Abort",IDCANCEL,159,104,50,14
+    PUSHBUTTON      "Help",IDHELP,220,104,50,14
 END
 
 IDD_REFLOG DIALOGEX 0, 0, 356, 196
@@ -2835,10 +2833,11 @@ BEGIN
         LEFTMARGIN, 7
         RIGHTMARGIN, 278
         VERTGUIDE, 19
+        VERTGUIDE, 22
         VERTGUIDE, 218
         VERTGUIDE, 270
         TOPMARGIN, 7
-        BOTTOMMARGIN, 122
+        BOTTOMMARGIN, 118
     END
 
     IDD_REFLOG, DIALOG
@@ -4909,6 +4908,9 @@ BEGIN
     IDS_PROC_PUSH_ALLBRANCHES "Do you really want to push all local branches?"
     IDS_CONFLICT_NOSUBMODULE "no submodule"
     IDS_CONFLICT_SUBMODULENOTINITIALIZED "not initialized"
+    IDS_CONFLICT_REFTOBEMERGED "changes to-be-integrated"
+    IDS_CONFLICT_REVERT     "Parent of %2!s!"
+    IDS_CONFLICT_INFOTEXT   "%1!s! (%2!s!)"
 END
 
 #endif    // English (U.S.) resources

src/TortoiseProc/RebaseDlg.cpp
@@ -122,7 +122,12 @@ void CRebaseDlg::CleanUpRebaseActiveFolder()
 		return;
 	CString adminDir;
 	if (GitAdminDir::GetAdminDirPath(g_Git.m_CurrentDir, adminDir))
-		RemoveDirectory(adminDir + L"tgitrebase.active");
+	{
+		CString dir(adminDir + L"tgitrebase.active");
+		::DeleteFile(dir + L"\\head-name");
+		::DeleteFile(dir + L"\\onto");
+		::RemoveDirectory(dir);
+	}
 }
 
 void CRebaseDlg::AddRebaseAnchor()
@@ -2245,7 +2250,12 @@ void CRebaseDlg::ListConflictFile(bool noStoreScrollPosition)
 	{
 		CString adminDir;
 		if (GitAdminDir::GetAdminDirPath(g_Git.m_CurrentDir, adminDir))
-			CreateDirectory(adminDir + L"tgitrebase.active", nullptr);
+		{
+			CString dir(adminDir + L"tgitrebase.active");
+			::CreateDirectory(dir, nullptr);
+			CStringUtils::WriteStringToTextFile(dir + L"\\head-name", m_BranchCtrl.GetString());
+			CStringUtils::WriteStringToTextFile(dir + L"\\onto", m_Onto.IsEmpty() ? m_UpstreamCtrl.GetString() : m_Onto);
+		}
 	}
 
 	this->m_FileListCtrl.GetStatus(&list,true);

src/TortoiseProc/SubmoduleResolveConflictDlg.cpp
@@ -121,6 +121,10 @@ BOOL CSubmoduleResolveConflictDlg::OnInitDialog()
 	GetDlgItem(IDC_MINECHANGETYPE)->SetWindowText(changeTypeTable[m_nChangeTypeMine]);
 	GetDlgItem(IDC_THEIRSCHANGETYPE)->SetWindowText(changeTypeTable[m_nChangeTypeTheirs]);
 
+	GetDlgItem(IDC_FROMGROUP)->SetWindowText(m_sBaseTitle);
+	GetDlgItem(IDC_TOGROUP)->SetWindowText(m_sMineTitle);
+	GetDlgItem(IDC_TOGROUP2)->SetWindowText(m_sTheirsTitle);
+
 	DialogEnableWindow(IDC_LOG, m_bBaseOK);
 	DialogEnableWindow(IDC_LOG2, m_bMineOK && m_nChangeTypeMine != CGitDiff::DeleteSubmodule);
 	DialogEnableWindow(IDC_LOG3, m_bTheirsOK && m_nChangeTypeTheirs != CGitDiff::DeleteSubmodule);
@@ -154,10 +158,11 @@ HBRUSH CSubmoduleResolveConflictDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlC
 	return CHorizontalResizableStandAloneDialog::OnCtlColor(pDC, pWnd, nCtlColor);
 }
 
-void CSubmoduleResolveConflictDlg::SetDiff(const CString& path, bool revertTheirMy, const CString& baseHash, const CString& baseSubject, bool baseOK, const CString& mineHash, const CString& mineSubject, bool mineOK, CGitDiff::ChangeType mineChangeType, const CString& theirsHash, const CString& theirsSubject, bool theirsOK, CGitDiff::ChangeType theirsChangeType)
+void CSubmoduleResolveConflictDlg::SetDiff(const CString& path, bool revertTheirMy, const CString& baseTitle, const CString& mineTitle, const CString& theirsTitle, const CString& baseHash, const CString& baseSubject, bool baseOK, const CString& mineHash, const CString& mineSubject, bool mineOK, CGitDiff::ChangeType mineChangeType, const CString& theirsHash, const CString& theirsSubject, bool theirsOK, CGitDiff::ChangeType theirsChangeType)
 {
 	m_sPath = path;
 
+	m_sBaseTitle = baseTitle;
 	m_sBaseHash = baseHash;
 	m_sBaseSubject = baseSubject;
 	m_bBaseOK = baseOK;
@@ -166,11 +171,13 @@ void CSubmoduleResolveConflictDlg::SetDiff(const CString& path, bool revertTheir
 
 	if (!m_bRevertTheirMy)
 	{
+		m_sMineTitle = mineTitle;
 		m_sMineHash = mineHash;
 		m_sMineSubject = mineSubject;
 		m_bMineOK = mineOK;
 		m_nChangeTypeMine = mineChangeType;
 
+		m_sTheirsTitle = theirsTitle;
 		m_sTheirsHash = theirsHash;
 		m_sTheirsSubject = theirsSubject;
 		m_bTheirsOK = theirsOK;
@@ -178,11 +185,13 @@ void CSubmoduleResolveConflictDlg::SetDiff(const CString& path, bool revertTheir
 	}
 	else
 	{
+		m_sMineTitle = theirsTitle;
 		m_sMineHash = theirsHash;
 		m_sMineSubject = theirsSubject;
 		m_bMineOK = theirsOK;
 		m_nChangeTypeMine = theirsChangeType;
 
+		m_sTheirsTitle = mineTitle;
 		m_sTheirsHash = mineHash;
 		m_sTheirsSubject = mineSubject;
 		m_bTheirsOK = mineOK;

src/TortoiseProc/SubmoduleResolveConflictDlg.h
@@ -1,6 +1,6 @@
 // TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2012, 2014 - TortoiseGit
+// Copyright (C) 2012, 2014, 2017 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -31,7 +31,7 @@ public:
 
 	enum { IDD = IDD_RESOLVESUBMODULECONFLICT };
 
-	void SetDiff(const CString& path, bool revertTheirMy, const CString& baseHash, const CString& baseSubject, bool baseOK, const CString& mineHash, const CString& mineSubject, bool mineOK, CGitDiff::ChangeType mineChangeType, const CString& theirsHash, const CString& theirsSubject, bool theirsOK, CGitDiff::ChangeType theirsChangeType);
+	void SetDiff(const CString& path, bool revertTheirMy, const CString& baseTitle, const CString& mineTitle, const CString& theirsTitle, const CString& baseHash, const CString& baseSubject, bool baseOK, const CString& mineHash, const CString& mineSubject, bool mineOK, CGitDiff::ChangeType mineChangeType, const CString& theirsHash, const CString& theirsSubject, bool theirsOK, CGitDiff::ChangeType theirsChangeType);
 
 	bool m_bResolved;
 
@@ -53,6 +53,9 @@ protected:
 
 	CString	m_sPath;
 
+	CString m_sBaseTitle;
+	CString m_sMineTitle;
+	CString m_sTheirsTitle;
 	CString	m_sBaseHash;
 	CString	m_sBaseSubject;
 	bool	m_bBaseOK;

commit dc92c6a40ee228903a85390498107b4856d6d1e1

DeleteConflictDlg: Fix display of reference information
This fixes a regression introduced in commit 45f4d3c1433f8989113cffd4c6b51fcd6a2aca32.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -1539,7 +1539,7 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 				{
 					popup.AppendMenuIcon(IDGITLC_RESOLVECONFLICT, IDS_STATUSLIST_CONTEXT_RESOLVED, IDI_RESOLVE);
 					CString tmp, mineTitle, theirsTitle;
-					CAppUtils::GetConflictTitles(nullptr, mineTitle, theirsTitle, m_bIsRevertTheirMy);
+					CAppUtils::GetConflictTitles(nullptr, mineTitle, nullptr, theirsTitle, nullptr, m_bIsRevertTheirMy);
 					tmp.Format(IDS_SVNPROGRESS_MENURESOLVEUSING, static_cast<LPCTSTR>(theirsTitle));
 					if (m_bIsRevertTheirMy)
 					{
@@ -2231,7 +2231,7 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 						if (nListItems >= 5)
 						{
 							CString tmp, mineTitle, theirsTitle;
-							CAppUtils::GetConflictTitles(nullptr, mineTitle, theirsTitle, m_bIsRevertTheirMy);
+							CAppUtils::GetConflictTitles(nullptr, mineTitle, nullptr, theirsTitle, nullptr, m_bIsRevertTheirMy);
 							tmp.LoadString(IDS_PROGRS_TITLE_RESOLVE);
 							if (m_bIsRevertTheirMy)
 							{

src/TortoiseProc/AppUtils.cpp
@@ -1764,7 +1764,7 @@ static bool ParseHashesFromLsFile(const BYTE_VECTOR& out, CGitHash& hash1, bool&
 	return false;
 }
 
-void CAppUtils::GetConflictTitles(CString* baseText, CString& mineText, CString& theirsText, bool rebaseActive)
+void CAppUtils::GetConflictTitles(CString* baseText, CString& mineText, CGitHash* mineHash, CString& theirsText, CGitHash* theirsHash, bool rebaseActive)
 {
 	if (baseText)
 		baseText->LoadString(IDS_PROC_DIFF_BASE);
@@ -1778,6 +1778,8 @@ void CAppUtils::GetConflictTitles(CString* baseText, CString& mineText, CString&
 			CGitHash hash;
 			if (!g_Git.GetHash(hash, L"rebase-apply/onto"))
 				g_Git.GuessRefForHash(mineText, hash);
+			if (mineHash)
+				*mineHash = hash;
 		}
 		theirsText = L"Branch being rebased";
 		if (!CStringUtils::ReadStringFromTextFile(adminDir + L"tgitrebase.active\\head-name", theirsText))
@@ -1806,6 +1808,8 @@ void CAppUtils::GetConflictTitles(CString* baseText, CString& mineText, CString&
 			else
 				g_Git.GuessRefForHash(guessedRef, hash);
 			theirsText.FormatMessage(infotext.theirstext, infotext.headref, static_cast<LPCTSTR>(guessedRef));
+			if (theirsHash)
+				*theirsHash = hash;
 			break;
 		}
 	}
@@ -1825,7 +1829,8 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 		return FALSE;
 
 	CString baseTitle, mineTitle, theirsTitle;
-	GetConflictTitles(&baseTitle, mineTitle, theirsTitle, isRebase);
+	CGitHash mineHash, theirsHash;
+	GetConflictTitles(&baseTitle, mineTitle, &mineHash, theirsTitle, &theirsHash, isRebase);
 
 	CGitHash baseHash, realBaseHash, localHash, remoteHash;
 	bool baseIsFile = true, localIsFile = true, remoteIsFile = true;
@@ -2044,16 +2049,20 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 		{
 			DescribeConflictFile(b_local, b_base, dlg.m_LocalStatus);
 			DescribeConflictFile(b_remote, b_base, dlg.m_RemoteStatus);
-			dlg.m_LocalHash = CGitHash::FromHexStrTry(mineTitle);
-			dlg.m_RemoteHash = CGitHash::FromHexStrTry(theirsTitle);
+			dlg.m_LocalHash = mineHash;
+			dlg.m_RemoteHash = theirsHash;
+			dlg.m_LocalRef = mineTitle;
+			dlg.m_RemoteRef = theirsTitle;
 			dlg.m_bDiffMine = b_local;
 		}
 		else
 		{
 			DescribeConflictFile(b_local, b_base, dlg.m_RemoteStatus);
 			DescribeConflictFile(b_remote, b_base, dlg.m_LocalStatus);
-			dlg.m_LocalHash = CGitHash::FromHexStrTry(theirsTitle);
-			dlg.m_RemoteHash = CGitHash::FromHexStrTry(mineTitle);
+			dlg.m_LocalHash = theirsHash;
+			dlg.m_RemoteHash = mineHash;
+			dlg.m_LocalRef = theirsTitle;
+			dlg.m_RemoteRef = mineTitle;
 			dlg.m_bDiffMine = !b_local;
 		}
 		dlg.m_bShowModifiedButton = b_base;

src/TortoiseProc/AppUtils.h
@@ -173,7 +173,7 @@ public:
 	static bool IgnoreFile(HWND hWnd, const CTGitPathList& filelist, bool IsMask);
 	static bool GitReset(HWND hWnd, const CString& ref, int type = 1);
 	static bool ConflictEdit(HWND hWnd, CTGitPath& file, bool bAlternativeTool = false, bool revertTheirMy = false, HWND resolveMsgHwnd = nullptr);
-	static void GetConflictTitles(CString* baseText, CString& mineText, CString& theirsText, bool rebaseActive);
+	static void GetConflictTitles(CString* baseText, CString& mineText, CGitHash* mineHash, CString& theirsText, CGitHash* theirsHash, bool rebaseActive);
 
 	static CString GetMergeTempFile(const CString& str, const CTGitPath& merge, bool returnAbsolutePath = true);
 	static bool	StashSave(HWND hWnd, const CString& msg = CString(), bool showPull = false, bool pullShowPush = false, bool showMerge = false, const CString& mergeRev = CString());

src/TortoiseProc/DeleteConflictDlg.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2019 - TortoiseGit
+// Copyright (C) 2009-2020 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -47,8 +47,8 @@ void CDeleteConflictDlg::DoDataExchange(CDataExchange* pDX)
 
 	DDX_Text(pDX, IDC_LOCAL_STATUS, m_LocalStatus);
 	DDX_Text(pDX, IDC_REMOTE_STATUS, m_RemoteStatus);
-	GetDlgItem(IDC_FROMHASH)->SetWindowText(m_LocalHash.ToString());
-	GetDlgItem(IDC_TOHASH)->SetWindowText(m_RemoteHash.ToString());
+	GetDlgItem(IDC_FROMHASH)->SetWindowText(m_LocalRef);
+	GetDlgItem(IDC_TOHASH)->SetWindowText(m_RemoteRef);
 }
 
 
@@ -77,9 +77,9 @@ BOOL CDeleteConflictDlg::OnInitDialog()
 	}
 	else
 		this->GetDlgItem(IDC_MODIFY)->SetWindowText(CString(MAKEINTRESOURCE(IDS_PROC_CREATED)));
-	if (m_LocalHash.IsEmpty())
+	if (m_LocalRef.IsEmpty())
 		GetDlgItem(IDC_LOG)->ShowWindow(SW_HIDE);
-	if (m_RemoteHash.IsEmpty())
+	if (m_RemoteRef.IsEmpty())
 		GetDlgItem(IDC_LOG2)->ShowWindow(SW_HIDE);
 
 	CString sWindowTitle;

src/TortoiseProc/DeleteConflictDlg.h
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2011, 2017, 2019 - TortoiseGit
+// Copyright (C) 2009-2011, 2017, 2019-2020 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -49,6 +49,8 @@ public:
 	CString m_RemoteStatus;
 	CGitHash m_LocalHash;
 	CGitHash m_RemoteHash;
+	CString m_LocalRef;
+	CString m_RemoteRef;
 	BOOL	m_bShowModifiedButton;
 	CTGitPath	m_File;
 	CTGitPath	m_FileBaseVersion;