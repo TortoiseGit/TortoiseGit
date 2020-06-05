45bcbb0fb8b7d315637007cbc50274e4ef2400b9
Sven Strickroth <email@cs-ware.de>
2020-06-05 23:51:01 +0200

    Apply backgroundcolors.patch
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- ext/scintilla/src/EditView.cxx
+++ ext/scintilla/src/EditView.cxx
@@ -56,6 +56,7 @@
 #include "MarginView.h"
 #include "EditView.h"
 #include "ElapsedPeriod.h"
+#include "Editor.h"
 
 using namespace Scintilla;
 
@@ -181,6 +182,7 @@ EditView::EditView() {
 	tabArrowHeight = 4;
 	customDrawTabArrow = nullptr;
 	customDrawWrapMarker = nullptr;
+	editor = nullptr;
 }
 
 EditView::~EditView() {
@@ -2108,7 +2110,15 @@ void EditView::DrawLine(Surface *surface, const EditModel &model, const ViewStyl
 	}
 
 	// See if something overrides the line background colour.
-	const ColourOptional background = vsDraw.Background(model.pdoc->GetMark(line), model.caret.active, ll->containsCaret);
+	ColourOptional background = vsDraw.Background(model.pdoc->GetMark(line), model.caret.active, ll->containsCaret);
+	SCNotification scn = { 0 };
+	scn.nmhdr.code = SCN_GETBKCOLOR;
+	scn.line = line;
+	scn.lParam = -1;
+	if (editor)
+		reinterpret_cast<Editor*>(editor)->NotifyParent(&scn);
+	if (scn.lParam != -1)
+		background = ColourOptional(true, scn.lParam);
 
 	const Sci::Position posLineStart = model.pdoc->LineStart(line);
 
