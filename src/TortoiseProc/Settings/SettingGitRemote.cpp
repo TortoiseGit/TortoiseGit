a664d9cb86505152d55be8550f97e59b59694823
Sven Strickroth <email@cs-ware.de>
2020-07-24 17:08:35 +0200

    Fixed issue #3617: 'Saving config failed (key: "fetch.prune", value: "")' in remote repository definition when `fetch.prune` is set in global config
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/Settings/SettingGitRemote.cpp
+++ src/TortoiseProc/Settings/SettingGitRemote.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2019 - TortoiseGit
+// Copyright (C) 2008-2020 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -455,7 +455,7 @@ BOOL CSettingGitRemote::OnApply()
 
 	if (m_ChangedMask & REMOTE_PRUNEALL)
 	{
-		if (!SaveGeneral(L"fetch.prune", m_bPruneAll == TRUE ? L"true" : L""))
+		if (!SaveGeneral(L"fetch.prune", m_bPruneAll == TRUE ? L"true" : L"false"))
 			return FALSE;
 		m_ChangedMask &= ~REMOTE_PRUNEALL;
 	}
