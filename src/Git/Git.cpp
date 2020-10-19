b9d35c30211c121c7f2ec321e257ad4020e1893b
remittor <remittor@gmail.com>
2020-10-19 13:17:24 +0300

    Dynamic changing environment before starting the GIT
    This is better than calling function CheckAndInitDll in the constructor of class CGit.

--- src/Git/Git.cpp
+++ src/Git/Git.cpp
@@ -259,6 +259,7 @@ int CGit::RunAsync(CString cmd, PROCESS_INFORMATION* piOut, HANDLE* hReadOut, HA
 {
 	CAutoGeneralHandle hRead, hWrite, hReadErr, hWriteErr, hWriteIn, hReadIn;
 	CAutoFile hStdioFile;
+	CEnvironment env;
 
 	SECURITY_ATTRIBUTES sa = { 0 };
 	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
@@ -301,20 +302,23 @@ int CGit::RunAsync(CString cmd, PROCESS_INFORMATION* piOut, HANDLE* hReadOut, HA
 	si.wShowWindow=SW_HIDE;
 	si.dwFlags=STARTF_USESTDHANDLES|STARTF_USESHOWWINDOW;
 
-	LPTSTR pEnv = m_Environment;
+	bool startsWithGit = CStringUtils::StartsWith(cmd, L"git");
+	bool cmdGitCfg = startsWithGit && CStringUtils::StartsWith(cmd, L"git.exe config ");
+	GetPreparedEnvironment(env, cmdGitCfg);
+
+	LPTSTR pEnv = env;
 	DWORD dwFlags = CREATE_UNICODE_ENVIRONMENT;
 	dwFlags |= CREATE_NEW_PROCESS_GROUP | DETACHED_PROCESS;
 
 	memset(&this->m_CurrentGitPi,0,sizeof(PROCESS_INFORMATION));
 
-	bool startsWithGit = CStringUtils::StartsWith(cmd, L"git");
-	if (ms_bMsys2Git && startsWithGit && !CStringUtils::StartsWith(cmd, L"git.exe config "))
+	if (ms_bMsys2Git && startsWithGit && !cmdGitCfg)
 	{
 		cmd.Replace(L"\\", L"\\\\\\\\");
 		cmd.Replace(L"\"", L"\\\"");
 		cmd = L'"' + CGit::ms_LastMsysGitDir + L"\\bash.exe\" -c \"/usr/bin/" + cmd + L'"';
 	}
-	else if (ms_bCygwinGit && startsWithGit && !CStringUtils::StartsWith(cmd, L"git.exe config "))
+	else if (ms_bCygwinGit && startsWithGit && !cmdGitCfg)
 	{
 		cmd.Replace(L'\\', L'/');
 		cmd.Replace(L"\"", L"\\\"");
@@ -2291,15 +2295,24 @@ BOOL CGit::CheckMsysGitDir(BOOL bFallback)
 	}
 #endif
 
+	m_bInitialized = TRUE;
+	return m_bInitialized;
+}
+
+bool CGit::GetPreparedEnvironment(CEnvironment & env, bool cmdGitCfg)
+{
+	env = m_Environment;
+
+	if (cmdGitCfg)
+		return true;
+
 	CString proxy = GetConfigValue(L"http.proxy");
 	if (!proxy.IsEmpty()) {
 		CString no_proxy = CRegString(L"Software\\TortoiseGit\\Git\\Servers\\global\\http-proxy-ignore", L"");
 		if (!no_proxy.IsEmpty()) {
-			m_Environment.SetEnv(L"NO_PROXY", no_proxy);
+			env.SetEnv(L"NO_PROXY", no_proxy);
 		}
 	}
-
-	m_bInitialized = TRUE;
 	return true;
 }
 
