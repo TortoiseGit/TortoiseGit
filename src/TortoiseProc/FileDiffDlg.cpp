TortoiseGit/TortoiseGit

commit bbbbc74053320cbb7ed8fe287a8c6883a7a5c94b

Fixed issue #3668: "Revert to revision" fails for added files
Signed-off-by: Shun Tsukamoto <Shun.G.Tsukamoto@gmail.com>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/FileDiffDlg.cpp
@@ -1333,7 +1333,15 @@ int CFileDiffDlg::RevertSelectedItemToVersion(CString rev)
 	{
 		CString cmd, out;
 		auto fentry = m_arFilteredList[index];
-		cmd.Format(L"git.exe checkout %s -- \"%s\"", static_cast<LPCTSTR>(rev), static_cast<LPCTSTR>(fentry->GetGitPathString()));
+		switch (fentry->m_Action)
+		{
+		case CTGitPath::LOGACTIONS_ADDED:
+			cmd.Format(L"git.exe rm --cached -- \"%s\"", static_cast<LPCTSTR>(fentry->GetGitPathString()));
+			break;
+		default:
+			cmd.Format(L"git.exe checkout %s -- \"%s\"", static_cast<LPCTSTR>(rev), static_cast<LPCTSTR>(fentry->GetGitPathString()));
+			break;
+		}
 		if (g_Git.Run(cmd, &out, CP_UTF8))
 		{
 			if (CMessageBox::Show(GetSafeHwnd(), out, L"TortoiseGit", 2, IDI_WARNING, CString(MAKEINTRESOURCE(IDS_IGNOREBUTTON)), CString(MAKEINTRESOURCE(IDS_ABORTBUTTON))) == 2)

commit 14944a0437b8060add6900615a4495505072ef9c

Fix "Revert to version" for all cases
Fixes a regression introduced in commit bbbbc74053320cbb7ed8fe287a8c6883a7a5c94b.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/FileDiffDlg.cpp
@@ -669,10 +669,10 @@ void CFileDiffDlg::OnContextMenu(CWnd* pWnd, CPoint point)
 			}
 			break;
 		case ID_REVERT1:
-			RevertSelectedItemToVersion(m_rev1.m_CommitHash.ToString());
+			RevertSelectedItemToVersion(m_rev1.m_CommitHash.ToString(), true);
 			break;
 		case ID_REVERT2:
-			RevertSelectedItemToVersion(m_rev2.m_CommitHash.ToString());
+			RevertSelectedItemToVersion(m_rev2.m_CommitHash.ToString(), false);
 			break;
 		case ID_BLAME:
 			{
@@ -1321,7 +1321,7 @@ void CFileDiffDlg::OnTextUpdate(CACEdit * /*pEdit*/)
 	this->m_cFileList.ShowText(L"Wait For input validate version");
 }
 
-int CFileDiffDlg::RevertSelectedItemToVersion(CString rev)
+int CFileDiffDlg::RevertSelectedItemToVersion(const CString& rev, bool isOldVersion)
 {
 	if (rev.IsEmpty() || rev == GIT_REV_ZERO)
 		return 0;
@@ -1333,15 +1333,10 @@ int CFileDiffDlg::RevertSelectedItemToVersion(CString rev)
 	{
 		CString cmd, out;
 		auto fentry = m_arFilteredList[index];
-		switch (fentry->m_Action)
-		{
-		case CTGitPath::LOGACTIONS_ADDED:
+		if ((isOldVersion && fentry->m_Action == CTGitPath::LOGACTIONS_ADDED) || (!isOldVersion && fentry->m_Action == CTGitPath::LOGACTIONS_DELETED))
 			cmd.Format(L"git.exe rm --cached -- \"%s\"", static_cast<LPCTSTR>(fentry->GetGitPathString()));
-			break;
-		default:
+		else
 			cmd.Format(L"git.exe checkout %s -- \"%s\"", static_cast<LPCTSTR>(rev), static_cast<LPCTSTR>(fentry->GetGitPathString()));
-			break;
-		}
 		if (g_Git.Run(cmd, &out, CP_UTF8))
 		{
 			if (CMessageBox::Show(GetSafeHwnd(), out, L"TortoiseGit", 2, IDI_WARNING, CString(MAKEINTRESOURCE(IDS_IGNOREBUTTON)), CString(MAKEINTRESOURCE(IDS_ABORTBUTTON))) == 2)

src/TortoiseProc/FileDiffDlg.h
@@ -102,7 +102,7 @@ protected:
 
 	void				EnableInputControl(bool b=true);
 
-	int					RevertSelectedItemToVersion(CString rev);
+	int					RevertSelectedItemToVersion(const CString& rev, bool isOldVersion);
 
 	bool				CheckMultipleDiffs();
 