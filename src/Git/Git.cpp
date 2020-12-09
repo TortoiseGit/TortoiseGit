TortoiseGit/TortoiseGit

commit 572519ce22cc264622a859a893d4b71bf7f35a72

Fixed issue #3517 Revision graph filter does not work
Signed-off-by: Shun Tsukamoto <Shun.G.Tsukamoto@gmail.com>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -987,7 +987,8 @@ CString CGit::GetLogCmd(CString range, const CTGitPath* path, int mask, CFilterD
 	if(mask& CGit::LOG_INFO_ALL_BRANCH)
 	{
 		param += L" --all";
-		range.Empty();
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+			range.Empty();
 	}
 
 	if (mask& CGit::LOG_INFO_BASIC_REFS)
@@ -997,13 +998,15 @@ CString CGit::GetLogCmd(CString range, const CTGitPath* path, int mask, CFilterD
 		param += L" --remotes";
 		param += L" --glob=stas[h]"; // require at least one glob operator
 		param += L" --glob=bisect";
-		range.Empty();
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+			range.Empty();
 	}
 
 	if(mask & CGit::LOG_INFO_LOCAL_BRANCHES)
 	{
 		param += L" --branches";
-		range.Empty();
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+			range.Empty();
 	}
 
 	if(mask& CGit::LOG_INFO_DETECT_COPYRENAME)

src/Git/Git.h
@@ -372,6 +372,7 @@ public:
 		LOG_INFO_LOCAL_BRANCHES = 0x8000,
 		LOG_INFO_BASIC_REFS = 0x10000,
 		LOG_INFO_SPARSE = 0x20000,
+		LOG_INFO_ALWAYS_APPLY_RANGE = 0x40000,
 	}LOG_INFO_MASK;
 
 	typedef enum

src/TortoiseProc/RevisionGraph/RevisionGraphDlgFunc.cpp
@@ -1,7 +1,7 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2011 - TortoiseSVN
-// Copyright (C) 2012-2019 - TortoiseGit
+// Copyright (C) 2012-2020 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -190,13 +190,27 @@ bool CRevisionGraphWnd::FetchRevisionData
 	this->m_LogCache.ClearAllParent();
 	this->m_logEntries.ClearAll();
 	CString range;
+	DWORD infomask = CGit::LOG_INFO_SIMPILFY_BY_DECORATION | (m_bShowBranchingsMerges ? CGit::LOG_ORDER_TOPOORDER | CGit::LOG_INFO_SPARSE : 0);
 	if (!m_ToRev.IsEmpty() && !m_FromRev.IsEmpty())
 		range.Format(L"%s..%s", static_cast<LPCTSTR>(g_Git.FixBranchName(m_FromRev)), static_cast<LPCTSTR>(g_Git.FixBranchName(m_ToRev)));
 	else if (!m_ToRev.IsEmpty())
-		range = m_ToRev;
+		range = g_Git.FixBranchName(m_ToRev);
 	else if (!m_FromRev.IsEmpty())
-		range = m_FromRev;
-	DWORD infomask = CGit::LOG_INFO_SIMPILFY_BY_DECORATION | (m_bCurrentBranch ? 0 : m_bLocalBranches ? CGit::LOG_INFO_LOCAL_BRANCHES : CGit::LOG_INFO_ALL_BRANCH) | (m_bShowBranchingsMerges ? CGit::LOG_ORDER_TOPOORDER | CGit::LOG_INFO_SPARSE : 0);
+	{
+		if (m_bCurrentBranch)
+			range.Format(L"^%s HEAD", static_cast<LPCTSTR>(g_Git.FixBranchName(m_FromRev)));
+		else
+		{
+			range.Format(L"^%s", static_cast<LPCTSTR>(g_Git.FixBranchName(m_FromRev)));
+			if (m_bLocalBranches)
+				infomask |= CGit::LOG_INFO_ALWAYS_APPLY_RANGE | CGit::LOG_INFO_LOCAL_BRANCHES;
+			else
+				infomask |= CGit::LOG_INFO_ALWAYS_APPLY_RANGE | CGit::LOG_INFO_ALL_BRANCH;
+		}
+	}
+	else if (m_ToRev.IsEmpty() && m_FromRev.IsEmpty())
+		infomask |= m_bCurrentBranch ? 0 : m_bLocalBranches ? CGit::LOG_INFO_LOCAL_BRANCHES : CGit::LOG_INFO_ALL_BRANCH;
+
 	m_logEntries.ParserFromLog(nullptr, 0, infomask, &range);
 
 	ReloadHashMap();

commit 9ba8eb09b0759b80e4330d83b186e8085f8c601e

Fix typo introduced be me in commit 572519ce22cc264622a859a893d4b71bf7f35a72
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/Git.cpp
@@ -987,7 +987,7 @@ CString CGit::GetLogCmd(CString range, const CTGitPath* path, int mask, CFilterD
 	if(mask& CGit::LOG_INFO_ALL_BRANCH)
 	{
 		param += L" --all";
-		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) == 0)
 			range.Empty();
 	}
 
@@ -998,14 +998,14 @@ CString CGit::GetLogCmd(CString range, const CTGitPath* path, int mask, CFilterD
 		param += L" --remotes";
 		param += L" --glob=stas[h]"; // require at least one glob operator
 		param += L" --glob=bisect";
-		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) == 0)
 			range.Empty();
 	}
 
 	if(mask & CGit::LOG_INFO_LOCAL_BRANCHES)
 	{
 		param += L" --branches";
-		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) != 0)
+		if ((mask & LOG_INFO_ALWAYS_APPLY_RANGE) == 0)
 			range.Empty();
 	}
 