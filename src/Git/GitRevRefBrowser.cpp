ecbded2deb8971e992cf70709de47f77d1f63b92
Sven Strickroth <email@cs-ware.de>
2021-05-15 17:35:49 +0200

    Use pragma once only in headers
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/Git/GitRevRefBrowser.cpp
+++ src/Git/GitRevRefBrowser.cpp
@@ -16,8 +16,6 @@
 // along with this program; if not, write to the Free Software Foundation,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
-
-#pragma once
 #include "stdafx.h"
 #include "gittype.h"
 #include "Git.h"
--- src/Utils/TaskbarUUID.cpp
+++ src/Utils/TaskbarUUID.cpp
@@ -17,7 +17,6 @@
 // along with this program; if not, write to the Free Software Foundation,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
-#pragma once
 #include "stdafx.h"
 #include "TaskbarUUID.h"
 #include "registry.h"
