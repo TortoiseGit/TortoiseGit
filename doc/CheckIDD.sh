a71bf40d625a03179563b68344a7e74b6b457de8
Sven Strickroth <email@cs-ware.de>
2021-12-23 14:00:47 +0100

    Add check scripts for manual references
    Based on TortoiseSVN with Python 3 fixes.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

---
+++ doc/CheckIDD.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Use a Python script to cross check IDD_ values in the resource file with
+# HIDD_ values in the docs to ensure that every dialog has a link to the help.
+tmpfile=`mktemp`
+dirname=$(dirname -- "$0")
+pushd "$dirname" > /dev/null
+python3 scripts/chkidd.py source/en/TortoiseGit ../src/Resources/TortoiseProcENG.rc scripts/TortoiseProcENG.ignore > "$tmpfile"
+python3 scripts/chkidd.py source/en/TortoiseMerge ../src/Resources/TortoiseMergeENG.rc scripts/TortoiseMergeENG.ignore >> "$tmpfile"
+popd > /dev/null
+retcode=0
+if grep -q -v '^#' "$tmpfile"; then
+	cat "$tmpfile"
+	retcode=1
+fi
+rm -- "$tmpfile"
+exit $retcode
