TortoiseGit/TortoiseGit

commit a54eb2ffac8e4918c69c6a47a18f75a8223db8ab

Move IsRebaseRunning method to CTGitPath
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/TGitPath.cpp
@@ -862,6 +862,16 @@ bool CTGitPath::IsBisectActive() const
 
 	return !!PathFileExists(dotGitPath + L"BISECT_START");
 }
+bool CTGitPath::IsRebaseActive() const
+{
+	if (!HasAdminDir())
+		return false;
+
+	CString dotGitPath;
+	GitAdminDir::GetWorktreeAdminDirPath(m_sProjectRoot, dotGitPath);
+
+	return PathIsDirectory(dotGitPath + L"rebase-apply") == TRUE || PathIsDirectory(dotGitPath + L"tgitrebase.active") == TRUE;
+}
 bool CTGitPath::IsCherryPickActive() const
 {
 	if (!HasAdminDir())

src/Git/Git.cpp
@@ -3448,17 +3448,6 @@ int CGit::GetWorkingTreeChanges(CTGitPathList& result, bool amend, const CTGitPa
 	return 0;
 }
 
-int CGit::IsRebaseRunning()
-{
-	CString adminDir;
-	if (!GitAdminDir::GetAdminDirPath(g_Git.m_CurrentDir, adminDir))
-		return -1;
-
-	if (PathIsDirectory(adminDir + L"rebase-apply") || PathIsDirectory(adminDir + L"tgitrebase.active"))
-		return 1;
-	return 0;
-}
-
 void CGit::GetBisectTerms(CString* good, CString* bad)
 {
 	static CString lastGood;

src/Git/Git.h
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2021 - TortoiseGit
+// Copyright (C) 2008-2022 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -434,7 +434,6 @@ public:
 	int HasWorkingTreeConflicts();
 	/** Returns 0 if no conflict, if a conflict was found and -1 in case of a failure */
 	int HasWorkingTreeConflicts(git_repository* repo);
-	int IsRebaseRunning();
 	void GetBisectTerms(CString* good, CString* bad);
 	int GetRefList(STRING_VECTOR &list);
 

src/Git/GitStatusListCtrl.cpp
@@ -443,9 +443,9 @@ BOOL CGitStatusListCtrl::GetStatus ( const CTGitPathList* pathList
 	m_bWaitCursor = true;
 	Invalidate();
 
-	bool hasLFS = CTGitPath(g_Git.m_CurrentDir).HasLFS();
-
-	m_bIsRevertTheirMy = g_Git.IsRebaseRunning() > 0;
+	CTGitPath repo{ g_Git.m_CurrentDir };
+	bool hasLFS = repo.HasLFS();
+	m_bIsRevertTheirMy = repo.IsRebaseActive();
 
 	int mask= CGitStatusListCtrl::FILELIST_MODIFY;
 	if(bShowIgnores)

src/Git/TGitPath.h
@@ -249,6 +249,7 @@ public:
 	bool HasSubmodules() const;
 	bool HasGitSVNDir() const;
 	bool IsBisectActive() const;
+	bool IsRebaseActive() const;
 	bool IsCherryPickActive() const;
 	bool IsMergeActive() const;
 	bool HasStashDir() const;

src/TortoiseProc/Commands/ConflictEditorCommand.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2009, 2015-2016, 2018-2019 - TortoiseGit
+// Copyright (C) 2009, 2015-2016, 2018-2019, 2021-2022 - TortoiseGit
 // Copyright (C) 2008-2009 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -24,12 +24,13 @@
 
 bool ConflictEditorCommand::Execute()
 {
-	if (!GitAdminDir::HasAdminDir(g_Git.m_CurrentDir))
+	CTGitPath repo{ g_Git.m_CurrentDir };
+	if (!repo.HasAdminDir())
 	{
 		CMessageBox::Show(GetExplorerHWND(), IDS_NOWORKINGCOPY, IDS_APPNAME, MB_ICONERROR);
 		return false;
 	}
 
 	bool bAlternativeTool = !!parser.HasKey(L"alternative");
-	return CAppUtils::ConflictEdit(GetExplorerHWND(), this->cmdLinePath, bAlternativeTool, g_Git.IsRebaseRunning() > 0);
+	return CAppUtils::ConflictEdit(GetExplorerHWND(), this->cmdLinePath, bAlternativeTool, repo.IsRebaseActive());
 }

commit 85c702f3d4bfb2764087eec5b8ae818901e0524a

Fix comparison return value of PathIsDirectory() in IsRebaseActive()
Regression introduced in commit a54eb2ffac8e4918c69c6a47a18f75a8223db8ab.

Signed-off-by: Michael Bauer <meigelb_dev@outlook.com>
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/TGitPath.cpp
@@ -870,7 +870,7 @@ bool CTGitPath::IsRebaseActive() const
 	CString dotGitPath;
 	GitAdminDir::GetWorktreeAdminDirPath(m_sProjectRoot, dotGitPath);
 
-	return PathIsDirectory(dotGitPath + L"rebase-apply") == TRUE || PathIsDirectory(dotGitPath + L"tgitrebase.active") == TRUE;
+	return PathIsDirectory(dotGitPath + L"rebase-apply") || PathIsDirectory(dotGitPath + L"tgitrebase.active");
 }
 bool CTGitPath::IsCherryPickActive() const
 {