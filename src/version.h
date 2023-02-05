e91a513097c65519901d11e50c9778da3583e863
Sven Strickroth <email@cs-ware.de>
2023-02-05 17:55:34 +0100

    Prepare release and bump version numbers to 2.14.0
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2022 - TortoiseGit
+// Copyright (C) 2008-2023 - TortoiseGit
 
 #define TGIT_VERMAJOR		2
-#define TGIT_VERMINOR		13
+#define TGIT_VERMINOR		14
 #define TGIT_VERMICRO		0
 #define TGIT_VERBUILD		0
 #define TGIT_VERDATE		__DATE__
