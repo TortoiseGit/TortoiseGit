6be3bf3e419382da9740bc258bede4082854ca94
Sven Strickroth <email@cs-ware.de>
2023-04-13 18:56:28 +0200

    Ignore empty lines in a /pathfile file
    Inspired by TortoiseSVN rev. 29547.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/Git/TGitPath.cpp
+++ src/Git/TGitPath.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseGit - a Windows shell extension for easy version control
 
-// Copyright (C) 2008-2022 - TortoiseGit
+// Copyright (C) 2008-2023 - TortoiseGit
 // Copyright (C) 2003-2008 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -1472,6 +1472,8 @@ bool CTGitPathList::LoadFromFile(const CTGitPath& filename)
 		CTGitPath path;
 		while (file.ReadString(strLine))
 		{
+			if (strLine.IsEmpty())
+				continue;
 			path.SetFromUnknown(strLine);
 			AddPath(path);
 		}
