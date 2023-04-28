6dea70e9ae0ecfeaccb9711a7757f971a11b7b22
Sven Strickroth <email@cs-ware.de>
2023-04-28 18:48:35 +0200

    Don't use outdated variable name any more
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- .gitlab-ci/check-commits.sh
+++ .gitlab-ci/check-commits.sh
@@ -12,7 +12,7 @@ function error() {
 git fetch $CI_REPOSITORY_URL --shallow-exclude=refs/heads/$CI_MERGE_REQUEST_TARGET_BRANCH_NAME refs/pipelines/$CI_PIPELINE_ID
 git fetch $CI_REPOSITORY_URL --depth=1 $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
 
-git rev-list --reverse FETCH_HEAD..$CI_BUILD_REF | ( while read commit_hash
+git rev-list --reverse FETCH_HEAD..$CI_COMMIT_SHA | ( while read commit_hash
 do
   echo "Checking commit $(git show --no-patch --pretty='%h ("%s")' $commit_hash)"
   git show --no-patch --format=%B $commit_hash | grep -q "Signed-off-by:" || error "Commit $commit_hash does not contain a Signed-off-by line! Please see <https://gitlab.com/tortoisegit/tortoisegit/blob/master/doc/HowToContribute.txt>"
