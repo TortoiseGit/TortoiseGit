TortoiseGit/TortoiseGit

commit 4e4dfffc6c7dab22e9ce1661a85036ceebeced8e

Use set instead of list
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -2610,40 +2610,38 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 						else
 						{
 							bool updateStatusList = false;
-							for (int i = 0 ; i < targetList.GetCount(); ++i)
+							std::set<CString> revertedFiles;
+							std::for_each(targetList.cbegin(), targetList.cend(), [&revertedFiles](auto& path) { revertedFiles.emplace(path.GetGitPathString()); });
+							const int nListboxEntries = GetItemCount();
+							for (int nItem = 0; nItem < nListboxEntries; ++nItem)
 							{
-								const int nListboxEntries = GetItemCount();
-								for (int nItem=0; nItem<nListboxEntries; ++nItem)
+								auto path = GetListEntry(nItem);
+								if (!revertedFiles.contains(path->GetGitPathString()))
+									continue;
+								if (!path->IsDirectory())
 								{
-									auto path = GetListEntry(nItem);
-									if (path->GetGitPathString() != targetList[i].GetGitPathString())
-										continue;
-									if (!path->IsDirectory())
+									if (path->m_Action & CTGitPath::LOGACTIONS_ADDED)
 									{
-										if(path->m_Action & CTGitPath::LOGACTIONS_ADDED)
-										{
-											path->m_Action = CTGitPath::LOGACTIONS_UNVER;
-											SetEntryCheck(path,nItem,false);
+										path->m_Action = CTGitPath::LOGACTIONS_UNVER;
+										SetEntryCheck(path,nItem,false);
 #if 0 // revert an added file and some entry will be cloned (part 1/2)
-											SetItemGroup(nItem,1);
-											this->m_StatusFileList.RemoveItem(*path);
-											this->m_UnRevFileList.AddPath(*path);
-											//this->m_IgnoreFileList.RemoveItem(*path);
+										SetItemGroup(nItem,1);
+										this->m_StatusFileList.RemoveItem(*path);
+										this->m_UnRevFileList.AddPath(*path);
+										//this->m_IgnoreFileList.RemoveItem(*path);
 #else
-											updateStatusList = true;
-#endif
-										}
-										else
-										{
-											if (GetCheck(nItem))
-												m_nSelected--;
-											RemoveListEntry(nItem);
-										}
-									}
-									else if (path->IsDirectory())
 										updateStatusList = true;
-									break;
+#endif
+									}
+									else
+									{
+										if (GetCheck(nItem))
+											m_nSelected--;
+										RemoveListEntry(nItem);
+									}
 								}
+								else if (path->IsDirectory())
+									updateStatusList = true;
 							}
 							SetRedraw(TRUE);
 #if 0 // revert an added file and some entry will be cloned (part 2/2)

commit 533a67ef7976c4a55ff9acb5ef9b9625939e579b

Fix possible nullptr dereference after reverting a file
Fixes a regression introduced in commit 4e4dfffc6c7dab22e9ce1661a85036ceebeced8e.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Git/GitStatusListCtrl.cpp
@@ -2573,11 +2573,12 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 							bool updateStatusList = false;
 							std::set<CString> revertedFiles;
 							std::for_each(targetList.cbegin(), targetList.cend(), [&revertedFiles](auto& path) { revertedFiles.emplace(path.GetGitPathString()); });
+							std::vector<int> toRemove;
 							const int nListboxEntries = GetItemCount();
 							for (int nItem = 0; nItem < nListboxEntries; ++nItem)
 							{
 								auto path = GetListEntry(nItem);
-								if (!revertedFiles.contains(path->GetGitPathString()))
+								if (!path || !revertedFiles.contains(path->GetGitPathString()))
 									continue;
 								if (!path->IsDirectory())
 								{
@@ -2592,25 +2593,36 @@ void CGitStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
 										//this->m_IgnoreFileList.RemoveItem(*path);
 #else
 										updateStatusList = true;
+										break;
 #endif
 									}
 									else
 									{
 										if (GetCheck(nItem))
 											m_nSelected--;
-										RemoveListEntry(nItem);
+										toRemove.emplace_back(nItem);
+										if (toRemove.size() == revertedFiles.size())
+											break;
 									}
 								}
 								else if (path->IsDirectory())
+								{
 									updateStatusList = true;
+									break;
+								}
 							}
-							SetRedraw(TRUE);
 #if 0 // revert an added file and some entry will be cloned (part 2/2)
 							Show(m_dwShow, 0, m_bShowFolders,updateStatusList,true);
 							NotifyCheck();
 #else
 							if (updateStatusList)
 								RefreshParent();
+							else
+							{
+								SetRedraw(FALSE);
+								std::for_each(toRemove.rbegin(), toRemove.rend(), [&](int nItem) { RemoveListEntry(nItem); });
+								SetRedraw(TRUE);
+							}
 #endif
 						}
 					}