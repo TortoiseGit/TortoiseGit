TortoiseGit/TortoiseGit

commit fb53bb1b4e8f694d79d530f62f00da4500e26457

Directly use the information we obtained from CGit::ParseConflictHashesFromLsFile
No need to parse the output again with CTGitPathList::ParserFromLsFile.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -1752,10 +1752,8 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 	CTGitPath directory = merge.GetDirectory();
 
 	BYTE_VECTOR vector;
-
 	CString cmd;
 	cmd.Format(L"git.exe ls-files -u -t -z -- \"%s\"", static_cast<LPCWSTR>(merge.GetGitPathString()));
-
 	if (g_Git.Run(cmd, &vector))
 		return FALSE;
 
@@ -1878,16 +1876,6 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 		return TRUE;
 	}
 
-	CTGitPathList list;
-	if (list.ParserFromLsFile(vector))
-	{
-		MessageBox(hWnd, L"Parse ls-files failed!", L"TortoiseGit", MB_OK | MB_ICONERROR);
-		return FALSE;
-	}
-
-	if (list.IsEmpty())
-		return FALSE;
-
 	CTGitPath theirs;
 	CTGitPath mine;
 	CTGitPath base;
@@ -1904,7 +1892,6 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 	}
 	base.SetFromGit(GetMergeTempFile(L"BASE",merge));
 
-	CString format = L"git.exe checkout-index --temp --stage=%d -- \"%s\"";
 	CFile tempfile;
 	//create a empty file, incase stage is not three
 	tempfile.Open(mine.GetWinPathString(),CFile::modeCreate|CFile::modeReadWrite);
@@ -1914,48 +1901,29 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 	tempfile.Open(base.GetWinPathString(),CFile::modeCreate|CFile::modeReadWrite);
 	tempfile.Close();
 
-	bool b_base=false, b_local=false, b_remote=false;
-
-	for (int i = 0; i< list.GetCount(); ++i)
-	{
-		CString outfile;
-		cmd.Empty();
-		outfile.Empty();
-
-		if( list[i].m_Stage == 1)
-		{
-			cmd.Format(format, list[i].m_Stage, static_cast<LPCWSTR>(list[i].GetGitPathString()));
-			b_base = true;
-			outfile = base.GetWinPathString();
-
-		}
-		if( list[i].m_Stage == 2 )
-		{
-			cmd.Format(format, list[i].m_Stage, static_cast<LPCWSTR>(list[i].GetGitPathString()));
-			b_local = true;
-			outfile = mine.GetWinPathString();
-
-		}
-		if( list[i].m_Stage == 3 )
-		{
-			cmd.Format(format, list[i].m_Stage, static_cast<LPCWSTR>(list[i].GetGitPathString()));
-			b_remote = true;
-			outfile = theirs.GetWinPathString();
-		}
+	CString format = L"git.exe checkout-index --temp --stage=%d -- \"%s\"";
+	auto prepareFile = [&hWnd, &merge, &format](int stage, const CString& outfile) {
+		CString cmd;
+		cmd.Format(format, stage, static_cast<LPCWSTR>(merge.GetGitPathString()));
 		CString output, err;
-		if(!outfile.IsEmpty())
-			if (!g_Git.Run(cmd, &output, &err, CP_UTF8))
-			{
-				CString file;
-				int start =0 ;
-				file = output.Tokenize(L"\t", start);
-				::MoveFileEx(file,outfile,MOVEFILE_REPLACE_EXISTING|MOVEFILE_COPY_ALLOWED);
-			}
-			else
-				CMessageBox::Show(hWnd, output + L'\n' + err, L"TortoiseGit", MB_OK | MB_ICONERROR);
-	}
+		if (!g_Git.Run(cmd, &output, &err, CP_UTF8))
+		{
+			CString file;
+			int start = 0;
+			file = output.Tokenize(L"\t", start);
+			::MoveFileEx(file, outfile, MOVEFILE_REPLACE_EXISTING | MOVEFILE_COPY_ALLOWED);
+		}
+		else
+			CMessageBox::Show(hWnd, output + L'\n' + err, L"TortoiseGit", MB_OK | MB_ICONERROR);
+	};
+	if (!baseHash.IsEmpty())
+		prepareFile(1, base.GetWinPathString());
+	if (!localHash.IsEmpty())
+		prepareFile(2, mine.GetWinPathString());
+	if (!remoteHash.IsEmpty())
+		prepareFile(2, theirs.GetWinPathString());
 
-	if(b_local && b_remote )
+	if (!localHash.IsEmpty() && !remoteHash.IsEmpty())
 	{
 		merge.SetFromWin(g_Git.CombinePath(merge));
 		if (isRebase)
@@ -1967,36 +1935,36 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 	{
 		::DeleteFile(mine.GetWinPathString());
 		::DeleteFile(theirs.GetWinPathString());
-		if (!b_base)
+		if (baseHash.IsEmpty())
 			::DeleteFile(base.GetWinPathString());
 
 		SCOPE_EXIT{
-			if (b_base)
+			if (!baseHash.IsEmpty())
 				::DeleteFile(base.GetWinPathString());
 		};
 
 		CDeleteConflictDlg dlg(GetExplorerHWND() == hWnd ? nullptr : CWnd::FromHandle(hWnd));
 		if (!isRebase)
 		{
-			DescribeConflictFile(b_local, b_base, dlg.m_LocalStatus);
-			DescribeConflictFile(b_remote, b_base, dlg.m_RemoteStatus);
+			DescribeConflictFile(!localHash.IsEmpty(), !baseHash.IsEmpty(), dlg.m_LocalStatus);
+			DescribeConflictFile(!remoteHash.IsEmpty(), !baseHash.IsEmpty(), dlg.m_RemoteStatus);
 			dlg.m_LocalHash = mineHash;
 			dlg.m_RemoteHash = theirsHash;
 			dlg.m_LocalRef = mineTitle;
 			dlg.m_RemoteRef = theirsTitle;
-			dlg.m_bDiffMine = b_local;
+			dlg.m_bDiffMine = !localHash.IsEmpty();
 		}
 		else
 		{
-			DescribeConflictFile(b_local, b_base, dlg.m_RemoteStatus);
-			DescribeConflictFile(b_remote, b_base, dlg.m_LocalStatus);
+			DescribeConflictFile(!localHash.IsEmpty(), !baseHash.IsEmpty(), dlg.m_RemoteStatus);
+			DescribeConflictFile(!remoteHash.IsEmpty(), !baseHash.IsEmpty(), dlg.m_LocalStatus);
 			dlg.m_LocalHash = theirsHash;
 			dlg.m_RemoteHash = mineHash;
 			dlg.m_LocalRef = theirsTitle;
 			dlg.m_RemoteRef = mineTitle;
-			dlg.m_bDiffMine = !b_local;
+			dlg.m_bDiffMine = localHash.IsEmpty();
 		}
-		dlg.m_bShowModifiedButton = b_base;
+		dlg.m_bShowModifiedButton = !baseHash.IsEmpty();
 		dlg.m_File = merge;
 		dlg.m_FileBaseVersion = base;
 		if(dlg.DoModal() == IDOK)

src/TortoiseProc/ProgressCommands/ResolveProgressCommand.cpp
@@ -95,7 +95,6 @@ bool ResolveProgressCommand::Run(CGitProgressList* list, CString& sWindowTitle, 
 	CMassiveGitTask checkoutFromIndexTask{ checkoutFromIndexTaskCmd };
 	for (auto& path : m_targetPathList)
 	{
-		bool b_local = false, b_remote = false;
 		bool baseIsFile = true, localIsFile = true, remoteIsFile = true;
 		CGitHash baseHash, localHash, remoteHash;
 		if (!gitIndex)
@@ -109,28 +108,13 @@ bool ResolveProgressCommand::Run(CGitProgressList* list, CString& sWindowTitle, 
 				return false;
 			}
 
-			CTGitPathList pathList;
-			if (pathList.ParserFromLsFile(vector))
-			{
-				list->ReportError(L"Parsing git ls-files failed!");
-				return false;
-			}
-
-			if (pathList.IsEmpty())
+			if (int ret = CGit::ParseConflictHashesFromLsFile(vector, baseHash, baseIsFile, localHash, localIsFile, remoteHash, remoteIsFile); ret == 1)
 			{
 				list->AddNotify(new CGitProgressList::WC_File_NotificationData(path, Git_WC_Notify_Action::Skip));
 				++m_itemCount;
 				continue;
 			}
-			for (int i = 0; i < pathList.GetCount(); ++i)
-			{
-				if (pathList[i].m_Stage == 2)
-					b_local = true;
-				if (pathList[i].m_Stage == 3)
-					b_remote = true;
-			}
-
-			if (CGit::ParseConflictHashesFromLsFile(vector, baseHash, baseIsFile, localHash, localIsFile, remoteHash, remoteIsFile))
+			else if (ret == -1)
 			{
 				list->ReportError(L"Parsing git ls-files for conflict info failed!");
 				return false;
@@ -160,19 +144,19 @@ bool ResolveProgressCommand::Run(CGitProgressList* list, CString& sWindowTitle, 
 			}
 			if (our) // stage == 2
 			{
-				b_local = true;
 				localHash = our->id;
 				localIsFile = (our->mode & S_IFDIR) != S_IFDIR;
 			}
 			if (their) // stage == 3
 			{
-				b_remote = true;
 				remoteHash = their->id;
 				remoteIsFile = (their->mode & S_IFDIR) != S_IFDIR;
 			}
 		}
 
-		if ((m_resolveWith == ResolveWith::Theirs && !b_remote) || (m_resolveWith == ResolveWith::Mine && !b_local))
+		ASSERT(m_resolveWith != ResolveWith::Current);
+		CGitHash destinationHash = (m_resolveWith == ResolveWith::Theirs ? remoteHash : localHash);
+		if (destinationHash.IsEmpty())
 		{
 			if (!PathIsDirectory(path.GetGitPathString()))
 			{
@@ -216,7 +200,6 @@ bool ResolveProgressCommand::Run(CGitProgressList* list, CString& sWindowTitle, 
 			continue;
 		}
 
-		CGitHash destinationHash = (m_resolveWith == ResolveWith::Theirs ? remoteHash : localHash);
 		if (bool targetWillBeAFile = (m_resolveWith == ResolveWith::Theirs ? remoteIsFile : localIsFile); !targetWillBeAFile)
 		{
 			CTGitPath fullPath;
@@ -274,8 +257,7 @@ bool ResolveProgressCommand::Run(CGitProgressList* list, CString& sWindowTitle, 
 			continue;
 		}
 
-		if (b_local && b_remote)
-			checkoutFromIndexTask.AddFile(path);
+		checkoutFromIndexTask.AddFile(path); // theoretically not needed for a deleted/modified conflcit as the file in the worktree already contains the modified version, but the user requested to use the staged version
 		addTask.AddFile(path);
 		CAppUtils::RemoveTempMergeFile(path);
 	}

commit ae198a64e0ee31ab606718e389b734b221287ef5

Resolve conflict shows wrong file for "their" version
Regression introduced in commit fb53bb1b4e8f694d79d530f62f00da4500e26457.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/AppUtils.cpp
@@ -1921,7 +1921,7 @@ bool CAppUtils::ConflictEdit(HWND hWnd, CTGitPath& path, bool bAlternativeTool /
 	if (!localHash.IsEmpty())
 		prepareFile(2, mine.GetWinPathString());
 	if (!remoteHash.IsEmpty())
-		prepareFile(2, theirs.GetWinPathString());
+		prepareFile(3, theirs.GetWinPathString());
 
 	if (!localHash.IsEmpty() && !remoteHash.IsEmpty())
 	{