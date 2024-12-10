TortoiseGit/TortoiseGit

commit bfba68be7a8bd6c1ccb4cb48f847a9f5a5416579

Generate and ship HTML files as offline help
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/CommonAppUtils.cpp
@@ -29,6 +29,7 @@
 #include "LoadIconEx.h"
 #include "IconBitmapUtils.h"
 #include "CreateProcessHelper.h"
+#include "SysInfo.h"
 
 extern CString sOrigCWD;
 extern CString g_sGroupingUUID;
@@ -409,68 +410,59 @@ bool CCommonAppUtils::StartHtmlHelp(DWORD_PTR id, CString page /* = L"index.html
 {
 	ATLASSERT(page == "index.html" || id == 0);
 
-	CWinApp* pApp = AfxGetApp();
-	ASSERT_VALID(pApp);
+#if !defined(IDR_HELPMAPPING) || !defined(IDS_APPNAME)
+	return false;
+#else
+	static std::map<DWORD_PTR, std::wstring> idMap;
 
-	CString helpFile{ pApp->m_pszHelpFilePath };
-
-	if (helpFile.IsEmpty() || !PathFileExists(helpFile))
+	if (idMap.empty())
 	{
-#if defined(IDR_HELPMAPPING) && defined(IDS_APPNAME)
-		static std::map<DWORD_PTR, std::wstring> idMap;
-
-		if (idMap.empty())
+		DWORD resSize = 0;
+		const char* resData = GetResourceData(L"help", IDR_HELPMAPPING, resSize);
+		if (resData)
 		{
-			DWORD resSize = 0;
-			const char* resData = GetResourceData(L"help", IDR_HELPMAPPING, resSize);
-			if (resData)
+			auto resString = std::string(resData, resSize);
+			std::vector<std::string> lines;
+			stringtok(lines, resString, true, "\r\n");
+			for (const auto& line : lines)
 			{
-				auto resString = std::string(resData, resSize);
-				std::vector<std::string> lines;
-				stringtok(lines, resString, true, "\r\n");
-				for (const auto& line : lines)
-				{
-					if (line.empty())
-						continue;
-					std::vector<std::string> lineParts;
-					stringtok(lineParts, line, true, "=");
-					if (lineParts.size() == 2)
-						idMap[std::stoi(lineParts[0], nullptr, 0)] = CUnicodeUtils::StdGetUnicode(lineParts[1]);
-				}
+				if (line.empty())
+					continue;
+				std::vector<std::string> lineParts;
+				stringtok(lineParts, line, true, "=");
+				if (lineParts.size() == 2)
+					idMap[std::stoi(lineParts[0], nullptr, 0)] = CUnicodeUtils::StdGetUnicode(lineParts[1]);
 			}
 		}
-
-		std::wstring baseUrl{ L"https://tortoisegit.org/docs/" };
-		CString appName(MAKEINTRESOURCE(IDS_APPNAME));
-
-		if (idMap.find(id) != idMap.end())
-		{
-			page = idMap[id].c_str();
-			page.Replace(L"@", L"%40");
-		}
-
-		baseUrl += appName.MakeLower();
-		baseUrl += L'/';
-		baseUrl += page;
-
-		return reinterpret_cast<INT_PTR>(ShellExecute(nullptr, L"open", baseUrl.c_str(), nullptr, nullptr, SW_SHOWNORMAL)) > 32;
-#else
-		return false;
-#endif
 	}
 
-	CString mapID;
-	if (id)
-		mapID.Format(L" -mapid %Iu", id);
-	else
-		helpFile += L"::/" + page;
+	if (idMap.find(id) != idMap.end())
+	{
+		page = idMap[id].c_str();
+		page.Replace(L"@", L"%40");
+	}
 
-	wchar_t windir[MAX_PATH]{ 0 };
-	if (!GetWindowsDirectory(windir, _countof(windir))) // MAX_PATH ok.
-		return false;
+	CString appName(MAKEINTRESOURCE(IDS_APPNAME));
+	if (CString appHelp = CPathUtils::GetAppDirectory() + appName + L"_en\\"; PathFileExists(appHelp))
+	{
+		// We have to find the default browser, ourselves, because ShellExecute cannot handle anchors on local HTML files
+		DWORD dwszBuffPathLen = MAX_PATH;
+		if (CString application; SUCCEEDED(::AssocQueryStringW(SysInfo::Instance().IsWin8OrLater() ? ASSOCF_IS_PROTOCOL : 0, ASSOCSTR_COMMAND, L"http", L"open", CStrBuf(application, dwszBuffPathLen), &dwszBuffPathLen)))
+		{
+			if (application.Find(L"%1") < 0)
+				application += L" %1";
 
-	CString cmd;
-	cmd.Format(L"%s\\HH.exe%s \"%s\"", windir, static_cast<LPCWSTR>(mapID), static_cast<LPCWSTR>(helpFile));
+			if (application.Find(L"\"%1\"") >= 0)
+				application.Replace(L"\"%1\"", L"%1");
 
-	return CCreateProcessHelper::CreateProcessDetached(nullptr, cmd);
+			appHelp.Replace(L" ", L"%20");
+			appHelp.Replace(L'\\', L'/');
+			application.Replace(L"%1", L"file:///" + appHelp + page); // if the path is not prefixed with file:///, the anchor separator may not be detected correctly
+			return CCreateProcessHelper::CreateProcessDetached(nullptr, application);
+		}
+	}
+	CString url;
+	url.Format(L"https://tortoisegit.org/docs/%s/%s", static_cast<LPCWSTR>(appName.MakeLower()), static_cast<LPCWSTR>(page));
+	return reinterpret_cast<INT_PTR>(ShellExecute(nullptr, L"open", url, nullptr, nullptr, SW_SHOWNORMAL)) > 32;
+#endif
 }

doc/doc.build
@@ -515,7 +515,16 @@ All user/site specific configuration is stored in doc.build.user.
 
 			<call target="xsltproc" />
 		</if>
-		<call target="helpmapping" if="${lang.base==lang.target and help.mapping=='1'}" />
+		<if test="${lang.base==lang.target and help.mapping=='1'}">
+			<call target="helpmapping"/>
+			<property name="help.wixfilelist" value="../src/TortoiseGitSetup/HTMLHelpfiles.wxi" unless="${property::exists('help.wixfilelist')}"/>
+			<exec program="${name.python}"
+						commandline="scripts/generate_wix_filelist.py"
+						output="${help.wixfilelist}"
+						verbose="false"/>
+			<exec program="sed" commandline=" -i 's/$/\r/' ${help.wixfilelist}" if="${platform::is-unix()}"/>
+			<echo message="Updated help WiX filelist: '${help.wixfilelist}'"/>
+		</if>
 	</target>
 
 <!-- ====================================================================== -->

doc/scripts/generate_wix_filelist.py
@@ -1,0 +1,87 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2024 the TortoiseGit team
+# This file is distributed under the same license as TortoiseGit
+#
+# Author: Sven Strickroth <email@cs-ware.de>, 2024
+#
+
+'''
+Developer tool to generate the WiX file list for the HTML help files
+
+Works similar to the WiX harvesting tool Heat.
+
+Usage:
+	generate_wix_filelist.py
+
+Example:
+	generate_wix_filelist.py > ../src/TortoiseGitSetup/HTMLHelpfiles.wxi
+'''
+
+import sys
+import os
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom import minidom
+import hashlib
+import uuid
+
+def generate_deterministic_guid(seed_string):
+	return str(uuid.UUID(hashlib.md5(seed_string.encode('utf-8')).hexdigest()))
+
+def generate_wix_directory(root_dir, directory_path, wix_dir, current_path=None, is_first_level=True):
+	current_path = current_path or {}
+	for root, dirs, files in os.walk(directory_path):
+		rel_dir = os.path.relpath(root, root_dir).replace('\\', '_').replace('/', '_')
+		dir_id = f"D__htmlhelp_{rel_dir}"
+
+		# Skip creating a Directory tag for the first directory
+		if is_first_level:
+			dir_element = wix_dir
+			is_first_level = False
+		else:
+			# Check if the directory has been processed before
+			if dir_id not in current_path:
+				dir_name = os.path.basename(root)
+				dir_element = SubElement(wix_dir, "Directory", Id=dir_id, Name=dir_name)
+				current_path[dir_id] = dir_element
+			else:
+				dir_element = current_path[dir_id]
+
+		# Add files to the current directory
+		for file in files:
+			if file == "alias.h":
+				continue
+			if file == "context.h" or file.endswith('.chm'):
+				raise Exception("Documentation needs to be generated using the docformat 'html'")
+			file_path = os.path.join(root, file).replace('/', '\\')
+			file_id = f"F__htmlhelp_{rel_dir}_{os.path.splitext(file)[0]}".replace('-', '_').replace(' ', '_')
+			component_guid = generate_deterministic_guid(file_id)
+			component = SubElement(dir_element, "Component", Id=f"C__{file_id[3:]}", Guid=component_guid, Win64="$(var.Win64YesNo)")
+			SubElement(component, "File", Id=file_id, Name=file, Source=f"..\\..\\doc\\{file_path}")
+
+		# Add subdirectories as child elements
+		for subdir in dirs:
+			subdir_path = os.path.join(root, subdir)
+			sub_rel_dir = os.path.relpath(subdir_path, root_dir).replace('\\', '_').replace('/', '_')
+			dir_name = subdir
+			if dir_name == "TortoiseMerge_en":
+				dir_name = "TortoiseGitMerge_en"
+			sub_dir_id = f"D__htmlhelp_{sub_rel_dir}"
+			current_path[sub_dir_id] = SubElement(dir_element, "Directory", Id=sub_dir_id, Name=dir_name)
+
+def main():
+	wix_root = Element("Include")
+	base_dir = SubElement(wix_root, "DirectoryRef", Id="D__Bin")
+
+	directory_path = 'output'
+
+	generate_wix_directory(directory_path, directory_path, base_dir)
+
+	compontentgroup = SubElement(wix_root, "ComponentGroup", Id="C__help_en")
+	for component in base_dir.findall(".//Component"):
+		SubElement(compontentgroup, "ComponentRef", Id=component.attrib["Id"])
+
+	sys.stdout.write(minidom.parseString(tostring(wix_root)).toprettyxml(indent="\t"))
+
+if __name__ == '__main__':
+	main()

src/TortoiseGitSetup/FeaturesFragment.wxi
@@ -67,7 +67,7 @@
 		<?endif ?>
 
 		<Feature Id="F_HELPFILES" Level="1" Title="Help/manual files" Description="If not installed, TortoiseGit will automatically open the manual on https://tortoisegit.org/docs/" TypicalDefault="install" Absent="allow" AllowAdvertise="no">
-			<ComponentRef Id="C__help_en" />
+			<ComponentGroupRef Id="C__help_en" />
 		</Feature>
 
 		<Feature Id="MoreIcons" Level="1" Title="Additional icon sets" Description="Additional sets of icon overlays in different styles" TypicalDefault="install" Absent="allow" AllowAdvertise="no">

src/TortoiseGitSetup/StructureFragment.wxi
@@ -288,12 +288,6 @@
 					</Component>
 					<?endif ?>
 
-					<Component Id="C__help_en" Guid="$(var.GuidHelpEN)" Win64="$(var.Win64YesNo)">
-						<File Id="F_TortoiseGit_en" Name="TortoiseGit_en.chm" DiskId="1" Source="..\..\doc\output\TortoiseGit_en.chm" KeyPath="yes" />
-						<File Id="F__TortoiseMerge_en" Name="TortoiseMerge_en.chm" DiskId="1" Source="..\..\doc\output\TortoiseMerge_en.chm" />
-						<RemoveFile Id="F__Tortoise_lang" Name="*.chm" On="uninstall" />
-					</Component>
-
 					<Component Id="C__gitlibs" Guid="$(var.GuidGitLibs)" Win64="$(var.Win64YesNo)">
 						<?if $(var.Platform) = "x86" ?>
 						<File Id="F__gitdll32" Name="gitdll32.dll" DiskId="1" Source="..\..\bin\$(var.ReleasePath32)\bin\gitdll32.dll" KeyPath="yes" />

src/TortoiseGitSetup/TortoiseGIT.wxs
@@ -129,6 +129,7 @@
 		<?endif ?>
 		<?include ProtocolSupport.wxi?>
 		<?include TortoiseUDiff.wxi?>
+		<?include HTMLHelpfiles.wxi?>
 
 		<!--
 		<FragmentRef Id="StructureFragment" />

src/TortoiseGitSetup/WiXSetup.wixproj
@@ -74,6 +74,7 @@
 		<Content Include="VersionNumberInclude.wxi" />
 		<Content Include="Win7LibraryAndExplorerToolbarActions.wxi" />
 		<Content Include="ProtocolSupport.wxi" />
+		<Content Include="HTMLHelpfiles.wxi" />
 	</ItemGroup>
 	<PropertyGroup>
 		<PreBuildEvent Condition="'$(Platform)'=='x64' AND '$(BuildWindows11ContextmenuExtension)'=='1'">

commit 45e8d2a1ffbe61edcde8225aa2b6624174844196

Fix warning
Regression introduced with commit bfba68be7a8bd6c1ccb4cb48f847a9f5a5416579.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/Utils/CommonAppUtils.cpp
@@ -411,6 +411,8 @@ bool CCommonAppUtils::StartHtmlHelp(DWORD_PTR id, CString page /* = L"index.html
 	ATLASSERT(page == "index.html" || id == 0);
 
 #if !defined(IDR_HELPMAPPING) || !defined(IDS_APPNAME)
+	UNREFERENCED_PARAMETER(id);
+	UNREFERENCED_PARAMETER(page);
 	return false;
 #else
 	static std::map<DWORD_PTR, std::wstring> idMap;