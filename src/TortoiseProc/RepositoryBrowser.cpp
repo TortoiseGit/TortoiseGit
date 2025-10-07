TortoiseGit/TortoiseGit

commit 639b767cd8edc4da6076291161e50a5e778d5aa2

Fixed issue #4225: Worktree list - remember column widths
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RepositoryBrowser.cpp
@@ -143,7 +143,6 @@ IMPLEMENT_DYNAMIC(CRepositoryBrowser, CResizableStandAloneDialog)
 CRepositoryBrowser::CRepositoryBrowser(CString rev, CWnd* pParent /*=nullptr*/)
 : CResizableStandAloneDialog(CRepositoryBrowser::IDD, pParent)
 , m_sRevision(rev)
-, m_ColumnManager(&m_RepoList)
 {
 }
 
@@ -168,7 +167,6 @@ BEGIN_MESSAGE_MAP(CRepositoryBrowser, CResizableStandAloneDialog)
 	ON_NOTIFY(NM_DBLCLK, IDC_REPOLIST, &CRepositoryBrowser::OnNMDblclk_RepoList)
 	ON_BN_CLICKED(IDC_BUTTON_REVISION, &CRepositoryBrowser::OnBnClickedButtonRevision)
 	ON_WM_SETCURSOR()
-	ON_WM_DESTROY()
 	ON_WM_MOUSEMOVE()
 	ON_WM_LBUTTONDOWN()
 	ON_WM_LBUTTONUP()
@@ -203,10 +201,10 @@ BOOL CRepositoryBrowser::OnInitDialog()
 	static int columnWidths[] = { CDPIAware::Instance().ScaleX(GetSafeHwnd(), 150), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100) };
 	static_assert(_countof(columnNames) == _countof(columnWidths));
 	DWORD dwDefaultColumns = (1 << eCol_Name) | (1 << eCol_Extension) | (1 << eCol_FileSize);
-	m_ColumnManager.SetNames(columnNames, _countof(columnNames));
+	m_RepoList.m_ColumnManager.SetNames(columnNames, _countof(columnNames));
 	constexpr int columnVersion = 6; // adjust when changing number/names/etc. of columns
-	m_ColumnManager.ReadSettings(dwDefaultColumns, 0, L"RepoBrowser", columnVersion, _countof(columnNames), columnWidths);
-	m_ColumnManager.SetRightAlign(m_ColumnManager.GetColumnByName(IDS_LOG_SIZE));
+	m_RepoList.m_ColumnManager.ReadSettings(dwDefaultColumns, 0, L"RepoBrowser", columnVersion, _countof(columnNames), columnWidths);
+	m_RepoList.m_ColumnManager.SetRightAlign(m_RepoList.m_ColumnManager.GetColumnByName(IDS_LOG_SIZE));
 
 	// set up the list control
 	// set the extended style of the list control
@@ -275,17 +273,6 @@ void CRepositoryBrowser::UpdateDiffWithFileFromReg()
 		m_sMarkForDiffFilename = diffLaterFile;
 		m_sMarkForDiffVersion.Empty();
 	}
-}
-
-void CRepositoryBrowser::OnDestroy()
-{
-	const int maxcol = m_ColumnManager.GetColumnCount();
-	for (int col = 0; col < maxcol; ++col)
-		if (m_ColumnManager.IsVisible(col))
-			m_ColumnManager.ColumnResized(col);
-	m_ColumnManager.WriteSettings();
-
-	CResizableStandAloneDialog::OnDestroy();
 }
 
 void CRepositoryBrowser::OnOK()

src/TortoiseProc/WorktreeListDlg.cpp
@@ -36,7 +36,6 @@ IMPLEMENT_DYNAMIC(CWorktreeListDlg, CResizableStandAloneDialog)
 
 CWorktreeListDlg::CWorktreeListDlg(CWnd* pParent /*=nullptr*/)
 	: CResizableStandAloneDialog(CWorktreeListDlg::IDD, pParent)
-	, m_ColumnManager(&m_WorktreeList)
 {
 }
 
@@ -53,7 +52,6 @@ void CWorktreeListDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CWorktreeListDlg, CResizableStandAloneDialog)
 	ON_WM_CONTEXTMENU()
 	ON_WM_SETCURSOR()
-	ON_WM_DESTROY()
 	ON_BN_CLICKED(IDC_BUTTON_ADD, &CWorktreeListDlg::OnBnClickedButtonAdd)
 	ON_BN_CLICKED(IDC_BUTTON_PRUNE, &CWorktreeListDlg::OnBnClickedButtonPrune)
 	ON_NOTIFY(NM_DBLCLK, IDC_WORKTREE_LIST, OnNMDblclkWorktreeList)
@@ -78,10 +76,10 @@ BOOL CWorktreeListDlg::OnInitDialog()
 	static int columnWidths[] = { CDPIAware::Instance().ScaleX(GetSafeHwnd(), 150), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100), CDPIAware::Instance().ScaleX(GetSafeHwnd(), 100) };
 	static_assert(_countof(columnNames) == _countof(columnWidths));
 	DWORD dwDefaultColumns = (1 << eCol_Path) | (1 << eCol_Hash) | (1 << eCol_Branch) | (1 << eCol_Locked) | (1 << eCol_Reason);
-	m_ColumnManager.SetNames(columnNames, _countof(columnNames));
+	m_WorktreeList.m_ColumnManager.SetNames(columnNames, _countof(columnNames));
 	constexpr int columnVersion = 0; // adjust when changing number/names/etc. of columns
-	m_ColumnManager.ReadSettings(dwDefaultColumns, 0, L"WorktreeList", columnVersion, _countof(columnNames), columnWidths);
-	m_ColumnManager.SetRightAlign(m_ColumnManager.GetColumnByName(IDS_REASON));
+	m_WorktreeList.m_ColumnManager.ReadSettings(dwDefaultColumns, 0, L"WorktreeList", columnVersion, _countof(columnNames), columnWidths);
+	m_WorktreeList.m_ColumnManager.SetRightAlign(m_WorktreeList.m_ColumnManager.GetColumnByName(IDS_REASON));
 
 	// set up the list control
 	// set the extended style of the list control
@@ -103,17 +101,6 @@ BOOL CWorktreeListDlg::OnInitDialog()
 	m_WorktreeList.SetFocus();
 
 	return FALSE;
-}
-
-void CWorktreeListDlg::OnDestroy()
-{
-	int maxcol = m_ColumnManager.GetColumnCount();
-	for (int col = 0; col < maxcol; ++col)
-		if (m_ColumnManager.IsVisible(col))
-			m_ColumnManager.ColumnResized(col);
-	m_ColumnManager.WriteSettings();
-
-	CResizableStandAloneDialog::OnDestroy();
 }
 
 void CWorktreeListDlg::OnOK()

src/TortoiseProc/WorktreeListDlg.h
@@ -22,7 +22,7 @@
 #include "gittype.h"
 #include "GitHash.h"
 #include "GestureEnabledControl.h"
-#include "ColumnManager.h"
+#include "ResizableColumnsListCtrl.h"
 
 class WorktreeDetails
 {
@@ -86,15 +86,13 @@ private:
 	DECLARE_MESSAGE_MAP()
 
 	afx_msg void OnOK() override;
-	afx_msg void OnDestroy();
 	BOOL OnInitDialog() override;
 	BOOL PreTranslateMessage(MSG* pMsg) override;
 	afx_msg void OnNMDblclkWorktreeList(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg void OnBnClickedButtonAdd();
 	afx_msg void OnBnClickedButtonPrune();
 
-	CGestureEnabledControlTmpl<CListCtrl> m_WorktreeList;
-	ColumnManager m_ColumnManager;
+	CGestureEnabledControlTmpl<CResizableColumnsListCtrl<CListCtrl>> m_WorktreeList;
 
 	std::vector<WorktreeDetails> m_Worktrees;
 

src/TortoiseProc/RepositoryBrowser.h
@@ -23,7 +23,7 @@
 #include "GitHash.h"
 #include "GestureEnabledControl.h"
 #include "TGitPath.h"
-#include "ColumnManager.h"
+#include "ResizableColumnsListCtrl.h"
 
 #define REPOBROWSER_CTRL_MIN_WIDTH	20
 
@@ -123,12 +123,10 @@ private:
 
 	afx_msg void			OnOK() override;
 	afx_msg void			OnCancel() override;
-	afx_msg void			OnDestroy();
 	BOOL					OnInitDialog() override;
 
 	CGestureEnabledControlTmpl<CTreeCtrl>	m_RepoTree;
-	CGestureEnabledControlTmpl<CListCtrl>	m_RepoList;
-	ColumnManager			m_ColumnManager;
+	CGestureEnabledControlTmpl<CResizableColumnsListCtrl<CListCtrl>> m_RepoList;
 
 	afx_msg void			OnLvnColumnclickRepoList(NMHDR *pNMHDR, LRESULT *pResult);
 	int						m_currSortCol = 0;

commit da32e5ccdb14d8e4b60a81106a2c83382c845d9b

Unbreak context menu
Regression introduced in commit 639b767cd8edc4da6076291161e50a5e778d5aa2.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/RepositoryBrowser.cpp
@@ -211,6 +211,7 @@ BOOL CRepositoryBrowser::OnInitDialog()
 	m_RepoList.Init();
 	m_RepoList.SetExtendedStyle(m_RepoList.GetExtendedStyle() | LVS_EX_DOUBLEBUFFER | LVS_EX_INFOTIP | LVS_EX_SUBITEMIMAGES);
 	m_RepoList.SetImageList(&SYS_IMAGE_LIST(), LVSIL_SMALL);
+	m_RepoList.SetListContextMenuHandler([&](CPoint point) { OnContextMenu_RepoList(point); });
 	CAppUtils::SetListCtrlBackgroundImage(m_RepoList.GetSafeHwnd(), IDI_REPOBROWSER_BKG);
 
 	m_RepoTree.SetImageList(&SYS_IMAGE_LIST(), TVSIL_NORMAL);
@@ -657,14 +658,8 @@ void CRepositoryBrowser::OnLvnItemchangedRepolist(NMHDR * /* pNMHDR */, LRESULT 
 
 void CRepositoryBrowser::OnContextMenu(CWnd* pWndFrom, CPoint point)
 {
-	if (pWndFrom == &m_RepoList)
-	{
-		CRect headerPosition;
-		m_RepoList.GetHeaderCtrl()->GetWindowRect(headerPosition);
-		if (!headerPosition.PtInRect(point))
-			OnContextMenu_RepoList(point);
-	}
-	else if (pWndFrom == &m_RepoTree)
+	// contextmenu for m_RepoList is handled using SetListContextMenuHandler
+	if (pWndFrom == &m_RepoTree)
 		OnContextMenu_RepoTree(point);
 }
 

src/TortoiseProc/WorktreeListDlg.cpp
@@ -50,7 +50,6 @@ void CWorktreeListDlg::DoDataExchange(CDataExchange* pDX)
 }
 
 BEGIN_MESSAGE_MAP(CWorktreeListDlg, CResizableStandAloneDialog)
-	ON_WM_CONTEXTMENU()
 	ON_WM_SETCURSOR()
 	ON_BN_CLICKED(IDC_BUTTON_ADD, &CWorktreeListDlg::OnBnClickedButtonAdd)
 	ON_BN_CLICKED(IDC_BUTTON_PRUNE, &CWorktreeListDlg::OnBnClickedButtonPrune)
@@ -86,6 +85,7 @@ BOOL CWorktreeListDlg::OnInitDialog()
 	m_WorktreeList.Init();
 	m_WorktreeList.SetExtendedStyle(m_WorktreeList.GetExtendedStyle() | LVS_EX_DOUBLEBUFFER | LVS_EX_SUBITEMIMAGES);
 	m_WorktreeList.SetImageList(&SYS_IMAGE_LIST(), LVSIL_SMALL);
+	m_WorktreeList.SetListContextMenuHandler([&](CPoint point) { OnContextMenu_WorktreeList(point); });
 	m_nIconFolder = SYS_IMAGE_LIST().GetDirIconIndex();
 
 	CAppUtils::SetListCtrlBackgroundImage(m_WorktreeList.GetSafeHwnd(), IDI_REPOBROWSER_BKG);
@@ -267,17 +267,6 @@ int CWorktreeListDlg::FillListCtrlWithWorktreeList(CString& error)
 	}
 
 	return 0;
-}
-
-void CWorktreeListDlg::OnContextMenu(CWnd* pWndFrom, CPoint point)
-{
-	if (pWndFrom == &m_WorktreeList)
-	{
-		CRect headerPosition;
-		m_WorktreeList.GetHeaderCtrl()->GetWindowRect(headerPosition);
-		if (!headerPosition.PtInRect(point))
-			OnContextMenu_WorktreeList(point);
-	}
 }
 
 void CWorktreeListDlg::OnContextMenu_WorktreeList(CPoint point)

src/TortoiseProc/WorktreeListDlg.h
@@ -98,7 +98,6 @@ private:
 
 	int m_nIconFolder = -1;
 
-	afx_msg void OnContextMenu(CWnd* pWndFrom, CPoint point);
 	void OnContextMenu_WorktreeList(CPoint point);
 	void ShowContextMenu(CPoint point, std::vector<int>& indexes);
 