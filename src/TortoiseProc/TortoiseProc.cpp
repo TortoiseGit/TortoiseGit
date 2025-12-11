TortoiseGit/TortoiseGit

commit c3e0dc95c61f1044e474029f78de3cf8c2f7e51e

Be more defensive and omit unnecessary calls to WinAPI
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/TortoiseProc.cpp
@@ -177,9 +177,9 @@ BOOL CTortoiseProcApp::InitInstance()
 	if (!sVal.IsEmpty())
 		hWndExplorer = reinterpret_cast<HWND>(_wcstoui64(sVal, nullptr, 16));
 
-	while (GetParent(hWndExplorer))
+	while (hWndExplorer && GetParent(hWndExplorer));
 		hWndExplorer = GetParent(hWndExplorer);
-	if (!IsWindow(hWndExplorer))
+	if (hWndExplorer && !IsWindow(hWndExplorer))
 		hWndExplorer = nullptr;
 
 	// if HKCU\Software\TortoiseGit\Debug is not 0, show our command line

commit d8fe024b51db0f7838f695544c3785653e26fb53

Fix strange typo causing an endless loop
Introduced in commit c3e0dc95c61f1044e474029f78de3cf8c2f7e51e.

Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/TortoiseProc.cpp
@@ -175,7 +175,7 @@ BOOL CTortoiseProcApp::InitInstance()
 	if (!sVal.IsEmpty())
 		hWndExplorer = reinterpret_cast<HWND>(_wcstoui64(sVal, nullptr, 16));
 
-	while (hWndExplorer && GetParent(hWndExplorer));
+	while (hWndExplorer && GetParent(hWndExplorer))
 		hWndExplorer = GetParent(hWndExplorer);
 	if (hWndExplorer && !IsWindow(hWndExplorer))
 		hWndExplorer = nullptr;